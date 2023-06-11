c176feb8a6dcb04c2626dd19166e1ae010754af5
chronark <dev@chronark.com>
2023-06-12 00:41:12 +0200

    feat(onboarding): add onboarding page for new tenants feat(tenant): add create tenant endpoint

--- apps/web/lib/trpc/routers/tenant.ts
+++ apps/web/lib/trpc/routers/tenant.ts
@@ -3,20 +3,19 @@ import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { t, auth } from "../trpc";
-import { newId } from "@unkey/id";
 
-export const teamRouter = t.router({
+export const tenantRouter = t.router({
   create: t.procedure
     .use(auth)
     .input(
       z.object({
-        id: z.string(),
+        tenantId: z.string(),
         name: z.string().min(1).max(50),
         slug: z.string().min(1).max(50).regex(/^[a-zA-Z0-9-_\.]+$/),
       }),
     )
     .mutation(async ({ input }) => {
-      const id = input.id;
+      const id = input.tenantId;
       await db.insert(schema.tenants).values({
         id,
         name: input.name,
