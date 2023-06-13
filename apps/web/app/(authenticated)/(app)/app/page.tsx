b5781452e210bd953dd1ff2f259f46c382b1bbf6
James Perkins <jamesperkins@hey.com>
2023-06-13 19:35:44 -0400

    Remove User check

--- apps/web/app/(authenticated)/(app)/app/page.tsx
+++ apps/web/app/(authenticated)/(app)/app/page.tsx
@@ -2,11 +2,9 @@ import { PageHeader } from "@/components/PageHeader";
 import { getTenantId } from "@/lib/auth";
 import { db, schema, eq } from "@unkey/db";
 import { redirect } from "next/navigation";
-import { currentUser } from "@clerk/nextjs";
 
 export default async function TenantOverviewPage() {
   const tenantId = getTenantId();
-  const user = await currentUser();
   let tenant = await db.query.tenants.findFirst({
     where: eq(schema.tenants.id, tenantId),
   });
