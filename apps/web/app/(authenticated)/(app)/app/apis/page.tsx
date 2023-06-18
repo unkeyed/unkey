d2f3b3cb8a474055248cf63ab895f3781aef8379
chronark <dev@chronark.com>
2023-06-18 19:49:34 +0200

    feat: /app/apis screen

--- apps/web/app/(authenticated)/(app)/app/apis/page.tsx
+++ apps/web/app/(authenticated)/(app)/app/apis/page.tsx
@@ -1,34 +1,86 @@
 import { PageHeader } from "@/components/PageHeader";
 import { CreateApiButton } from "./CreateAPI";
 import { getTenantId } from "@/lib/auth";
-import { db, schema, eq } from "@unkey/db";
+import { db, schema, eq, sql } from "@unkey/db";
 import { notFound } from "next/navigation";
 import { Separator } from "@/components/ui/separator";
-import { Row } from "./row";
+import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 export default async function TenantOverviewPage() {
   const tenantId = getTenantId();
   const workspace = await db.query.workspaces.findFirst({
     where: eq(schema.workspaces.tenantId, tenantId),
     with: {
-      apis: true,
+      apis: {
+        with: {
+          keys: {},
+        },
+      },
     },
   });
   if (!workspace) {
     return notFound();
   }
+
+  const apis = await Promise.all(
+    workspace.apis.map(async (api) => ({
+      id: api.id,
+      name: api.name,
+      keys: await db
+        .select({ count: sql<number>`count(*)` })
+        .from(schema.keys)
+        .where(eq(schema.keys.apiId, api.id)),
+    })),
+  );
+
   return (
     <div>
-      <PageHeader
-        title="Applications"
-        description="Manage your different APIs"
-        actions={[<CreateApiButton key="createApi" />]}
-      />
+      <PageHeader title="Applications" description="Manage your APIs" />
+
       <Separator className="my-6" />
 
-      {workspace.apis.map((api) => (
-        <Row key={api.id} api={api} />
-      ))}
+      <ul role="list" className="grid grid-cols-1 gap-x-6 gap-y-8 lg:grid-cols-3 xl:gap-x-8">
+        <Card className="duration-500 hover:border-primary bg-muted">
+          <CardHeader>
+            <CardTitle>Create New API</CardTitle>
+          </CardHeader>
+
+          <CardContent></CardContent>
+          <CardFooter>
+            <CreateApiButton key="createApi" />
+          </CardFooter>
+        </Card>
+        {apis.map((api) => (
+          <Link key={api.id} href={`/app/${api.id}`}>
+            <Card className="duration-500 hover:border-primary">
+              <CardHeader>
+                <CardTitle>{api.name}</CardTitle>
+                <CardDescription>{api.id}</CardDescription>
+              </CardHeader>
+
+              <CardContent>
+                <dl className="text-sm leading-6 divide-y divide-gray-100 ">
+                  <div className="flex justify-between py-3 gap-x-4">
+                    <dt className="text-gray-500">API Keys</dt>
+                    <dd className="flex items-start gap-x-2">
+                      <div className="font-medium text-gray-900">{api.keys.length}</div>
+                    </dd>
+                  </div>
+                </dl>
+              </CardContent>
+            </Card>
+          </Link>
+        ))}
+      </ul>
     </div>
   );
 }
