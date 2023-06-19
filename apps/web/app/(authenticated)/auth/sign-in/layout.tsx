e0ecd113de72490df1f7d4d40b6bbfa08875d1c7
James P <jamesperkins@hey.com>
2023-06-19 15:19:33 -0400

    Bug fixing (#35)
    * Fixing redirect 500 issue
    
    * Clear form, fix error on bytes
    
    * Fix deletion
    
    * Fixing bytes and form reset

--- apps/web/app/(authenticated)/auth/sign-in/layout.tsx
+++ apps/web/app/(authenticated)/auth/sign-in/layout.tsx
@@ -1,4 +1,12 @@
+import { auth } from "@clerk/nextjs"
+import { redirect } from "next/navigation";
+
 export default function AuthLayout(props: { children: React.ReactNode }) {
+  const { userId } = auth();
+
+  if (userId) {
+    redirect("/app")
+  }
   return (
     <>
       <div className="grid h-screen place-items-center">
