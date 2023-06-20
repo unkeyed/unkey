fe1dfc0ab2c457f1c208f350fb795eb4e6e618f1
chronark <dev@chronark.com>
2023-06-20 12:22:02 +0200

    feat: waitlist email stuff

---
+++ packages/emails/early-access.ts
@@ -0,0 +1,15 @@
+import { Email } from "./client";
+
+
+
+async function main(){
+    const email = new Email({apiKey: process.env.RESEND_API_KEY!});
+
+    const to = "dev@chronark.com"
+    const inviteLink = `https://unkey.dev/early-access/${Buffer.from(to).toString("base64url")}`
+
+    await email.sendEarlyAccessInvitation({to, inviteLink})
+
+}
+
+main()
