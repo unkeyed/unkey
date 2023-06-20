2e4833ff0f6535dd6de33a546e3d37654ee11def
Andreas Thomas <dev@chronark.com>
2023-06-20 10:53:45 +0200

    style: refactor(api): change auto_stop_machines and auto_start_machines to false in fly.toml (#38)
    style(api): change keyCache ttlSeconds from 60 to 10 in main.ts
    style(api): remove empty line in env.ts
    style(api): add admin connection and disconnection in kafka.ts
    style(api): add signal traps to disconnect consumer in kafka.ts
    style(api): remove empty line in router.ts
    style(web): remove semicolons in layout.tsx files
    style(web): remove empty line in table.tsx

--- apps/web/app/(authenticated)/auth/sign-up/layout.tsx
+++ apps/web/app/(authenticated)/auth/sign-up/layout.tsx
@@ -10,43 +10,43 @@ const testimonials: {
     title: string;
   };
 }[] = [
-    {
-      quote: <p>“I build stuff before asking if anyone wants it“</p>,
-      author: {
-        image: "https://pbs.twimg.com/profile_images/1437670380957835264/gu8S0olw_400x400.jpg",
-        name: "Andreas Thomas",
-        title: "Random Dude at Upstash",
-      },
+  {
+    quote: <p>“I build stuff before asking if anyone wants it“</p>,
+    author: {
+      image: "https://pbs.twimg.com/profile_images/1437670380957835264/gu8S0olw_400x400.jpg",
+      name: "Andreas Thomas",
+      title: "Random Dude at Upstash",
     },
-    {
-      quote: (
-        <p>
-          “When using a 3rd party authentication system, it is most important to replicate a user
-          table in your own database!“
-        </p>
-      ),
-      author: {
-        image: "https://pbs.twimg.com/profile_images/1613589907133403161/zGtDomUL_400x400.jpg",
-        name: "James R Perkins",
-        title: "Random Dude at Clerk",
-      },
+  },
+  {
+    quote: (
+      <p>
+        “When using a 3rd party authentication system, it is most important to replicate a user
+        table in your own database!“
+      </p>
+    ),
+    author: {
+      image: "https://pbs.twimg.com/profile_images/1613589907133403161/zGtDomUL_400x400.jpg",
+      name: "James R Perkins",
+      title: "Random Dude at Clerk",
     },
-    {
-      quote: <p>“I've been passionate about API Keys from a young age actually.“</p>,
-      author: {
-        image: "https://pbs.twimg.com/profile_images/1629470718630002692/Dax4prIG_400x400.jpg",
-        name: "Dom Eccleston",
-        title: "Random Dude at Vercel",
-      },
+  },
+  {
+    quote: <p>“I've been passionate about API Keys from a young age actually.“</p>,
+    author: {
+      image: "https://pbs.twimg.com/profile_images/1629470718630002692/Dax4prIG_400x400.jpg",
+      name: "Dom Eccleston",
+      title: "Random Dude at Vercel",
     },
-  ];
+  },
+];
 
 export default function AuthLayout(props: { children: React.ReactNode }) {
   const _testimonial = testimonials.at(Math.floor(Math.random() * testimonials.length));
   const { userId } = auth();
 
   if (userId) {
-    redirect("/app")
+    redirect("/app");
   }
   return (
     <>
