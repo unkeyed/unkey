be064c831458033674dccad3777c4321c2444461
James Perkins <jamesperkins@hey.com>
2023-06-21 19:58:59 -0400

    Adding old useEffect and add process to turn on debugging

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,8 +1,11 @@
 import { authMiddleware } from "@clerk/nextjs";
 
+const DEBUG_ON = process.env.CLERK_DEBUG === "true";
+
 export default authMiddleware({
   publicRoutes: ["/", "/auth(.*)"],
   signInUrl: "/auth/sign-in",
+  debug: DEBUG_ON,
 });
 
 export const config = {
