b4b54621c0682eccd0c0cb748f8ec8033467ecbc
James Perkins <jamesperkins@hey.com>
2023-06-21 19:58:59 -0400

    Adding old useEffect and add process to turn on debugging

--- apps/web/app/(authenticated)/auth/sso-callback/[[...sso-callback]]/page.tsx
+++ apps/web/app/(authenticated)/auth/sso-callback/[[...sso-callback]]/page.tsx
@@ -1,15 +1,22 @@
 "use client";
-import { AuthenticateWithRedirectCallback } from "@clerk/nextjs";
-
+import { useClerk } from "@clerk/nextjs";
+import { useEffect } from "react";
+import type { HandleOAuthCallbackParams } from "@clerk/types";
 import { Icons } from "@/components/ui/icons";
 
 export const runtime = "edge";
 
-export default function SSOCallback() {
+export default function SSOCallback(props: {
+  searchParams: HandleOAuthCallbackParams;
+}) {
+  const { handleRedirectCallback } = useClerk();
+
+  useEffect(() => {
+    void handleRedirectCallback(props.searchParams);
+  }, [props.searchParams, handleRedirectCallback]);
   return (
     <div className="h-screen flex items-center justify-center ">
       <Icons.spinner className="mr-2 h-16 w-16 animate-spin" />
-      <AuthenticateWithRedirectCallback afterSignInUrl="/app/apis" afterSignUpUrl="/onboarding" />
     </div>
   );
 }
