89fc36ed3249e21f437b010ea9732e1089c02b20
James Perkins <jamesperkins@hey.com>
2023-06-21 21:46:56 -0400

    Fix redirect infinite loop

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,11 +1,37 @@
 import { authMiddleware } from "@clerk/nextjs";
-
+import { redirectToSignIn } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import { db, schema, eq } from "@unkey/db";
 const DEBUG_ON = process.env.CLERK_DEBUG === "true";
+
+const checktenancy = async ({ tenantId }: { tenantId: string }) => {
+  const workspace = await db.query.workspaces.findFirst({
+    where: eq(schema.workspaces.tenantId, tenantId),
+    with: {
+      apis: true,
+    },
+  });
+  return workspace;
+}
 
 export default authMiddleware({
   publicRoutes: ["/", "/auth(.*)"],
   signInUrl: "/auth/sign-in",
   debug: DEBUG_ON,
+  async afterAuth(auth, req) {
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+    if (auth.userId && req.nextUrl.pathname === "/app/apis") {
+      const tenantId = auth.orgId ?? auth.userId;
+      const workspace = await checktenancy({ tenantId });
+      if (!workspace) {
+        return NextResponse.redirect(new URL('/onboarding', req.url))
+      }
+
+    }
+
+  }
 });
 
 export const config = {
