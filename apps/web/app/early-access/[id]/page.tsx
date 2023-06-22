7d9eafde4750869bee473dd56958e06d5ab8746b
James Perkins <jamesperkins@hey.com>
2023-06-21 20:58:47 -0400

    all redirects

--- apps/web/app/early-access/[id]/page.tsx
+++ apps/web/app/early-access/[id]/page.tsx
@@ -3,7 +3,7 @@ import { redirect } from "next/navigation";
 
 const redis = Redis.fromEnv();
 
-export default async function (props: { params: { id: string } }) {
+export default async function(props: { params: { id: string } }) {
   const id = props.params.id;
   console.log({ id });
   const email = Buffer.from(id, "base64url").toString();
