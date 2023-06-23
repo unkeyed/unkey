6d31e38d0f0b2649e5fce526d979b423538a6c0b
Andreas Thomas <dev@chronark.com>
2023-06-24 00:33:52 +0200

    refactor(CreateKey.tsx, CreateKeyButton.tsx, page.tsx, code.tsx): improve code readability and add new component Code (#61)

--- apps/web/app/(landing)/page.tsx
+++ apps/web/app/(landing)/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 import { Github } from "lucide-react";
-import { db, eq, schema, sql } from "@unkey/db";
+import { db, schema, sql } from "@unkey/db";
 
 export const revalidate = 60;
 
@@ -44,7 +44,7 @@ export default async function LandingPage() {
                 <div className="relative inline-flex mt-10 group">
                   <div className="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-lg blur-lg filter group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200" />
                   <Link
-                    href="https://unkey.dev/auth/sign-up"
+                    href="/auth/sign-up"
                     className="relative inline-flex items-center justify-center px-8 py-4 text-lg font-bold text-white transition-all duration-200 bg-gray-900 rounded-lg font-pj focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
                   >
                     Start Building
