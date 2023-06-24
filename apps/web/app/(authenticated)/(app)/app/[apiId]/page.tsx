fa3656110dbd5229622eb71a36bfa3fd71111f11
chronark <dev@chronark.com>
2023-06-24 12:24:45 +0200

    feat: per-api and per-key metrics

--- apps/web/app/(authenticated)/(app)/app/[apiId]/page.tsx
+++ apps/web/app/(authenticated)/(app)/app/[apiId]/page.tsx
@@ -8,11 +8,12 @@ import Link from "next/link";
 import { Badge } from "@/components/ui/badge";
 import { CopyButton } from "@/components/CopyButton";
 import { Button } from "@/components/ui/button";
-import { getUsage, Tinybird } from "@unkey/tinybird";
+import { getActiveCount, getUsage, Tinybird } from "@unkey/tinybird";
 import { env } from "@/lib/env";
 import { fillRange } from "@/lib/utils";
 import { ColumnChart } from "@/components/charts";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { sql } from "drizzle-orm";
 export default async function ApiPage(props: { params: { apiId: string } }) {
   const tenantId = getTenantId();
 
@@ -26,6 +27,18 @@ export default async function ApiPage(props: { params: { apiId: string } }) {
     return redirect("/onboarding");
   }
 
+  const keysP = db
+    .select({ count: sql<number>`count(*)` })
+    .from(schema.keys)
+    .where(eq(schema.keys.apiId, api.id))
+    .execute()
+    .then((res) => res.at(0)?.count ?? 0);
+
+  const activeP = getActiveCount(new Tinybird({ token: env.TINYBIRD_TOKEN }))({
+    workspaceId: api.workspaceId,
+    apiId: api.id,
+  });
+
   const usage = await getUsage(new Tinybird({ token: env.TINYBIRD_TOKEN }))({
     workspaceId: api.workspaceId,
     apiId: api.id,
@@ -36,6 +49,9 @@ export default async function ApiPage(props: { params: { apiId: string } }) {
   const start = new Date(year, month, 0, 0, 0, 0, 0);
   const end = new Date(start.getTime());
   end.setUTCMonth(end.getUTCMonth() + 1);
+
+  const keys = await keysP;
+  const active = await activeP;
 
   const usageOverTime = fillRange(
     usage.data.map(({ time, usage }) => ({ value: usage, time })),
@@ -48,8 +64,26 @@ export default async function ApiPage(props: { params: { apiId: string } }) {
   }));
 
   return (
-    <div>
-      <Card>
+    <div className="grid grid-cols-3 gap-4">
+      <Card className="col-span-1">
+        <CardHeader>
+          <CardTitle>{keys}</CardTitle>
+          <CardDescription>Total Keys</CardDescription>
+        </CardHeader>
+      </Card>
+      <Card className="col-span-1">
+        <CardHeader>
+          <CardTitle>{active.data.at(0)?.active ?? 0}</CardTitle>
+          <CardDescription>Active Keys (30 days)</CardDescription>
+        </CardHeader>
+      </Card>
+      <Card className="col-span-1">
+        <CardHeader>
+          <CardTitle>{usage.data.reduce((sum, day) => sum + day.usage, 0)}</CardTitle>
+          <CardDescription>Verifications (30 days)</CardDescription>
+        </CardHeader>
+      </Card>
+      <Card className="col-span-3">
         <CardHeader>
           <CardTitle>Usage in the last 30 days</CardTitle>
           <CardDescription>This includes all key verifications in this API</CardDescription>
