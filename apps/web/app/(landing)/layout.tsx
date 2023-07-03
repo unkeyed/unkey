fe0c024847fd4c9a4028371b8d0057ed8bdb49de
chronark <dev@chronark.com>
2023-07-04 00:29:56 +0200

    refactor(layout.tsx): wrap content in a div with min-h-screen and bg-gray-50 classes refactor(page.tsx): remove bg-gray-50 class from the outer div

--- apps/web/app/(landing)/layout.tsx
+++ apps/web/app/(landing)/layout.tsx
@@ -6,9 +6,9 @@ export default function LandingLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
+    <div className="min-h-screen bg-gray-50">
       <NavigationBar />
       {children}
-    </>
+    </div>
   );
 }
