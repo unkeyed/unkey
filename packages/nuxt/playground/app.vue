8032a35e51bbba1a9318fa1a9a3c2c59a0fe23ea
Daniel Roe <daniel@roe.dev>
2023-07-06 21:42:33 +0100

    feat: add new `@unkey/nuxt` module (#89)
    * feat: add new `@unkey/nuxt` module
    
    * fix: correct key
    
    * fix: handle key-not-found 404s
    
    * chore: add example `.env` file
    
    * chore: update build command + add node types
    
    * fix: ensure context augmentation happens for server routes too
    
    * style: lint/format nuxt package
    
    * fix: allow using `useUnkey` until it supports undefined token
    
    * test: add basic test suite
    
    * ci: add test/build + turbo workflow
    
    * docs: add nuxt docs
    
    * chore: use env variable for testing

---
+++ packages/nuxt/playground/app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>You can see the state if you hit this with an <code>Authorization</code> header</h1>
+    <pre>Authorization: {{ header }}</pre>
+    <pre>{{ verifiedState }}</pre>
+  </div>
+</template>
+
+<script setup>
+const verifiedState = useState(() => ({ unkey: useRequestEvent()?.context?.unkey }))
+const header = useState(() => useRequestEvent()?.headers?.authorization)
+</script>
