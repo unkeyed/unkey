69791924cc30ae62a2a5b01826c3317c064acb03
Daniel Roe <daniel@roe.dev>
2023-07-06 21:42:33 +0100

    feat: add new `@unkey/nuxt` module (#89)
    * feat: add new `@unkey/nuxt` module
    
    * fix: correct key
    
    * fix: handle key-not-found 404s
    
    * chore: add example `.env` file
    
    * chore: update build command + add node types
    
    * fix: ensure context augmentation happens for server routes too
    
    * style: lint/format nuxt package
    
    * fix: allow using `useUnkey` until it supports undefined token
    
    * test: add basic test suite
    
    * ci: add test/build + turbo workflow
    
    * docs: add nuxt docs
    
    * chore: use env variable for testing

---
+++ packages/nuxt/src/runtime/server/middleware/unkey.ts
@@ -0,0 +1,20 @@
+import { defineEventHandler, getRequestHeader, useRuntimeConfig } from "#imports";
+// import { useUnkey } from '../utils/unkey'
+
+export default defineEventHandler(async (event) => {
+  const config = useRuntimeConfig(event);
+  const key = getRequestHeader(event, "authorization")
+    ?.split(config.unkey?.authPrefix ?? "Bearer")[1]
+    ?.trim();
+  if (!key) {
+    return;
+  }
+
+  // TODO: refactor to SDK once it is released
+  event.context.unkey = (await $fetch("https://api.unkey.dev/v1/keys/verify", {
+    method: "POST",
+    body: { key },
+  }).catch((e) => e.data)) as any;
+  // When `useUnkey` can taken an undefined token we can safely use the belowa
+  // event.context.unkey = await useUnkey().keys.verify({ key })
+});
---
+++ packages/nuxt/src/runtime/server/types.d.ts
@@ -0,0 +1,7 @@
+import type { Unkey } from "@unkey/api";
+
+declare module "h3" {
+  interface H3EventContext {
+    unkey?: Awaited<ReturnType<Unkey["keys"]["verify"]>>;
+  }
+}
