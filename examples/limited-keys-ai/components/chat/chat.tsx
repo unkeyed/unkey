29a4d74562bd97fd616dfb87bf728d080e6b5680
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2023-07-08 11:01:05 +0100

    chore: add Clerks user button

--- examples/limited-keys-ai/components/chat/chat.tsx
+++ examples/limited-keys-ai/components/chat/chat.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useChat } from "ai/react";
+import { UserButton, useUser } from "@clerk/nextjs";
 import MessageContainer from "../Messages/container";
 import Message from "../Messages/message";
 
@@ -8,44 +9,52 @@ const Chat = () => {
   const { messages, input, handleInputChange, handleSubmit } = useChat({
     api: "/api/completions",
   });
+  const { isSignedIn, isLoaded } = useUser();
 
   return (
-    <MessageContainer>
-      {messages && messages.length > 0 ? (
-        messages.map((message) => (
-          <Message
-            key={message.id}
-            role={message.role}
-            content={message.content}
-          />
-        ))
-      ) : (
-        <p>No messages yet...</p>
+    <div>
+      {isSignedIn && isLoaded && (
+        <div className="ml-auto">
+          <UserButton />
+        </div>
       )}
-
-      <form
-        className=" w-full flex gap-3 justify-center items-center md:left-[1%]"
-        onSubmit={handleSubmit}
-      >
-        {
-          <>
-            <textarea
-              className="w-5/6 p-3 border-2 border-black break-words whitespace-pre-line border-solid text-base"
-              placeholder="Message"
-              name="message"
-              value={input}
-              onChange={handleInputChange}
+      <MessageContainer>
+        {messages && messages.length > 0 ? (
+          messages.map((message) => (
+            <Message
+              key={message.id}
+              role={message.role}
+              content={message.content}
             />
-            <button
-              type="submit"
-              className="bg-green-200 text-black/60 justify-center items-center flex gap-2 mt-2  rounded-md py-2 px-3 mb-2 text-base  md:text-2xl"
-            >
-              <span> Send</span> <span className="">🚁</span>
-            </button>
-          </>
-        }
-      </form>
-    </MessageContainer>
+          ))
+        ) : (
+          <p>No messages yet...</p>
+        )}
+
+        <form
+          className=" w-full flex gap-3 justify-center items-center md:left-[1%]"
+          onSubmit={handleSubmit}
+        >
+          {
+            <>
+              <textarea
+                className="w-5/6 p-3 border-2 border-black break-words whitespace-pre-line border-solid text-base"
+                placeholder="Message"
+                name="message"
+                value={input}
+                onChange={handleInputChange}
+              />
+              <button
+                type="submit"
+                className="bg-green-200 text-black/60 justify-center items-center flex gap-2 mt-2  rounded-md py-2 px-3 mb-2 text-base  md:text-2xl"
+              >
+                <span> Send</span> <span className="">🚁</span>
+              </button>
+            </>
+          }
+        </form>
+      </MessageContainer>
+    </div>
   );
 };
 
