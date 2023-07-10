84e9f3d1e842fcc3d492fa6573121bc410238b60
James Perkins <jamesperkins@hey.com>
2023-07-10 09:44:55 -0400

    Adding email changelog

--- packages/emails/client.tsx
+++ packages/emails/client.tsx
@@ -2,14 +2,18 @@ import EarlyAccessInvitation from "./emails/EarlyAccessInvitation";
 import { render } from "@react-email/render";
 import { Resend } from "resend";
 import { ResetWarning } from "./emails/ResetWarning";
+import Changelog from "./emails/Changelog";
 
+interface ChangelogProps {
+  item?: { id: number; shortDescription: string }[];
+}
 export class Email {
   private client: Resend;
 
   constructor(opts: { apiKey: string }) {
     this.client = new Resend(opts.apiKey);
   }
-
+  
   public async sendEarlyAccessInvitation(opts: {
     to: string;
     inviteLink: string;
@@ -34,6 +38,23 @@ export class Email {
       to: opts.to,
       reply_to: "unkey@chronark.com",
       subject: "Unkey: API Key Reset (Sunday)",
+      html,
+    });
+  }
+
+  public async sendChangelog(opts: {
+    to: string;
+    preview: string;
+    changelog: ChangelogProps;
+    date: string;
+    changelogUrl: string;
+  }) {
+    const html = render(<Changelog preview={opts.preview} changelog={opts.changelog} date={opts.date} changelogUrl={opts.changelogUrl}  />);
+    return await this.client.sendEmail({
+      from: "james@unkey.dev",
+      to: opts.to,
+      reply_to: "james@unkey.dev",
+      subject: opts.preview,
       html,
     });
   }
