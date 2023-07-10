650a0d3f3f2adad1cff0ff4948e4a826eda3fce4
James P <jamesperkins@hey.com>
2023-07-10 10:22:37 -0400

    Adding email changelog (#137)
    * Adding email changelog
    
    * Adding changelog and changelog changes for a cleaner look

--- apps/web/app/og/route.tsx
+++ apps/web/app/og/route.tsx
@@ -60,7 +60,6 @@ export async function GET(request: Request) {
       },
     );
   } catch (e: any) {
-    console.log(`${e.message}`);
     return new Response("Failed to generate the image", {
       status: 500,
     });
