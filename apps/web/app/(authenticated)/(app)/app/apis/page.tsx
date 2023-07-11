604205dd2e6649f75c21a5b687e0c2a365de5387
Andreas Thomas <dev@chronark.com>
2023-07-11 23:25:26 +0200

    ci: deploy canary cause bluegreen seems broken (#144)

--- apps/web/app/(authenticated)/(app)/app/apis/page.tsx
+++ apps/web/app/(authenticated)/(app)/app/apis/page.tsx
@@ -39,81 +39,83 @@ export default async function TenantOverviewPage() {
         .from(schema.keys)
         .where(eq(schema.keys.apiId, api.id)),
     })),
-    );
-    const unpaid = workspace.tenantId.startsWith("org_") && workspace.plan === "free";
-    return(
-      <div>
-
-   {unpaid ? (
-      <div>
-      <PageHeader title="Applications" description="Manage your APIs" />
-
-      <Separator className="my-6" />
-
-      <div className="flex justify-center items-center">
-        <Card className="duration-500 hover:border-primary bg-muted w-3xl ">
-          <CardHeader>
-            <CardTitle>Please add billing to your account</CardTitle>
-          </CardHeader>
-
-          <CardContent>
-            <p className="text-gray-500">
-             Team workspaces is a paid feature. Please add billing to your account to continue using it.
-            </p>
-          </CardContent>
-          <CardFooter>
-          <Link
-            href="/app/stripe"
-            target="_blank"
-            className="px-4 py-2 mr-3 text-sm font-medium text-center text-white bg-gray-800 rounded-lg hover:bg-gray-500 focus:ring-4 focus:outline-none focus:ring-gray-300 dark:bg-gray-600 dark:hover:bg-gray-500 dark:focus:ring-gray-800"
-          >
-            Add billing
-          </Link>
-          </CardFooter>
-        </Card>
-        </div>
-    </div>
-    ) : (
+  );
+  const unpaid = workspace.tenantId.startsWith("org_") && workspace.plan === "free";
+  return (
     <div>
-      <PageHeader title="Applications" description="Manage your APIs" />
+      {unpaid ? (
+        <div>
+          <PageHeader title="Applications" description="Manage your APIs" />
 
-      <Separator className="my-6" />
+          <Separator className="my-6" />
 
-      <ul role="list" className="grid grid-cols-1 gap-x-6 gap-y-8 lg:grid-cols-3 xl:gap-x-8">
-        <Card className="duration-500 hover:border-primary bg-muted">
-          <CardHeader>
-            <CardTitle>Create New API</CardTitle>
-          </CardHeader>
-
-          <CardContent />
-          <CardFooter>
-            <CreateApiButton key="createApi" />
-          </CardFooter>
-        </Card>
-        {apis.map((api) => (
-          <Link key={api.id} href={`/app/${api.id}`}>
-            <Card className="duration-500 hover:border-primary">
+          <div className="flex justify-center items-center">
+            <Card className="duration-500 hover:border-primary bg-muted w-3xl ">
               <CardHeader>
-                <CardTitle>{api.name}</CardTitle>
-                <CardDescription>{api.id}</CardDescription>
+                <CardTitle>Please add billing to your account</CardTitle>
               </CardHeader>
 
               <CardContent>
-                <dl className="text-sm leading-6 divide-y divide-gray-100 ">
-                  <div className="flex justify-between py-3 gap-x-4">
-                    <dt className="text-gray-500">API Keys</dt>
-                    <dd className="flex items-start gap-x-2">
-                      <div className="font-medium text-gray-900">{api.keys.at(0)?.count ?? 0}</div>
-                    </dd>
-                  </div>
-                </dl>
+                <p className="text-gray-500">
+                  Team workspaces is a paid feature. Please add billing to your account to continue
+                  using it.
+                </p>
               </CardContent>
+              <CardFooter>
+                <Link
+                  href="/app/stripe"
+                  target="_blank"
+                  className="px-4 py-2 mr-3 text-sm font-medium text-center text-white bg-gray-800 rounded-lg hover:bg-gray-500 focus:ring-4 focus:outline-none focus:ring-gray-300 dark:bg-gray-600 dark:hover:bg-gray-500 dark:focus:ring-gray-800"
+                >
+                  Add billing
+                </Link>
+              </CardFooter>
             </Card>
-          </Link>
-        ))}
-      </ul>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <PageHeader title="Applications" description="Manage your APIs" />
+
+          <Separator className="my-6" />
+
+          <ul role="list" className="grid grid-cols-1 gap-x-6 gap-y-8 lg:grid-cols-3 xl:gap-x-8">
+            <Card className="duration-500 hover:border-primary bg-muted">
+              <CardHeader>
+                <CardTitle>Create New API</CardTitle>
+              </CardHeader>
+
+              <CardContent />
+              <CardFooter>
+                <CreateApiButton key="createApi" />
+              </CardFooter>
+            </Card>
+            {apis.map((api) => (
+              <Link key={api.id} href={`/app/${api.id}`}>
+                <Card className="duration-500 hover:border-primary">
+                  <CardHeader>
+                    <CardTitle>{api.name}</CardTitle>
+                    <CardDescription>{api.id}</CardDescription>
+                  </CardHeader>
+
+                  <CardContent>
+                    <dl className="text-sm leading-6 divide-y divide-gray-100 ">
+                      <div className="flex justify-between py-3 gap-x-4">
+                        <dt className="text-gray-500">API Keys</dt>
+                        <dd className="flex items-start gap-x-2">
+                          <div className="font-medium text-gray-900">
+                            {api.keys.at(0)?.count ?? 0}
+                          </div>
+                        </dd>
+                      </div>
+                    </dl>
+                  </CardContent>
+                </Card>
+              </Link>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
-  )}
-  </div>
   );
 }
