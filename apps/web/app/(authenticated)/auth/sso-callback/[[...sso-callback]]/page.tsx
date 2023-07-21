b4d5eeb17ef1cd572c8ee98a3653bf05dd2a9619
Bereket Engida <bekacru@gmail.com>
2023-07-21 22:44:08 +0300

    lint fixes

--- apps/web/app/(authenticated)/auth/sso-callback/[[...sso-callback]]/page.tsx
+++ apps/web/app/(authenticated)/auth/sso-callback/[[...sso-callback]]/page.tsx
@@ -9,10 +9,7 @@ export default function SSOCallback() {
   return (
     <div className="h-screen flex items-center justify-center ">
       <Icons.spinner className="mr-2 h-16 w-16 animate-spin" />
-      <AuthenticateWithRedirectCallback
-        afterSignInUrl="/app/apis"
-        afterSignUpUrl="/onboarding"
-      />
+      <AuthenticateWithRedirectCallback afterSignInUrl="/app/apis" afterSignUpUrl="/onboarding" />
     </div>
   );
 }
