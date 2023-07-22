ca5c95d1e60961247276f75982d5f6c60b422b7a
Andreas Thomas <dev@chronark.com>
2023-07-22 11:24:57 +0200

    feat: sentry (#164)

---
+++ apps/web/next.config.mjs
@@ -0,0 +1,57 @@
+/** @type {import('next').NextConfig} */
+import { withContentlayer } from "next-contentlayer";
+import { withSentryConfig } from "@sentry/nextjs";
+
+const nextConfig = {
+  pageExtensions: ["tsx", "mdx", "ts", "js"],
+  experimental: {
+    serverActions: true,
+    esmExternals: "loose",
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
+
+let config = withContentlayer(nextConfig);
+
+const sentryOrg = process.env.SENTRY_ORG;
+const sentryProject = process.env.SENTRY_PROJECT;
+if (sentryOrg && sentryProject) {
+  config = withSentryConfig(
+    config,
+    {
+      // For all available options, see:
+      // https://github.com/getsentry/sentry-webpack-plugin#options
+
+      // Suppresses source map uploading logs during build
+      silent: true,
+
+      org: sentryOrg,
+      project: sentryProject,
+    },
+    {
+      // For all available options, see:
+      // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+      // Upload a larger set of source maps for prettier stack traces (increases build time)
+      widenClientFileUpload: true,
+
+      // Transpiles SDK to be compatible with IE11 (increases bundle size)
+      transpileClientSDK: true,
+
+      // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+      tunnelRoute: "/monitoring",
+
+      // Hides source maps from generated client bundles
+      hideSourceMaps: true,
+
+      // Automatically tree-shake Sentry logger statements to reduce bundle size
+      disableLogger: true,
+    },
+  );
+}
+
+export default config;
