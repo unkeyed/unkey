90cc33d7054e15fcb384e6192a3f3155a7956e51
chronark <dev@chronark.com>
2023-08-12 19:18:06 +0200

    fix(page.tsx): update heading text from "API management made easy" to "API authentication made easy" fix(page.tsx): update paragraph text to remove mention of Unkey's API management platform

--- apps/web/app/(landing)/page.tsx
+++ apps/web/app/(landing)/page.tsx
@@ -135,11 +135,10 @@ export default async function Home() {
       <Container className="mt-24 sm:mt-32 md:mt-56">
         <FadeIn className="max-w-3xl">
           <h1 className="font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl font-sans">
-            API management made easy
+            API authentication made easy
           </h1>
           <p className={"mt-6 text-xl text-neutral-600"}>
-            Seriously fast and easy to use. Unkey&apos;s API management platform helps developers
-            secure, manage, and scale their APIs.
+            Seriously fast and easy to use. Unkey helps developers secure, manage, and scale their APIs.
           </p>
         </FadeIn>
         <Button href="/app" className="px-8 py-3 mt-4">
