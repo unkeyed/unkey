22c9e1b0006d14bc5f47ce897f550be26ff6ae57
Andreas Thomas <dev@chronark.com>
2023-08-23 21:50:59 +0200

    feat: add verifyKey function to sdk (#220)

---
+++ packages/sdk/src/verify.ts
@@ -0,0 +1,29 @@
+import { Unkey } from "./client";
+
+/**
+ * Verify a key
+ *
+ * @example
+ * ```ts
+ * const { result, error } = await verifyKey("key_123")
+ * if (error){
+ *   // handle potential network or bad request error
+ *   // a link to our docs will be in the `error.docs` field
+ *   console.error(error.message)
+ *   return
+ * }
+ * if (!result.valid) {
+ *   // do not grant access
+ *   return
+ * }
+ *
+ * // process request
+ * console.log(result)
+ * ```
+ */
+export function verifyKey(key: string) {
+  // yes this is empty to make typescript happy but we don't need a token for verifying keys
+  // it's not the cleanest but it works for now :)
+  const unkey = new Unkey({ token: "" });
+  return unkey.keys.verify({ key });
+}
