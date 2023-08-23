b46663a7cb80fa7ab964abb72c3bbe2f3e04dd5b
James Perkins <jamesperkins@hey.com>
2023-08-23 19:26:18 -0400

    Fix bg black Add Features on the left on sign up.

--- apps/web/app/(authenticated)/auth/sign-up/layout.tsx
+++ apps/web/app/(authenticated)/auth/sign-up/layout.tsx
@@ -2,47 +2,26 @@ import { Particles } from "@/components/dashboard/particles";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-const testimonials: {
-  quote: React.ReactNode;
-  author: {
-    image: string;
-    name: string;
-    title: string;
-  };
+const features: {
+  title: string;
+  description: string;
 }[] = [
-  {
-    quote: <p>“I build stuff before asking if anyone wants it“</p>,
-    author: {
-      image: "https://pbs.twimg.com/profile_images/1437670380957835264/gu8S0olw_400x400.jpg",
-      name: "Andreas Thomas",
-      title: "Random Dude at Upstash",
+    {
+      title: "Save development time",
+      description: "Issue, manage, and revoke keys for your APIs in seconds with built in analytics."
     },
-  },
-  {
-    quote: (
-      <p>
-        “When using a 3rd party authentication system, it is most important to replicate a user
-        table in your own database!“
-      </p>
-    ),
-    author: {
-      image: "https://pbs.twimg.com/profile_images/1613589907133403161/zGtDomUL_400x400.jpg",
-      name: "James R Perkins",
-      title: "Random Dude at Clerk",
+    {
+      title: "Globally distributed",
+      description: "Unkey Globally distrubtes keys in 35+ locations, making it fast for every user."
     },
-  },
-  {
-    quote: <p>“I've been passionate about API Keys from a young age actually.“</p>,
-    author: {
-      image: "https://pbs.twimg.com/profile_images/1629470718630002692/Dax4prIG_400x400.jpg",
-      name: "Dom Eccleston",
-      title: "Random Dude at Vercel",
+    {
+      title: "Features for any use case",
+      description: "Each key has unique settings such as rate limiting, expiration, and limited uses."
     },
-  },
-];
+  ];
 
 export default function AuthLayout(props: { children: React.ReactNode }) {
-  const _testimonial = testimonials.at(Math.floor(Math.random() * testimonials.length));
+
   const { userId } = auth();
 
   if (userId) {
@@ -51,27 +30,20 @@ export default function AuthLayout(props: { children: React.ReactNode }) {
   return (
     <>
       <div className="relative grid min-h-screen grid-cols-1 overflow-hidden md:grid-cols-3 lg:grid-cols-2">
-        <div className="relative flex items-center justify-center bg-white md:bg-black bg-gradient-to-t from-violet-400/0 to-violet-400/20 ">
-          <Particles
-            className="absolute inset-0"
-            vy={-1}
-            quantity={50}
-            staticity={200}
-            color="#7c3aed"
-          />
-          <div className="lg:pr-4 lg:pt-4 sm:visibility: hidden">
-            <div className="lg:max-w-lg">
-              <h2 className="text-base font-semibold leading-7 text-violet-500">
-                Powerful API Key Management without the hassle
-              </h2>
-              <p className="mt-2 text-3xl font-bold tracking-tight text-gray-100 sm:text-4xl">
-                Save development time
-              </p>
-              <p className="mt-6 text-lg leading-8 text-gray-400">
-                Issue, manage, and revoke keys for any of your APIs in seconds. Comes with included
-                per-key ratelimiting and authorization rules.
-              </p>
-            </div>
+        <div className="relative flex items-center justify-center bg-white md:bg-black">
+          <div className="lg:pr-4 lg:pt-4 hidden md:block">
+            {
+              features.map((feature, index) => (
+                <div key={index} className="mb-8 lg:max-w-lg">
+                  <h3 className="my-2 text-3xl font-bold tracking-tight text-gray-100 sm:text-4xl">
+                    {feature.title}
+                  </h3>
+                  <p className="text-lg leading-8 text-gray-400">
+                    {feature.description}
+                  </p>
+                </div>
+              ))
+            }
           </div>
         </div>
         <div className="absolute inset-0 bg-gradient-to-t from-background to-background/60 md:hidden" />
