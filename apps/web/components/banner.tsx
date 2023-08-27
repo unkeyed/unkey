eb5fab8acb93922f09582afb9df551cd89ca6312
chronark <dev@chronark.com>
2023-08-27 23:50:29 +0200

    feat: show trial and exceede usage banners

--- apps/web/components/banner.tsx
+++ apps/web/components/banner.tsx
@@ -1,20 +1,31 @@
-import React, { PropsWithChildren } from "react";
+"use client"
+
+import React, { PropsWithChildren, useState } from "react";
 
 import { cn } from "@/lib/utils";
+import { X } from "lucide-react";
 
 type Props = {
   variant?: "alert";
 };
 
 export const Banner: React.FC<PropsWithChildren<Props>> = ({ children, variant }) => {
+  const [visible, setVisible] = useState(true)
+  if (!visible){
+    return null
+  }
   return (
-    <div
-      className={cn(" flex items-center justify-center  py-1 px-6 sm:px-3.5", {
-        "bg-stone-900 text-white": !variant,
-        "bg-red-600 text-red-50": variant === "alert",
-      })}
-    >
-      <p className="text-xs ">{children}</p>
-    </div>
+    <div className="fixed inset-x-0 bottom-0 z-50 pointer-events-none sm:flex sm:justify-center sm:px-6 sm:pb-5 lg:px-8">
+        <div className={cn("pointer-events-auto flex items-center justify-between gap-x-6  px-6 py-2.5 sm:rounded-lg sm:py-3 sm:pl-4 sm:pr-3.5",{
+          "bg-primary text-primary-foreground": variant === undefined,
+          "bg-destructive text-destructive-foreground": variant === "alert",
+        })}>
+      
+      {children}
+      <button type="button" className="-m-1.5 flex-none p-1.5" onClick={()=>setVisible(false)}>
+            <span className="sr-only">Close</span>
+            <X className="w-5 h-5 " aria-hidden="true" />
+          </button>
+    </div></div>
   );
 };
