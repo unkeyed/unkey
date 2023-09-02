14a58f2e2bfc2cc78441f7fd64a8ef42d7989de6
chronark <dev@chronark.com>
2023-09-02 23:18:37 +0200

    chore(user-button.tsx): remove unused import and menu item for Plans & Billing page

--- apps/web/app/(authenticated)/(app)/app/user-button.tsx
+++ apps/web/app/(authenticated)/(app)/app/user-button.tsx
@@ -10,7 +10,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { SignOutButton, useUser } from "@clerk/nextjs";
-import { Book, ChevronRight, LogOut, Rocket, Settings, Zap } from "lucide-react";
+import { Book, ChevronRight, LogOut, Rocket, Settings } from "lucide-react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React from "react";
@@ -50,12 +50,6 @@ export const UserButton: React.FC = () => {
             <DropdownMenuItem className="cursor-pointer">
               <Book className="w-4 h-4 mr-2" />
               <span>Docs</span>
-            </DropdownMenuItem>
-          </Link>
-          <Link href="/app/stripe">
-            <DropdownMenuItem className="cursor-pointer">
-              <Zap className="w-4 h-4 mr-2" />
-              <span>Plans & Billing</span>
             </DropdownMenuItem>
           </Link>
           <Link href="/app/settings/user">
