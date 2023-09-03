76fe2b2e92c77153d683e1ee7099069a64b44825
Andreas Thomas <dev@chronark.com>
2023-09-03 21:47:42 +0200

    nextjs package (#238)
    * feat(@unkey/nextjs): adds new unstable prototype for nextjs route wrapper
    
    * chore: configure commitizen
    
    * revert: remove changesets
    
    * ci: add nextjs ci

--- examples/nextjs/app/keys/verify/route.ts
+++ examples/nextjs/app/keys/verify/route.ts
@@ -1,38 +1,20 @@
-import { Unkey } from "@unkey/api";
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequestWithUnkeyContext, unstable__withUnkey } from "@unkey/nextjs";
+import { NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {
-  const unkeyToken = process.env.UNKEY_TOKEN;
-  if (!unkeyToken) {
-    return new NextResponse("UNKEY_TOKEN is undefined", { status: 500 });
+async function handler(req: NextRequestWithUnkeyContext) {
+  if (!req.unkey.valid) {
+    return new NextResponse("unauthorized", { status: 403 });
   }
-  const unkey = new Unkey({ token: unkeyToken });
-  // This part is a bit ugly, but we just use it here to make it easy to access this example
-  // in real world code, you usually have the token in a header, where it's much easier to access
-  const key = new URL(req.url).searchParams.get("key");
-  if (!key) {
-    return NextResponse.json({ error: "No key provided" }, { status: 400 });
-  }
-
-  const { result, error } = await unkey.keys.verify({ key });
-
-  if (error) {
-    return NextResponse.json({ error }, { status: 500 });
-  }
-
-  if (!result.valid) {
-    // Do not grant access to your user!
-    return NextResponse.json(result, { status: 400 });
-  }
-
-  // process the request here
 
   return new NextResponse(`Your API key is valid!
 
-Here's the key that was generated for you: ${key}
-
-And some more information from the verification:
-${JSON.stringify(result, null, 2)}
-
-  `);
+${JSON.stringify(req.unkey, null, 2)}`);
 }
+
+export const GET = unstable__withUnkey(handler, {
+  /**
+   * Just for this demo we're passing the key in searchparams,
+   * by default it is loaded from the `Authorization` header
+   */
+  getKey: (req) => new URL(req.url).searchParams.get("key"),
+});
