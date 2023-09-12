b0dda0acce66dede45e2827da9eed07c61439fa4
James P <jamesperkins@hey.com>
2023-09-12 12:14:01 -0400

    Update route.tsx

--- apps/web/app/og/blog/route.tsx
+++ apps/web/app/og/blog/route.tsx
@@ -49,7 +49,6 @@ export async function GET(req: NextRequest) {
             left: 80,
           }}
         >
-          <title>Gradient</title>
           <rect
             x="113"
             width="113"
