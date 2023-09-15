c35b5ed20ac49e35c7bf4b20899afe51d70237be
chronark <dev@chronark.com>
2023-09-15 13:53:07 +0200

    middleware-analytics

--- apps/web/lib/analytics.tsx
+++ apps/web/lib/analytics.tsx
@@ -7,25 +7,30 @@ const tb = new Tinybird({ token: env.TINYBIRD_TOKEN });
 const cookieName = "__unkey_session";
 
 export const ingestPageView = tb.buildIngestEndpoint({
-    datasource: "pageviews__v1",
-    event: z.object({
-        time: z.number().int(),
-        userId: z.string().default(""),
-        sessionId: z.string(),
-        tenantId: z.string().default(""),
-        path: z.string(),
-    }),
+  datasource: "pageviews__v1",
+  event: z.object({
+    sessionId: z.string(),
+    path: z.string(),
+    time: z.number().int(),
+    userId: z.string().optional(),
+    tenantId: z.string().optional(),
+    region: z.string().optional(),
+    country: z.string().optional(),
+    city: z.string().optional(),
+    userAgent: z.string().optional(),
+
+  }),
 });
 
 export function getSessionId(req: NextRequest, res: NextResponse): string {
 
-    let sessionId = req.cookies.get(cookieName)?.value;
-    if (!sessionId) {
-        sessionId = ["sess", crypto.randomUUID().replaceAll(/-/g, "")].join("_");
-        res.cookies.set(cookieName, sessionId);
-    }
+  let sessionId = req.cookies.get(cookieName)?.value;
+  if (!sessionId) {
+    sessionId = ["sess", crypto.randomUUID().replaceAll(/-/g, "")].join("_");
+    res.cookies.set(cookieName, sessionId);
+  }
 
-    return sessionId;
+  return sessionId;
 }
 
 
