d5848088c3e5440a8d09263218a1b64d3ca49d65
chronark <dev@chronark.com>
2023-09-15 23:31:40 +0200

    fix: deleting an api in the dashboard

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -5,7 +5,6 @@ export default {
   out: "./drizzle",
   driver: "mysql2",
   dbCredentials: {
-    // rome-ignore lint: suspicious/noNonNullAssertion
     connectionString: process.env.DRIZZLE_DATABASE_URL!,
   },
 } satisfies Config;
