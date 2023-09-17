a26d99603285e9dd22637933133a10e18e9db62e
Andreas Thomas <dev@chronark.com>
2023-09-17 14:50:47 +0200

    feat(cache): store origin misses (#264)
    repeated requests against non-existing resources will now be caught by the cache

--- apps/agent/pkg/cache/interface.go
+++ apps/agent/pkg/cache/interface.go
@@ -7,10 +7,26 @@ import (
 type Cache[T any] interface {
 	// Get returns the value for the given key.
 	// If the key is not found, found will be false.
-	Get(ctx context.Context, key string) (value T, found bool)
+	Get(ctx context.Context, key string) (value T, hit CacheHit)
 
 	// Sets the value for the given key.
 	Set(ctx context.Context, key string, value T)
+
+	// Sets the given key to null, indicating that the value does not exist in the origin.
+	SetNull(ctx context.Context, key string)
+
 	// Removes the key from the cache.
 	Remove(ctx context.Context, key string)
 }
+
+type CacheHit int
+
+const (
+	Null CacheHit = iota
+	// The entry was in the cache and can be used
+	Hit
+	// The entry was not in the cache
+	Miss
+	// The entry did not exist in the origin
+
+)
--- apps/agent/pkg/cache/noop.go
+++ apps/agent/pkg/cache/noop.go
@@ -6,16 +6,14 @@ import (
 
 type noopCache[T any] struct{}
 
-func (c *noopCache[T]) Get(ctx context.Context, key string) (value T, found bool) {
+func (c *noopCache[T]) Get(ctx context.Context, key string) (value T, hit CacheHit) {
 	var t T
-	return t, false
+	return t, Miss
 }
-func (c *noopCache[T]) Set(ctx context.Context, key string, value T) {
+func (c *noopCache[T]) Set(ctx context.Context, key string, value T) {}
+func (c *noopCache[T]) SetNull(ctx context.Context, key string)      {}
 
-}
-func (c *noopCache[T]) Remove(ctx context.Context, key string) {
-
-}
+func (c *noopCache[T]) Remove(ctx context.Context, key string) {}
 
 func NewNoopCache[T any]() Cache[T] {
 	return &noopCache[T]{}
