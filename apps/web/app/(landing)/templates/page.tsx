bcc8ea5f929a760dc4ce2a9e79ab8298271bbc9f
chronark <dev@chronark.com>
2023-09-17 23:18:53 +0200

    chore(opengraph-image.tsx): remove unused imports and code chore(twitter-image.tsx): remove unused imports and code feat(page.tsx): add generateMetadata function to generate metadata for the template page
    refactor(page.tsx): remove unused import of FadeIn and FadeInStagger components
    refactor(page.tsx): remove unused 'eyebrow' prop from PageIntro component
    refactor(page.tsx): remove unnecessary 'use client' statement
    refactor(page.tsx): remove unnecessary empty alt attribute in img tag
    refactor(page.tsx): change 'lauto-rows-fr' class to 'auto-rows-fr' in grid div
    refactor(page.tsx): remove unnecessary FadeInStagger component wrapping Link component
    refactor(page.tsx): remove unnecessary 'group-hover:text-gray-800' class from span element
    refactor(page.tsx): remove unnecessary 'text-gray-200' class from VenetianMask component
    refactor(page.tsx): remove unnecessary 'duration-200' class from div element
    refactor(page.tsx): remove unnecessary 'hover:scale-[1.01]' class from Link component
    refactor(page
    
    feat(web): add new route for generating Open Graph images
    refactor(web): remove FadeIn component from PageIntro component

--- apps/web/app/(landing)/templates/page.tsx
+++ apps/web/app/(landing)/templates/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { Container } from "@/components/landing/container";
-import { FadeIn, FadeInStagger } from "@/components/landing/fade-in";
 import { PageIntro } from "@/components/landing/page-intro";
 import { Checkbox } from "@/components/ui/checkbox";
 import {
@@ -84,163 +83,160 @@ export default function Templates() {
 
   return (
     <>
-      <PageIntro eyebrow="Templates" title="Find your Template">
+      <PageIntro title="Find your Template">
         <p>Jumpstart your api development with our pre-built solutions.</p>
       </PageIntro>
 
       <Container className="pt-16 mt-24 border-t">
-        <FadeIn>
-          <div className="flex flex-col lg:space-x-8 lg:flex-row ">
-            <div className="w-full lg:w-1/4">
-              <Form {...form}>
-                <h2 className="mb-8 font-semibold">Filter Templates</h2>
+        <div className="flex flex-col lg:space-x-8 lg:flex-row ">
+          <div className="w-full lg:w-1/4">
+            <Form {...form}>
+              <h2 className="mb-8 font-semibold">Filter Templates</h2>
 
-                <FormField
-                  control={form.control}
-                  name="search"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormControl>
-                        <Input placeholder="Search ..." {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
+              <FormField
+                control={form.control}
+                name="search"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input placeholder="Search ..." {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-                <FormField
-                  control={form.control}
-                  name="languages"
-                  render={() => (
-                    <FormItem className="mt-8 mb-4">
-                      <FormLabel className="text-base">Languages</FormLabel>
-                      <FormDescription>
-                        Select the programming languages you want to explore.
-                      </FormDescription>
-                      {Object.entries(languages).map(([language, occurences]) => (
-                        <FormField
-                          key={language}
-                          control={form.control}
-                          name="languages"
-                          render={({ field }) => {
-                            return (
-                              <FormItem
-                                key={language}
-                                className="flex flex-row items-center p-2 space-x-3 space-y-0 duration-150 rounded bg-gray-50 group hover:bg-gray-100"
-                              >
-                                <FormControl>
-                                  <Checkbox
-                                    checked={field.value?.includes(language)}
-                                    onCheckedChange={(checked) => {
-                                      console.log({ checked, field, language });
-                                      return checked
-                                        ? field.onChange([...field.value, language])
-                                        : field.onChange(
-                                            field.value?.filter((value) => value !== language),
-                                          );
-                                    }}
-                                  />
-                                </FormControl>
-                                <FormLabel className="flex items-center justify-between w-full">
-                                  <span className="text-sm font-normal">{language}</span>
-                                  <span className="p-1 text-xs text-gray-500 duration-150 bg-gray-100 rounded-full group-hover:text-gray-800">
-                                    {occurences}
-                                  </span>
-                                </FormLabel>
-                              </FormItem>
-                            );
-                          }}
-                        />
-                      ))}
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
+              <FormField
+                control={form.control}
+                name="languages"
+                render={() => (
+                  <FormItem className="mt-8 mb-4">
+                    <FormLabel className="text-base">Languages</FormLabel>
+                    <FormDescription>
+                      Select the programming languages you want to explore.
+                    </FormDescription>
+                    {Object.entries(languages).map(([language, occurences]) => (
+                      <FormField
+                        key={language}
+                        control={form.control}
+                        name="languages"
+                        render={({ field }) => {
+                          return (
+                            <FormItem
+                              key={language}
+                              className="flex flex-row items-center p-2 space-x-3 space-y-0 duration-150 rounded bg-gray-50 group hover:bg-gray-100"
+                            >
+                              <FormControl>
+                                <Checkbox
+                                  checked={field.value?.includes(language)}
+                                  onCheckedChange={(checked) => {
+                                    console.log({ checked, field, language });
+                                    return checked
+                                      ? field.onChange([...field.value, language])
+                                      : field.onChange(
+                                          field.value?.filter((value) => value !== language),
+                                        );
+                                  }}
+                                />
+                              </FormControl>
+                              <FormLabel className="flex items-center justify-between w-full">
+                                <span className="text-sm font-normal">{language}</span>
+                                <span className="p-1 text-xs text-gray-500 duration-150 bg-gray-100 rounded-full group-hover:text-gray-800">
+                                  {occurences}
+                                </span>
+                              </FormLabel>
+                            </FormItem>
+                          );
+                        }}
+                      />
+                    ))}
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-                <FormField
-                  control={form.control}
-                  name="frameworks"
-                  render={() => (
-                    <FormItem className="mt-8 mb-4">
-                      <FormLabel className="text-base">Frameworks</FormLabel>
-                      <FormDescription>Fancy a specific framework? Select it here.</FormDescription>
-                      {Object.entries(frameworks).map(([framework, occurences]) => (
-                        <FormField
-                          key={framework}
-                          control={form.control}
-                          name="frameworks"
-                          render={({ field }) => {
-                            return (
-                              <FormItem
-                                key={framework}
-                                className="flex flex-row items-center p-2 space-x-3 space-y-0 duration-150 rounded bg-gray-50 group hover:bg-gray-100"
-                              >
-                                <FormControl>
-                                  <Checkbox
-                                    checked={field.value?.includes(framework)}
-                                    onCheckedChange={(checked) => {
-                                      console.log({ checked, field, framework });
-                                      return checked
-                                        ? field.onChange([...field.value, framework])
-                                        : field.onChange(
-                                            field.value?.filter((value) => value !== framework),
-                                          );
-                                    }}
-                                  />
-                                </FormControl>
-                                <FormLabel className="flex items-center justify-between w-full">
-                                  <span className="text-sm font-normal">{framework}</span>
-                                  <span className="p-1 text-xs text-gray-500 duration-150 bg-gray-100 rounded-full group-hover:text-gray-800">
-                                    {occurences}
-                                  </span>
-                                </FormLabel>
-                              </FormItem>
-                            );
-                          }}
-                        />
-                      ))}
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </Form>
-            </div>
-            <div className="grid w-full grid-cols-1 gap-8 lg:w-3/4 auto-rows-fr lg:grid-cols-3">
-              {Object.entries(filteredTemplates).map(([id, template]) => (
-                <FadeInStagger key={id}>
-                  <Link
-                    href={`/templates/${id}`}
-                    className="flex flex-col items-start h-96 overflow-hidden duration-200 border border-gray-200 shadow rounded-xl hover:shadow-md hover:scale-[1.01]"
-                  >
-                    <div className="relative flex justify-center items-center h-full w-full aspect-[16/9] sm:aspect-[2/1] lg:aspect-[3/2]">
-                      {template.image ? (
-                        <img src={template.image} alt="" className="object-cover w-full h-full" />
-                      ) : (
-                        <VenetianMask className="w-16 h-16 text-gray-200" />
-                      )}
-                    </div>
-                    <div className="flex flex-col justify-between h-full px-4 pb-4">
-                      <div>
-                        <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600 line-clamp-2">
-                          {template.title}
-                        </h3>
-                        <p className="mt-5 text-sm leading-6 text-gray-500 line-clamp-2">
-                          {template.description}
-                        </p>
-                      </div>
-                      <div className="flex items-center justify-between mt-5">
-                        <p className="text-sm leading-6 text-gray-500 ">
-                          by {template.authors.join(", ")}
-                        </p>
-                        <ExternalLink className="w-4 h-4 text-gray-400" />
-                      </div>
-                    </div>
-                  </Link>
-                </FadeInStagger>
-              ))}
-            </div>
+              <FormField
+                control={form.control}
+                name="frameworks"
+                render={() => (
+                  <FormItem className="mt-8 mb-4">
+                    <FormLabel className="text-base">Frameworks</FormLabel>
+                    <FormDescription>Fancy a specific framework? Select it here.</FormDescription>
+                    {Object.entries(frameworks).map(([framework, occurences]) => (
+                      <FormField
+                        key={framework}
+                        control={form.control}
+                        name="frameworks"
+                        render={({ field }) => {
+                          return (
+                            <FormItem
+                              key={framework}
+                              className="flex flex-row items-center p-2 space-x-3 space-y-0 duration-150 rounded bg-gray-50 group hover:bg-gray-100"
+                            >
+                              <FormControl>
+                                <Checkbox
+                                  checked={field.value?.includes(framework)}
+                                  onCheckedChange={(checked) => {
+                                    console.log({ checked, field, framework });
+                                    return checked
+                                      ? field.onChange([...field.value, framework])
+                                      : field.onChange(
+                                          field.value?.filter((value) => value !== framework),
+                                        );
+                                  }}
+                                />
+                              </FormControl>
+                              <FormLabel className="flex items-center justify-between w-full">
+                                <span className="text-sm font-normal">{framework}</span>
+                                <span className="p-1 text-xs text-gray-500 duration-150 bg-gray-100 rounded-full group-hover:text-gray-800">
+                                  {occurences}
+                                </span>
+                              </FormLabel>
+                            </FormItem>
+                          );
+                        }}
+                      />
+                    ))}
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </Form>
           </div>
-        </FadeIn>
+          <div className="grid w-full grid-cols-1 gap-8 lg:w-3/4 lauto-rows-fr lg:grid-cols-3">
+            {Object.entries(filteredTemplates).map(([id, template]) => (
+              <Link
+                key={id}
+                href={`/templates/${id}`}
+                className="flex flex-col items-start h-96 overflow-hidden duration-200 border border-gray-200 shadow rounded-xl hover:shadow-md hover:scale-[1.01]"
+              >
+                <div className="relative flex justify-center items-center h-full w-full aspect-[16/9] sm:aspect-[2/1] lg:aspect-[3/2]">
+                  {template.image ? (
+                    <img src={template.image} alt="" className="object-cover w-full h-full" />
+                  ) : (
+                    <VenetianMask className="w-16 h-16 text-gray-200" />
+                  )}
+                </div>
+                <div className="flex flex-col justify-between h-full px-4 pb-4">
+                  <div>
+                    <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600 line-clamp-2">
+                      {template.title}
+                    </h3>
+                    <p className="mt-5 text-sm leading-6 text-gray-500 line-clamp-2">
+                      {template.description}
+                    </p>
+                  </div>
+                  <div className="flex items-center justify-between mt-5">
+                    <p className="text-sm leading-6 text-gray-500 ">
+                      by {template.authors.join(", ")}
+                    </p>
+                    <ExternalLink className="w-4 h-4 text-gray-400" />
+                  </div>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
       </Container>
     </>
   );
--- apps/web/components/landing/page-intro.tsx
+++ apps/web/components/landing/page-intro.tsx
@@ -1,7 +1,6 @@
 import clsx from "clsx";
 
 import { Container } from "@/components/landing/container";
-import { FadeIn } from "@/components/landing/fade-in";
 
 export function PageIntro({
   eyebrow,
@@ -9,32 +8,30 @@ export function PageIntro({
   children,
   centered = false,
 }: {
-  eyebrow: string;
+  eyebrow?: string;
   title: string;
   children: React.ReactNode;
   centered?: boolean;
 }) {
   return (
     <Container className={clsx("mt-24 sm:mt-32 lg:mt-40", centered && "text-center")}>
-      <FadeIn>
-        <h1>
-          <span className="block font-display text-base font-semibold text-gray-950 font-sans">
-            {eyebrow}
-          </span>
-          <span className="sr-only"> - </span>
-          <span
-            className={clsx(
-              "mt-6 block max-w-5xl font-display text-5xl font-medium tracking-tight text-gray-950 [text-wrap:balance] sm:text-6xl",
-              centered && "mx-auto",
-            )}
-          >
-            {title}
-          </span>
-        </h1>
-        <div className={clsx("mt-6 max-w-3xl text-xl text-gray-600", centered && "mx-auto")}>
-          {children}
-        </div>
-      </FadeIn>
+      <h1>
+        <span className="block font-sans text-base font-semibold font-display text-gray-950">
+          {eyebrow}
+        </span>
+        <span className="sr-only"> - </span>
+        <span
+          className={clsx(
+            "mt-6 block max-w-5xl font-display text-5xl font-medium tracking-tight text-gray-950 [text-wrap:balance] sm:text-6xl",
+            centered && "mx-auto",
+          )}
+        >
+          {title}
+        </span>
+      </h1>
+      <div className={clsx("mt-6 max-w-3xl text-xl text-gray-600", centered && "mx-auto")}>
+        {children}
+      </div>
     </Container>
   );
 }
