7f7b6414daa9ee9c3ecbcdd1a2943ed0124ac5c5
James P <jamesperkins@hey.com>
2023-09-20 08:58:46 -0400

    Homepage mobile fix (#272)
    * fix(homepage): fixes mobile overflow issue
    
    This fixes the mobile overflow issue that causes whitespace on the right
    
    Fix #217
    
    * fix: add w-full
    
    adds
    
    * Fix home page

--- apps/web/app/(landing)/page.tsx
+++ apps/web/app/(landing)/page.tsx
@@ -75,14 +75,14 @@ function Features() {
           has you covered.
         </p>
       </SectionIntro>
-      <Container className="mt-16">
-        <div className="lg:flex lg:items-center lg:justify-end">
+      <Container className="mt-16 overflow-x-hidden	">
+      <div className="lg:flex lg:items-center lg:justify-end">
           <div className="flex justify-center lg:w-1/2 lg:justify-end lg:pr-12">
             <FadeIn className="w-[33.75rem] flex-none lg:w-[45rem]">
               <StylizedImage
                 src={laptopImage}
                 sizes="(min-width: 1024px) 41rem, 31rem"
-                className="justify-center lg:justify-end"
+                className="justify-center lg:justify-end w-full"
               />
             </FadeIn>
           </div>
