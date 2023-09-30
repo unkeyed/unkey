b4d66ed45987a8162def645b8e527dfdd7f3da6f
Andreas Thomas <dev@chronark.com>
2023-09-30 17:10:48 +0200

    install pnpm in ci (#309)
    * chore(deps): remove changeset file for "@unkey/db" package and bump mysql2 version
    
    * chore(changesets.yaml): remove unnecessary step to check for errors
    chore(changesets.yaml): update build command to use 'pnpm build' instead of 'pnpm build:cli'
    
    * chore(changesets.yaml): update build step to include all packages
    
    * fix(changesets.yaml): simplify build step by using 'pnpm build' command
    fix(page.tsx): update environment variable access to use env() function
    fix(page.tsx): update environment variable access to use env() function
    fix(page.tsx): update environment variable access to use env() function
    fix(page.tsx): update environment variable access to use env() function
    fix(analytics.tsx): update environment variable access to use env() function
    fix(index.ts): update environment variable access to use env() function
    fix(db.ts): update environment variable access to use env() function
    fix(env.ts): update env() function to return parsed environment variables
    fix(env.ts): update env() function to return parsed environment variables
    fix(env.ts): update env() function to return parsed environment variables
    fix(env.ts): update env() function to return parsed environment variables
    fix(env.ts): update env() function to return parsed environment variables
    fix(env.ts): update

--- apps/web/lib/env.ts
+++ apps/web/lib/env.ts
@@ -1,30 +1,34 @@
 import { z } from "zod";
 
-export const env = z
-  .object({
-    VERCEL_ENV: z.enum(["development", "preview", "production"]).optional().default("development"),
-    VERCEL_URL: z.string().optional(),
-    DATABASE_HOST: z.string(),
-    DATABASE_USERNAME: z.string(),
-    DATABASE_PASSWORD: z.string(),
+export const env = () =>
+  z
+    .object({
+      VERCEL_ENV: z
+        .enum(["development", "preview", "production"])
+        .optional()
+        .default("development"),
+      VERCEL_URL: z.string().optional(),
+      DATABASE_HOST: z.string(),
+      DATABASE_USERNAME: z.string(),
+      DATABASE_PASSWORD: z.string(),
 
-    UNKEY_WORKSPACE_ID: z.string(),
-    UNKEY_API_ID: z.string(),
+      UNKEY_WORKSPACE_ID: z.string(),
+      UNKEY_API_ID: z.string(),
 
-    UPSTASH_REDIS_REST_URL: z.string().optional(),
-    UPSTASH_REDIS_REST_TOKEN: z.string().optional(),
+      UPSTASH_REDIS_REST_URL: z.string().optional(),
+      UPSTASH_REDIS_REST_TOKEN: z.string().optional(),
 
-    TINYBIRD_TOKEN: z.string(),
+      TINYBIRD_TOKEN: z.string(),
 
-    UNKEY_API_URL: z.string().url().default("https://api.unkey.dev"),
-    UNKEY_APP_AUTH_TOKEN: z.string(),
+      UNKEY_API_URL: z.string().url().default("https://api.unkey.dev"),
+      UNKEY_APP_AUTH_TOKEN: z.string(),
 
-    CLERK_WEBHOOK_SECRET: z.string().optional(),
-    LOOPS_API_KEY: z.string().optional(),
+      CLERK_WEBHOOK_SECRET: z.string().optional(),
+      LOOPS_API_KEY: z.string().optional(),
 
-    UPTIME_CRON_URL_COLLECT_BILLING: z.string().optional(),
-  })
-  .parse(process.env);
+      UPTIME_CRON_URL_COLLECT_BILLING: z.string().optional(),
+    })
+    .parse(process.env);
 
 const stripeSchema = z.object({
   STRIPE_SECRET_KEY: z.string(),
@@ -37,4 +41,4 @@ const stripeSchema = z.object({
 });
 
 const stripeParsed = stripeSchema.safeParse(process.env);
-export const stripeEnv = stripeParsed.success ? stripeParsed.data : null;
+export const stripeEnv = () => (stripeParsed.success ? stripeParsed.data : null);
