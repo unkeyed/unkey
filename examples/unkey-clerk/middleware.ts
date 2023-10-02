6fe7982c0151ad39e3b0154d2532feb7525d8828
James Perkins <jamesperkins@hey.com>
2023-10-02 10:06:34 -0400

    refactor(examples): moves examples to our monorepo
    Moves all examples to monorepo

---
+++ examples/unkey-clerk/middleware.ts
@@ -0,0 +1,10 @@
+import { authMiddleware } from "@clerk/nextjs";
+ 
+export default authMiddleware({
+      publicRoutes: "/api/secret",
+});
+ 
+export const config = {
+      matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
+ 
