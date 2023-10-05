4d82fdff56b2165120cdc16ae914257605da38e0
James Perkins <jamesperkins@hey.com>
2023-10-05 13:35:05 -0400

    Adding page update and blog

--- apps/web/app/(landing)/vercel/page.tsx
+++ apps/web/app/(landing)/vercel/page.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
 import {
-    Accordion,
-    AccordionContent,
-    AccordionItem,
-    AccordionTrigger,
-  } from "@/components/ui/accordion";
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Text } from "@/components/dashboard/text";
 export default function Example() {
   return (
     <div>
@@ -14,11 +15,11 @@ export default function Example() {
             Zero config API Authentication
           </h1>
           <p className="mt-6 text-lg leading-8 text-gray-600">
-            Integrate your Unkey project with Vercel copy blah blah. 
+            Integrate Unkey in your Vercel workflow, and we'll automatically inject your environment variables. No more manual configuration or copy and pasting.
           </p>
           <div className="flex items-center mt-10 gap-x-6">
             <Link
-              href="/auth/sign-up"
+              href="/todo"
               className="rounded-md border border-primary bg-primary px-3.5 py-1.5 text-sm font-semibold text-primary-foreground shadow-sm hover:bg-secondary hover:text-secondary-foreground focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Integrate for free
@@ -32,32 +33,32 @@ export default function Example() {
             src="/unkey-vercel.png"
           />
         </div>
-        
+
       </div>
       <div className="flex flex-col mx-auto max-w-2xl">
         <h2 className="text-2xl">FAQs</h2>
-      <Accordion type="multiple" className="w-full">
-                        <AccordionItem value="what-is-unkey">
-                          <AccordionTrigger dir="">What is Unkey?</AccordionTrigger>
-                          <AccordionContent>
-                            something
-                            </AccordionContent>
-                    </AccordionItem>
-                    <AccordionItem value="unkey-integrate">
-                          <AccordionTrigger dir="">How does Unkey integrate with Vercel?</AccordionTrigger>
-                          <AccordionContent>
-                            something
-                            </AccordionContent>
-                    </AccordionItem>
-                    <AccordionItem value="unkey-free">
-                          <AccordionTrigger dir="">Is Unkey Free?</AccordionTrigger>
-                          <AccordionContent>
-                            something
-                            </AccordionContent>
-                    </AccordionItem>
+        <Accordion type="multiple" className="w-full">
+          <AccordionItem value="what-is-unkey">
+            <AccordionTrigger dir="">What is Unkey?</AccordionTrigger>
+            <AccordionContent>
+              <Text>Unkey is an open source API management platform that helps developers secure, manage, and scale their APIs. Unkey has built-in features that can make it easier than ever to provide an API to your end users</Text>
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="unkey-integrate">
+            <AccordionTrigger dir="">How does Unkey integrate with Vercel?</AccordionTrigger>
+            <AccordionContent>
+            Once Unkey is connected with Vercel, you can select which workspace should be connected to your Vercel deployment. We will then insert all of your environment variables required to use Unkey.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="unkey-free">
+            <AccordionTrigger dir="">Is Unkey Free?</AccordionTrigger>
+            <AccordionContent>
+              Unkey has a generous free tier and is open source. We also offer a pro tier for businesses and high usage projects. Check out our <Link className="underline" href="/pricing">pricing page</Link> for more details.
+            </AccordionContent>
+          </AccordionItem>
 
         </Accordion>
-        </div>
+      </div>
     </div>
   );
 }
