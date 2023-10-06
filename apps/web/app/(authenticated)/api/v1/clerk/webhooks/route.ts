6efdb29520329aa1001305e81f7ab99cb78f6865
James Perkins <jamesperkins@hey.com>
2023-10-06 14:31:52 -0400

    Update Clerk to use Route handler

--- apps/web/app/(authenticated)/api/v1/clerk/webhooks/route.ts
+++ apps/web/app/(authenticated)/api/v1/clerk/webhooks/route.ts
@@ -1,34 +1,61 @@
-import type { IncomingHttpHeaders } from "http";
-import type { WebhookEvent } from "@clerk/nextjs/server";
-import type { NextApiRequest, NextApiResponse } from "next";
-import type { WebhookRequiredHeaders } from "svix";
-import { Webhook } from "svix";
-
-const webhookSecret: string | undefined = process.env.CLERK_WEBHOOK_SECRET;
-const loopsAPIKey: string | undefined = process.env.LOOPS_API_KEY;
-async function handler(req: NextApiRequestWithSvixRequiredHeaders, res: NextApiResponse) {
-  const payload = JSON.stringify(req.body);
-  const headers = req.headers;
-  if (!(webhookSecret && loopsAPIKey)) {
-    // just return a 400 here, it will never happen but it's good to be safe
-    return res.status(400).json({ Error: "Missing environment variables" });
+import { Webhook } from 'svix'
+import { headers } from 'next/headers'
+import { WebhookEvent } from '@clerk/nextjs/server'
+import { NextRequest } from 'next/server'
+ 
+async function handler(req: NextRequest) {
+ 
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
+  const loopsAPIKey = process.env.LOOPS_API_KEY
+  if (!WEBHOOK_SECRET) {
+    throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
   }
-  const wh = new Webhook(webhookSecret);
-
-  let evt: WebhookEvent;
+ 
+  // Get the headers
+  const headerPayload = headers();
+  const svix_id = headerPayload.get("svix-id");
+  const svix_timestamp = headerPayload.get("svix-timestamp");
+  const svix_signature = headerPayload.get("svix-signature");
+ 
+  // If there are no headers, error out
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    return new Response('Error occured -- no svix headers', {
+      status: 400
+    })
+  }
+ 
+  // Get the body
+  const payload = await req.json()
+  const body = JSON.stringify(payload);
+ 
+  // Create a new SVIX instance with your secret.
+  const wh = new Webhook(WEBHOOK_SECRET);
+ 
+  let evt: WebhookEvent
+ 
+  // Verify the payload with the headers
   try {
-    evt = wh.verify(payload, headers) as WebhookEvent;
-  } catch (_) {
-    // Don't log an error, just return a 400 because the webhook signature was invalid
-    return res.status(400).json({});
+    evt = wh.verify(body, {
+      "svix-id": svix_id,
+      "svix-timestamp": svix_timestamp,
+      "svix-signature": svix_signature,
+    }) as WebhookEvent
+  } catch (err) {
+    console.error('Error verifying webhook:', err);
+    return new Response('Error occured', {
+      status: 400
+    })
   }
-
+ 
+  // Get the eventType
   const eventType = evt.type;
   if (eventType === "user.created") {
     // we only care about the first email address, so we can just grab the first one
     const email = evt.data.email_addresses[0].email_address;
     if (!email) {
-      return res.status(400).json({ Error: "No email address found" });
+      return new Response('Error occured -- no email address found', {
+        status: 400
+      });
     }
     try {
       const loopsResponse = await fetch("https://app.loops.so/api/v1/contacts/create", {
@@ -41,21 +68,20 @@ async function handler(req: NextApiRequestWithSvixRequiredHeaders, res: NextApiR
       });
 
       if (loopsResponse.status < 200 || loopsResponse.status >= 300) {
-        return res
-          .status(500)
-          .json({ error: "Loops API Error ", jsonResponse: await loopsResponse.json() });
+        console.error('Error creating a user in loops:', await loopsResponse.json());
+        return new Response('Error occured -- Creating a user in loops' , {
+          status: 500
+        });
       }
-      return res.status(200).json({});
+      return new Response('Success', {
+        status: 200
+      });
     } catch (err) {
-      return res.status(400).json({
-        error: (err as Error).message,
+      console.error('Error sending to loops:', err);
+      return new Response('Error occured -- loops API error', {
+        status: 400
       });
     }
   }
 }
-
-type NextApiRequestWithSvixRequiredHeaders = NextApiRequest & {
-  headers: IncomingHttpHeaders & WebhookRequiredHeaders;
-};
-
 export { handler as POST };
