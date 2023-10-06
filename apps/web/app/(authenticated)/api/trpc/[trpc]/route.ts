b5c5ccef2d0fa0ab86b1e550946d0956862ac5f0
James Perkins <jamesperkins@hey.com>
2023-10-06 11:09:48 -0400

    fix(trpc route): fixes runtime requirements
    runtime now requires the use of export const runtime versus the original config

--- apps/web/app/(authenticated)/api/trpc/[trpc]/route.ts
+++ apps/web/app/(authenticated)/api/trpc/[trpc]/route.ts
@@ -2,9 +2,7 @@ import { createContext } from "@/lib/trpc/context";
 import { router } from "@/lib/trpc/routers";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import type { NextRequest } from "next/server";
-export const config = {
-  runtime: "edge",
-};
+export const runtime = "edge";
 
 // export API handler
 async function handler(req: NextRequest) {
