837079846bc98388d78dd90cf5043687220a99e1
ethan-stone <ethan.stone9352@gmail.com>
2023-10-15 16:08:37 -0400

    add example to templates

--- apps/web/app/(landing)/templates/data.ts
+++ apps/web/app/(landing)/templates/data.ts
@@ -4,7 +4,13 @@ type StrArrayToUnion<TArr extends readonly string[]> = TArr[number];
 export const frameworks = ["Django", "Next.js", "Svelte", "Express"] as const;
 export type Framework = StrArrayToUnion<typeof frameworks>;
 // id -> label
-export const languages = ["Typescript", "Python", "Golang", "Rust", "Elixir"] as const;
+export const languages = [
+  "Typescript",
+  "Python",
+  "Golang",
+  "Rust",
+  "Elixir",
+] as const;
 export type Language = StrArrayToUnion<typeof languages>;
 
 export type Template = {
@@ -42,10 +48,23 @@ export type Template = {
 export const templates: Record<string, Template> = {
   "nextjs-boilderplate": {
     title: "Next.js Boilerplate",
-    description: "A simple Next.js app using Unkey to generate and verify API keys.",
+    description:
+      "A simple Next.js app using Unkey to generate and verify API keys.",
     authors: ["chronark"],
     repository: "https://github.com/unkeyed/unkey/tree/main/examples/nextjs",
-    readmeUrl: "https://raw.githubusercontent.com/unkeyed/unkey/main/examples/nextjs/README.md",
+    readmeUrl:
+      "https://raw.githubusercontent.com/unkeyed/unkey/main/examples/nextjs/README.md",
+    language: "Typescript",
+    framework: "Next.js",
+  },
+  "nextjs-expiration": {
+    title: "Next.js Example with Temporary API Keys",
+    description:
+      "A simple Next.js app using Unkey to generate and verify API keys that expire after 60 seconds.",
+    authors: ["ethan-stone"],
+    repository: "https://github.com/unkeyed/unkey/tree/main/examples/nextjs",
+    readmeUrl:
+      "https://raw.githubusercontent.com/unkeyed/unkey/main/examples/nextjs-expiration/README.md",
     language: "Typescript",
     framework: "Next.js",
   },
@@ -67,48 +86,57 @@ export const templates: Record<string, Template> = {
     authors: ["mxkaske", "thibaultleouay"],
     repository: "https://github.com/openstatusHQ/openstatus",
     image: "/templates/openstatus.png",
-    readmeUrl: "https://raw.githubusercontent.com/openstatusHQ/openstatus/main/README.md",
+    readmeUrl:
+      "https://raw.githubusercontent.com/openstatusHQ/openstatus/main/README.md",
     language: "Typescript",
     framework: "Next.js",
     url: "https://openstatus.dev?ref=unkey.dev",
   },
   "atridadl-atash": {
     title: "Atash",
-    description: "A template to build type-safe full-stack serverless applications!",
+    description:
+      "A template to build type-safe full-stack serverless applications!",
     authors: ["atridadl"],
     image: "/templates/atash.png",
     repository: "https://github.com/atridadl/Atash",
-    readmeUrl: "https://raw.githubusercontent.com/atridadl/Atash/main/README.md",
+    readmeUrl:
+      "https://raw.githubusercontent.com/atridadl/Atash/main/README.md",
     url: "https://atash.atri.dad/",
     language: "Typescript",
     framework: "Next.js",
   },
   "uselessdev-iojinha": {
     title: "Iojinha",
-    description: "A template to build type-safe full-stack serverless applications!",
+    description:
+      "A template to build type-safe full-stack serverless applications!",
     authors: ["uselessdev"],
     repository: "https://github.com/uselessdev/Iojinha",
-    readmeUrl: "https://raw.githubusercontent.com/uselessdev/Iojinha/main/README.md",
+    readmeUrl:
+      "https://raw.githubusercontent.com/uselessdev/Iojinha/main/README.md",
     language: "Typescript",
     framework: "Next.js",
   },
   "atridadl-sprintpadawan": {
     title: "sprintpadawan",
-    description: "A scrum poker tool that helps agile teams plan their sprints in real-time.",
+    description:
+      "A scrum poker tool that helps agile teams plan their sprints in real-time.",
     authors: ["atridadl"],
     image: "/templates/sprintpadawan.png",
     repository: "https://github.com/atridadl/sprintpadawan",
-    readmeUrl: "https://raw.githubusercontent.com/atridadl/sprintpadawan/main/README.md",
+    readmeUrl:
+      "https://raw.githubusercontent.com/atridadl/sprintpadawan/main/README.md",
     url: "https://sprintpadawan.dev",
     language: "Typescript",
     framework: "Next.js",
   },
   "unkey-clerk": {
     title: "Unkey and Clerk",
-    description: "A simple template that shows how to use Unkey with an authentication provider",
+    description:
+      "A simple template that shows how to use Unkey with an authentication provider",
     authors: ["perkinsjr"],
     repository: "https://github.com/perkinsjr/unkey-clerk",
-    readmeUrl: "https://raw.githubusercontent.com/perkinsjr/unkey-clerk/main/README.md",
+    readmeUrl:
+      "https://raw.githubusercontent.com/perkinsjr/unkey-clerk/main/README.md",
     url: "https://github.com/perkinsjr/unkey-clerk",
     language: "Typescript",
     framework: "Next.js",
@@ -118,7 +146,8 @@ export const templates: Record<string, Template> = {
     description: "OCR API as a Service using Unkey",
     authors: ["WilfredAlmeida"],
     repository: "https://github.com/WilfredAlmeida/unkey-ocr",
-    readmeUrl: "https://raw.githubusercontent.com/WilfredAlmeida/unkey-ocr/main/README.md",
+    readmeUrl:
+      "https://raw.githubusercontent.com/WilfredAlmeida/unkey-ocr/main/README.md",
     language: "Typescript",
     url: "https://unkey.dev/blog/ocr-service",
     framework: "Express",
