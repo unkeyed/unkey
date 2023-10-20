2f046aea9c2b9d64ab3d8391dbf83e10999d72e3
chronark <dev@chronark.com>
2023-10-20 09:29:19 +0200

    revert: remove expired root keys

--- apps/web/app/(authenticated)/(app)/app/settings/root-keys/page.tsx
+++ apps/web/app/(authenticated)/(app)/app/settings/root-keys/page.tsx
@@ -1,7 +1,7 @@
 import { PageHeader } from "@/components/dashboard/page-header";
 import { RootKeyTable } from "@/components/dashboard/root-key-table";
 import { getTenantId } from "@/lib/auth";
-import { db, eq, schema } from "@/lib/db";
+import { Key, db, eq, schema } from "@/lib/db";
 import { redirect } from "next/navigation";
 import { CreateRootKeyButton } from "./create-root-key-button";
 
@@ -22,10 +22,26 @@ export default async function SettingsKeysPage(props: { params: { apiId: string 
     return redirect("/onboarding");
   }
 
-  const keys = await db.query.keys.findMany({
+  const allKeys = await db.query.keys.findMany({
     where: eq(schema.keys.forWorkspaceId, workspace.id),
     limit: 100,
   });
+
+  const keys: Key[] = [];
+  const expired: Key[] = [];
+
+  for (const k of allKeys) {
+    if (k.expires && k.expires.getTime() < Date.now()) {
+      expired.push(k);
+    }
+    // remove temp keys from the list of keys.
+    if (!k.expires) {
+      keys.push(k);
+    }
+  }
+  if (expired.length > 0) {
+    await Promise.all(expired.map((k) => db.delete(schema.keys).where(eq(schema.keys.id, k.id))));
+  }
 
   return (
     <div className="min-h-screen">
