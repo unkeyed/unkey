8d0e7024b9d331430b8c3b739cb82ac230238984
Mike Silva <michael@unkey.dev>
2023-10-31 14:59:41 -0400

    fix(loading): loading for dashboard
    KEY-113

---
+++ apps/web/app/(authenticated)/(app)/app/apis/[apiId]/loading.tsx
@@ -0,0 +1,8 @@
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return (
+    <div className="h-auto flex items-center justify-center">
+      <h1 className="font-12xl">Loading</h1>
+    </div>
+  );
+}
