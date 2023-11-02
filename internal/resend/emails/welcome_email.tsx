9d4f861ca755b4f0d129899bc0de4eef095d474a
chronark <dev@chronark.com>
2023-11-02 09:25:47 +0100

    chore(welcome_email.tsx): add horizontal rule component to separate sections

--- internal/resend/emails/welcome_email.tsx
+++ internal/resend/emails/welcome_email.tsx
@@ -2,6 +2,7 @@
 import { Container } from "@react-email/container";
 import { Head } from "@react-email/head";
 import { Heading } from "@react-email/heading";
+import { Hr } from "@react-email/hr";
 import { Html } from "@react-email/html";
 import { Link } from "@react-email/link";
 import { Section } from "@react-email/section";
@@ -45,6 +46,7 @@ export function WelcomeEmail() {
                 </li>
               </Text>
             </Section>
+            <Hr />
             <Text>
               We love feedback, so feel free to respond to this email as you start using Unkey. We
               read and reply to every single one.
