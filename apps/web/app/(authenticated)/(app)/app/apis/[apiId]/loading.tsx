0fec117ed50155d1fdbe24b1c4c8691400b10c28
Mike Silva <michael@unkey.dev>
2023-11-02 14:44:41 -0400

    Added overview loading page for apis

--- apps/web/app/(authenticated)/(app)/app/apis/[apiId]/loading.tsx
+++
@@ -1,8 +0,0 @@
-export default function Loading() {
-  // You can add any UI inside Loading, including a Skeleton.
-  return (
-    <div className="h-auto flex items-center justify-center">
-      <h1 className="font-12xl">Loading</h1>
-    </div>
-  );
-}
