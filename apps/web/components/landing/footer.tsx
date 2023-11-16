6aa8c2b775e39a9ca7f8da88129f7d2f22ac8186
Devinda Senanayaka <3579142+dca123@users.noreply.github.com>
2023-11-16 20:50:22 +0800

    fix: update footer navigation for documentation  (#516)
    Corrects the URL for docs

--- apps/web/components/landing/footer.tsx
+++ apps/web/components/landing/footer.tsx
@@ -15,7 +15,7 @@ const navigation = [
       { title: "Changelog", href: "/changelog" },
       { title: "Analytics", href: "https://plausible.io/unkey.dev" },
       { title: "Source Code", href: "https://github.com/unkeyed/unkey" },
-      { title: "Documentation", href: "/docs" },
+      { title: "Documentation", href: "https://unkey.dev/docs" },
     ],
   },
   {
