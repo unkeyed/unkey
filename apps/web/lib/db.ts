283054bebb0c7ea88ab1c813aec8dc7e0b4222e4
nafees nazik <nafeesnazik21@gmail.com>
2023-11-16 09:04:54 +0530

    feat: add adapters conditionally

--- apps/web/lib/db.ts
+++ apps/web/lib/db.ts
@@ -1,25 +1,44 @@
-import { drizzle } from "drizzle-orm/planetscale-serverless";
+import { drizzle as drizzleMysql } from "drizzle-orm/mysql2";
+import { drizzle as drizzlePS } from "drizzle-orm/planetscale-serverless";
 
 import { dbEnv } from "@/lib/env";
-import { connect } from "@planetscale/database";
+import { Config, connect } from "@planetscale/database";
 import { schema } from "@unkey/db";
 
-export const db = drizzle(
-  connect({
-    host: dbEnv().DATABASE_HOST,
-    username: dbEnv().DATABASE_USERNAME,
-    password: dbEnv().DATABASE_PASSWORD,
+import mysql from "mysql2/promise";
+
+const { DATABASE_ADAPTER, DATABASE_HOST, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_NAME } =
+  dbEnv();
+
+const commonConfig = {
+  host: DATABASE_HOST,
+  password: DATABASE_PASSWORD,
+};
+
+const planetScaleConfig: Config = {
+  ...commonConfig,
+  username: DATABASE_USERNAME,
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  fetch: (url: string, init: any) => {
     // biome-ignore lint/suspicious/noExplicitAny: TODO
-    fetch: (url: string, init: any) => {
-      // biome-ignore lint/suspicious/noExplicitAny: TODO
-      (init as any).cache = undefined; // Remove cache header
-      return fetch(url, init);
-    },
-  }),
-  {
-    schema,
+    (init as any).cache = undefined; // Remove cache header
+    return fetch(url, init);
   },
-);
+};
+
+const mySqlConfig: mysql.PoolOptions = {
+  ...commonConfig,
+  user: DATABASE_USERNAME,
+  database: DATABASE_NAME,
+};
+
+export const db =
+  DATABASE_ADAPTER === "planet-scale"
+    ? drizzlePS(connect(planetScaleConfig), { schema })
+    : drizzleMysql(mysql.createPool(mySqlConfig), {
+        schema,
+        mode: "default",
+      });
 
 export * from "@unkey/db";
 export * from "drizzle-orm";
--- tools/bootstrap/main.ts
+++ tools/bootstrap/main.ts
@@ -1,16 +1,20 @@
 import crypto from "node:crypto";
-import { connect } from "@planetscale/database";
+import { Config, connect } from "@planetscale/database";
 import { schema } from "@unkey/db";
-import { drizzle } from "drizzle-orm/planetscale-serverless";
+import { drizzle as drizzleMysql } from "drizzle-orm/mysql2";
+import { drizzle as drizzlePS } from "drizzle-orm/planetscale-serverless";
+
 import { z } from "zod";
 
 import baseX from "base-x";
+import mysql from "mysql2/promise";
 
 const envSchema = z.object({
   DATABASE_HOST: z.string(),
   DATABASE_USERNAME: z.string(),
   DATABASE_PASSWORD: z.string(),
-
+  DATABASE_NAME: z.string().optional(),
+  DATABASE_ADAPTER: z.enum(["planet-scale", "mysql"]).default("planet-scale"),
   /**
    * The `organizationId` or `userId` from clerk
    */
@@ -27,30 +31,50 @@ function newId(prefix: "api" | "ws" | "key_auth") {
 }
 
 async function main() {
-  const env = envSchema.parse(process.env);
+  const {
+    DATABASE_ADAPTER,
+    DATABASE_HOST,
+    DATABASE_PASSWORD,
+    DATABASE_USERNAME,
+    TENANT_ID,
+    DATABASE_NAME,
+  } = envSchema.parse(process.env);
 
-  const db = drizzle(
-    connect({
-      host: env.DATABASE_HOST,
-      username: env.DATABASE_USERNAME,
-      password: env.DATABASE_PASSWORD,
+  const commonConfig = {
+    host: DATABASE_HOST,
+    password: DATABASE_PASSWORD,
+  };
+
+  const planetScaleConfig: Config = {
+    ...commonConfig,
+    username: DATABASE_USERNAME,
+    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+    fetch: (url: string, init: any) => {
       // biome-ignore lint/suspicious/noExplicitAny: TODO
-      fetch: (url: string, init: any) => {
-        // biome-ignore lint/suspicious/noExplicitAny: TODO
-        (init as any).cache = undefined; // Remove cache header
-        return fetch(url, init);
-      },
-    }),
-    {
-      schema,
+      (init as any).cache = undefined; // Remove cache header
+      return fetch(url, init);
     },
-  );
+  };
+
+  const mySqlConfig: mysql.ConnectionOptions = {
+    ...commonConfig,
+    user: DATABASE_USERNAME,
+    database: DATABASE_NAME,
+  };
+
+  const db =
+    DATABASE_ADAPTER === "planet-scale"
+      ? drizzlePS(connect(planetScaleConfig), { schema })
+      : drizzleMysql(await mysql.createConnection(mySqlConfig), {
+          schema,
+          mode: "default",
+        });
 
   const workspaceId = newId("ws");
 
   const workspace = {
     id: workspaceId,
-    tenantId: env.TENANT_ID,
+    tenantId: TENANT_ID,
     name: "Unkey",
     internal: true,
     betaFeatures: {},
