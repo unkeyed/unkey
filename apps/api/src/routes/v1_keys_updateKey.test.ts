0605e80473d51cd0132398ffa09c718511b47d08
Rohit Lakhotia <rohitlakhotiya99@gmail.com>
2023-11-22 21:29:07 +0530

    feat: add route and test

---
+++ apps/api/src/routes/v1_keys_updateKey.test.ts
@@ -0,0 +1,50 @@
+import { expect, test } from "bun:test";
+
+import { init } from "@/pkg/global";
+import { sha256 } from "@/pkg/hash/sha256";
+import { newApp } from "@/pkg/hono/app";
+import { newId } from "@/pkg/id";
+import { KeyV1 } from "@/pkg/keys/v1";
+import { testEnv } from "@/pkg/testutil/env";
+import { fetchRoute } from "@/pkg/testutil/request";
+import { seed } from "@/pkg/testutil/seed";
+import { schema } from "@unkey/db";
+import { V1KeysUpdateKeyResponse, registerV1KeysUpdate } from "./v1_keys_updateKey";
+
+test("returns 200", async () => {
+  const env = testEnv();
+  // @ts-ignore
+  init({ env });
+  const app = newApp();
+  registerV1KeysUpdate(app);
+
+  const r = await seed(env);
+
+  const key = {
+    id: newId("key"),
+    keyAuthId: r.userKeyAuth.id,
+    workspaceId: r.userWorkspace.id,
+    start: "test",
+    name: "test",
+    hash: await sha256(new KeyV1({ byteLength: 16 }).toString()),
+    createdAt: new Date(),
+  };
+  await r.database.insert(schema.keys).values(key);
+
+  const res = await fetchRoute<Record<string, any>, V1KeysUpdateKeyResponse>(app, {
+    method: "GET",
+    url: "/v1/keys.updateKey",
+    headers: {
+      Authorization: `Bearer ${r.rootKey}`,
+    },
+    body: {
+      keyId: key.id,
+      name: "test2",
+      ownerId: "test2",
+      meta: { test: "test" },
+      expires: new Date(),
+    },
+  });
+
+  expect(res.status).toEqual(200);
+});
---
+++ apps/api/src/routes/v1_keys_updateKey.ts
@@ -0,0 +1,174 @@
+import { db, keyService, usageLimiter } from "@/pkg/global";
+import { App } from "@/pkg/hono/app";
+import { createRoute, z } from "@hono/zod-openapi";
+
+import { UnkeyApiError, openApiErrorResponses } from "@/pkg/errors";
+import { schema } from "@unkey/db";
+import { eq } from "drizzle-orm";
+
+const route = createRoute({
+  method: "post",
+  path: "/v1/keys.updateKey",
+  request: {
+    headers: z.object({
+      authorization: z.string().regex(/^Bearer [a-zA-Z0-9_]+/).openapi({
+        description: "A root key to authorize the request formatted as bearer token",
+        example: "Bearer unkey_1234",
+      }),
+    }),
+
+    body: {
+      required: true,
+      content: {
+        "application/json": {
+          schema: z.object({
+            keyId: z.string().openapi({
+              description: "The id of the key you want to modify",
+              example: "key_123",
+            }),
+            name: z.string().nullish().openapi({
+              description: "The name of the key",
+              example: "Customer X",
+            }),
+            ownerId: z.string().nullish().openapi({
+              description:
+                "The id of the tenant associated with this key. Use whatever reference you have in your system to identify the tenant. When verifying the key, we will send this field back to you, so you know who is accessing your API.",
+              example: "user_123",
+            }),
+            meta: z
+              .record(z.unknown())
+              .nullish()
+              .openapi({
+                description: "Any additional metadata you want to store with the key",
+                example: {
+                  roles: ["admin", "user"],
+                  stripeCustomerId: "cus_1234",
+                },
+              }),
+            expires: z.number().nullish().openapi({
+              description:
+                "The unix timestamp in milliseconds when the key will expire. If this field is null or undefined, the key is not expiring.",
+              example: Date.now(),
+            }),
+            ratelimit: z
+              .object({
+                type: z.enum(["fast", "consistent"]).openapi({
+                  description:
+                    "Fast ratelimiting doesn't add latency, while consistent ratelimiting is more accurate.",
+                  externalDocs: {
+                    description: "Learn more",
+                    url: "https://unkey.dev/docs/features/ratelimiting",
+                  },
+                }),
+                limit: z.number().int().min(1).openapi({
+                  description: "The total amount of burstable requests.",
+                }),
+                refillRate: z.number().int().min(1).openapi({
+                  description: "How many tokens to refill during each refillInterval.",
+                }),
+                refillInterval: z.number().int().min(1).openapi({
+                  description:
+                    "Determines the speed at which tokens are refilled, in milliseconds.",
+                }),
+              })
+              .nullish()
+              .openapi({
+                description: "Unkey comes with per-key ratelimiting out of the box.",
+                example: {
+                  type: "fast",
+                  limit: 10,
+                  refillRate: 1,
+                  refillInterval: 60,
+                },
+              }),
+            remaining: z.number().nullish().openapi({
+              description:
+                "The number of requests that can be made with this key before it becomes invalid. If this field is null or undefined, the key has no request limit.",
+              example: 1000,
+            }),
+          }),
+        },
+      },
+    },
+  },
+  responses: {
+    200: {
+      description:
+        "The key was successfully updated, it may take up to 30s for this to take effect in all regions",
+      content: {
+        "application/json": {
+          schema: z.object({}),
+        },
+      },
+    },
+    ...openApiErrorResponses,
+  },
+});
+
+export type Route = typeof route;
+export type V1KeysUpdateKeyRequest = z.infer<
+  typeof route.request.body.content["application/json"]["schema"]
+>;
+export type V1KeysUpdateKeyResponse = z.infer<
+  typeof route.responses[200]["content"]["application/json"]["schema"]
+>;
+
+export const registerV1KeysUpdate = (app: App) =>
+  app.openapi(route, async (c) => {
+    const authorization = c.req.header("authorization")!.replace("Bearer ", "");
+
+    // Get root key and check for API errors
+    const rootKey = await keyService.verifyKey(c, { key: authorization });
+    if (rootKey.error) {
+      throw new UnkeyApiError({ code: "INTERNAL_SERVER_ERROR", message: rootKey.error.message });
+    }
+    if (!rootKey.value.valid) {
+      throw new UnkeyApiError({ code: "UNAUTHORIZED", message: "the root key is not valid" });
+    }
+    if (!rootKey.value.isRootKey) {
+      throw new UnkeyApiError({ code: "UNAUTHORIZED", message: "root key required" });
+    }
+
+    const req = c.req.valid("json");
+
+    const key = await db.query.keys.findFirst({
+      where: (table, { eq }) => eq(table.id, req.keyId),
+    });
+
+    if (!key || key.workspaceId !== rootKey.value.authorizedWorkspaceId) {
+      throw new UnkeyApiError({ code: "NOT_FOUND", message: `key ${req.keyId} not found` });
+    }
+
+    // Create the updated key object
+    const updatedKey = {
+      name: req.name,
+      ownerId: req.ownerId,
+      meta: req.meta === null ? null : JSON.stringify(req.meta),
+      expires: req.expires === null ? null : new Date(req.expires!),
+      remainingRequests: req.remaining,
+      ratelimitType: req.ratelimit === null ? null : req.ratelimit?.type,
+      ratelimitLimit: req.ratelimit === null ? null : req.ratelimit?.limit,
+      ratelimitRefillRate: req.ratelimit === null ? null : req.ratelimit?.refillRate,
+      ratelimitRefillInterval: req.ratelimit === null ? null : req.ratelimit?.refillInterval,
+    };
+
+    // Await the db call since workers are serverless (the call might not succeed but the response will be sent)
+    await db
+      .update(schema.keys)
+      .set({ ...updatedKey } as any)
+      .where(eq(schema.keys.id, req.keyId));
+
+    await usageLimiter.revalidate({ keyId: key.id });
+    const keyAfterUpdate = await db.query.keys.findFirst({
+      where: (table, { eq }) => eq(table.id, req.keyId),
+    });
+
+    if (!keyAfterUpdate) {
+      throw new UnkeyApiError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "key not found after update, this should not happen",
+      });
+    }
+
+    return c.jsonT({});
+  });
