fc1187b92d0e7b127526a6cbd8006b032e8dd75a
chronark <dev@chronark.com>
2023-11-22 19:43:56 +0100

    refactor: id and hash packages

--- apps/api/src/routes/v1_apis_createApi.ts
+++ apps/api/src/routes/v1_apis_createApi.ts
@@ -3,8 +3,8 @@ import { App } from "@/pkg/hono/app";
 import { createRoute, z } from "@hono/zod-openapi";
 
 import { UnkeyApiError, openApiErrorResponses } from "@/pkg/errors";
-import { newId } from "@/pkg/id";
 import { schema } from "@unkey/db";
+import { newId } from "@unkey/id";
 
 const route = createRoute({
   method: "post",
