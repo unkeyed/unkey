d9f09d8fbe1b57c4879e82b77cc5a9c6906c1f0b
nafees nazik <nafeesnazik21@gmail.com>
2023-11-22 10:27:47 +0530

    feat: register securitySchemes

--- apps/api/src/pkg/hono/app.ts
+++ apps/api/src/pkg/hono/app.ts
@@ -2,6 +2,7 @@ import type { Env } from "@/pkg/env";
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { prettyJSON } from "hono/pretty-json";
 import { handleError, handleZodError } from "../errors";
+import { SECURITY_SCHEME_NAME } from "./openapi/create-auth-route";
 
 export type HonoEnv = {
   Bindings: Env;
@@ -23,6 +24,11 @@ export function newApp() {
       title: "Unkey Api",
       version: "1.0.0",
     },
+  });
+
+  app.openAPIRegistry.registerComponent("securitySchemes", SECURITY_SCHEME_NAME, {
+    type: "http",
+    scheme: "bearer",
   });
 
   return app;
