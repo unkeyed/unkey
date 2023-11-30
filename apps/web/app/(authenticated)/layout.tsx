0adca36ce71c2ecde5d769b375c102084538432f
James Perkins <jamesperkins@hey.com>
2023-11-30 17:11:18 -0500

    Edge test

--- apps/web/app/(authenticated)/layout.tsx
+++ apps/web/app/(authenticated)/layout.tsx
@@ -3,7 +3,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { ClerkProvider } from "@clerk/nextjs";
 
 export const dynamic = "force-dynamic";
-export const runtime = "edge";
 
 export default function AuthenticatedLayout({
   children,
