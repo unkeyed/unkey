02d3119a081c61b04a99759e65817502954c837a
Andreas Thomas <dev@chronark.com>
2023-12-04 21:01:03 +0100

    feat(remaining_is_consistent.test.ts): add integration test for verifying remaining count (#651)
    * feat(remaining_is_consistent.test.ts): add integration test for verifying remaining count
    
    This commit adds a new integration test file `remaining_is_consistent.test.ts` that tests the consistency of the remaining count in the API. The test verifies that the remaining count consistently counts down as keys are verified.
    
    The test performs the following steps:
    1. Creates a new API using the `v1/apis.createApi` endpoint.
    2. Creates a new key for the API using the `v1/keys.createKey` endpoint, with a specified remaining count.
    3. Verifies the key by calling the `v1/keys.verifyKey` endpoint in a loop, decrementing the remaining count each time.
    4. Checks that the key is valid and the remaining count is correct after each verification.
    5. Checks that the key becomes invalid and the remaining count reaches 0 after all verifications.
    6. Retrieves the key using the `v1/keys.getKey` endpoint and checks
    
    * fix(request.ts): fix linting issues and add ExecutionContext to app.request call

---
+++ apps/api/src/integration/remaining_is_consistent.test.ts
@@ -0,0 +1,109 @@
+import { integrationTestEnv } from "@/pkg/testutil/env";
+import { step } from "@/pkg/testutil/request";
+import type { V1ApisCreateApiRequest, V1ApisCreateApiResponse } from "@/routes/v1_apis_createApi";
+import type { V1ApisDeleteApiRequest, V1ApisDeleteApiResponse } from "@/routes/v1_apis_deleteApi";
+import type { V1KeysCreateKeyRequest, V1KeysCreateKeyResponse } from "@/routes/v1_keys_createKey";
+import { V1KeysGetKeyResponse } from "@/routes/v1_keys_getKey";
+import type { V1KeysVerifyKeyRequest, V1KeysVerifyKeyResponse } from "@/routes/v1_keys_verifyKey";
+import { expect, test } from "bun:test";
+
+const env = integrationTestEnv.parse(process.env);
+
+test("remaining consistently counts down", async () => {
+  const createApiResponse = await step<V1ApisCreateApiRequest, V1ApisCreateApiResponse>({
+    url: `${env.UNKEY_BASE_URL}/v1/apis.createApi`,
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${env.UNKEY_ROOT_KEY}`,
+    },
+    body: {
+      name: "scenario-test-pls-delete",
+    },
+  });
+  expect(createApiResponse.status).toEqual(200);
+  expect(createApiResponse.body.apiId).toBeDefined();
+  expect(createApiResponse.headers).toHaveProperty("unkey-request-id");
+
+  const remaining = 1000;
+
+  const createKeyResponse = await step<V1KeysCreateKeyRequest, V1KeysCreateKeyResponse>({
+    url: `${env.UNKEY_BASE_URL}/v1/keys.createKey`,
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${env.UNKEY_ROOT_KEY}`,
+    },
+    body: {
+      apiId: createApiResponse.body.apiId,
+      byteLength: 32,
+      prefix: "test",
+      remaining,
+    },
+  });
+  console.log(JSON.stringify(createKeyResponse));
+  expect(createKeyResponse.status).toEqual(200);
+
+  for (let i = remaining - 1; i >= 0; i--) {
+    const valid = await step<V1KeysVerifyKeyRequest, V1KeysVerifyKeyResponse>({
+      url: `${env.UNKEY_BASE_URL}/v1/keys.verifyKey`,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${env.UNKEY_ROOT_KEY}`,
+      },
+      body: {
+        apiId: createApiResponse.body.apiId,
+        key: createKeyResponse.body.key,
+      },
+    });
+
+    expect(valid.status).toEqual(200);
+    expect(valid.body.valid).toBeTrue();
+    expect(valid.body.remaining).toEqual(i);
+  }
+
+  const invalid = await step<V1KeysVerifyKeyRequest, V1KeysVerifyKeyResponse>({
+    url: `${env.UNKEY_BASE_URL}/v1/keys.verifyKey`,
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${env.UNKEY_ROOT_KEY}`,
+    },
+    body: {
+      apiId: createApiResponse.body.apiId,
+      key: createKeyResponse.body.key,
+    },
+  });
+  expect(invalid.status).toEqual(200);
+  expect(invalid.body.valid).toBeFalse();
+  expect(invalid.body.remaining).toEqual(0);
+
+  const key = await step<never, V1KeysGetKeyResponse>({
+    url: `${env.UNKEY_BASE_URL}/v1/keys.getKey?keyId=${createKeyResponse.body.keyId}`,
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${env.UNKEY_ROOT_KEY}`,
+    },
+  });
+  expect(key.status).toEqual(200);
+  expect(key.body.id).toEqual(createKeyResponse.body.keyId);
+  expect(key.body.remaining).toBeDefined();
+  expect(key.body.remaining).toEqual(0);
+
+  /**
+   * Teardown
+   */
+  const deleteApi = await step<V1ApisDeleteApiRequest, V1ApisDeleteApiResponse>({
+    url: `${env.UNKEY_BASE_URL}/v1/apis.deleteApi`,
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${env.UNKEY_ROOT_KEY}`,
+    },
+    body: {
+      apiId: createApiResponse.body.apiId,
+    },
+  });
+  expect(deleteApi.status).toEqual(200);
+});
