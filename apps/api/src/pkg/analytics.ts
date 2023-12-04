a2370621113b5995515a25fa67feabff1dd1bca2
chronark <dev@chronark.com>
2023-12-04 17:37:52 +0100

    refactor(analytics.ts): rename tb variable to client for clarity feat(analytics.ts): add default values for optional properties in ingestKeyVerification event schema feat(analytics.ts): add deprecated properties ratelimited and usageExceeded to ingestKeyVerification event schema

--- apps/api/src/pkg/analytics.ts
+++ apps/api/src/pkg/analytics.ts
@@ -1,27 +1,31 @@
-import { NoopTinybird, Tinybird as Client } from "@chronark/zod-bird";
+import { NoopTinybird, Tinybird } from "@chronark/zod-bird";
 import { z } from "zod";
 
 export class Analytics {
-  private readonly tb: Client | NoopTinybird;
+  public readonly client: Tinybird | NoopTinybird;
 
   constructor(token?: string) {
-    this.tb = token ? new Client({ token }) : new NoopTinybird();
+    this.client = token ? new Tinybird({ token }) : new NoopTinybird();
   }
 
   public get ingestKeyVerification() {
-    return this.tb.buildIngestEndpoint({
+    return this.client.buildIngestEndpoint({
       datasource: "key_verifications__v2",
       event: z.object({
         workspaceId: z.string(),
         apiId: z.string(),
         keyId: z.string(),
-        denied: z.enum(["RATE_LIMITED", "USAGE_EXCEEDED", "FORBIDDEN"]).optional(),
+        deniedReason: z.enum(["RATE_LIMITED", "USAGE_EXCEEDED", "FORBIDDEN"]).optional(),
         time: z.number(),
-        ipAddress: z.string().optional(),
-        userAgent: z.string().optional(),
-        requestedResource: z.string().optional(),
-        edgeRegion: z.string().optional(),
-        region: z.string().optional(),
+        ipAddress: z.string().default(""),
+        userAgent: z.string().default(""),
+        requestedResource: z.string().default(""),
+        edgeRegion: z.string().default(""),
+        region: z.string(),
+        // deprecated, use deniedReason
+        ratelimited: z.boolean().default(false),
+        // deprecated, use deniedReason
+        usageExceeded: z.boolean().default(false),
       }),
     });
   }
