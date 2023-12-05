5e3a6313c9c87d0e3503141d748a7de03123f51d
James Perkins <jamesperkins@hey.com>
2023-12-05 07:47:46 -0500

    Add default value to frontend API only

--- apps/web/providers/PostHogProvider.tsx
+++ apps/web/providers/PostHogProvider.tsx
@@ -7,7 +7,7 @@ import { PostHogProvider } from "posthog-js/react";
 import { useEffect } from "react";
 if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://app.posthog.com",
     capture_pageview: false,
     disable_session_recording: false,
   });
