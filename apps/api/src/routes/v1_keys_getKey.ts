4a60013027f9744f34096011d349510f8e28a485
Mike Silva <michael@unkey.dev>
2023-12-11 14:51:29 -0500

    Started on replenish docs

--- apps/api/src/routes/v1_keys_getKey.ts
+++ apps/api/src/routes/v1_keys_getKey.ts
@@ -90,8 +90,10 @@ export const registerV1KeysGetKey = (app: App) =>
       createdAt: data.key.createdAt.getTime() ?? undefined,
       expires: data.key.expires?.getTime() ?? undefined,
       remaining: data.key.remaining ?? undefined,
-      refillInterval: data.key.refillInterval ?? undefined,
-      refillIncrement: data.key.refillIncrement ?? undefined,
-      lastRefillAt: data.key.lastRefillAt?.getTime() ?? undefined,
+      refill: {
+        refillInterval: data.key.refillInterval ?? undefined,
+        refillIncrement: data.key.refillIncrement ?? undefined,
+        lastRefillAt: data.key.lastRefillAt?.getTime() ?? undefined,
+      },
     });
   });
