504be2a7480bdcd13637bcdc2a679cb204ccaf5a
chronark <dev@chronark.com>
2023-12-12 08:00:46 +0100

    ci.yaml: add support for creating and connecting to a docker network env.ts: add default value for DATABASE_NAME as "unkey"

--- apps/api/src/pkg/env.ts
+++ apps/api/src/pkg/env.ts
@@ -5,6 +5,7 @@ export const zEnv = z.object({
   DATABASE_HOST: z.string(),
   DATABASE_USERNAME: z.string(),
   DATABASE_PASSWORD: z.string(),
+  DATABASE_NAME: z.string().default("unkey"),
   AXIOM_TOKEN: z.string().optional(),
   CLOUDFLARE_API_KEY: z.string().optional(),
   CLOUDFLARE_ZONE_ID: z.string().optional(),
