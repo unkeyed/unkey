1db49b4859d75b5c54d557cf3b1d759b14270cb9
James Perkins <jamesperkins@hey.com>
2023-12-13 08:13:32 -0500

    Back to normal.

--- apps/web/app/(authenticated)/auth/sso-callback/[[...sso-callback]]/page.tsx
+++ apps/web/app/(authenticated)/auth/sso-callback/[[...sso-callback]]/page.tsx
@@ -6,7 +6,7 @@ import { AuthenticateWithRedirectCallback } from "@clerk/nextjs";
 export default function SSOCallback() {
   return (
     <div className="flex h-screen items-center justify-center ">
-      <Loading className="h-14 w-14" />
+      <Loading className="h-12 w-12" />
       <AuthenticateWithRedirectCallback afterSignInUrl="/app/apis" afterSignUpUrl="/new" />
     </div>
   );
