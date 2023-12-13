c61a0c91e901dfe038ba133c3dfbef9f296ff75a
James Perkins <jamesperkins@hey.com>
2023-12-13 07:19:56 -0500

    test push

--- apps/web/app/(authenticated)/auth/sso-callback/[[...sso-callback]]/page.tsx
+++ apps/web/app/(authenticated)/auth/sso-callback/[[...sso-callback]]/page.tsx
@@ -6,7 +6,7 @@ import { AuthenticateWithRedirectCallback } from "@clerk/nextjs";
 export default function SSOCallback() {
   return (
     <div className="flex h-screen items-center justify-center ">
-      <Loading className="h-16 w-16" />
+      <Loading className="h-32 w-32" />
       <AuthenticateWithRedirectCallback afterSignInUrl="/app/apis" afterSignUpUrl="/new" />
     </div>
   );
