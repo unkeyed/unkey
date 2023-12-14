345f5e637d11d5e527b099b5f8f900c684328cd3
chronark <dev@chronark.com>
2023-12-14 14:08:26 +0100

    revert: res.ok check

--- packages/api/src/client.ts
+++ packages/api/src/client.ts
@@ -145,7 +145,7 @@ export class Unkey {
         err = e;
         return null; // set `res` to `null`
       });
-      if (res && res.status < 500) {
+      if (res?.ok) {
         return { result: (await res.json()) as TResult };
       }
       const backoff = this.retry.backoff(i);
