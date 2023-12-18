93a220b20f1bcdfdc7fc95f649c5197416da1fb7
Andreas Thomas <dev@chronark.com>
2023-12-18 08:50:43 +0100

    feat: set createdAt on insertion (#696)
    * feat: set createdAt on insertion
    
    * ci: rename

--- internal/db/src/schema/keyAuth.ts
+++ internal/db/src/schema/keyAuth.ts
@@ -1,5 +1,5 @@
 import { relations } from "drizzle-orm";
-import { mysqlTable, varchar } from "drizzle-orm/mysql-core";
+import { datetime, mysqlTable, varchar } from "drizzle-orm/mysql-core";
 import { apis } from "./apis";
 import { keys } from "./keys";
 import { workspaces } from "./workspaces";
@@ -7,6 +7,8 @@ import { workspaces } from "./workspaces";
 export const keyAuth = mysqlTable("key_auth", {
   id: varchar("id", { length: 256 }).primaryKey(),
   workspaceId: varchar("workspace_id", { length: 256 }).notNull(),
+  createdAt: datetime("created_at", { fsp: 3 }),
+  deletedAt: datetime("deleted_at", { fsp: 3 }),
 });
 
 export const keyAuthRelations = relations(keyAuth, ({ one, many }) => ({
