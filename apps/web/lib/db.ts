8c5923af1f08be7e08b17046793e96c69bb90861
chronark <dev@chronark.com>
2023-12-20 17:05:12 +0100

    ci: make ci pass please

--- apps/web/lib/db.ts
+++ apps/web/lib/db.ts
@@ -12,7 +12,12 @@ export const db = drizzle(
 
     fetch: (url: string, init: any) => {
       (init as any).cache = undefined; // Remove cache header
-      return fetch(url, init);
+      const u = new URL(url);
+      // set protocol to http if localhost for CI testing
+      if (u.host.includes("localhost")) {
+        u.protocol = "http";
+      }
+      return fetch(u, init);
     },
   }),
   {
