1bd5380ed8ccc5f379fed86ee7369812d7d3555c
Mike Silva <michael@unkey.dev>
2023-12-22 11:12:01 -0500

    feat(success page): new page with our goals for success

--- apps/web/app/(authenticated)/(app)/app/success/page.tsx
+++ apps/web/app/(authenticated)/(app)/app/success/page.tsx
@@ -1,78 +1,72 @@
 import { AreaChart } from "@/components/dashboard/charts";
 import { PageHeader } from "@/components/dashboard/page-header";
-import { Card } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Progress } from "@/components/ui/progress";
 import { Separator } from "@/components/ui/separator";
-
 import { db } from "@/lib/db";
 import { getQ1ActiveWorkspaces } from "@/lib/tinybird";
-// import { notFound } from "next/navigation";
-import React, { useEffect, useState } from "react";
 
 async function getPaidCustomerCount() {
   const ws = await db.query.workspaces.findMany({
-    where: (table, { and, eq, isNotNull }) =>
-      and(eq(table.plan, "pro"), isNotNull(table.subscriptions)),
+    where: (table, { and, eq, isNotNull, not }) =>
+      and(isNotNull(table.subscriptions), not(eq(table.plan, "free"))),
   });
-  console.log(ws);
-
   return ws.length;
 }
 
-const chartData = [
-  {
-    x: "2010-01",
-    y: 1998,
-  },
-  {
-    x: "2010-02",
-    y: 1850,
-  },
-  {
-    x: "2010-03",
-    y: 1720,
-  },
-  {
-    x: "2010-04",
-    y: 1818,
-  },
-  {
-    x: "2010-05",
-    y: 1920,
-  },
-];
+function getChartData(data: any[]) {
+  const chartData = data.map((element) => {
+    return {
+      x: new Date(element.time).toLocaleDateString(),
+      y: element.workspaces,
+    };
+  });
+  return chartData;
+}
+
 export default async function SuccessPage() {
   const activeWorkspaces = await getQ1ActiveWorkspaces({});
+  const chartData = getChartData(activeWorkspaces.data);
   const activeWorkspaceValue = activeWorkspaces.data[0];
   const date = new Date(activeWorkspaceValue.time);
+  const paidCustomers = await getPaidCustomerCount();
+  const customerGoal = 6;
+  const activeWorkspaceGoal = 300;
   return (
     <div>
-      <PageHeader title="Success Metrics" description="Unkey success metrics" />
-      <h1 className="text-2xl font-semibold mb-8" />
-      <Separator />
-      <div />
-      <div>
-        <AreaChart data={chartData} timeGranularity="day" tooltipLabel="Active Workspaces" />
+      <div className="w-full">
+        <PageHeader title="Success Metrics" description="Unkey success metrics" />
+        <h1 className="text-2xl font-semibold mb-8" />
+        <Separator />
       </div>
-      <div className="flex flex-row gap-6">
-        <Card className="flex w-72 mt-6">
-          <div className="flex-col w-full py-4 px-6">
-            <p className="text-sm text-content-subtle">Active Workspaces</p>
-            <div className="text-2xl font-semibold leading-none tracking-tight mt-2">
-              {activeWorkspaceValue.workspaces}
+      <div className="flex gap-6 p-6 w-full">
+        <Card className="w-full">
+          <CardHeader>
+            <CardTitle>Active Workspaces</CardTitle>
+            <CardDescription>{`Current goal of ${activeWorkspaceGoal}`}</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div>
+              <AreaChart data={chartData} timeGranularity="day" tooltipLabel="Active Workspaces" />
             </div>
-            <div className=" text-sm text-content-subtle">{date.toDateString()}</div>
-          </div>
+          </CardContent>
         </Card>
-        <Card className="flex w-72 mt-6">
-          <div className="flex-col w-full py-4 px-6">
-            <p className="text-sm text-content-subtle">Paying Customers</p>
-            <div className="text-2xl font-semibold leading-none tracking-tight mt-2">
-              {await getPaidCustomerCount()}
-            </div>
-            <div className=" text-sm text-content-subtle">{date.toDateString()}</div>
 
-            {/* <div>JSON.stringify(activeWorkspaces.data, null, 2)}</div> */}
-          </div>
+        <Card className="flex flex-col h-fit w-full">
+          <CardHeader>
+            <CardTitle>Paying Customers</CardTitle>
+            <CardDescription>Current goal of 6</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-semibold leading-none tracking-tight mt-2">
+              {paidCustomers}
+            </div>
+            <div className="mt-4">
+              <Progress value={(paidCustomers / customerGoal) * 100} />
+            </div>
+
+            <div className=" text-sm text-content-subtle mt-6">{date.toDateString()}</div>
+          </CardContent>
         </Card>
       </div>
     </div>
