2322bdc5c3d993c4d4457f9d49863b116f00bf53
chronark <dev@chronark.com>
2023-12-23 09:16:54 +0100

    fix: clean up fetching logic

--- apps/web/app/(authenticated)/(app)/app/success/page.tsx
+++ apps/web/app/(authenticated)/(app)/app/success/page.tsx
@@ -3,33 +3,45 @@ import { PageHeader } from "@/components/dashboard/page-header";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Progress } from "@/components/ui/progress";
 import { Separator } from "@/components/ui/separator";
-import { db } from "@/lib/db";
+import { getTenantId } from "@/lib/auth";
+import { and, db, eq, isNotNull, not, schema, sql } from "@/lib/db";
 import { getQ1ActiveWorkspaces } from "@/lib/tinybird";
+import { notFound } from "next/navigation";
 
-async function getPaidCustomerCount() {
-  const ws = await db.query.workspaces.findMany({
-    where: (table, { and, eq, isNotNull, not }) =>
-      and(isNotNull(table.subscriptions), not(eq(table.plan, "free"))),
-  });
-  return ws.length;
-}
-
-function getChartData(data: any[]) {
-  const chartData = data.map((element) => {
-    return {
-      x: new Date(element.time).toLocaleDateString(),
-      y: element.workspaces,
-    };
-  });
-  return chartData;
-}
+export const revalidate = 60;
 
 export default async function SuccessPage() {
+  const tenantId = getTenantId();
+
+  const workspace = await db.query.workspaces.findFirst({
+    where: (table, { and, eq, isNull }) =>
+      and(eq(table.tenantId, tenantId), isNull(table.deletedAt)),
+  });
+
+  if (!workspace?.features.successPage) {
+    return notFound();
+  }
+
+  const payingWorkspaces = await db
+    .select({ count: sql<string>`count(*)` })
+    .from(schema.workspaces)
+    .where(
+      and(
+        isNotNull(schema.workspaces.subscriptions),
+        not(eq(schema.workspaces.plan, "free")),
+        isNotNull(schema.workspaces.subscriptions),
+      ),
+    )
+    .then((rows) => {
+      const count = rows.at(0)?.count;
+      return count ? parseInt(count) : 0;
+    });
+
   const activeWorkspaces = await getQ1ActiveWorkspaces({});
-  const chartData = getChartData(activeWorkspaces.data);
-  const activeWorkspaceValue = activeWorkspaces.data[0];
-  const date = new Date(activeWorkspaceValue.time);
-  const paidCustomers = await getPaidCustomerCount();
+  const chartData = activeWorkspaces.data.map(({ time, workspaces }) => ({
+    x: new Date(time).toLocaleDateString(),
+    y: workspaces,
+  }));
   const customerGoal = 6;
   const activeWorkspaceGoal = 300;
   return (
@@ -55,17 +67,15 @@ export default async function SuccessPage() {
         <Card className="flex flex-col h-fit w-full">
           <CardHeader>
             <CardTitle>Paying Customers</CardTitle>
-            <CardDescription>Current goal of 6</CardDescription>
+            <CardDescription>Current goal of {customerGoal}</CardDescription>
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-semibold leading-none tracking-tight mt-2">
-              {paidCustomers}
+              {payingWorkspaces}
             </div>
             <div className="mt-4">
-              <Progress value={(paidCustomers / customerGoal) * 100} />
+              <Progress value={(payingWorkspaces / customerGoal) * 100} />
             </div>
-
-            <div className=" text-sm text-content-subtle mt-6">{date.toDateString()}</div>
           </CardContent>
         </Card>
       </div>
