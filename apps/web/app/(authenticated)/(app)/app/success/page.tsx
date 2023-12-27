1dff6324bd0b17a00b0bc2b59cd56f02ec0ac4af
Andreas Thomas <dev@chronark.com>
2023-12-27 13:39:30 +0100

    fix: load customers from stripe (#734)
    * fix: load customers from stripe
    
    * fix: load invoices from stripe

--- apps/web/app/(authenticated)/(app)/app/success/page.tsx
+++ apps/web/app/(authenticated)/(app)/app/success/page.tsx
@@ -4,13 +4,23 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/com
 import { Progress } from "@/components/ui/progress";
 import { Separator } from "@/components/ui/separator";
 import { getTenantId } from "@/lib/auth";
-import { and, db, eq, isNotNull, not, schema, sql } from "@/lib/db";
+import { db } from "@/lib/db";
+import { stripeEnv } from "@/lib/env";
 import { getQ1ActiveWorkspaces } from "@/lib/tinybird";
 import { notFound } from "next/navigation";
+import Stripe from "stripe";
 
 export const revalidate = 60;
 
 export default async function SuccessPage() {
+  const e = stripeEnv();
+  if (!e) {
+    return <div>no stripe env</div>;
+  }
+  const stripe = new Stripe(e.STRIPE_SECRET_KEY, {
+    apiVersion: "2022-11-15",
+    typescript: true,
+  });
   const tenantId = getTenantId();
 
   const workspace = await db.query.workspaces.findFirst({
@@ -22,27 +32,42 @@ export default async function SuccessPage() {
     return notFound();
   }
 
-  const payingWorkspaces = await db
-    .select({ count: sql<string>`count(*)` })
-    .from(schema.workspaces)
-    .where(
-      and(
-        isNotNull(schema.workspaces.subscriptions),
-        not(eq(schema.workspaces.plan, "free")),
-        isNotNull(schema.workspaces.subscriptions),
-      ),
-    )
-    .then((rows) => {
-      const count = rows.at(0)?.count;
-      return count ? parseInt(count) : 0;
-    });
+  const allInvoices: Stripe.Invoice[] = [];
+  let hasMore = true;
+  let startingAfter: string | undefined = undefined;
+  while (hasMore) {
+    await stripe.invoices
+      .list({
+        starting_after: startingAfter,
+      })
+      .then((res) => {
+        allInvoices.push(...res.data);
+        hasMore = res.has_more;
+        startingAfter = res.data.at(-1)?.id;
+      });
+  }
+
+  const billableInvoices = allInvoices.filter(
+    (invoice) =>
+      invoice.status !== "draft" &&
+      invoice.total > 0 &&
+      invoice.created >= Math.floor(Date.now() / 1000) - 45 * 24 * 60 * 60,
+  );
+  let customers = 0;
+  const customerIds = new Set();
+  billableInvoices.forEach((invoice) => {
+    if (!customerIds.has(invoice.customer)) {
+      customers += 1;
+      customerIds.add(invoice.customer);
+    }
+  });
 
   const activeWorkspaces = await getQ1ActiveWorkspaces({});
   const chartData = activeWorkspaces.data.map(({ time, workspaces }) => ({
     x: new Date(time).toLocaleDateString(),
     y: workspaces,
   }));
-  const customerGoal = 6;
+  const customerGoal = 7;
   const activeWorkspaceGoal = 300;
   return (
     <div>
@@ -71,10 +96,10 @@ export default async function SuccessPage() {
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-semibold leading-none tracking-tight mt-2">
-              {payingWorkspaces}
+              {customers}
             </div>
             <div className="mt-4">
-              <Progress value={(payingWorkspaces / customerGoal) * 100} />
+              <Progress value={(customers / customerGoal) * 100} />
             </div>
           </CardContent>
         </Card>
