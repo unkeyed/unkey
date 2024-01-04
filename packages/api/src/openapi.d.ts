18901370ab0e7569ba509d7415d43b0152d55104
chronark <dev@chronark.com>
2024-01-04 10:27:56 +0100

    fix: do not count deleted keys

--- packages/api/src/openapi.d.ts
+++ packages/api/src/openapi.d.ts
@@ -731,6 +731,92 @@ export interface paths {
       };
     };
   };
+  "/vx/keys.getVerifications": {
+    get: {
+      parameters: {
+        query?: {
+          keyId?: string;
+          ownerId?: string;
+          start?: number | null;
+          end?: number | null;
+          granularity?: "hour" | "day" | "month";
+        };
+      };
+      responses: {
+        /** @description The configuration for a single key */
+        200: {
+          content: {
+            "application/json": {
+              verifications: {
+                /**
+                 * @description The timestamp of the usage data
+                 * @example 1620000000000
+                 */
+                time: number;
+                /**
+                 * @description The number of successful requests
+                 * @example 100
+                 */
+                success: number;
+                /**
+                 * @description The number of requests that were rate limited
+                 * @example 10
+                 */
+                rateLimited: number;
+                /**
+                 * @description The number of requests that exceeded the usage limit
+                 * @example 0
+                 */
+                usageExceeded: number;
+              }[];
+            };
+          };
+        };
+        /** @description The server cannot or will not process the request due to something that is perceived to be a client error (e.g., malformed request syntax, invalid request message framing, or deceptive request routing). */
+        400: {
+          content: {
+            "application/json": components["schemas"]["ErrBadRequest"];
+          };
+        };
+        /** @description Although the HTTP standard specifies "unauthorized", semantically this response means "unauthenticated". That is, the client must authenticate itself to get the requested response. */
+        401: {
+          content: {
+            "application/json": components["schemas"]["ErrUnauthorized"];
+          };
+        };
+        /** @description The client does not have access rights to the content; that is, it is unauthorized, so the server is refusing to give the requested resource. Unlike 401 Unauthorized, the client's identity is known to the server. */
+        403: {
+          content: {
+            "application/json": components["schemas"]["ErrForbidden"];
+          };
+        };
+        /** @description The server cannot find the requested resource. In the browser, this means the URL is not recognized. In an API, this can also mean that the endpoint is valid but the resource itself does not exist. Servers may also send this response instead of 403 Forbidden to hide the existence of a resource from an unauthorized client. This response code is probably the most well known due to its frequent occurrence on the web. */
+        404: {
+          content: {
+            "application/json": components["schemas"]["ErrNotFound"];
+          };
+        };
+        /** @description This response is sent when a request conflicts with the current state of the server. */
+        409: {
+          content: {
+            "application/json": components["schemas"]["ErrConflict"];
+          };
+        };
+        /** @description The user has sent too many requests in a given amount of time ("rate limiting") */
+        429: {
+          content: {
+            "application/json": components["schemas"]["ErrTooManyRequests"];
+          };
+        };
+        /** @description The server has encountered a situation it does not know how to handle. */
+        500: {
+          content: {
+            "application/json": components["schemas"]["ErrInternalServerError"];
+          };
+        };
+      };
+    };
+  };
   "/v1/apis.getApi": {
     get: {
       parameters: {
@@ -2019,7 +2105,7 @@ export interface components {
        */
       apiId?: string;
       /**
-       * @description The name of the key, give keys a name to easily identifiy their purpose
+       * @description The name of the key, give keys a name to easily identify their purpose
        * @example Customer X
        */
       name?: string;
