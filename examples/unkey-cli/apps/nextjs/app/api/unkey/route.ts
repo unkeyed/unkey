ff2db5499af0fa5efb30d94981abf347401afba4
Dom <domeccleston@gmail.com>
2024-01-10 20:26:09 +0000

    cleanup

--- examples/unkey-cli/apps/nextjs/app/api/unkey/route.ts
+++ examples/unkey-cli/apps/nextjs/app/api/unkey/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   const { id, redirect, code } = await request.json();
-  if (!process.env.UNKEY_ROOT_KEY || !process.env.UNKEY_API) {
+  if (!process.env.UNKEY_ROOT_KEY || !process.env.UNKEY_API_ID) {
     return NextResponse.json({
       statusCode: 500,
       message: "Unkey root key and API ID must be provided.",
@@ -12,7 +12,7 @@ export async function POST(request: Request) {
   const unkey = new Unkey({ rootKey: process.env.UNKEY_ROOT_KEY });
 
   const { result, error } = await unkey.keys.create({
-    apiId: process.env.UNKEY_API,
+    apiId: process.env.UNKEY_API_ID,
     prefix: "cli_demo",
     ownerId: id,
   });
