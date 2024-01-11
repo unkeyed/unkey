fd8ba15a42672ea1e352f88c033804a4edac4a27
Dom <domeccleston@gmail.com>
2024-01-11 13:40:22 +0000

    remove /examples

--- examples/unkey-clerk/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { authMiddleware } from "@clerk/nextjs";
-
-export default authMiddleware({
-  publicRoutes: "/api/secret",
-});
-
-export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-};
