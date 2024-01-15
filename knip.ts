45f9397f1cd3b5860acaa07e07ba914eb7665f90
Dom <domeccleston@gmail.com>
2024-01-15 13:09:15 +0000

    clean output

--- knip.ts
+++ knip.ts
@@ -1,7 +1,7 @@
 import type { KnipConfig } from "knip";
 
 const config: KnipConfig = {
-  ignore: ["**/*.test.ts", "**/*.spec.ts"],
+  ignore: ["**/*.test.ts", "**/*.spec.ts", "packages/nuxt/**/*"],
   workspaces: {
     ".": {
       entry: "checkly.config.ts",
