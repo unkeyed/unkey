00b3164b942826c2a21c258edec7e168a4c3b37a
Dom Eccleston <31101792+domeccleston@users.noreply.github.com>
2024-01-15 15:34:46 +0000

    chore: use knip to remove unused files and deps (#799)
    * chore: use knip to remove unused files and deps
    
    * lockfile
    
    * add contentlayer deps back in
    
    * second pass
    
    * clean output
    
    * fix nuxt deps
    
    * depfix
    
    * fix issue with nuxt tests
    
    * add ci workflow
    
    * final
    
    * lockfile
    
    * use install
    
    * add script back
    
    * fix build and entrypoints
    
    * ignore packages/nuxt
    
    * better nuxt solution

---
+++ knip.ts
@@ -0,0 +1,51 @@
+import type { KnipConfig } from "knip";
+
+const config: KnipConfig = {
+  ignoreWorkspaces: ["packages/nuxt"],
+  workspaces: {
+    ".": {
+      entry: "checkly.config.ts",
+    },
+    "apps/web": {
+      entry: ["contentlayer.config.ts", "lib/trpc/index.ts", "trpc.config.ts"],
+    },
+    "apps/api": {
+      entry: ["**/*.test.ts", "src/pkg/testutil/*.ts", "src/worker.ts"],
+    },
+    "internal/billing": {
+      entry: ["src/index.ts", "**/*.test.ts"],
+    },
+    "internal/db": {
+      entry: "src/index.ts",
+    },
+    "internal/hash": {
+      entry: ["src/index.ts", "**/*.test.ts"],
+    },
+    "internal/id": {
+      entry: "src/index.ts",
+    },
+    "internal/keys": {
+      entry: ["src/index.ts", "**/*.test.ts"],
+    },
+    "internal/resend": {
+      entry: "src/index.ts",
+    },
+    "internal/vercel": {
+      entry: "src/index.ts",
+    },
+    "packages/*": {
+      entry: ["**/*.test.ts"],
+    },
+    "tools/bootstrap": {
+      entry: "main.ts",
+    },
+    "tools/k6": {
+      entry: "load.js",
+    },
+    "tools/migrate": {
+      entry: "main.ts",
+    },
+  },
+};
+
+export default config;
