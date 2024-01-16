edf5bc74c182292b024042d0520491a208055b6f
Mike Silva <michael@unkey.dev>
2024-01-16 09:33:30 -0500

    added next-env.d.ts

--- knip.ts
+++ knip.ts
@@ -8,7 +8,7 @@ const config: KnipConfig = {
       entry: "checkly.config.ts",
     },
     "apps/web": {
-      entry: ["contentlayer.config.ts", "lib/trpc/index.ts", "trpc.config.ts"],
+      entry: ["contentlayer.config.ts", "lib/trpc/index.ts", "trpc.config.ts", "next-env.d.ts"],
     },
     "apps/api": {
       entry: ["**/*.test.ts", "src/pkg/testutil/*.ts", "src/worker.ts", "./vitest.*.ts"],
