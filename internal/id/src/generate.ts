ace382fad3135a2590530ae28c5228cfba4f7cdc
chronark <dev@chronark.com>
2024-01-24 17:47:29 +0100

    fix: platform crypto

--- internal/id/src/generate.ts
+++ internal/id/src/generate.ts
@@ -1,4 +1,3 @@
-import { randomBytes } from "crypto";
 import baseX from "base-x";
 
 const b58 = baseX("123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz");
@@ -17,7 +16,7 @@ const prefixes = {
 } as const;
 
 export function newId<TPrefix extends keyof typeof prefixes>(prefix: TPrefix) {
-  const buf = new Uint8Array(20);
+  const buf = crypto.getRandomValues(new Uint8Array(20));
 
   /**
    * epoch starts more recently so that the 32-bit number space gives a
@@ -32,8 +31,6 @@ export function newId<TPrefix extends keyof typeof prefixes>(prefix: TPrefix) {
   buf[1] = (t >>> 16) & 255;
   buf[2] = (t >>> 8) & 255;
   buf[3] = t & 255;
-
-  buf.set(randomBytes(16), 4);
 
   return `${prefixes[prefix]}_${b58.encode(buf)}` as const;
 }
