5112f8e5b9918da953c7eda0eb493f9d6c01d530
MichaelUnkey <148160799+MichaelUnkey@users.noreply.github.com>
2024-01-25 15:11:41 -0500

    feat: footer component for landing page (#890)
    * footer component added and styled
    
    * feat(landing footer): added a landing page footer component
    
    landing footer component
    
    eng-467

---
+++ apps/landing/components/footer/footer.tsx
@@ -0,0 +1,110 @@
+"use client";
+import Link from "next/link";
+import { UnkeyFooterLogo, UnkeyLogoSmall } from "./footer-svgs";
+import { socialMediaProfiles } from "./social-media";
+const navigation = [
+  {
+    title: "Company",
+    links: [
+      { title: "About", href: "/about" },
+      { title: "Blog", href: "/blog" },
+      { title: "Changelog", href: "/changelog" },
+      {
+        title: "Analytics",
+        href: "https://us.posthog.com/shared/HwZNjaKOLtgtpj6djuSo3fgOqrQm0Q?whitelabel",
+      },
+      {
+        title: "Source Code",
+        href: "https://github.com/unkeyed/unkey",
+      },
+      {
+        title: "Docs",
+        href: "https://unkey.dev/docs",
+      },
+    ],
+  },
+  {
+    title: "Connect",
+    links: [...socialMediaProfiles],
+  },
+  {
+    title: "Legal",
+    links: [
+      { title: "Terms of Service", href: "/policies/terms" },
+      { title: "Privacy Policy", href: "/policies/privacy" },
+    ],
+  },
+];
+
+function CompanyInfo() {
+  return (
+    <div className="flex flex-col">
+      <UnkeyLogoSmall />
+      <div className="font-normal text-sm leading-6 text-[rgba(255,255,255,0.5)] mt-8">
+        Seriously Fast API Authentication.
+      </div>
+      <div className="font-normal text-sm leading-6 text-[rgba(255,255,255,0.3)]">
+        Unkeyed, Inc. 2023
+      </div>
+    </div>
+  );
+}
+
+function Navigation() {
+  return (
+    <nav>
+      <ul className="flex max-sm:space-x-4 sm:space-x-20 md:space-x-2 lg:space-x-28 xl:space-x-52">
+        {navigation.map((section) => (
+          <li key={section.title}>
+            <div className="font-display text-sm text-white font-medium tracking-wider">
+              {section.title}
+            </div>
+            <ul className="text-sm text-[rgba(255,255,255,0.7)] font-normal">
+              {section.links.map((link) => (
+                <li key={link.href} className="mt-12 max-sm:mt-6">
+                  {link.href.startsWith("https://") ? (
+                    <a
+                      href={link.href}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="transition hover:text-[rgba(255,255,255,0.4)] "
+                    >
+                      {link.title}
+                    </a>
+                  ) : (
+                    <Link
+                      href={link.href}
+                      className="transition hover:text-[rgba(255,255,255,0.4)] "
+                    >
+                      {link.title}
+                    </Link>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+export function Footer() {
+  return (
+    <div className="bg-[radial-gradient(69.41%_100%_at_50%_0%,rgba(255,255,255,0.056)_0%,rgba(0,0,0,0.2)_50%,rgba(0,0,0,1)_100%)] pt-32 max-sm:pt-8">
+      <div className="flex flex-col lg:w-fit mx-auto max-sm:w-full ">
+        <div className="flex flex-row max-sm:flex-col justify-center sm:flex-col md:flex-row lg:gap-20 xl:gap-48">
+          <div className="flex lg:mx-auto max-sm:pl-12 max-sm:flex sm:flex-row sm:w-full mb-8 sm:pl-28 lg:pl-14 md:w-fit shrink-0 xl:pl-28">
+            <CompanyInfo />
+          </div>
+          <div className="flex w-full max-sm:pl-12 max-sm:pt-6 max-sm:mt-22 sm:pl-28 md:pl-18 lg:pl-6 max-sm:mb-8">
+            <Navigation />
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center lg:mt-24 w-full">
+        <UnkeyFooterLogo />
+      </div>
+    </div>
+  );
+}
