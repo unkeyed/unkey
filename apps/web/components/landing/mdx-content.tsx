fdd1547b99d4d362dab00e220d13b6acaa8a4005
James Perkins <jamesperkins@hey.com>
2024-01-26 11:12:14 -0500

    Next Image optimized images

--- apps/web/components/landing/mdx-content.tsx
+++ apps/web/components/landing/mdx-content.tsx
@@ -1,13 +1,15 @@
 "use client"; // This is required!
-
 import { MDXRemote, type MDXRemoteSerializeResult } from "next-mdx-remote";
+import Image from "next/Image";
 
 type MdxContentProps = {
   source: MDXRemoteSerializeResult;
 };
 
 /** Custom components here!*/
-const MdxComponents = {};
+const MdxComponents = {
+  Image: Image,
+};
 
 export function MdxContent({ source }: MdxContentProps) {
   return <MDXRemote {...source} components={MdxComponents} />;
