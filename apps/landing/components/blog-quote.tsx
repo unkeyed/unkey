4d7e9d668c4026eead3786748a2380fe3aee6345
Mike Silva <michael@unkey.dev>
2024-01-29 11:03:55 -0500

    started component

--- apps/landing/components/blog-quote.tsx
+++ apps/landing/components/blog-quote.tsx
@@ -3,39 +3,46 @@ import { cn } from "@/lib/utils";
 type BlogQuoteProps = {
   children?: React.ReactNode;
   className?: string;
+  quote?: string;
+  author?: string;
 };
 
-export function BlogQuote({ children, className }: BlogQuoteProps) {
+export function BlogQuote({ children, className, ...props }: BlogQuoteProps) {
   return (
-    <ul className={cn("flex flex-col text-lg border-l-white/20 py-4 pr-16 pl-12", className)}>
-      {children}
-    </ul>
+    <div className={cn("flex flex-col text-lg border-l-2 border-white/20 my-12 ml-8 py-4")}>
+      <div className="align-middle h-fit p-0 m-0">
+        <blockquote className="my-auto pl-12 font-medium text-white leading-8 pr-16">
+          {children}
+        </blockquote>
+        <p className="font-normal text-white/40 leading-8">{props.author}</p>
+      </div>
+    </div>
   );
 }
-export function BlogQuoteText({ children, className }: BlogQuoteProps) {
-  return (
-    <p
-      className={cn(
-        "font-medium text-white",
+// export function BlogQuoteText({ children, className }: BlogQuoteProps) {
+//   return (
+//     <p
+//       className={cn(
+//         "font-medium text-red-500",
 
-        className,
-      )}
-    >
-      {children}
-    </p>
-  );
-}
-export function BlogQuoteAuthor({ children, className }: BlogQuoteProps) {
-  return (
-    <p
-      className={cn(
-        "font-normal leading-8 text-white/40",
+//         className,
+//       )}
+//     >
+//       {children}
+//     </p>
+//   );
+// }
+// export function BlogQuoteAuthor({ children, className }: BlogQuoteProps) {
+//   return (
+//     <p
+//       className={cn(
+//         "font-normal leading-8 text-white/40",
 
-        className,
-      )}
-    >
-      <span>— </span>
-      {children}
-    </p>
-  );
-}
+//         className,
+//       )}
+//     >
+//       <span>— </span>
+//       {children}
+//     </p>
+//   );
+// }
--- apps/landing/components/mdx-content.tsx
+++ apps/landing/components/mdx-content.tsx
@@ -1,8 +1,10 @@
 "use client";
+import { authors } from "@/content/blog/authors";
 import { MDXRemote, type MDXRemoteSerializeResult } from "next-mdx-remote";
 import Image from "next/image";
 import { BlogQuote } from "./blog-quote";
 import { Alert } from "./ui/alert/alert";
+
 type MdxContentProps = {
   source: MDXRemoteSerializeResult;
 };
@@ -11,13 +13,18 @@ type MdxContentProps = {
 const MdxComponents = {
   Image: Image,
   Callout: Alert,
-  Blockquote: BlogQuote,
+  blockquote: (props: any) => BlogQuote(props),
   a: (props: any) => <a {...props} className="text-blue-500" />,
 
   h2: (props: any) => (
     <h2 {...props} className="text-2xl font-medium leading-8 blog-heading-gradient" />
   ),
-  p: (props: any) => <p {...props} className="text-lg font-normal leading-8" />,
+  p: (props: any) => (
+    <p
+      {...props}
+      className="text-lg font-normal leading-8 xl:pl-20 max-sm:pl-4 sm-pl-4 md:pl-8 lg:mt-18 xl:mt-20"
+    />
+  ),
 };
 
 export function MdxContent({ source }: MdxContentProps) {
