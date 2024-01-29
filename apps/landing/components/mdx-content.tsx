6d9105f7ea1cd33dfd98035604ebcd49ad2d59bd
MichaelUnkey <148160799+MichaelUnkey@users.noreply.github.com>
2024-01-29 15:44:56 -0500

    feat: blog list component (#908)
    * feat(blog list component): added blog list component
    
    blog list and other blog setup stuff for landing
    
    eng-478
    
    * fix lock file

--- apps/landing/components/mdx-content.tsx
+++ apps/landing/components/mdx-content.tsx
@@ -1,8 +1,8 @@
 "use client";
 import { MDXRemote, type MDXRemoteSerializeResult } from "next-mdx-remote";
 import Image from "next/image";
+import { BlogList, BlogListItem } from "./blog-list";
 import { Alert } from "./ui/alert/alert";
-
 type MdxContentProps = {
   source: MDXRemoteSerializeResult;
 };
@@ -11,6 +11,13 @@ type MdxContentProps = {
 const MdxComponents = {
   Image: Image,
   Callout: Alert,
+  a: (props: any) => <a {...props} className="text-blue-500" />,
+  ul: (props: any) => BlogList(props),
+  li: (props: any) => BlogListItem(props),
+  h2: (props: any) => (
+    <h2 {...props} className="text-2xl font-medium leading-8 blog-heading-gradient" />
+  ),
+  p: (props: any) => <p {...props} className="text-lg font-normal leading-8" />,
 };
 
 export function MdxContent({ source }: MdxContentProps) {
--- apps/landing/tailwind.config.ts
+++ apps/landing/tailwind.config.ts
@@ -28,7 +28,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 } satisfies Config;
 
 export default config;
