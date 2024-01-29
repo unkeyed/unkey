51afc3cac2fa95e70808d18b54b6da1b19906db2
Mike Silva <michael@unkey.dev>
2024-01-29 15:59:10 -0500

    pnpm fmt

--- apps/landing/components/mdx-content.tsx
+++ apps/landing/components/mdx-content.tsx
@@ -1,9 +1,8 @@
 "use client";
 import { MDXRemote, type MDXRemoteSerializeResult } from "next-mdx-remote";
 import Image from "next/image";
-
-import { BlogQuote } from "./blog-quote";
 import { BlogList, BlogListItem } from "./blog-list";
+import { BlogQuote } from "./blog-quote";
 import { Alert } from "./ui/alert/alert";
 type MdxContentProps = {
   source: MDXRemoteSerializeResult;
@@ -22,7 +21,6 @@ const MdxComponents = {
     <h2 {...props} className="text-2xl font-medium leading-8 blog-heading-gradient" />
   ),
   p: (props: any) => <p {...props} className="text-lg font-normal leading-8" />,
-
 };
 
 export function MdxContent({ source }: MdxContentProps) {
