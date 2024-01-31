0e01344902d6e0ef55d405bf2927e79d1f740f7d
James P <jamesperkins@hey.com>
2024-01-31 02:37:05 -0500

    fix: Stats suspense (#918)
    * adding suspense
    
    * fallback null for now
    
    * Move stats to numbers served and wrap in suspense

--- apps/web/app/(landing)/page.tsx
+++ apps/web/app/(landing)/page.tsx
@@ -1,69 +1,14 @@
 import { Container } from "@/components/landing/container";
-import { FadeIn, FadeInStagger } from "@/components/landing/fade-in";
+import { FadeIn } from "@/components/landing/fade-in";
 import { List, ListItem } from "@/components/landing/list";
+import { NumbersServed } from "@/components/landing/numbers-served";
 import { SectionIntro } from "@/components/landing/section-intro";
-import { StatList, StatListItem } from "@/components/landing/stat-list";
 import { StylizedImage } from "@/components/landing/stylized-image";
 import { Button } from "@/components/ui/button";
 import laptopImage from "@/images/computer-user.jpg";
-import { db, schema, sql } from "@/lib/db";
-import { getTotalVerifications } from "@/lib/tinybird";
 import { Github } from "lucide-react";
 import Link from "next/link";
-
-const [workspaces, apis, keys, totalVerifications] = await Promise.all([
-  db
-    .select({ count: sql<number>`count(*)` })
-    .from(schema.workspaces)
-    .then((res) => res.at(0)?.count ?? 0),
-  db
-    .select({ count: sql<number>`count(*)` })
-    .from(schema.apis)
-    .then((res) => res.at(0)?.count ?? 0),
-  db
-    .select({ count: sql<number>`count(*)` })
-    .from(schema.keys)
-    .then((res) => res.at(0)?.count ?? 0),
-  getTotalVerifications({}).then((res) => {
-    return res.data.reduce((acc, curr) => acc + curr.verifications, 0);
-  }),
-  { next: { revalidate: 3600 } },
-]);
-
-function NumbersServed() {
-  return (
-    <div className="rounded-4xl mt-24 sm:mt-32 lg:mt-32">
-      <Container className="">
-        <FadeIn className="flex items-center gap-x-8">
-          <h2 className="font-display mb-8 text-center text-2xl font-semibold tracking-wider text-black sm:text-left">
-            We serve
-          </h2>
-          <div className="h-px flex-auto" />
-        </FadeIn>
-        <FadeInStagger faster>
-          <StatList>
-            <StatListItem
-              value={Intl.NumberFormat("en", { notation: "compact" }).format(workspaces)}
-              label="Workspaces"
-            />
-            <StatListItem
-              value={Intl.NumberFormat("en", { notation: "compact" }).format(apis)}
-              label="APIs"
-            />
-            <StatListItem
-              value={Intl.NumberFormat("en", { notation: "compact" }).format(keys)}
-              label="Keys"
-            />
-            <StatListItem
-              value={Intl.NumberFormat("en", { notation: "compact" }).format(totalVerifications)}
-              label="Verifications"
-            />
-          </StatList>
-        </FadeInStagger>
-      </Container>
-    </div>
-  );
-}
+import { Suspense } from "react";
 
 function Features() {
   return (
@@ -200,8 +145,9 @@ export default async function Home() {
           </div>
         </FadeIn>
       </Container>
-      <NumbersServed />
-
+      <Suspense fallback={null}>
+        <NumbersServed />
+      </Suspense>
       <Features />
     </>
   );
