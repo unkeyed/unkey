e1d797df2cff5b816ef84fcfe1162b12a02b6b9a
Dom <domeccleston@gmail.com>
2024-02-01 19:13:33 +0000

    implement v1 of verifyKey

--- docker/server/index.ts
+++ docker/server/index.ts
@@ -9,45 +9,58 @@ import { KeyV1, newId, sha256 } from "../keys";
 const port = 3000;
 const app = new Hono();
 
+const validateCreateKey = validator("json", (value, c) => {
+  // TODO implement validation logic
+  return value;
+});
+
+const validateVerifyKey = validator("json", (value, c) => {
+  // TODO implement validation logic
+  return value;
+});
+
 app.get("/v1/liveness", async (c) => {
   const res = await db.select().from(keys);
   console.log(res);
   return c.text("Running");
 });
 
-app.post(
-  "/v1/keys/createKey",
-  validator("json", (value, _c) => {
-    // TODO implement validation logic
-    return value;
-  }),
-  async (c) => {
-    const req = c.req.valid("json");
-    const key = new KeyV1({
-      byteLength: req.byteLength ?? 16,
-      prefix: req.prefix,
-    }).toString();
-    const start = key.slice(0, (req.prefix?.length ?? 0) + 5);
-    const keyId = newId("key");
-    const hash = await sha256(key.toString());
+app.post("/v1/keys/verifyKey", validateVerifyKey, async (c) => {
+  const req = c.req.valid("json");
+  const hash = await sha256(req.key);
+  const dbRes = await db.query.keys.findFirst({
+    where: (table, { eq }) => eq(table.hash, hash),
+  });
+  console.log(dbRes);
+  return c.text("Running");
+});
 
-    const newKey = await db
-      .insert(keys)
-      .values({
-        id: keyId,
-        name: req.name,
-        hash,
-        start,
-        ownerId: req.ownerId,
-        createdAt: new Date(),
-        meta: req.meta ? JSON.stringify(req.meta) : null,
-        remaining: req.remaining,
-      })
-      .returning();
+app.post("/v1/keys/createKey", validateCreateKey, async (c) => {
+  const req = c.req.valid("json");
+  const key = new KeyV1({
+    byteLength: req.byteLength ?? 16,
+    prefix: req.prefix,
+  }).toString();
+  const start = key.slice(0, (req.prefix?.length ?? 0) + 5);
+  const keyId = newId("key");
+  const hash = await sha256(key.toString());
 
-    return c.json({ newKey });
-  },
-);
+  const newKey = await db
+    .insert(keys)
+    .values({
+      id: keyId,
+      name: req.name,
+      hash,
+      start,
+      ownerId: req.ownerId,
+      createdAt: new Date(),
+      meta: req.meta ? JSON.stringify(req.meta) : null,
+      remaining: req.remaining,
+    })
+    .returning();
+
+  return c.json({ newKey });
+});
 
 console.log(`Server is running on port ${port}`);
 
