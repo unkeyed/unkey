9120ba652990bcc95a307a3d1616f3009984f5a9
chronark <dev@chronark.com>
2024-02-06 10:40:10 +0100

    wip

--- internal/db/src/schema/rbac.ts
+++ internal/db/src/schema/rbac.ts
@@ -9,6 +9,8 @@ export const permissions = mysqlTable(
     id: varchar("id", { length: 256 }).primaryKey(),
     workspaceId: varchar("workspace_id", { length: 256 }).notNull(),
     name: varchar("name", { length: 512 }).notNull(),
+    key: varchar("key", { length: 512 }),
+    description: varchar("description", { length: 512 }),
   },
   (table) => ({
     uniqueNamePerWorkspace: uniqueIndex("unique_name_per_workspace_idx").on(
@@ -84,84 +86,84 @@ export const roles = mysqlTable(
     workspaceIdIndex: index("workspace_id_idx").on(table.workspaceId),
   }),
 );
-export const rolesRelations = relations(roles, ({ one, many }) => ({
-  workspace: one(workspaces, {
-    fields: [roles.workspaceId],
-    references: [workspaces.id],
-  }),
-  keys: many(keysRoles, {
-    relationName: "keys_roles_roles_relations",
-  }),
-  permissions: many(rolesPermissions, {
-    relationName: "roles_rolesPermissions",
-  }),
-}));
+// export const rolesRelations = relations(roles, ({ one, many }) => ({
+//   workspace: one(workspaces, {
+//     fields: [roles.workspaceId],
+//     references: [workspaces.id],
+//   }),
+//   keys: many(keysRoles, {
+//     relationName: "keys_roles_roles_relations",
+//   }),
+//   permissions: many(rolesPermissions, {
+//     relationName: "roles_rolesPermissions",
+//   }),
+// }));
 
-/**
- * N:M table to connect roles and permissions
- */
-export const rolesPermissions = mysqlTable(
-  "roles_permissions",
-  {
-    roleId: varchar("role_id", { length: 256 })
-      .notNull()
-      .references(() => roles.id, { onDelete: "cascade" }),
-    permissionId: varchar("permission_id", { length: 256 })
-      .notNull()
-      .references(() => permissions.id, { onDelete: "cascade" }),
-    workspaceId: varchar("workspace_id", { length: 256 })
-      .notNull()
-      .references(() => workspaces.id, { onDelete: "cascade" }),
-  },
-  (table) => ({
-    primaryKey: primaryKey({ columns: [table.roleId, table.permissionId, table.workspaceId] }),
-    uniquePermissionIdRoleId: uniqueIndex("unique_tuple_permission_id_role_id").on(
-      table.permissionId,
-      table.roleId,
-    ),
-  }),
-);
+// /**
+//  * N:M table to connect roles and permissions
+//  */
+// export const rolesPermissions = mysqlTable(
+//   "roles_permissions",
+//   {
+//     roleId: varchar("role_id", { length: 256 })
+//       .notNull()
+//       .references(() => roles.id, { onDelete: "cascade" }),
+//     permissionId: varchar("permission_id", { length: 256 })
+//       .notNull()
+//       .references(() => permissions.id, { onDelete: "cascade" }),
+//     workspaceId: varchar("workspace_id", { length: 256 })
+//       .notNull()
+//       .references(() => workspaces.id, { onDelete: "cascade" }),
+//   },
+//   (table) => ({
+//     primaryKey: primaryKey({ columns: [table.roleId, table.permissionId, table.workspaceId] }),
+//     uniquePermissionIdRoleId: uniqueIndex("unique_tuple_permission_id_role_id").on(
+//       table.permissionId,
+//       table.roleId,
+//     ),
+//   }),
+// );
 
-export const rolesPermissionsRelations = relations(rolesPermissions, ({ one }) => ({
-  role: one(roles, {
-    fields: [rolesPermissions.roleId],
-    references: [roles.id],
-    relationName: "roles_rolesPermissions",
-  }),
-  permission: one(permissions, {
-    fields: [rolesPermissions.permissionId],
-    references: [permissions.id],
-    relationName: "roles_permissions",
-  }),
-}));
+// export const rolesPermissionsRelations = relations(rolesPermissions, ({ one }) => ({
+//   role: one(roles, {
+//     fields: [rolesPermissions.roleId],
+//     references: [roles.id],
+//     relationName: "roles_rolesPermissions",
+//   }),
+//   permission: one(permissions, {
+//     fields: [rolesPermissions.permissionId],
+//     references: [permissions.id],
+//     relationName: "roles_permissions",
+//   }),
+// }));
 
-export const keysRoles = mysqlTable(
-  "keys_roles",
-  {
-    keyId: varchar("key_id", { length: 256 }).notNull(),
-    roleId: varchar("role_id", { length: 256 })
-      .notNull()
-      .references(() => roles.id, { onDelete: "cascade" }),
-    workspaceId: varchar("workspace_id", { length: 256 })
-      .notNull()
-      .references(() => workspaces.id, { onDelete: "cascade" }),
-  },
-  (table) => ({
-    primaryKey: primaryKey({ columns: [table.roleId, table.keyId, table.workspaceId] }),
+// export const keysRoles = mysqlTable(
+//   "keys_roles",
+//   {
+//     keyId: varchar("key_id", { length: 256 }).notNull().references(()=>keys.id,{onDelete:"cascade"}),
+//     roleId: varchar("role_id", { length: 256 })
+//       .notNull()
+//       .references(() => roles.id, { onDelete: "cascade" }),
+//     workspaceId: varchar("workspace_id", { length: 256 })
+//       .notNull()
+//       .references(() => workspaces.id, { onDelete: "cascade" }),
+//   },
+//   (table) => ({
+//     primaryKey: primaryKey({ columns: [table.roleId, table.keyId, table.workspaceId] }),
 
-    uniqueTuples: uniqueIndex("unique_key_id_role_id").on(table.keyId, table.roleId),
-  }),
-);
+//     uniqueTuples: uniqueIndex("unique_key_id_role_id").on(table.keyId, table.roleId),
+//   }),
+// );
 
-export const keysRolesRelations = relations(keysRoles, ({ one }) => ({
-  role: one(roles, {
-    fields: [keysRoles.roleId],
-    references: [roles.id],
-    relationName: "keys_roles_roles_relations",
-  }),
-  key: one(keys, {
-    fields: [keysRoles.keyId],
-    references: [keys.id],
-    relationName: "keys_roles_key_relations",
-  }),
-}));
+// export const keysRolesRelations = relations(keysRoles, ({ one }) => ({
+//   role: one(roles, {
+//     fields: [keysRoles.roleId],
+//     references: [roles.id],
+//     relationName: "keys_roles_roles_relations",
+//   }),
+//   key: one(keys, {
+//     fields: [keysRoles.keyId],
+//     references: [keys.id],
+//     relationName: "keys_roles_key_relations",
+//   }),
+// }));
