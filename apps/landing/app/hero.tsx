897e784d35a929e3fc836ff122b9cafc242c84d0
Dom Eccleston <31101792+domeccleston@users.noreply.github.com>
2024-02-09 15:22:28 +0000

    feat: Landing latest (#958)
    * feat: landing starter
    
    * add hiring button above hero
    
    * hero: aligment
    
    * begin work on code block
    
    * begin fixing section title
    
    * fix section title again
    
    * create next iteration of code block component
    
    * clean up landing page content
    
    * fix: heading alignment
    
    * normalize padding
    
    * fix mobile nav alignment
    
    * fix feature grid alignment
    
    * add framework switching + syntax highlighting
    
    * fix styling of top of code editor
    
    * Add code snippets to code editor
    
    * add second state to analytics bento
    
    * bento styling fixes
    
    * fix bento icons
    
    * add fade effect to rate limits bento
    
    * fix: types
    
    * fix hero buttons on mobile
    
    * hero responsiveness
    
    * lockfile
    
    * add container class
    
    * fmt
    
    ---------
    
    Co-authored-by: chronark <dev@chronark.com>

--- apps/landing/app/hero.tsx
+++ apps/landing/app/hero.tsx
@@ -4,17 +4,59 @@ import { PrimaryButton, SecondaryButton } from "../components/button";
 
 export const Hero: React.FC = () => {
   return (
-    <div className="flex min-h-[100vh] items-center justify-between container mx-auto">
-      <div>
-        <div>We're Hiring</div>
+    <div className="flex flex-col xl:flex-row justify-between items-center xl:items-start mt-[300px] sm:mt-[250px] xl:mt-[200px] max-w-[1440px]">
+      <div className="relative text-center xl:text-left flex flex-col items-center xl:items-start">
+        <div className="absolute top-[-50px] hero-hiring-gradient text-white text-sm flex space-x-2 py-1.5 px-2 items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+          >
+            <g clip-path="url(#clip0_840_5284)">
+              <path
+                d="M13 0.75C13 1.89705 11.8971 3 10.75 3C11.8971 3 13 4.10295 13 5.25C13 4.10295 14.1029 3 15.25 3C14.1029 3 13 1.89705 13 0.75Z"
+                stroke="white"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M13 10.75C13 11.8971 11.8971 13 10.75 13C11.8971 13 13 14.1029 13 15.25C13 14.1029 14.1029 13 15.25 13C14.1029 13 13 11.8971 13 10.75Z"
+                stroke="white"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M5 3.75C5 5.91666 2.91666 8 0.75 8C2.91666 8 5 10.0833 5 12.25C5 10.0833 7.0833 8 9.25 8C7.0833 8 5 5.91666 5 3.75Z"
+                stroke="white"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </g>
+            <defs>
+              <clipPath id="clip0_840_5284">
+                <rect width="16" height="16" fill="white" />
+              </clipPath>
+            </defs>
+          </svg>
+          <Link href="/careers">We are hiring!</Link>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
+          >
+            <path d="M12 8.5L8 4.5M12 8.5L8 12.5M12 8.5H4" stroke="white" />
+          </svg>
+        </div>
 
-        <h1 className="bg-gradient-to-br text-transparent bg-gradient-stop  bg-clip-text from-white via-white via-30% to-white/30 font-medium text-[4rem] leading-[4rem]  ">
-          Build your API,
-          <br />
-          not Auth
+        <h1 className="bg-gradient-to-br text-transparent bg-gradient-stop bg-clip-text from-white via-white max-w-[546px] via-30% to-white/30 font-medium text-[32px] leading-[48px] xs:text-[48px] xs:leading-[56px] sm:text-[56px] sm:leading-[72px] md:text-[64px] md:leading-[80px] xl:text-[64px] xl:leading-[80px]  ">
+          Build your API, not Auth
         </h1>
 
-        <p className="mt-8 bg-gradient-to-br text-transparent bg-gradient-stop bg-clip-text from-white via-white via-40% to-white/30 max-w-lg ">
+        <p className="mt-8 bg-gradient-to-br text-transparent bg-gradient-stop bg-clip-text from-white via-white via-40% to-white/30 max-w-lg text-sm xs:text-[15px] sm:text-base leading-[28px]">
           Unkey is an open source API authentication and authorization platform for scaling user
           facing APIs. Create, verify and manage low latency API keys in seconds.
         </p>
@@ -24,13 +66,13 @@ export const Hero: React.FC = () => {
             <PrimaryButton IconLeft={LogIn} label="Get Started" className="h-10" />
           </Link>
 
-          <Link href="/docs">
+          <Link href="/docs" className="hidden sm:flex">
             <SecondaryButton IconLeft={BookOpen} label="Documentation" IconRight={ChevronRight} />
           </Link>
         </div>
       </div>
 
-      <div className="rounded-[38px] bg-white/5 border border-gray-800 z-10 relative">
+      <div className="rounded-[38px] bg-white/5 border border-gray-800 z-10 mt-16 xl:mt-0 ">
         <div className="m-[10px] rounded-[28px] border border-gray-800">
           <img src="/images/hero.png" alt="Youtube" />
         </div>
