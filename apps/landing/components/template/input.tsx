289bd53313aaaec4284ca0ea9c0d451305bbd78b
MichaelUnkey <148160799+MichaelUnkey@users.noreply.github.com>
2024-02-13 14:18:16 -0500

    feat: Style template cards and template page (#968)
    * feat(template page intro): component with intro for template page
    
    template page intro
    
    eng-543
    
    * started re style
    
    * feat(template cards and styling): functioning template pages and styling
    
    styled template pages and components
    
    eng-544
    
    * Removed un used
    
    * Properly saved

--- apps/landing/components/template/input.tsx
+++ apps/landing/components/template/input.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
+import { Search } from "lucide-react";
 
 export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
@@ -10,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-8 w-full rounded-md border border-border bg-transparent px-3 py-2 text-sm focus:border-primary file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-content-subtle focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
+          "flex h-10 w-full rounded-md border border-border bg-transparent px-3 py-2 text-sm focus:border-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-white/40 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
           className,
         )}
         ref={ref}
@@ -22,3 +23,27 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
 Input.displayName = "Input";
 
 export { Input };
+
+const SearchInput = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <div
+        className={cn(
+          "flex flex-row h-10 w-full rounded-md border border-border p-0 m-0 bg-transparent focus:border-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-white/40 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",
+          className,
+        )}
+      >
+        <Search className="h-full py-2 mx-2 text-white" />
+        <input
+          type={type}
+          ref={ref}
+          {...props}
+          className="bg-transparent text-white/60 p-0 m-0 w-full border-none h-full"
+        />
+      </div>
+    );
+  },
+);
+Input.displayName = "Input";
+
+export { SearchInput };
