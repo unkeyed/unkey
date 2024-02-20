3953342a74009b1d10b83e2d37e93ffa8c054e39
Mike Silva <michael@unkey.dev>
2024-02-20 14:26:08 -0500

    feat(changelog migration to landing): moved changelog to new landing page
    Moved and setup changelog to new landing page
    
    eng-583

---
+++ apps/landing/app/changelog/[date]/page.tsx
@@ -0,0 +1,125 @@
+import { Container } from "@/components/container";
+import { FadeIn } from "@/components/fade-in";
+// import { PageIntro } from "@/components/page-intro";
+// import { PageLinks } from "@/components/page-links";
+import { notFound } from "next/navigation";
+
+import { CTA } from "@/components/cta";
+import { MdxContent } from "@/components/mdx-content";
+import { CHANGELOG_PATH, getChangelog, getContentData, getFilePaths } from "@/lib/mdx-helper";
+import type { Metadata } from "next";
+
+type Props = {
+  params: { date: string };
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { frontmatter } = await getChangelog(params.date);
+
+  if (!frontmatter) {
+    return notFound();
+  }
+  const baseUrl = process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : "http://localhost:3000";
+  const ogUrl = new URL("/og/changelog", baseUrl);
+
+  ogUrl.searchParams.set("title", frontmatter.title ?? "");
+  ogUrl.searchParams.set("date", params.date ?? "");
+
+  return {
+    title: `${frontmatter?.title} | Unkey`,
+    description: frontmatter?.description,
+    openGraph: {
+      title: `${frontmatter?.title} | Unkey`,
+      description: frontmatter?.description,
+      url: `https://unkey.dev/changelog/${params.date}?title=${encodeURIComponent(
+        frontmatter?.title,
+      )}`,
+      siteName: "unkey.dev",
+      images: [
+        {
+          url: ogUrl.toString(),
+          width: 1200,
+          height: 630,
+          alt: frontmatter.title,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: `${frontmatter?.title} | Unkey`,
+      description: frontmatter?.description,
+      site: "@unkeydev",
+      creator: "@unkeydev",
+      images: [
+        {
+          url: ogUrl.toString(),
+          width: 1200,
+          height: 630,
+          alt: frontmatter.title,
+        },
+      ],
+    },
+    icons: {
+      shortcut: "/images/landing/unkey.png",
+    },
+  };
+}
+
+export const generateStaticParams = async () => {
+  const changelogs = await getFilePaths(CHANGELOG_PATH);
+  // Remove file extensions for page paths
+  changelogs.map((path) => path.replace(/\.mdx?$/, "")).map((date) => ({ params: { date } }));
+  return changelogs;
+};
+
+export default async function ChangeLogLayout({
+  params,
+}: {
+  params: { date: string };
+}) {
+  const { frontmatter, serialized } = await getChangelog(params.date);
+
+  if (!serialized) {
+    return notFound();
+  }
+
+  const _moreChangelogs = await getContentData({
+    contentPath: CHANGELOG_PATH,
+    filepath: params.date,
+  });
+
+  return (
+    <>
+      <article className="mt-24 sm:mt-32 lg:mt-40">
+        <header>
+          {/* <PageIntro
+            eyebrow={new Date(frontmatter.date).toDateString()}
+            title={frontmatter.title}
+            centered
+          >
+            <p>{frontmatter.description}</p>
+          </PageIntro> */}
+        </header>
+
+        <Container className="mt-24 sm:mt-32 lg:mt-40">
+          <FadeIn>
+            <div className="prose lg:prose-md prose-neutral dark:prose-invert prose-pre:border prose-pre:border-border prose-pre:rounded-lg prose-img:rounded-lg prose-img:border prose-img:border-border mx-auto max-w-5xl ">
+              <MdxContent source={serialized} />
+            </div>
+          </FadeIn>
+        </Container>
+      </article>
+      <CTA />
+      {/* {moreChangelogs.length > 0 && (
+        <PageLinks
+          className="mt-24 sm:mt-32 lg:mt-40"
+          title="Read more changelogs"
+          pages={moreChangelogs}
+          contentType="changelog"
+        />
+      )} */}
+    </>
+  );
+}
