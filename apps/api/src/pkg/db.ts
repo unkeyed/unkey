afa523519ecc1f5efb39766eb7064b4c20c20cc0
Ankcorn <thomasankcorn@gmail.com>
2024-02-22 12:32:43 +0000

    that fetch patching :)

--- apps/api/src/pkg/db.ts
+++ apps/api/src/pkg/db.ts
@@ -21,8 +21,13 @@ export function createConnection(opts: ConnectionOptions): { db: Database; rawDB
         password: opts.password,
 
         fetch: (url: string, init: any) => {
-          delete init.cache;
-          return fetch(url, init);
+          (init as any).cache = undefined; // Remove cache header
+          const u = new URL(url);
+          // set protocol to http if localhost for CI testing
+          if (u.host.includes("localhost")) {
+            u.protocol = "http";
+          }
+          return fetch(u, init);
         },
       }),
       {
@@ -35,8 +40,13 @@ export function createConnection(opts: ConnectionOptions): { db: Database; rawDB
       password: opts.password,
 
       fetch: (url: string, init: any) => {
-        delete init.cache;
-        return fetch(url, init);
+        (init as any).cache = undefined; // Remove cache header
+        const u = new URL(url);
+        // set protocol to http if localhost for CI testing
+        if (u.host.includes("localhost")) {
+          u.protocol = "http";
+        }
+        return fetch(u, init);
       },
     }),
   };
