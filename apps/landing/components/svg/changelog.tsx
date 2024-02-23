ffac27725ec388e1348c0bc23c1177ea1d940e78
MichaelUnkey <148160799+MichaelUnkey@users.noreply.github.com>
2024-02-23 11:54:15 -0500

    feat: add tags to changelogs frontmatter (#1027)
    * add blank target to twitter link
    
    * feat(changelog labels): added labels to changelog and change page structure
    
    labels and configured new changelog layout
    
    eng-591

---
+++ apps/landing/components/svg/changelog.tsx
@@ -0,0 +1,178 @@
+import { cn } from "@/lib/utils";
+export const ChangelogBackgroundLines = ({ className }: { className?: string }) => (
+  <svg
+    className={cn(className, "absolute top-0 left-0 right-0 -z-10")}
+    viewBox="0 0 1340 432"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <mask id="path-1-inside-1_6033_44" fill="white">
+      <path d="M250 0H1090V432H250V0Z" />
+    </mask>
+    <path
+      d="M1089.5 0V432H1090.5V0H1089.5ZM250.5 432V0H249.5V432H250.5Z"
+      fill="url(#paint0_linear_6033_44)"
+      mask="url(#path-1-inside-1_6033_44)"
+    />
+    <path
+      d="M1089.5 0V432H1090.5V0H1089.5ZM250.5 432V0H249.5V432H250.5Z"
+      fill="url(#paint1_angular_6033_44)"
+      fill-opacity="0.25"
+      mask="url(#path-1-inside-1_6033_44)"
+    />
+    <path
+      d="M1089.5 0V432H1090.5V0H1089.5ZM250.5 432V0H249.5V432H250.5Z"
+      fill="url(#paint2_angular_6033_44)"
+      fill-opacity="0.25"
+      mask="url(#path-1-inside-1_6033_44)"
+    />
+    <mask id="path-3-inside-2_6033_44" fill="white">
+      <path d="M0 0H1340V432H0V0Z" />
+    </mask>
+    <path
+      d="M1339.5 0V432H1340.5V0H1339.5ZM0.5 432V0H-0.5V432H0.5Z"
+      fill="url(#paint3_linear_6033_44)"
+      mask="url(#path-3-inside-2_6033_44)"
+    />
+    <path
+      d="M1339.5 0V432H1340.5V0H1339.5ZM0.5 432V0H-0.5V432H0.5Z"
+      fill="url(#paint4_angular_6033_44)"
+      fill-opacity="0.15"
+      mask="url(#path-3-inside-2_6033_44)"
+    />
+    <path
+      d="M1339.5 0V432H1340.5V0H1339.5ZM0.5 432V0H-0.5V432H0.5Z"
+      fill="url(#paint5_angular_6033_44)"
+      fill-opacity="0.15"
+      mask="url(#path-3-inside-2_6033_44)"
+    />
+    <mask id="path-5-inside-3_6033_44" fill="white">
+      <path d="M125 0H1215V322H125V0Z" />
+    </mask>
+    <path
+      d="M1214.5 0V322H1215.5V0H1214.5ZM125.5 322V0H124.5V322H125.5Z"
+      fill="url(#paint6_linear_6033_44)"
+      mask="url(#path-5-inside-3_6033_44)"
+    />
+    <path
+      d="M1214.5 0V322H1215.5V0H1214.5ZM125.5 322V0H124.5V322H125.5Z"
+      fill="url(#paint7_angular_6033_44)"
+      fill-opacity="0.25"
+      mask="url(#path-5-inside-3_6033_44)"
+    />
+    <path
+      d="M1214.5 0V322H1215.5V0H1214.5ZM125.5 322V0H124.5V322H125.5Z"
+      fill="url(#paint8_angular_6033_44)"
+      fill-opacity="0.25"
+      mask="url(#path-5-inside-3_6033_44)"
+    />
+    <defs>
+      <linearGradient
+        id="paint0_linear_6033_44"
+        x1="670"
+        y1="0"
+        x2="670"
+        y2="432"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="white" stop-opacity="0.15" />
+        <stop offset="0.5" stop-color="white" stop-opacity="0.07" />
+        <stop offset="1" stop-color="white" stop-opacity="0" />
+      </linearGradient>
+      <radialGradient
+        id="paint1_angular_6033_44"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(1008 276) scale(105 86.8269)"
+      >
+        <stop stop-color="white" stop-opacity="0" />
+        <stop offset="0.0001" stop-color="white" />
+        <stop offset="0.175" stop-color="white" stop-opacity="0" />
+      </radialGradient>
+      <radialGradient
+        id="paint2_angular_6033_44"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(299 216) rotate(180) scale(116 114)"
+      >
+        <stop stop-color="white" stop-opacity="0" />
+        <stop offset="0.0001" stop-color="white" />
+        <stop offset="0.175" stop-color="white" stop-opacity="0" />
+      </radialGradient>
+      <linearGradient
+        id="paint3_linear_6033_44"
+        x1="670"
+        y1="-5.02224e-07"
+        x2="670"
+        y2="432"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset="0.189815" stop-color="white" stop-opacity="0.12" />
+        <stop offset="0.814815" stop-color="white" stop-opacity="0" />
+      </linearGradient>
+      <radialGradient
+        id="paint4_angular_6033_44"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(1265.27 238.807) scale(158.481 118.193)"
+      >
+        <stop stop-color="white" stop-opacity="0" />
+        <stop offset="0.0001" stop-color="white" />
+        <stop offset="0.175" stop-color="white" stop-opacity="0" />
+      </radialGradient>
+      <radialGradient
+        id="paint5_angular_6033_44"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(70 292) rotate(180) scale(141 164)"
+      >
+        <stop stop-color="white" stop-opacity="0" />
+        <stop offset="0.0001" stop-color="white" />
+        <stop offset="0.175" stop-color="white" stop-opacity="0" />
+      </radialGradient>
+      <linearGradient
+        id="paint6_linear_6033_44"
+        x1="670"
+        y1="-3.74343e-07"
+        x2="670"
+        y2="322"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset="0.189815" stop-color="white" stop-opacity="0.15" />
+        <stop offset="1" stop-color="white" stop-opacity="0" />
+      </linearGradient>
+      <radialGradient
+        id="paint7_angular_6033_44"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(1154.21 178) scale(128.913 123)"
+      >
+        <stop stop-color="white" stop-opacity="0" />
+        <stop offset="0.0001" stop-color="white" />
+        <stop offset="0.175" stop-color="white" stop-opacity="0" />
+      </radialGradient>
+      <radialGradient
+        id="paint8_angular_6033_44"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(167 140) rotate(180) scale(101 144.55)"
+      >
+        <stop stop-color="white" stop-opacity="0" />
+        <stop offset="0.0001" stop-color="white" />
+        <stop offset="0.175" stop-color="white" stop-opacity="0" />
+      </radialGradient>
+    </defs>
+  </svg>
+);
