eb4a744fe783b084128c046f4152b75a26369dc2
chronark <dev@chronark.com>
2024-02-27 23:51:29 +0100

    chore(api): remove empty noExternal array from tsup config

--- packages/api/tsup.config.js
+++ packages/api/tsup.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["cjs", "esm"],
-  noExternal: [],
   splitting: false,
   sourcemap: true,
   clean: true,
