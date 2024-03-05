ebd6b88fa4f59958e2489355df57288a06c2e974
chronark <dev@chronark.com>
2024-03-05 15:05:20 +0100

    feat: fresh auth pages

--- apps/api/src/routes/legacy_keys_verifyKey.ts
+++ apps/api/src/routes/legacy_keys_verifyKey.ts
@@ -121,7 +121,7 @@ A key could be invalid for a number of reasons, for example if it has expired, h
               .optional()
               .openapi({
                 description: `If the key is invalid this field will be set to the reason why it is invalid.
-Possible vals are:
+Possible values are:
 - NOT_FOUND: the key does not exist or has expired
 - FORBIDDEN: the key is not allowed to access the api
 - USAGE_EXCEEDED: the key has exceeded its request limit
