c59b34bafd5aee25042efc0183ae7efb2157f4cb
Dom <domeccleston@gmail.com>
2024-03-06 19:29:49 +0000

    change copy button to checkmark on hover

--- apps/landing/app/code-examples.tsx
+++ apps/landing/app/code-examples.tsx
@@ -539,11 +539,14 @@ export const CodeExamples: React.FC<Props> = ({ className }) => {
                 setCopied(true);
                 setTimeout(() => {
                   setCopied(false);
-                }, 1000);
+                }, 2000);
               }}
             >
               {copied ? (
-                <Check className="w-6 h-6 text-slate-500" />
+                <svg className="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
+                  <circle className="checkmark__circle" cx="26" cy="26" r="25" fill="none" />
+                  <path className="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8" />
+                </svg>
               ) : (
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
