f3f93b02d75a17d1a3aa59a896f0c3a5efc456a2
Andreas Thomas <dev@chronark.com>
2024-03-12 11:32:37 +0100

    test: run tests in parallel (#1155)
    * test: run tests in parallel
    
    * chore(test_api.yaml): set fail-fast option to false in test_routes job configuration
    
    * chore(test_api.yaml): update test script to use thread pool for test execution
    
    * chore(deploy_api.yaml): update job name from 'End to End Test Canary' to 'Benchmark Canary'
    refactor(harness.ts): refactor deleteWorkspaces method to retry 5 times with
    exponential backoff if an error occurs
    
    * test(v1_ratelimit_limit.accuracy.test.ts): remove concurrent modifier from test function

--- apps/api/vitest.routes.ts
+++ apps/api/vitest.routes.ts
@@ -8,12 +8,7 @@ export default defineConfig({
     alias: {
       "@/": new URL("./src/", import.meta.url).pathname,
     },
-    pool: "threads",
-    poolOptions: {
-      threads: {
-        singleThread: true,
-      },
-    },
+
     // starting the worker takes a bit of time
     testTimeout: 60_000,
   },
