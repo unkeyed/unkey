94b97b7ca492ba70ec626906ff78614b5f391811
chronark <dev@chronark.com>
2024-03-12 11:16:00 +0100

    test(v1_ratelimit_limit.accuracy.test.ts): remove concurrent modifier from test function

--- apps/api/src/routes/v1_ratelimit_limit.accuracy.test.ts
+++ apps/api/src/routes/v1_ratelimit_limit.accuracy.test.ts
@@ -122,7 +122,7 @@ const testCases: {
 ];
 
 for (const { name, limit, duration, rps, seconds, expected } of testCases) {
-  test.concurrent(
+  test(
     `${name}, [~${seconds}s], passed requests are within [${expected.min} - ${expected.max}]`,
     async (t) => {
       const h = await RouteHarness.init(t);
