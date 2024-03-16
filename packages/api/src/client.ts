68d58d03b424255451801f77304b8779dd62e78c
Andreas Thomas <dev@chronark.com>
2024-03-16 10:52:09 +0100

    ratelimit ui fun (#1181)
    * feat: ui
    
    * fix(api): update route path from "/v1/ratelimit.limit" to "/v1/ratelimits.limit" in test files and route file
    feat(api): add new documentation file for ratelimits limit endpoint
    
    * feat: sdk

--- packages/api/src/client.ts
+++ packages/api/src/client.ts
@@ -354,4 +354,21 @@ export class Unkey {
       },
     };
   }
+  public get ratelimits() {
+    return {
+      limit: async (
+        req: paths["/v1/ratelimit.limit"]["post"]["requestBody"]["content"]["application/json"],
+      ): Promise<
+        Result<
+          paths["/v1/ratelimit.limit"]["post"]["responses"]["200"]["content"]["application/json"]
+        >
+      > => {
+        return await this.fetch({
+          path: ["v1", "ratelimit.limit"],
+          method: "POST",
+          body: req,
+        });
+      },
+    };
+  }
 }
