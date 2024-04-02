e3dee08b83eeb2189cb4d004a2c2e59d781d425d
Dom <domeccleston@gmail.com>
2024-04-02 11:03:08 +0100

    add hover effect to highlight button

--- apps/landing/components/button.tsx
+++ apps/landing/components/button.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@/lib/utils";
 import type { LucideIcon } from "lucide-react";
 import { SparkleIcon } from "./svg/template-page";
+import { HoverBorderGradient } from "./ui/hover-border-gradient";
 
 type Props = {
   className?: string;
@@ -46,15 +47,30 @@ export const SecondaryButton: React.FC<Props> = ({ className, IconLeft, label, I
 
 export const RainbowDarkButton: React.FC<Props> = ({ className, label, IconRight }) => {
   return (
-    <div className={cn("p-[.75px] hero-hiring-gradient rounded-full w-fit mx-auto", className)}>
-      <button
-        type="button"
-        className="items-center gap-4 px-3 py-1.5 bg-black text-white rounded-full flex flex-block text-sm"
+    <div className={cn("p-[.75px] w-fit mx-auto", className)}>
+      <HoverBorderGradient
+        containerClassName="rounded-full"
+        as="button"
+        className="bg-black text-white flex items-center space-x-2"
       >
-        <SparkleIcon className="text-white" />
+        <SparkleIcon className="text-white mr-2" />
         {label}
         {IconRight ? <IconRight className="w-4 h-4" /> : null}
-      </button>
+      </HoverBorderGradient>
     </div>
   );
 };
+
+export function HoverBorderGradientButton() {
+  return (
+    <div className="m-40 flex justify-center text-center">
+      <HoverBorderGradient
+        containerClassName="rounded-full"
+        as="button"
+        className="bg-black text-white flex items-center space-x-2"
+      >
+        <span>Aceternity UI</span>
+      </HoverBorderGradient>
+    </div>
+  );
+}
