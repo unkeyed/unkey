1bc7b2cf1f4ced921de2b261c3087422c45f3642
Dom <domeccleston@gmail.com>
2024-04-03 23:37:58 +0100

    make changes to rate limits bento

--- apps/landing/components/rate-limits-bento.tsx
+++ apps/landing/components/rate-limits-bento.tsx
@@ -26,8 +26,8 @@ export function RateLimits() {
       <div className="flex flex-col mt-8 ratelimits-fade-gradient">
         <div className="flex items-center">
           <div className="font-mono text-xs text-white sm:font-sm whitespace-nowrap">
-            <span className="text-[#ffffff]/40">Creating</span> keys
-            <span className="tracking-[-5px]">...</span>
+            <span className="text-[#ffffff]/40">Rate limiting</span>
+            <span className="text-[#ffffff]/40 tracking-[-5px]">...</span>
             <span className="inline-flex w-[4px] h-[12px] bg-white ratelimits-bar-shadow ml-3 relative top-[1px] flicker" />
           </div>
           <div className="inline-flex items-center overflow-hidden ml-4 h-[36px] text-white font-mono text-sm ratelimits-key-gradient border-[.75px] border-[#ffffff]/20 rounded-xl">
@@ -121,58 +121,66 @@ export function RateLimits() {
         <div className="inline-flex w-[205px] opacity-70 items-center ml-[150px] mt-[30px] h-[36px] text-white font-mono text-sm ratelimits-link ratelimits-key-gradient border-[.75px] border-[#ffffff]/20 rounded-xl">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="62"
-            height="36"
-            viewBox="0 0 62 36"
+            width="84"
+            height="84"
+            viewBox="0 0 84 84"
             fill="none"
           >
-            <g filter="url(#filter0_d_840_1934)">
+            <g filter="url(#filter0_d_61_98)">
               <rect
-                x="8"
-                y="6"
+                x="30"
+                y="30"
                 width="24"
                 height="24"
                 rx="6"
                 fill="#6E56CF"
-                shapeRendering="crispEdges"
+                shape-rendering="crispEdges"
               />
               <rect
-                x="8"
-                y="6"
+                x="30"
+                y="30"
                 width="24"
                 height="24"
                 rx="6"
                 fill="black"
-                fillOpacity="0.15"
-                shapeRendering="crispEdges"
+                fill-opacity="0.15"
+                shape-rendering="crispEdges"
               />
               <rect
-                x="8.375"
-                y="6.375"
+                x="30.375"
+                y="30.375"
                 width="23.25"
                 height="23.25"
                 rx="5.625"
                 stroke="white"
-                strokeOpacity="0.1"
-                strokeWidth="0.75"
-                shapeRendering="crispEdges"
+                stroke-opacity="0.1"
+                stroke-width="0.75"
+                shape-rendering="crispEdges"
               />
               <path
-                d="M21.5 15L23 16.5M14.5 23.5H17.5V21.5H19.5V20.5L21.5 18.5L19.5 16.5L14.5 21.5V23.5ZM18 15L23 20L26.5 16.5L21.5 11.5L18 15Z"
+                d="M46.9 38H38.1C37.4925 38 37 38.4477 37 39V45C37 45.5523 37.4925 46 38.1 46H46.9C47.5075 46 48 45.5523 48 45V39C48 38.4477 47.5075 38 46.9 38Z"
                 stroke="white"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M48 40L43.0665 42.8519C42.8967 42.9487 42.7004 43 42.5 43C42.2996 43 42.1033 42.9487 41.9335 42.8519L37 40"
+                stroke="white"
+                stroke-linecap="round"
+                stroke-linejoin="round"
               />
             </g>
             <defs>
               <filter
-                id="filter0_d_840_1934"
-                x="-22"
-                y="-24"
+                id="filter0_d_61_98"
+                x="0"
+                y="0"
                 width="84"
                 height="84"
                 filterUnits="userSpaceOnUse"
-                colorInterpolationFilters="sRGB"
+                color-interpolation-filters="sRGB"
               >
-                <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                <feFlood flood-opacity="0" result="BackgroundImageFix" />
                 <feColorMatrix
                   in="SourceAlpha"
                   type="matrix"
@@ -186,21 +194,17 @@ export function RateLimits() {
                   type="matrix"
                   values="0 0 0 0 0.431373 0 0 0 0 0.337255 0 0 0 0 0.811765 0 0 0 1 0"
                 />
-                <feBlend
-                  mode="normal"
-                  in2="BackgroundImageFix"
-                  result="effect1_dropShadow_840_1934"
-                />
+                <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_61_98" />
                 <feBlend
                   mode="normal"
                   in="SourceGraphic"
-                  in2="effect1_dropShadow_840_1934"
+                  in2="effect1_dropShadow_61_98"
                   result="shape"
                 />
               </filter>
             </defs>
           </svg>
-          <p className="relative text-xs right-4">sk_UNWrXjYp6AF2h7Nx</p>
+          <p className="relative text-xs right-4">dom@unkey.dev</p>
         </div>
       </div>
     </div>
