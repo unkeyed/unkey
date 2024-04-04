e5a95466b20f0ff7c1ff8e24da5bd3d9eb8ed68f
Dom <domeccleston@gmail.com>
2024-04-04 14:31:00 +0100

    apply hover effect to any part of yt video

--- apps/landing/components/youtube-embed.tsx
+++ apps/landing/components/youtube-embed.tsx
@@ -8,7 +8,7 @@ export function YoutubeEmbed() {
   const [toggler, setToggler] = useState(false);
 
   return (
-    <div className="rounded-[38px] bg-white/5 border border-gray-800/40 z-10 mt-16 xl:mt-0 ">
+    <div className="rounded-[38px] bg-white/5 border border-gray-800/40 z-10 mt-16 xl:mt-0 group">
       <div className="m-[10px] rounded-[28px] flex items-center justify-center">
         <div className="flex items-center justify-center">
           <button
@@ -17,7 +17,7 @@ export function YoutubeEmbed() {
             onClick={() => setToggler(!toggler)}
           >
             <Image src="/images/hero.png" alt="Youtube" width={600} height={340} />
-            <div className="group absolute top-[calc(50%-80px/2)] duration-200 left-[calc(50%-112px/2)] bg-[#ffffff/30] h-[80px] w-[112px] bg-yt-button-gradient hover:bg-white transition-all rounded-[20px] flex items-center justify-center">
+            <div className="group absolute top-[calc(50%-80px/2)] duration-200 left-[calc(50%-112px/2)] bg-[#ffffff/30] h-[80px] w-[112px] bg-yt-button-gradient group-hover:bg-white transition-all rounded-[20px] flex items-center justify-center">
               <BorderBeam className="duration-200 group-hover:opacity-0" />
               <svg
                 className="text-white duration-200 fill-current group-hover:text-black"
