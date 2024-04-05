8be00192b5a05177bebea87f6ea6b3984ac82ed8
chronark <dev@chronark.com>
2024-04-05 09:48:51 +0200

    fix: remove unused broken code

--- apps/api/src/pkg/testutil/request.ts
+++ apps/api/src/pkg/testutil/request.ts
@@ -1,4 +1,3 @@
-import type { App } from "@/pkg/hono/app";
 export type StepRequest<TRequestBody> = {
   url: string;
   method: "POST" | "GET" | "PUT" | "DELETE";
@@ -19,38 +18,6 @@ export async function step<TRequestBody = unknown, TResponseBody = unknown>(
     headers: req.headers,
     body: JSON.stringify(req.body),
   });
-
-  return {
-    status: res.status,
-    headers: headersToRecord(res.headers),
-    body: (await res.json().catch((err) => {
-      console.error(`${req.url} didn't return json`, err);
-      return {};
-    })) as TResponseBody,
-  };
-}
-
-export async function fetchRoute<TRequestBody = unknown, TResponseBody = unknown>(
-  app: App,
-  req: StepRequest<TRequestBody>,
-): Promise<StepResponse<TResponseBody>> {
-  const eCtx: ExecutionContext = {
-    waitUntil: (promise: Promise<any>) => {
-      promise.catch(() => {});
-    },
-    passThroughOnException: () => {},
-  };
-
-  const res = await app.request(
-    req.url,
-    {
-      method: req.method,
-      headers: req.headers,
-      body: JSON.stringify(req.body),
-    },
-    {}, // Env
-    eCtx,
-  );
 
   return {
     status: res.status,
