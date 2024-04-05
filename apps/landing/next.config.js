01f97346d08d1cb9d6ce89305f85f844fddbc0f9
chronark <dev@chronark.com>
2024-04-05 13:27:53 +0200

    fix(next.config.js): update APP_URL from "https://unkey.dev" to "https://app.unkey.com"

--- apps/landing/next.config.js
+++ apps/landing/next.config.js
@@ -1,6 +1,6 @@
 const { withContentlayer } = require("next-contentlayer");
 /** @type {import('next').NextConfig} */
-const APP_URL = "https://unkey.dev";
+const APP_URL = "https://app.unkey.com";
 
 const nextConfig = {
   pageExtensions: ["tsx", "mdx", "ts", "js"],
