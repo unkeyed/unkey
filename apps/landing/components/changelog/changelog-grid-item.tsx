299265ff72e337287f0c0001cceffc6f5e0ff25a
Dom <domeccleston@gmail.com>
2024-04-08 10:09:30 +0100

    responsive changelog date

--- apps/landing/components/changelog/changelog-grid-item.tsx
+++ apps/landing/components/changelog/changelog-grid-item.tsx
@@ -21,20 +21,22 @@ export async function ChangelogGridItem({ className, changelog }: Props) {
   return (
     <div id={changelog.tableOfContents.slug} className={cn("w-full", className)}>
       <div className="2xl:pl-36">
-        <div className="flex flex-row pb-10 gap-4 ">
+        <div className="flex flex-col sm:flex-row pb-10 gap-4 font-medium">
           {new Date(changelog.date).toLocaleDateString("en-US", {
             year: "numeric",
             month: "long",
             day: "numeric",
           })}
-          {changelog.tags?.map((tag) => (
-            <span
-              key={tag}
-              className="text-white text-xs bg-white/10 rounded-full px-3 py-[0.15rem] leading-[1.5]"
-            >
-              {tag.charAt(0).toUpperCase() + tag.slice(1)}
-            </span>
-          ))}
+          <div className="flex flex-row gap-x-3">
+            {changelog.tags?.map((tag) => (
+              <span
+                key={tag}
+                className="text-white inline-flex text-xs bg-white/10 rounded-full px-3 py-[0.15rem] leading-[1.5]"
+              >
+                {tag.charAt(0).toUpperCase() + tag.slice(1)}
+              </span>
+            ))}
+          </div>
         </div>
         <h3 className="font-display text-4xl font-medium blog-heading-gradient ">
           <Link href={`#${changelog.tableOfContents.slug}`} scroll={false} replace={true}>
