bcd17c2a98eec14b457e7e08bf7801e0f65cf413
chronark <dev@chronark.com>
2024-04-08 14:47:43 +0200

    chore(logdrainv2): update Dockerfile to add missing command for running the app feat(logdrainv2): add debug.ts file for base64 decoding and data processing feat(logdrainv2): update index.ts to include base64 encoding and data processing feat(logdrainv2): update package.json to add unzipper dependency and types
    feat(logdrainv2): add x.ts file with base64 encoded data, write to gzip file,
    and log the data after decoding it.

--- apps/logdrainv2/index.ts
+++ apps/logdrainv2/index.ts
@@ -3,7 +3,12 @@ Bun.serve({
   async fetch(req) {
     const body = await req.text();
 
-    console.log(req.url, JSON.stringify(req.headers), body);
+    console.log(JSON.stringify(req.headers));
+
+    const buf = Buffer.from(body, "utf-8");
+    console.log("base64", buf.toString("base64"));
+    const data = Bun.gunzipSync(buf);
+    console.log(data);
 
     return new Response("ok");
   },
