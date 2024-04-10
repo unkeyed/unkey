3a94019838aba0a1eceff6eaee1599cdc4ad72d0
James Perkins <jamesperkins@hey.com>
2024-04-10 22:22:35 +0100

    Add inline code highlight

--- apps/landing/components/template/mdx-components.tsx
+++ apps/landing/components/template/mdx-components.tsx
@@ -40,8 +40,8 @@ export const TemplateComponents = {
   hr: (_props: any) => <Separator orientation="horizontal" />,
   code: (props: any) => (
     <code
+      className="rounded-md border border-gray-200 bg-gray-50 px-2 py-1 font-medium text-gray-600 text-nowrap overflow-x-auto before:hidden after:hidden"
       {...props}
-      className="inline font-mono text-xs rounded-lg leading-6 sm:text-xs md:text-base font-normal bg-white/10 text-white px-1.5 py-1 w-full text-nowrap overflow-x-auto"
     />
   ),
   pre: (props: any) => <CodeBlock {...props} />,
