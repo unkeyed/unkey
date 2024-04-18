73dde98396ccd6850a660d1930b81cdf216c784f
MichaelUnkey <148160799+MichaelUnkey@users.noreply.github.com>
2024-04-18 15:49:47 +0100

    fix(blog): blog code block spacing again (#1440)
    fixed a thing
    
    eng-956

--- apps/landing/app/blog/[slug]/page.tsx
+++ apps/landing/app/blog/[slug]/page.tsx
@@ -190,7 +190,7 @@ const BlogArticleWrapper = async ({ params }: { params: { slug: string } }) => {
                 </div>
               </div>
             </div>
-            <div className="lg:pr-24 prose-sm md:prose-md text-white/60 sm:mx-6 prose-strong:text-white/90 prose-code:text-white/80 prose-code:bg-white/10 prose-code:px-2 prose-code:py-1 prose-code:border-white/20 prose-code:rounded-md mt-12">
+            <div className="lg:pr-24 prose-sm md:prose-md text-white/60 sm:mx-6 prose-strong:text-white/90 prose-code:text-white/80 prose-code:bg-white/10 prose-code:px-2 prose-code:py-1 prose-code:border-white/20 prose-code:rounded-md mt-12 prose-pre:p-0 prose-pre:m-0 prose-pre:leading-6">
               <MDX code={post.body.code} />
             </div>
           </div>
