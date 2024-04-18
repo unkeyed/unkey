071dc65f9bcb641de02b2127ddf287cf91ae4ac7
MichaelUnkey <148160799+MichaelUnkey@users.noreply.github.com>
2024-04-18 18:28:55 +0100

    fix(about page investors): random fixes to investors section (#1444)
    investors section
    
    eng-954

--- apps/landing/components/blog/blog-card.tsx
+++ apps/landing/components/blog/blog-card.tsx
@@ -38,29 +38,26 @@ export function BlogCard({
           </div>
         </Frame>
       </div>
-      <div className="flex flex-col p-6 h-80">
-        <div className="flex flex-row h-20 mt-2 gap-4">
-          {tags?.map((tag) => (
-            <div className="text-white/50 text-sm bg-white/10 px-[9px] rounded-md h-fit">
-              {tag.charAt(0).toUpperCase() + tag.slice(1)}
-            </div>
-          ))}
-          : null
-        </div>
-        <div className="flex flex-row h-24">
-          <h2 className="flex text-3xl font-medium leading-10 sm:text-xl blog-heading-gradient">
+      <div className="flex flex-col p-6 h-full ">
+        <div className="flex flex-col h-80 justify-evenly">
+          <div className="flex flex-inline h-8 gap-4">
+            {tags?.map((tag) => (
+              <div className="text-white/50 text-sm bg-white/10 px-[9px] rounded-md h-fit">
+                {tag.charAt(0).toUpperCase() + tag.slice(1)}
+              </div>
+            ))}
+            : null
+          </div>
+          <h2 className="md:text-2xl h-18 lg:h-32 font-medium leading-10 text-xl sm:text-2xl blog-heading-gradient">
             {title}
           </h2>
-        </div>
-        <div className="flex flex-row h-36">
-          <p className="flex flex-col text-base font-normal leading-6 sm:text-sm text-white/60  ">
+
+          <p className="text-base h-18 font-normal leading-6 sm:text-sm text-white/60">
             {subTitle}
           </p>
-        </div>
-        <div className="flex flex-col justify-end">
-          <div className="flex flex-inline ">
-            {/* Todo: Needs ability to add multiple authors at some point */}
-            <Avatar className="w-8 h-8 mt-2">
+          {/* Todo: Needs ability to add multiple authors at some point */}
+          <div className="flex flex-inline items-end h-20">
+            <Avatar className="w-8 h-8">
               <AvatarImage alt={author.name} src={author.image.src} width={12} height={12} />
               <AvatarFallback />
             </Avatar>
@@ -68,7 +65,6 @@ export function BlogCard({
             <p className="pt-3 ml-6 text-sm font-normal text-white/40">
               {format(new Date(publishDate!), "MMM dd, yyyy")}
             </p>
-            <div />
           </div>
         </div>
       </div>
