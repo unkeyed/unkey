d987148eee0d709753139595f9cf38a2c70fbb6e
Andreas Thomas <dev@chronark.com>
2024-04-19 20:20:07 +0200

    style(open-source.tsx): adjust positioning of OpenSource component for better alignment (#1491)

--- apps/landing/components/open-source.tsx
+++ apps/landing/components/open-source.tsx
@@ -13,7 +13,7 @@ import { MeteorLines } from "./ui/meteorLines";
 export const OpenSource: React.FC = () => {
   return (
     <div className="pt-[00px] flex items-center flex-col md:flex-row relative">
-      <div className="absolute top-[-480px] md:right-[120px] -z-[10]">
+      <div className="absolute top-[-320px] md:top-[-480px] xl:right-[120px] -z-[10]">
         {/* TODO: horizontal scroll */}
         <OssLight className="scale-[2]" />
         <div className="absolute right-[270px] top-[250px] -z-50">
