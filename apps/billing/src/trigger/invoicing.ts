3cf90723e272e2282a770b08fba569d1ddea2a76
chronark <dev@chronark.com>
2024-04-29 18:14:07 +0200

    feat: try trigger v3

---
+++ apps/billing/src/trigger/invoicing.ts
@@ -0,0 +1,57 @@
+import { logger, schedules } from "@trigger.dev/sdk/v3";
+
+import { connectDatabase } from "@/lib/db";
+
+import Stripe from "stripe";
+import { createInvoiceTask } from "./create-invoice";
+import { downgradeTask } from "./downgrade-requests";
+
+export const invoicingTask = schedules.task({
+  id: "billing_invoicing",
+  run: async () => {
+    logger.info("task starting..");
+    const db = connectDatabase();
+
+    let workspaces = await db.query.workspaces.findMany({
+      where: (table, { isNotNull, isNull, not, eq, and }) =>
+        and(
+          isNotNull(table.stripeCustomerId),
+          isNotNull(table.subscriptions),
+          not(eq(table.plan, "free")),
+          isNull(table.deletedAt),
+        ),
+    });
+    // hack to filter out workspaces with `{}` as subscriptions
+    workspaces = workspaces.filter(
+      (ws) => ws.subscriptions && Object.keys(ws.subscriptions).length > 0,
+    );
+
+    logger.info(`found ${workspaces.length} workspaces`);
+
+    /**
+     * Dates gymnastics to get the previous month's number, ie: if it's December now, it returns -> 11
+     */
+    const t = new Date();
+    t.setUTCMonth(t.getUTCMonth() - 1);
+    const year = t.getUTCFullYear();
+    const month = t.getUTCMonth() + 1; // months are 0 indexed
+
+    if (workspaces.length > 0) {
+      await createInvoiceTask.batchTriggerAndWait(
+        workspaces.map((ws) => ({
+          payload: {
+            workspaceId: ws.id,
+            year,
+            month,
+          },
+        })),
+      );
+    }
+
+    await downgradeTask.triggerAndWait();
+
+    return {
+      workspaceIds: workspaces.map((w) => w.id),
+    };
+  },
+});
---
+++ apps/billing/trigger.config.ts
@@ -0,0 +1,18 @@
+import type { TriggerConfig } from "@trigger.dev/sdk/v3";
+
+export const config: TriggerConfig = {
+  project: "proj_jclajjbtjgzypjvseciy",
+  logLevel: "debug",
+  enableConsoleLogging: true,
+  retries: {
+    enabledInDev: true,
+    default: {
+      maxAttempts: 3,
+      minTimeoutInMs: 1000,
+      maxTimeoutInMs: 10000,
+      factor: 2,
+      randomize: true,
+    },
+  },
+  dependenciesToBundle: [/^@unkey\//],
+};
