cb95c97a772f71950fb2b69980665b5c2811f8eb
Andreas Thomas <dev@chronark.com>
2024-04-30 16:40:08 +0200

    feat: pnpm local (#1572)
    * feat: pnpm local
    
    * chore: refactor
    
    * fix(dependabot.yml): correct indentation for update-types array in ignore section

--- internal/db/drizzle.config.ts
+++ internal/db/drizzle.config.ts
@@ -3,7 +3,6 @@ import type { Config } from "drizzle-kit";
 export default {
   verbose: true,
   schema: "./src/schema/index.ts",
-  out: "./drizzle",
   driver: "mysql2",
   dbCredentials: {
     uri: process.env.DRIZZLE_DATABASE_URL!,
