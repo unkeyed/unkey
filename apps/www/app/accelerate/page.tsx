ab5f842f496b7782a340dd2e02acd625725914e9
Guilherme Rodz <gui.rodz.dev@gmail.com>
2024-05-04 19:47:34 -0300

    chore(accelerate): create landing structure

---
+++ apps/www/app/accelerate/page.tsx
@@ -0,0 +1,17 @@
+export default function AcceleratePage() {
+  return (
+    <div className="container min-h-[100dvh] flex flex-col justify-between">
+      <header className="flex w-full justify-between items-center h-24">
+        <div>Left</div>
+        <div>Right</div>
+      </header>
+
+      <div className="w-full">Hero</div>
+
+      <footer className="flex w-full justify-between items-center h-24">
+        <div>Left</div>
+        <div>Right</div>
+      </footer>
+    </div>
+  );
+}
