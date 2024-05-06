1b6e655a2b29e96d4d4e1991c7c57cd3e47480de
David Thyresson <dthyresson@gmail.com>
2024-05-06 14:09:22 -0400

    get to green

---
+++ packages/redwoodjs/src/ratelimit/__tests__/util/defaultRatelimitIdentifier.test.ts
@@ -0,0 +1,31 @@
+import { MiddlewareRequest } from "@redwoodjs/vite/middleware";
+import { assert, describe, expect, it } from "vitest";
+import { defaultRatelimitIdentifier } from "../../util";
+
+describe("defaultRatelimitIdentifier", () => {
+  it("should return correct identifier", () => {
+    const request = new Request("http://localhost:8910/api/user");
+    const req = new MiddlewareRequest(request);
+    assert.equal(defaultRatelimitIdentifier(req), "192.168.1.1");
+  });
+
+  it("when authenticated, should return correct identifier", () => {
+    const request = new Request("http://localhost:8910/api/user");
+    const req = new MiddlewareRequest(request);
+    req.serverAuthContext.set({
+      isAuthenticated: true,
+      currentUser: { id: 1 },
+    });
+    assert.equal(defaultRatelimitIdentifier(req), "eyJpZCI6MX0=");
+  });
+
+  it("when not authenticated, should return correct identifier", () => {
+    const request = new Request("http://localhost:8910/api/user");
+    const req = new MiddlewareRequest(request);
+    req.serverAuthContext.set({
+      isAuthenticated: false,
+      currentUser: { id: 1 },
+    });
+    assert.equal(defaultRatelimitIdentifier(req), "192.168.1.1");
+  });
+});
--- packages/redwoodjs/src/ratelimit/util.ts
+++ packages/redwoodjs/src/ratelimit/util.ts
@@ -1,10 +1,13 @@
-import { type MatchFunction, match } from "path-to-regexp";
-
 import type { MiddlewareRequest } from "@redwoodjs/vite/middleware";
 import { MiddlewareResponse } from "@redwoodjs/vite/middleware";
+import { match } from "path-to-regexp";
+import type { MatchFunction } from "path-to-regexp";
 
 export type MiddlewarePathMatcher = string | string[];
 
+/**
+ * defaultRatelimitIdentifier is the default function to generate a ratelimit identifier
+ */
 export const defaultRatelimitIdentifier = (req: MiddlewareRequest) => {
   const authContext = req?.serverAuthContext?.get();
   const identifier = authContext?.isAuthenticated
@@ -13,25 +16,39 @@ export const defaultRatelimitIdentifier = (req: MiddlewareRequest) => {
   return identifier;
 };
 
-export const matchesPath = (path: string, matcher: MiddlewarePathMatcher): boolean => {
-  // Convert matcher to an array if it's not already one
-  const matchers = Array.isArray(matcher) ? matcher : [matcher];
-
-  console.debug(">>>> in matchesPath", matchers, path);
-
-  // Create a list of matching functions from the matchers
-  const matchingFunctions: MatchFunction[] = matchers.map((pattern) =>
-    match(pattern, { decode: decodeURIComponent }),
-  );
-
-  // Check if the path matches any of the patterns
-  return matchingFunctions.some((matchFunc) => matchFunc(path) !== false);
-};
-
+/*
+ * defaultRatelimitExceededResponse is the default response when the rate limit is exceeded
+ */
 export const defaultRatelimitExceededResponse = (_req: MiddlewareRequest) => {
   return new MiddlewareResponse("Rate limit exceeded", { status: 429 });
 };
 
+/**
+ * defaultRatelimitErrorResponse is the default response when there is an error with the rate limit
+ */
 export const defaultRatelimitErrorResponse = (_req: MiddlewareRequest) => {
   return new MiddlewareResponse("Internal server error", { status: 500 });
+};
+
+/**
+ * matchesPath checks if a path matches a pattern
+ */
+export const matchesPath = (path: string, matcher: MiddlewarePathMatcher): boolean => {
+  try {
+    // Convert matcher to an array if it's not already one
+    const matchers = Array.isArray(matcher) ? matcher : [matcher];
+
+    console.debug(">>>> in matchesPath", matchers, path);
+
+    // Create a list of matching functions from the matchers
+    const matchingFunctions: MatchFunction[] = matchers.map((pattern) => {
+      return match(pattern, { decode: decodeURIComponent });
+    });
+
+    // Check if the path matches any of the patterns
+    return matchingFunctions.some((matchFunc) => matchFunc(path) !== false);
+  } catch (e) {
+    console.error("Error in matchesPath", e);
+    throw e;
+  }
 };
