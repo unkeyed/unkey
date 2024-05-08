e3214fdb48b987bce80ac9c27905a7c23cefe4d6
chronark <dev@chronark.com>
2024-05-08 23:00:52 +0200

    fix: hide webhooks

--- apps/dashboard/app/(app)/settings/layout.tsx
+++ apps/dashboard/app/(app)/settings/layout.tsx
@@ -36,11 +36,11 @@ export default function SettingsLayout({
       href: "/settings/vercel",
       segment: "vercel",
     },
-    {
-      label: "Webhooks",
-      href: "/settings/webhooks",
-      segment: "webhooks",
-    },
+    // {
+    //   label: "Webhooks",
+    //   href: "/settings/webhooks",
+    //   segment: "webhooks",
+    // },
     {
       label: "User",
       href: "/settings/user",
