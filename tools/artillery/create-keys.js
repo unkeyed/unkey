ead81afc8344a67d72207d020a09a052c9193339
chronark <dev@chronark.com>
2024-05-09 14:19:20 +0200

    ci: use inputs

--- tools/artillery/create-keys.js
+++ tools/artillery/create-keys.js
@@ -25,7 +25,7 @@ async function main() {
       }),
     });
     const { key, keyId } = await res.json();
-    console.log(i, "created", keyId);
+    console.info(i, "created", keyId);
     keys.push(key);
   }
 
