15f40407c40e927da78302b16bacbae8e7818f35
David Thyresson <dthyresson@gmail.com>
2024-05-10 08:27:33 -0400

    WIP key auth middleware

--- packages/redwoodjs/src/ratelimit/index.ts
+++ packages/redwoodjs/src/ratelimit/index.ts
@@ -1,17 +1,18 @@
-import type { Logger } from "@redwoodjs/api/logger";
-import type {
-  // Middleware,
-  MiddlewareRequest,
-  MiddlewareResponse,
-} from "@redwoodjs/vite/middleware";
-
+import { Unkey } from "@unkey/api";
 import { Ratelimit } from "@unkey/ratelimit";
-import type { withUnkeyConfig } from "./types";
+
+import { version } from "../../package.json";
+
 import {
   defaultRatelimitErrorResponse,
   defaultRatelimitExceededResponse,
   defaultRatelimitIdentifier,
 } from "./util";
+
+import type { Logger } from "@redwoodjs/api/logger";
+
+import type { MiddlewareRequest, MiddlewareResponse } from "@redwoodjs/vite/middleware";
+import type { withUnkeyConfig } from "./types";
 
 const defaultLogger = require("abstract-logging") as Logger;
 
@@ -43,7 +44,7 @@ const defaultLogger = require("abstract-logging") as Logger;
  * import type { withUnkeyConfig } from '@unkey/redwoodjs'
  *
  * export const registerMiddleware = () => {
- *  const options: withUnkeyConfig = {
+ *  const config: withUnkeyConfig = {
  *     ratelimit: {
  *       config: {
  *         rootKey: process.env.UNKEY_ROOT_KEY,
@@ -63,51 +64,123 @@ const defaultLogger = require("abstract-logging") as Logger;
  *
  */
 const withUnkey = (config: withUnkeyConfig) => {
-  if (!config.ratelimit) {
-    throw new Error("ratelimit configuration is required");
+  if (!config) {
+    throw new Error("withUnkey requires a configuration object");
   }
 
-  const unkey = new Ratelimit(config.ratelimit.config);
+  if (!config.ratelimit && !config.auth) {
+    throw new Error("withUnkey requires a ratelimit or auth configuration object");
+  }
+
+  const logger = config.logger || defaultLogger;
+
+  if (config.auth) {
+    return async (req: MiddlewareRequest, res: MiddlewareResponse) => {
+      /**
+       * Get key from request and return a response early if not found
+       */
+      const key = config.auth?.getKey
+        ? config.auth.getKey(req)
+        : req.headers.get("authorization")?.replace("Bearer ", "") ?? null;
+
+      if (key === null) {
+        res.body = "Unauthorized";
+        res.status = 401;
+        return res;
+      }
+
+      if (typeof key !== "string") {
+        return key;
+      }
+
+      const unkey = new Unkey({
+        rootKey: "public",
+        wrapperSdkVersion: `@unkey/redwoodjs@${version}`,
+        disableTelemetry: config.auth?.disableTelemetry,
+      });
+
+      const unkeyVerificationResult = await unkey.keys.verify(
+        config.auth?.apiId ? { key, apiId: config.auth?.apiId } : { key },
+      );
+
+      if (unkeyVerificationResult.error) {
+        if (config.auth?.onError) {
+          return config.auth.onError(
+            req,
+            //unkeyVerificationResult.error
+          );
+        }
+        console.error(
+          `unkey error: [CODE: ${unkeyVerificationResult.error.code}] - [TRACE: ${unkeyVerificationResult.error.requestId}] - ${unkeyVerificationResult.error.message} - read more at ${unkeyVerificationResult.error.docs}`,
+        );
+
+        res.body = "Internal Server Error";
+        res.status = 500;
+      }
+
+      if (!unkeyVerificationResult.result?.valid) {
+        if (config.auth?.onInvalidKey) {
+          return config.auth.onInvalidKey(
+            req, //res.result
+          );
+        }
+
+        res.body = "Unauthorized";
+        res.status = 401;
+
+        // return new NextResponse("Unauthorized", { status: 500 });
+      }
+
+      return res;
+    };
+  }
+
+  if (config.ratelimit) {
+    const unkeyRateLimiter = new Ratelimit(config.ratelimit.config);
+
+    return async (req: MiddlewareRequest, res: MiddlewareResponse) => {
+      const getIdentifier = config.ratelimit?.getIdentifier || defaultRatelimitIdentifier;
+
+      const onExceeded = config.ratelimit?.onExceeded || defaultRatelimitExceededResponse;
+
+      const onError = config.ratelimit?.onError || defaultRatelimitErrorResponse;
+
+      try {
+        const identifier = getIdentifier(req);
+
+        const ratelimit = await unkeyRateLimiter.limit(identifier);
+
+        if (!ratelimit.success) {
+          logger.debug("Rate limit exceeded", {
+            ratelimit,
+            identifier,
+          });
+          const response = onExceeded(req);
+          if (response.status !== 429) {
+            logger.warn("Rate limit exceeded response is not 429. Overriding status.", response);
+            response.status = 429;
+          }
+          return response;
+        }
+      } catch (e) {
+        logger.error("Error in withUnkey", e);
+        const errorResponse = onError(req);
+        if (errorResponse.status !== 500) {
+          logger.warn(
+            `Rate limit error response is ${errorResponse.status}. Consider changing status to 500.`,
+            errorResponse,
+          );
+        }
+
+        return errorResponse;
+      }
+
+      return res;
+    };
+  }
 
   return async (req: MiddlewareRequest, res: MiddlewareResponse) => {
-    const logger = config.logger || defaultLogger;
-
-    const getIdentifier = config.ratelimit?.getIdentifier || defaultRatelimitIdentifier;
-
-    const onExceeded = config.ratelimit?.onExceeded || defaultRatelimitExceededResponse;
-
-    const onError = config.ratelimit?.onError || defaultRatelimitErrorResponse;
-
-    try {
-      const identifier = getIdentifier(req);
-
-      const ratelimit = await unkey.limit(identifier);
-
-      if (!ratelimit.success) {
-        logger.debug("Rate limit exceeded", {
-          ratelimit,
-          identifier,
-        });
-        const response = onExceeded(req);
-        if (response.status !== 429) {
-          logger.warn("Rate limit exceeded response is not 429. Overriding status.", response);
-          response.status = 429;
-        }
-        return response;
-      }
-    } catch (e) {
-      logger.error("Error in withUnkey", e);
-      const errorResponse = onError(req);
-      if (errorResponse.status !== 500) {
-        logger.warn(
-          `Rate limit error response is ${errorResponse.status}. Consider changing status to 500.`,
-          errorResponse,
-        );
-      }
-
-      return errorResponse;
-    }
-
+    logger.error(req, "No configuration provided. Skipping middleware.");
     return res;
   };
 };
--- packages/redwoodjs/src/ratelimit/types.ts
+++ packages/redwoodjs/src/ratelimit/types.ts
@@ -3,13 +3,70 @@ import type { MiddlewareRequest } from "@redwoodjs/vite/middleware";
 import type { MiddlewareResponse } from "@redwoodjs/vite/middleware";
 import type { RatelimitConfig } from "@unkey/ratelimit";
 
-export type withUnkeyRatelimitConfig = {
-  config: RatelimitConfig;
-  getIdentifier?: (req: MiddlewareRequest) => string;
-  onExceeded?: (req: MiddlewareRequest) => MiddlewareResponse;
+export type withUnkeyApiKeyConfig = {
+  /**
+   * The apiId to verify against.
+   *
+   * This will be required soon.
+   */
+  apiId?: string;
+
+  /**
+   *
+   * By default telemetry data is enabled, and sends:
+   * runtime (Node.js / Edge)
+   * platform (Node.js / Vercel / AWS)
+   * SDK version
+   */
+  disableTelemetry?: boolean;
+
+  /**
+   * How to get the key from the request
+   * Usually the key is provided in an `Authorization` header, but you can do what you want.
+   *
+   * Return the key as string, or null if it doesn't exist.
+   *
+   * You can also override the response given to the caller by returning a `NextResponse`
+   *
+   * @default `req.headers.get("authorization")?.replace("Bearer ", "") ?? null`
+   */
+  getKey?: (req: MiddlewareRequest) => string;
+
+  /**
+   * Automatically return a custom response when a key is invalid
+   */
+  onInvalidKey?: (req: MiddlewareRequest) => MiddlewareResponse;
+
+  /**
+   * What to do if things go wrong
+   */
   onError?: (req: MiddlewareRequest) => MiddlewareResponse;
 };
+
+export type withUnkeyRatelimitConfig = {
+  /**
+   * The Unkey configuration for the rate limiter
+   */
+  config: RatelimitConfig;
+
+  /**
+   * Custom function to get the identifier for the rate limiter
+   */
+  getIdentifier?: (req: MiddlewareRequest) => string;
+
+  /**
+   * Custom function to handle when the rate limit is exceeded
+   */
+  onExceeded?: (req: MiddlewareRequest) => MiddlewareResponse;
+
+  /**
+   * Custom function to handle when an error occurs
+   */
+  onError?: (req: MiddlewareRequest) => MiddlewareResponse;
+};
+
 export type withUnkeyConfig = {
+  auth?: withUnkeyApiKeyConfig;
   ratelimit?: withUnkeyRatelimitConfig;
   logger?: Logger;
 };
