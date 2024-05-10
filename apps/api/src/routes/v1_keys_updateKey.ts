dbf6da2a2e26760eaf5398b340cb8daedc8df0f1
Andreas Thomas <dev@chronark.com>
2024-05-10 09:36:11 +0200

    feat: add operationIds (#1623)

--- apps/api/src/routes/v1_keys_updateKey.ts
+++ apps/api/src/routes/v1_keys_updateKey.ts
@@ -8,6 +8,8 @@ import { buildUnkeyQuery } from "@unkey/rbac";
 import { eq } from "drizzle-orm";
 
 const route = createRoute({
+  tags: ["keys"],
+  operationId: "v1.keys.updateKey",
   method: "post",
   path: "/v1/keys.updateKey",
   security: [{ bearerAuth: [] }],
