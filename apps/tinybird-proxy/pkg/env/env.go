b379e44d9c9011f8259d62bacf9c57be4a555162
chronark <dev@chronark.com>
2024-05-15 13:43:27 +0200

    style: fmt

--- apps/tinybird-proxy/pkg/env/env.go
+++ apps/tinybird-proxy/pkg/env/env.go
@@ -7,15 +7,14 @@ import (
 
 // String returns the value of the environment variable.
 //
-// If the environment variable is empty and no defaultValue is provided, it panics
+// # If the environment variable is empty and no defaultValue is provided, it panics
 //
 // Example:
 //
-// 	port := env.String("PORT", "8080")
-//
-func String(name string, defaultValue ...string) string  {
+//	port := env.String("PORT", "8080")
+func String(name string, defaultValue ...string) string {
 	value := os.Getenv(name)
-	if (value != "") {
+	if value != "" {
 		return value
 	}
 	if len(defaultValue) > 0 {
@@ -24,18 +23,16 @@ func String(name string, defaultValue ...string) string  {
 	panic("Environment variable " + name + " is empty")
 }
 
-
-// Int64 returns the value of the environment variable named by the key 
-// 
-// If the environment variable is empty and no defaultValue is provided, it panics
+// Int64 returns the value of the environment variable named by the key
+//
+// # If the environment variable is empty and no defaultValue is provided, it panics
 //
 // Example:
 //
-// 	port := env.String("PORT", "8080")
-//
-func Int64(name string, defaultValue ...int64) int64  {
+//	port := env.String("PORT", "8080")
+func Int64(name string, defaultValue ...int64) int64 {
 	value := os.Getenv(name)
-	if (value != "") {
+	if value != "" {
 		i, ok := strconv.ParseInt(value, 10, 64)
 		if ok != nil {
 			panic("Environment variable " + name + " is not a valid int64")
