995ad663c0c0e78602bb45a36090484a5d07980b
Dom <domeccleston@gmail.com>
2024-05-22 10:01:03 +0100

    wip: db

---
+++ apps/semantic-cache/src/db.ts
@@ -0,0 +1,35 @@
+import { type PlanetScaleDatabase, drizzle } from "drizzle-orm/planetscale-serverless";
+
+import { Client } from "@planetscale/database";
+import { schema } from "@unkey/db";
+export type Database = PlanetScaleDatabase<typeof schema>;
+
+type ConnectionOptions = {
+  host: string;
+  username: string;
+  password: string;
+};
+
+export function createConnection(opts: ConnectionOptions): Database {
+  return drizzle(
+    new Client({
+      host: opts.host,
+      username: opts.username,
+      password: opts.password,
+
+      fetch: (url: string, init: any) => {
+        (init as any).cache = undefined; // Remove cache header
+        const u = new URL(url);
+        // set protocol to http if localhost for CI testing
+        if (u.host.includes("localhost")) {
+          u.protocol = "http";
+        }
+        return fetch(u, init);
+      },
+    }),
+    {
+      schema,
+    },
+  );
+}
+export * from "@unkey/db";
