0cc974b4fbe551165294067f388c9b6e8979251d
chronark <dev@chronark.com>
2024-05-26 12:41:40 +0200

    test(encryption.test.ts): add test for rolling the encryption key in cache middleware

--- packages/cache/src/middleware/encryption.test.ts
+++ packages/cache/src/middleware/encryption.test.ts
@@ -24,3 +24,46 @@ test("encrypts and decrypts", async (t) => {
   t.expect(res.err).not.toBeDefined();
   t.expect(res.val!.value).toEqual(value);
 });
+
+describe("rolling the encryption key", () => {
+  test("causes cache misses, not errors", async (t) => {
+    const map = new Map();
+    const memory: Store<"namespace", string> = new MemoryStore({ persistentMap: map });
+
+    // generated with `openssl rand -base64 32`
+    const encryptionKey = "gVXaB49mnCZILHqXNpZ/cH22TsYoM5QbzjX3Nu15lKo=";
+    const store = await EncryptedStore.withBase64Key(memory, encryptionKey);
+
+    const key = "key";
+    const value = "value";
+
+    await store.set("namespace", key, {
+      value,
+      freshUntil: Date.now() + 1000000,
+      staleUntil: Date.now() + 100000000,
+    });
+
+    t.expect(map.size).toBe(1);
+
+    const newEncryptionKey = "Rd1ghtfwkcdlj5Kq2IPdPwWGrCCOBU1q9W+8ipYo3P8=";
+    const newStore = await EncryptedStore.withBase64Key(memory, newEncryptionKey);
+
+    // should miss the cache as the key has changed
+    const res = await newStore.get("namespace", key);
+    t.expect(res.err).not.toBeDefined();
+    t.expect(res.val).not.toBeDefined();
+
+    await newStore.set("namespace", key, {
+      value,
+      freshUntil: Date.now() + 1000000,
+      staleUntil: Date.now() + 100000000,
+    });
+
+    t.expect(map.size).toBe(2);
+
+    const res2 = await newStore.get("namespace", key);
+    console.log(res2);
+    t.expect(res2.err).not.toBeDefined();
+    t.expect(res2.val?.value).toBe(value);
+  });
+});
