b4604ec6fcec268e2b81fd30538abaa5047bfb2f
Guilherme Rodz <gui.rodz.dev@gmail.com>
2024-05-28 10:06:32 -0300

    feat(accelerate): add first hero prototype

---
+++ apps/www/app/accelerate/components/rive.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import Rive from "@rive-app/react-canvas-lite";
+
+export const RiveAccelerate = () => (
+  <Rive src="assets/accelerate/rive/accelerate.riv" stateMachines="sms" />
+);
