9fa5aeca5564ef05265a5b97145c11c1d00a1d74
Dom <domeccleston@gmail.com>
2024-05-31 09:55:17 +0100

    add v1 logs detail view

--- apps/dashboard/app/(app)/semantic-cache/logs/page.tsx
+++ apps/dashboard/app/(app)/semantic-cache/logs/page.tsx
@@ -1,4 +1,16 @@
 import { PageHeader } from "@/components/dashboard/page-header";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Separator } from "@/components/ui/separator";
 
 import {
@@ -18,7 +30,6 @@ import { getAllSemanticCacheLogs } from "@/lib/tinybird";
 export const formatDate = (timestamp: string | number | Date): string => {
   const date = new Date(timestamp);
   const options: Intl.DateTimeFormatOptions = {
-    year: "numeric",
     month: "long",
     day: "numeric",
     hour: "2-digit",
@@ -41,22 +52,27 @@ export default async function SemanticCacheLogsPage() {
             <TableHead>Model</TableHead>
             <TableHead>Cache status</TableHead>
             <TableHead>Query</TableHead>
-            <TableHead>Response</TableHead>
             <TableHead>Request ID</TableHead>
             <TableHead>Request timing</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           {data.map((data) => (
-            <TableRow key={data.requestId}>
-              <TableCell className="font-medium">{formatDate(data.timestamp)}</TableCell>
-              <TableCell>{data.model}</TableCell>
-              <TableCell>{data.cache === 0 ? "Miss" : "Hit"}</TableCell>
-              <TableCell>{data.query}</TableCell>
-              <TableCell>{data.response}</TableCell>
-              <TableCell>{data.requestId}</TableCell>
-              <TableCell>{data.timing}</TableCell>
-            </TableRow>
+            <Dialog>
+              <DialogTrigger asChild>
+                <TableRow key={data.requestId}>
+                  <TableCell className="font-medium">{formatDate(data.timestamp)}</TableCell>
+                  <TableCell>{data.model}</TableCell>
+                  <TableCell>{data.cache === 0 ? "Miss" : "Hit"}</TableCell>
+                  <TableCell>{data.query}</TableCell>
+                  <TableCell>{data.requestId}</TableCell>
+                  <TableCell>{data.timing}</TableCell>
+                </TableRow>
+              </DialogTrigger>
+              <DialogContent className="translate-x-0 translate-y-0 right-0 top-0 left-[unset] h-screen">
+                {data.response}
+              </DialogContent>
+            </Dialog>
           ))}
         </TableBody>
       </Table>
