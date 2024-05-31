bab92e03ae6f4cee584bcd261fc0c9e39606660c
chronark <dev@chronark.com>
2024-05-31 10:23:23 +0200

    feat(deploy.yaml): add mintlify_deployment job to deploy docs fix(key_migrations.ts): update message field type to include detailed schema information

--- internal/db/src/schema/key_migrations.ts
+++ internal/db/src/schema/key_migrations.ts
@@ -13,7 +13,38 @@ export const keyMigrationErrors = mysqlTable("key_migration_errors", {
     .notNull()
     .references(() => workspaces.id, { onDelete: "cascade" }),
 
-  message: json("message").notNull(),
+  message: json("message")
+    .$type<{
+      migrationId: string;
+      workspaceId: string;
+      keyAuthId: string;
+      rootKeyId: string;
+      prefix?: string;
+
+      name?: string;
+      hash: string;
+      start?: string;
+      ownerId?: string;
+      meta?: Record<string, unknown>;
+      roles?: string[];
+      permissions?: string[];
+      expires?: number;
+      remaining?: number;
+      refill?: { interval: "daily" | "monthly"; amount: number };
+      ratelimit?: { async: boolean; limit: number; duration: number };
+      enabled: boolean;
+      environment?: string;
+      encrypted?: {
+        encrypted: string;
+        keyId: string;
+      };
+
+      auditLogContext: {
+        location: string;
+        userAgent: string;
+      };
+    }>()
+    .notNull(),
 });
 
 export const keyMigrationErrorsRelations = relations(keyMigrationErrors, ({ one }) => ({
