6ed93de48008c0dd011571bce59f1da9329bce79
MichaelUnkey <michael@unkey.dev>
2024-06-03 13:28:08 -0400

    Playground added

--- apps/www/app/playground/page.tsx
+++ apps/www/app/playground/page.tsx
@@ -1,109 +1,199 @@
-import { Container } from "@/components/container";
-import { CTA } from "@/components/cta";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
+"use client";
+
+import { GeistMono } from "geist/font/mono";
+import { KeyRound, SquareArrowOutUpRight } from "lucide-react";
+import { useCallback, useEffect, useRef, useState } from "react";
+
+import TerminalInput from "@/components/playground/terminalInput";
+import TextAnimator from "@/components/playground/textAnimator";
+import startData, { type Message } from "@/lib/playground/data";
+import { handleCurlServer } from "@/lib/playground/helper";
 import { cn } from "@/lib/utils";
 
-export const metadata = {
-  title: "Playground | Unkey",
-  description: "Try unkey without signing up.",
-  openGraph: {
-    title: "About | Unkey",
-    description: "Learn more about Unkey and how we operate.",
-    url: "https://unkey.com/playground",
-    siteName: "unkey.dev",
-    images: [
-      {
-        url: "https://unkey.com/images/landing/og.png",
-        width: 1200,
-        height: 675,
-      },
-    ],
-  },
-  twitter: {
-    title: "Playground | Unkey",
-    card: "summary_large_image",
-  },
-  icons: {
-    shortcut: "/images/landing/unkey.png",
-  },
-};
+export default function PlaygroundHome() {
+  const apiId = process.env.NEXT_PUBLIC_UNKEY_API_ID;
+  const [historyItems, updateHistoryItems] = useState(startData[0].messages);
+  const step = useRef<number>(0);
+  const timeStamp = useRef<number>(Date.now() + 24 * 60 * 60 * 1000);
+  const keyId = useRef<string>();
+  const keyName = useRef<string>();
+  const scrollRef = useRef<null | HTMLDivElement>(null);
 
-export default async function Page() {
+  useEffect(() => {
+    scrollRef?.current?.scrollIntoView({ behavior: "smooth" });
+  }, [historyItems]);
+  const parseCurlCommand = useCallback(
+    (stepString: string) => {
+      let tempString = stepString;
+      tempString = tempString.replace("<timeStamp>", timeStamp.current.toString());
+      if (apiId) {
+        tempString = apiId.length > 0 ? tempString.replace("<apiId>", apiId) : tempString;
+      }
+      tempString = keyId.current ? tempString.replace("<keyId>", keyId.current) : tempString;
+      tempString = keyName.current ? tempString.replace("<key>", keyName.current) : tempString;
+      return tempString;
+    },
+    [apiId, keyId, keyName, timeStamp],
+  );
+  function handleSubmit(cmd: string) {
+    postNewLine(cmd, "text-violet-500");
+    handleCurl(cmd);
+  }
+
+  function postNewLine(input: string, color: string) {
+    const temp = historyItems;
+    temp.push({ content: input, color: color });
+    updateHistoryItems([...temp]);
+  }
+
+  function handleKeyDown(event: React.KeyboardEvent<HTMLButtonElement>, item: string) {
+    if (event.key === "i" || event.key === "I") {
+      handleSubmit(item);
+    }
+  }
+  async function handleCurl(curlString: string) {
+    postNewLine("Processing...", "text-green-500");
+    if (!curlString.includes("curl")) {
+      postNewLine(`{"Error", "Invalid Curl Command"}`, "text-red-500");
+      return;
+    }
+    const parsedCurlString = curlString.replace("--data", "--data-raw");
+    const response = await handleCurlServer(parsedCurlString);
+    if (response) {
+      const resJson = JSON.parse(JSON.stringify(response, null, 2));
+      if (resJson.error) {
+        postNewLine(JSON.stringify(response, null, 2), "text-red-500");
+        return;
+      }
+      const result = resJson.result;
+      // Response from server to Terminal
+      postNewLine(JSON.stringify(result, null, 2), "text-blue-500");
+
+      if (result.keyId) {
+        keyId.current = result.keyId;
+      }
+      if (result.key) {
+        keyName.current = result.key;
+      }
+
+      const newCurl = parseCurlCommand(startData[step.current + 1].curlCommand ?? "");
+      postNewLine(startData[step.current + 1].header, "text-white");
+      const newMessages = startData[step.current + 1].messages;
+      newMessages.map((item: Message) => {
+        const cmd = parseCurlCommand(item.content);
+        postNewLine(cmd, "text-white");
+      });
+      postNewLine(newCurl, "text-white");
+      step.current += 1;
+    }
+  }
+
+  const HistoryList = () => {
+    return historyItems?.map((item, index) => {
+      {
+        const isLast = index === historyItems.length - 1;
+        const isCurl = item.content.includes("curl --request");
+        if (isLast) {
+          return (
+            <div className="h-full snap-end mt-4" ref={scrollRef}>
+              <button
+                type="button"
+                onClick={() => handleSubmit(item.content)}
+                onKeyUp={(e) => handleKeyDown(e, item.content)}
+              >
+                <pre
+                  key={`history${index.toString()}`}
+                  className={cn(
+                    "flex flex-row text-lg font-medium leading-7 snap-end",
+                    item.color,
+                    GeistMono.className,
+                    isCurl
+                      ? "transition duration-500 hover:-translate-y-1 hover:translate-x-1 snap-end"
+                      : "",
+                  )}
+                >
+                  <TextAnimator
+                    input={item.content}
+                    repeat={0}
+                    style={
+                      "background-color: #111827; color: #4C0DB2; padding: 0.5rem; border-radius: 0.5rem; "
+                    }
+                  />
+                </pre>
+              </button>
+            </div>
+          );
+        }
+        if (!isLast && isCurl) {
+          return (
+            <div
+              key={`history${index.toString()}`}
+              className={cn(
+                `flex flex-row snap-end mt-4 delay-[${index * 500}ms]`,
+                GeistMono.className,
+              )}
+            >
+              <pre
+                className={cn(
+                  ":flex flex-row text-lg font-medium leading-7 snap-end",
+                  item.color,
+                  GeistMono.className,
+                )}
+              >
+                {item.content}
+              </pre>
+            </div>
+          );
+        }
+        return (
+          <div
+            key={`history${index.toString()}`}
+            className={cn("flex flex-row snap-end mt-4 text-pretty", GeistMono.className)}
+          >
+            <p
+              className={cn(
+                ":flex flex-row text-lg font-medium leading-7 snap-end text-pretty",
+                item.color,
+                GeistMono.className,
+              )}
+            >
+              {item.content}
+            </p>
+          </div>
+        );
+      }
+    });
+  };
   return (
-    <Container className="text-lg">
-      <div className="mt-32">
-        <h2 className="text-2xl">Unkey Playground</h2>
-      </div>
-      <div className="flex w-full mt-8 h-full">
-        <div className="flex flex-col w-full h-full">
-          <Accordion type="single" collapsible className="w-full">
-            <AccordionItem value="item-1" className="">
-              <AccordionTrigger className="px-4 justify-between bg-slate-900 rounded-2xl">
-                1. Create Key
-              </AccordionTrigger>
-              <AccordionContent className="p-4 bg-slate-950 rounded-2xl px-6">
-                <p className="text-center leading-7">There are two methods to create a key.</p>
-                <ol className="list-decimal pl-4 text-lg leading-7 pt-2">
-                  <li>
-                    You can create a key by clicking on the "Create Key" button on the top right
-                    corner of your api page in our dashboard.
-                  </li>
-                  <li>You can also create a key using Unkey's API.</li>
-                </ol>
-                {/* <button className={cn("mt-4", "button", "button-primary")}>Create Key</button> */}
-              </AccordionContent>
-            </AccordionItem>
-            <AccordionItem value="item-2">
-              <AccordionTrigger className="px-4 justify-between">2. Verify Key</AccordionTrigger>
-              <AccordionContent className="p-4">
-                Yes. It comes with default styles that matches the other components&apos; aesthetic.
-              </AccordionContent>
-            </AccordionItem>
-            <AccordionItem value="item-3">
-              <AccordionTrigger className="px-4 justify-between">3. Update Key</AccordionTrigger>
-              <AccordionContent className="p-4">
-                Yes. It&apos;s animated by default, but you can disable it if you prefer.
-              </AccordionContent>
-            </AccordionItem>
-            <AccordionItem value="item-3">
-              <AccordionTrigger className="px-4 justify-between">4. Verify key</AccordionTrigger>
-              <AccordionContent className="p-4">
-                Yes. It&apos;s animated by default, but you can disable it if you prefer.
-              </AccordionContent>
-            </AccordionItem>
-            <AccordionItem value="item-3">
-              <AccordionTrigger className="px-4 justify-between">5. Get Analytics</AccordionTrigger>
-              <AccordionContent className="p-4">
-                Yes. It&apos;s animated by default, but you can disable it if you prefer.
-              </AccordionContent>
-            </AccordionItem>
-            <AccordionItem value="item-3">
-              <AccordionTrigger className="px-4 justify-between">6. Revoke Key</AccordionTrigger>
-              <AccordionContent className="p-4">
-                Yes. It&apos;s animated by default, but you can disable it if you prefer.
-              </AccordionContent>
-            </AccordionItem>
-            <AccordionItem value="item-3">
-              <AccordionTrigger className="px-4 justify-between">7. Verify Key</AccordionTrigger>
-              <AccordionContent className="p-4">
-                Yes. It&apos;s animated by default, but you can disable it if you prefer.
-              </AccordionContent>
-            </AccordionItem>
-          </Accordion>
-        </div>
-        <div className="flex w-full h-full p-6">
-          <input
-            className="w-full h-10 bg-slate-800 text-white rounded-lg  p-4 ring-2 ring-slate-900 text-pretty justify-end"
-            placeholder="Playground/unkey > "
-          />
+    <div className="flex flex-col w-full h-full justify-center ">
+      <div className="mx-auto w-full h-full justify-center max-w-[1440px]">
+        <h1 className="section-title-heading-gradient max-sm:mx-6 max-sm:text-4xl font-medium text-[4rem] leading-[4rem] max-w-xl text-left mt-16 py-2">
+          Unkey API Playground
+        </h1>
+        <div className=" min-w-full h-full mt-12">
+          <div className="flex flex-row w-full h-8 bg-[#383837]/60 rounded-t-lg drop-shadow-[0_2px_1px_rgba(0,0,0,0.7)]">
+            <div className="flex flex-col w-1/3">
+              <KeyRound size={18} className="mx-2 mt-1" />
+            </div>
+            <div className="flex flex-col w-1/3 justify-center">
+              <p className="text-center">Heading</p>
+            </div>
+            <div className="flex flex-row w-1/3 justify-end my-auto">
+              Step <SquareArrowOutUpRight size={18} className="pt-1 mx-2" />
+            </div>
+          </div>
+          <div className="flex flex-col min-w-full h-[900px] bg-[#1F1F1E]/80 overflow-hidden ">
+            <div
+              onChange={() => scrollTo()}
+              className="flex flex-col w-full rounded-lg pt-4 pl-6 scrollbar-hide overflow-y-scroll scroll-smooth snap-y pr-24"
+            >
+              <HistoryList />
+              <div ref={scrollRef} />
+            </div>
+          </div>
+          <TerminalInput sendInput={(cmd) => handleSubmit(cmd)} />
         </div>
       </div>
-      <CTA />
-    </Container>
+    </div>
   );
 }
---
+++ apps/www/lib/playground/data.ts
@@ -0,0 +1,216 @@
+const apiId = process.env.PLAYGROUND_API_ID;
+if (!apiId) {
+  throw new Error("Missing apiId");
+}
+
+export type Message = {
+  content: string;
+  color: string; // Can be a CSS color name or hex code
+};
+
+export type Step = {
+  header: string;
+  messages: Message[];
+  curlCommand?: string;
+};
+
+const steps: Step[] = [];
+// Step 1 Create your first key
+const step1Header = "";
+const step1Messages: Message[] = [
+  { content: "Step 1: Create your first key", color: "text-white" },
+  {
+    content:
+      "Welcome to the Unkey playground. Here, you can test how Unkey's API works using curl commands. The first step is to create a key for a user. They will use this key to authenticate with your API. Normally, you would need an apiId and root key, but we've already set this up for you. Usually, this setup would be done in your Unkey dashboard. For now, leave the <token> in place. This is where your bearer token or root key would go. For each step, type in or copy the curl command below into the input at the bottom and press enter. Alternatively, you can just click the curl command and it will be sent off for you.",
+    color: "text-white",
+  },
+  {
+    content:
+      "As you can see its a typical POST request with a url, headers and data. The Bearer in the first header authenticates you in the Unkey system. The data object passes what api you would like to create a key for.",
+    color: "text-white",
+  },
+  {
+    content: `curl --request POST --url https://api.unkey.dev/v1/keys.createKey
+--header 'Authorization: Bearer <token>
+--header 'Content-Type: application/json
+--data '{"apiId": "${apiId}"}'`,
+    color: "text-violet-600",
+  },
+];
+const step1CurlCommand = `curl --request POST --url https://api.unkey.dev/v1/keys.createKey
+--header 'Authorization: Bearer <token>
+--header 'Content-Type: application/json
+--data '{"apiId": "${apiId}"}'`;
+
+// Step 2 Get the key we just created
+const step2Header = "Step 2: Get the key we just created";
+const step2Messages: Message[] = [
+  {
+    content:
+      "Nice Job! Now that we have created a user key let us use the getKey endpoint to get some info. A typical key will have useful information like roles, permissions, remaining uses, ownerId and more. A full list can be found on our docs https://www.unkey.com/docs/api-reference/keys/get",
+    color: "text-white",
+  },
+];
+const step2CurlCommand = `curl --request GET
+--url https://api.unkey.dev/v1/keys.getKey?keyId=<keyId> 
+--header 'Authorization: Bearer <token>'`;
+
+// Step 3 Verify the key
+const step3Header = "Step 3: Verify the key";
+const step3Messages: Message[] = [
+  {
+    content:
+      "lets go! Looks like that worked and returned some data. This object can get much larger depending on the options added to the key. Now we can verify the key we just created to make sure it will work for a user. Each verification will add some analytics data we will get in a later step.",
+    color: "text-white",
+  },
+];
+const step3CurlCommand = `curl --request POST
+--url https://api.unkey.dev/v1/keys.verifyKey
+--header 'Content-Type: application/json'
+--data '{"apiId": "${apiId}", "key": "<key>"}'`;
+
+// Step 4  Update the key with ownerId
+const step4Header = "Step 4: Update the key with ownerId";
+const step4Messages: Message[] = [
+  {
+    content:
+      "You will notice the enabled: true meaning meaning it can be used for authenticating. Now lets try to update the key with an ownerId. The ownerId can help you link your user to a spacific key or set of keys. For example if our customer was Acme we could mark all the keys with Acme_Company. This could then be searched to see all keys witht he Acme_Company ownerId. Making it easier for you to track keys for each customer. Lets add that now. Feel free to put any OwnerId you want in place of user_1234.",
+    color: "text-white",
+  },
+];
+const step4CurlCommand = `curl --request POST
+--url https://api.unkey.dev/v1/keys.updateKey
+--header 'Authorization: Bearer <token>'
+--header 'Content-Type: application/json'
+--data '{"keyId": "<keyId>", "ownerId": "user_1234"}'`;
+
+//Step 5 Verify the key again
+const step5Header = "Step 5: Verify the key again";
+const step5Messages: Message[] = [
+  {
+    content:
+      "If this worked correctly you should get back a {} in response. This is normal and signifies success. If there was an error you would have gotten back an error in stead. Lets now verify the key just to make sure your key is updated with the ownerId. Just to give you piece of mind.",
+    color: "text-white",
+  },
+];
+const step5CurlCommand = `curl --request POST
+--url https://api.unkey.dev/v1/keys.verifyKey
+--header 'Content-Type: application/json'
+--data '{"apiId": "${apiId}", "key": "<key>"}'`;
+
+// Step 6 Update with an expiration date
+const step6Header = "Step 6: Update with an expiration date";
+const step6Messages: Message[] = [
+  {
+    content:
+      "The response from step 5 should show the ownerId you enter in step 4. If you want to only allow a user access for a day or month. This can be done by adding an expiration date in unix timestamp in milliseconds. This will disable the key from being used after that time has passed. You may also change that expiration to a later time after it expires. Say a user pays for another month it can be re activated.",
+    color: "text-white",
+  },
+];
+const step6CurlCommand = `curl --request POST
+--url https://api.unkey.dev/v1/keys.updateKey
+--header 'Authorization: Bearer <token>'
+--header 'Content-Type: application/json'
+--data '{"keyId": "<keyId>", "expires": <timeStamp>}'`;
+
+// Step 7 Verify the key again
+const step7Header = "Step 7: Verify the key again";
+const step7Messages: Message[] = [
+  {
+    content:
+      "Again we should have gotten {} in response. Lets verify the key to make sure expiration was set correctly. It will also give up more analytics data for the next step.",
+    color: "text-white",
+  },
+];
+const step7CurlCommand = `curl --request POST
+--url https://api.unkey.dev/v1/keys.verifyKey
+--header 'Content-Type: application/json'
+--data '{"apiId": "${apiId}","key": "<key>"}'`;
+
+// Step 8
+const step8Header = "Step 8: Get analytics data";
+const step8Messages: Message[] = [
+  {
+    content:
+      "Nice! it looks like the expires value now holds the value you entered. Now lets see the analytics data on our key. This will show how many times the key was successfuly verified, ratelimited, or the usage has been exceeeded.",
+    color: "text-white",
+  },
+];
+const step8CurlCommand = `curl --request GET
+--url https://api.unkey.dev/v1/keys.getVerifications?keyId=<keyId>
+--header 'Authorization: Bearer <token>'`;
+
+//Step 9
+const step9Header = "Step 9: Delete the key";
+const step9Messages: Message[] = [
+  {
+    content:
+      "If it was a new key and all our verify keys worked correctly. success should show a value of 3. Lets say we no longer want this key to have any access. While we can use updateKey to set enabled to false. Here lets delete the key so it has not option of being used again. Maybe the key was leaked to a bad actor or it is no longer needed for any reason.",
+    color: "text-white",
+  },
+];
+const step9CurlCommand = `curl --request POST
+--url https://api.unkey.dev/v1/keys.deleteKey
+--header 'Content-Type: application/json'
+--data '{"keyId": "<keyId>"}'`;
+
+//Step 10 Last Verification
+const step10Header = "Step 10: Last verification";
+const step10Messages: Message[] = [
+  {
+    content:
+      "Now lets verify that deleting a key worked as it should. Use the verifyKey route to check the key again. This should now return valid: false meaning this key is no longer a valid Unkey key.",
+    color: "text-white",
+  },
+];
+const step10CurlCommand = `curl --request POST
+--url https://api.unkey.dev/v1/keys.verifyKey
+--header 'Content-Type: application/json'
+--data '{"apiId": "${apiId}", "key": "<key>"}'`;
+
+//Step 11 Congrats!
+const step11Header = "Congrats!";
+const step11Messages: Message[] = [
+  {
+    content:
+      "Nice job. I hope this was useful in some way on learning about some Unkey basics. To learn more visit our docs. To get started feel free to setup an unkey dashboard for your project.",
+    color: "text-white",
+  },
+];
+const step11CurlCommand = "";
+
+const step1: Step = { header: step1Header, messages: step1Messages, curlCommand: step1CurlCommand };
+const step2: Step = { header: step2Header, messages: step2Messages, curlCommand: step2CurlCommand };
+const step3: Step = { header: step3Header, messages: step3Messages, curlCommand: step3CurlCommand };
+const step4: Step = { header: step4Header, messages: step4Messages, curlCommand: step4CurlCommand };
+const step5: Step = { header: step5Header, messages: step5Messages, curlCommand: step5CurlCommand };
+const step6: Step = { header: step6Header, messages: step6Messages, curlCommand: step6CurlCommand };
+const step7: Step = { header: step7Header, messages: step7Messages, curlCommand: step7CurlCommand };
+const step8: Step = { header: step8Header, messages: step8Messages, curlCommand: step8CurlCommand };
+const step9: Step = { header: step9Header, messages: step9Messages, curlCommand: step9CurlCommand };
+const step10: Step = {
+  header: step10Header,
+  messages: step10Messages,
+  curlCommand: step10CurlCommand,
+};
+const step11: Step = {
+  header: step11Header,
+  messages: step11Messages,
+  curlCommand: step11CurlCommand,
+};
+
+steps.push(step1);
+steps.push(step2);
+steps.push(step3);
+steps.push(step4);
+steps.push(step5);
+steps.push(step6);
+steps.push(step7);
+steps.push(step8);
+steps.push(step9);
+steps.push(step10);
+steps.push(step11);
+
+const startData = steps;
+
+export default startData;
