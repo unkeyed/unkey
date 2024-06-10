1e48c2b03022f80ceefa2904f5df9f46fb968637
Andreas Thomas <dev@chronark.com>
2024-06-10 15:37:52 +0200

    feat: update tinybird pipes (#1730)
    * feat: update tinybird pipes
    
    * feat(streaming.ts): import Tokenizer from tokens module
    feat(tokens.ts): add Tokenizer class with init method and count function
    feat(datasources): add daily and hourly semantic cache datasources
    feat(pipes): add daily and hourly semantic cache pipes

--- apps/semantic-cache/scripts/openai-stream.ts
+++ apps/semantic-cache/scripts/openai-stream.ts
@@ -23,7 +23,7 @@ async function main() {
   });
 
   for await (const chunk of chatCompletion) {
-    console.info(chunk.choices[0].delta.content);
+    process.stdout.write(chunk.choices[0].delta.content);
   }
 }
 
