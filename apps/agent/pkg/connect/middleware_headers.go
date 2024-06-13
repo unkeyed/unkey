d5998e7c2dc7df1190d69270eae8b88aad4ce58b
chronark <dev@chronark.com>
2024-06-13 09:54:57 +0200

    feat(agent): add header middleware to set "Unkey-AWS-Region" header feat(agent): update service to use new header middleware feat(api): improve error handling and logging in DurableRateLimiter

---
+++ apps/agent/pkg/connect/middleware_headers.go
@@ -0,0 +1,20 @@
+package connect
+
+import (
+	"net/http"
+	"os"
+)
+
+type headerMiddleware struct {
+	handler http.Handler
+}
+
+func newHeaderMiddleware(handler http.Handler) http.Handler {
+	return &headerMiddleware{handler}
+}
+
+func (h *headerMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Unkey-AWS-Region", os.Getenv("FC_AWS_REGION"))
+	h.handler.ServeHTTP(w, r)
+
+}
