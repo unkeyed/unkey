127063629897aac48ec61ad0fead11f1ddda6fd6
Andreas Thomas <dev@chronark.com>
2024-06-17 17:54:38 +0200

    docs: draft for cache docs done (#1760)
    * docs: draft for cache docs done
    
    * chore(docs): update cache overview with code snippet and common issues
    fix(docs): fix typo in documentation about Unkey usage in production
    
    ---------
    
    Co-authored-by: Dom Eccleston <31101792+domeccleston@users.noreply.github.com>

--- apps/api/src/pkg/cache/index.ts
+++ apps/api/src/pkg/cache/index.ts
@@ -32,9 +32,9 @@ export function initCache(c: Context<HonoEnv>, metrics: Metrics): C<CacheNamespa
     stores.push(cloudflare);
   }
 
-  const emetricsMiddleware = withMetrics(metrics);
+  const metricsMiddleware = withMetrics(metrics);
 
-  const storesWithMetrics = stores.map((s) => emetricsMiddleware.wrap(s));
+  const storesWithMetrics = stores.map((s) => metricsMiddleware.wrap(s));
 
   const defaultOpts = {
     stores: storesWithMetrics,
