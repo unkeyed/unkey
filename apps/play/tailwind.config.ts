ae22e49085a871f2f56bcb031774bebf42e23c61
Guilherme Rodz <gui.rodz.dev@gmail.com>
2024-06-23 16:53:14 -0300

    chore(play): change default border width to .5px

--- apps/play/tailwind.config.ts
+++ apps/play/tailwind.config.ts
@@ -18,6 +18,9 @@ const config = {
       },
     },
     extend: {
+      borderWidth: {
+        DEFAULT: ".75px",
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
