ae5740d0dc9ae5dae783e0d34a3ed58bc4ef57af
chronark <dev@chronark.com>
2024-06-30 20:10:32 +0200

    fix(agent): decrease max_unavailable to 1 for canary deployment strategy refactor(agent): remove defer statement for stopping time ticker in Every function

--- apps/agent/pkg/repeat/every.go
+++ apps/agent/pkg/repeat/every.go
@@ -2,10 +2,10 @@ package repeat
 
 import "time"
 
+// Every runs the given function in a go routine every d duration until the returned function is called.
 func Every(d time.Duration, fn func()) func() {
 	stop := make(chan struct{})
 	t := time.NewTicker(d)
-	defer t.Stop()
 	go func() {
 		for {
 
