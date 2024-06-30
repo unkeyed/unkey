64771c0c2bf4278fdd6f54e48a04edd1aef3a61c
chronark <dev@chronark.com>
2024-06-30 20:11:25 +0200

    refactor(every.go): simplify Every function to use range over ticker.C fix(every.go): replace stop channel with stopping the ticker to properly stop the routine

--- apps/agent/pkg/repeat/every.go
+++ apps/agent/pkg/repeat/every.go
@@ -4,20 +4,13 @@ import "time"
 
 // Every runs the given function in a go routine every d duration until the returned function is called.
 func Every(d time.Duration, fn func()) func() {
-	stop := make(chan struct{})
 	t := time.NewTicker(d)
 	go func() {
-		for {
-
-			select {
-			case <-t.C:
-				fn()
-			case <-stop:
-				return
-			}
+		for range t.C {
+			fn()
 		}
 	}()
 	return func() {
-		stop <- struct{}{}
+		t.Stop()
 	}
 }
