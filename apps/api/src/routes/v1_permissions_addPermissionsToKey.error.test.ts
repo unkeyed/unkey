e12dadae776ddb3e0d8b96016c099e71c1f22f66
chronark <dev@chronark.com>
2024-07-09 13:37:42 +0200

    feat(api): add tests for error cases in adding permissions to key feat(api): add tests for successful cases in adding permissions to key feat(api): add security tests for adding permissions to key
    feat(api): add route for adding permissions to a key in v1_permissions
    
    feat(api): add route for adding permissions to a key in v1_permissions

---
+++ apps/api/src/routes/v1_permissions_addPermissionsToKey.error.test.ts
@@ -0,0 +1,66 @@
+import { expect, test } from "vitest";
+
+import { IntegrationHarness } from "src/pkg/testutil/integration-harness";
+
+import type {
+  V1PermissionsAddPermissionsToKeyRequest,
+  V1PermissionsAddPermissionsToKeyResponse,
+} from "./v1_permissions_addPermissionsToKey";
+
+test("empty keyId", async (t) => {
+  const h = await IntegrationHarness.init(t);
+  const { key: rootKey } = await h.createRootKey(["*"]);
+
+  const res = await h.post<
+    V1PermissionsAddPermissionsToKeyRequest,
+    V1PermissionsAddPermissionsToKeyResponse
+  >({
+    url: "/v1/permissions.addPermissionsToKey",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${rootKey}`,
+    },
+    body: {
+      keyId: "",
+      permissions: [{ id: "perm_123" }],
+    },
+  });
+
+  expect(res.status).toEqual(400);
+  expect(res.body).toMatchObject({
+    error: {
+      code: "BAD_REQUEST",
+      docs: "https://unkey.dev/docs/api-reference/errors/code/BAD_REQUEST",
+      message: "keyId: String must contain at least 1 character(s)",
+    },
+  });
+});
+
+test("empty permissions", async (t) => {
+  const h = await IntegrationHarness.init(t);
+  const { key: rootKey } = await h.createRootKey(["*"]);
+
+  const res = await h.post<
+    V1PermissionsAddPermissionsToKeyRequest,
+    V1PermissionsAddPermissionsToKeyResponse
+  >({
+    url: "/v1/permissions.addPermissionsToKey",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${rootKey}`,
+    },
+    body: {
+      keyId: "key_123",
+      permissions: [],
+    },
+  });
+
+  expect(res.status).toEqual(400);
+  expect(res.body).toMatchObject({
+    error: {
+      code: "BAD_REQUEST",
+      docs: "https://unkey.dev/docs/api-reference/errors/code/BAD_REQUEST",
+      message: "permissions: Array must contain at least 1 element(s)",
+    },
+  });
+});
