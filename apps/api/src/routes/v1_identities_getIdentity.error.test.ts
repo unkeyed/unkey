430f61f5e33bf34068aaa30a788edbd3159df300
chronark <dev@chronark.com>
2024-07-17 10:22:40 +0200

    test(v1_identities_getIdentity.error.test.ts): update root key permissions to only allow read_identity for identity resource.

--- apps/api/src/routes/v1_identities_getIdentity.error.test.ts
+++ apps/api/src/routes/v1_identities_getIdentity.error.test.ts
@@ -9,7 +9,7 @@ test("identity does not exist", async (t) => {
   const h = await IntegrationHarness.init(t);
   const identityId = newId("test");
 
-  const root = await h.createRootKey(["*"]);
+  const root = await h.createRootKey(["identity.*.read_identity"]);
 
   const res = await h.get<V1IdentitiesGetIdentityResponse>({
     url: `/v1/identities.getIdentity?identityId=${identityId}`,
