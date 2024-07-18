3f79a62727e65ca0a80f7b4f163a72c27b065d74
James Perkins <james@unkey.dev>
2024-07-18 09:51:23 -0400

    Add ability to use unoptimized and pass alt

--- apps/www/components/blog/blog-image.tsx
+++ apps/www/components/blog/blog-image.tsx
@@ -5,12 +5,20 @@ export function BlogImage({
 }: {
   size: "sm" | "md" | "lg";
   className?: string;
+  unoptimized?: boolean;
   imageUrl: {
-    src?: string;
+    src: string;
     alt?: string;
   };
 }) {
   return (
-    <ImageWithBlur src={imageUrl.src!} width={1920} height={1080} alt="" className="rounded-md" />
+    <ImageWithBlur
+      src={imageUrl.src}
+      width={1920}
+      unoptimized
+      height={1080}
+      alt={imageUrl.alt || ""}
+      className="rounded-md"
+    />
   );
 }
