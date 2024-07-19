c195dc4300732f13114108d2886116a5aa332912
chronark <dev@chronark.com>
2024-07-19 21:00:02 +0200

    fix(ratelimit): remove commented out code in flushPushPull function feat(cache): update MemoryStoreConfig to accept frequency and maxItems for unstableEvictOnSet

--- packages/cache/src/stores/memory.ts
+++ packages/cache/src/stores/memory.ts
@@ -8,15 +8,24 @@ export type MemoryStoreConfig<TValue> = {
    *
    * This flag is unstable and may be changed or removed later.
    *
-   * Provide a number between 0 and 1 to calculate whether eviction should run on each set.
-   * @example
-   * ```
-   * 1 -> run eviction on every `set`
-   * 0.5 -> run eviction on every 2nd `set`
-   * 0 or undefined -> disable
    * ```
    */
-  unstableEvictOnSet?: number;
+  unstableEvictOnSet?: {
+    /**
+     * Provide a number between 0 and 1 to calculate whether eviction should run on each set.
+     * @example
+     * ```
+     * 1 -> run eviction on every `set`
+     * 0.5 -> run eviction on every 2nd `set`
+     * 0 or undefined -> disable
+     */
+    frequency: number;
+
+    /**
+     * Remove items until the number of items in the map is lower than `maxItems`
+     */
+    maxItems: number;
+  };
   persistentMap: Map<string, TValue>;
 };
 
@@ -24,12 +33,13 @@ export class MemoryStore<TNamespace extends string, TValue = any>
   implements Store<TNamespace, TValue>
 {
   private readonly state: Map<string, { expires: number; entry: Entry<TValue> }>;
-  private readonly unstableEvictOnSet: number;
+  private readonly unstableEvictOnSet?: { frequency: number; maxItems: number };
+
   public readonly name = "memory";
 
   constructor(config: MemoryStoreConfig<{ expires: number; entry: Entry<TValue> }>) {
     this.state = config.persistentMap;
-    this.unstableEvictOnSet = config.unstableEvictOnSet ?? 0;
+    this.unstableEvictOnSet = config.unstableEvictOnSet;
   }
 
   private buildCacheKey(namespace: TNamespace, key: string): string {
@@ -60,13 +70,27 @@ export class MemoryStore<TNamespace extends string, TValue = any>
       entry,
     });
 
-    if (this.unstableEvictOnSet > 0 && Math.random() < this.unstableEvictOnSet) {
+    if (this.unstableEvictOnSet && Math.random() < this.unstableEvictOnSet.frequency) {
+      /**
+       * First delete all the expired ones
+       */
       const now = Date.now();
       this.state.forEach((value, key, map) => {
         if (value.expires < now) {
           map.delete(key);
         }
       });
+
+      /**
+       * If that's not enough, we delete oldest first
+       */
+      for (const [k] of this.state) {
+        if (this.state.size <= this.unstableEvictOnSet!.maxItems) {
+          break;
+        }
+
+        this.state.delete(k);
+      }
     }
 
     return Promise.resolve(Ok());
