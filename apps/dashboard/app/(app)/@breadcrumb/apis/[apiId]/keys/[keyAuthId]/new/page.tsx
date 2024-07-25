a8962a1306c3b3d339feaeebd2ff574303346252
chronark <dev@chronark.com>
2024-07-25 10:00:38 +0200

    fix: add create-new-key page again

---
+++ apps/dashboard/app/(app)/@breadcrumb/apis/[apiId]/keys/[keyAuthId]/new/page.tsx
@@ -0,0 +1,62 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+import { getTenantId } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { unstable_cache as cache } from "next/cache";
+import { redirect } from "next/navigation";
+// export const dynamic = "force-dynamic";
+// export const runtime = "edge";
+
+export default async function PageBreadcrumb(props: {
+  params: { apiId: string; keyAuthId: string; keyId: string };
+}) {
+  const tenantId = getTenantId();
+
+  const getApiById = cache(
+    async (apiId: string) =>
+      db.query.apis.findFirst({
+        where: (table, { eq, and, isNull }) => and(eq(table.id, apiId), isNull(table.deletedAt)),
+
+        with: {
+          workspace: true,
+        },
+      }),
+    ["apiById"],
+  );
+
+  const api = await getApiById(props.params.apiId);
+  if (!api || api.workspace.tenantId !== tenantId) {
+    return null;
+  }
+
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/apis">APIs</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbLink href={`/apis/${props.params.apiId}`}>{api.name}</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbLink href={`/apis/${props.params.apiId}/keys/${props.params.keyAuthId}`}>
+            Keys
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbPage>Create new key</BreadcrumbPage>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+}
---
+++ apps/dashboard/app/(app)/apis/[apiId]/keys/[keyAuthId]/new/page.tsx
@@ -0,0 +1,27 @@
+import { getTenantId } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { notFound } from "next/navigation";
+import { CreateKey } from "./client";
+
+export default async function CreateKeypage(props: {
+  params: {
+    apiId: string;
+    keyAuthId: string;
+  };
+}) {
+  const tenantId = getTenantId();
+
+  const keyAuth = await db.query.keyAuth.findFirst({
+    where: (table, { eq, and, isNull }) =>
+      and(eq(table.id, props.params.keyAuthId), isNull(table.deletedAt)),
+    with: {
+      workspace: true,
+      api: true,
+    },
+  });
+  if (!keyAuth || keyAuth.workspace.tenantId !== tenantId) {
+    return notFound();
+  }
+
+  return <CreateKey keyAuthId={keyAuth.id} />;
+}
