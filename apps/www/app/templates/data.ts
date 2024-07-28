786e8e1aee5c342f498f2a22d9181da721d7d80f
chronark <dev@chronark.com>
2024-07-28 10:54:33 +0200

    docs: keys in snippet template

--- apps/www/app/templates/data.ts
+++ apps/www/app/templates/data.ts
@@ -40,6 +40,17 @@ export type Template = {
 };
 
 export const templates: Record<string, Template> = {
+  "docs-with-keys": {
+    title: "Documentation with embedded api keys",
+    description: "Embed api keys in your documentation for easy copy-pasting",
+    authors: ["chronark"],
+    repository: "https://github.com/unkeyed/examples/tree/main/docs-with-real-keys",
+    image: "/templates/docs-with-real-keys.png",
+    readmeUrl:
+      "https://raw.githubusercontent.com/unkeyed/examples/main/docs-with-real-keys/README.md",
+    language: "Typescript",
+    framework: "Next.js",
+  },
   "license-keys-nextjs": {
     title: "License keys for selfhosting",
     description: "Protect your Next.js routes with license keys at runtime",
