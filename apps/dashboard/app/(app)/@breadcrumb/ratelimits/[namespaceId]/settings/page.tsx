054460c861b6aeef3fd015509b4c1da3a2fa78f3
Guilherme Rodz <gui.rodz.dev@gmail.com>
2024-07-31 05:28:19 -0300

    chore(dashboard/ratelimit): add breadcrumb

---
+++ apps/dashboard/app/(app)/@breadcrumb/ratelimits/[namespaceId]/settings/page.tsx
@@ -0,0 +1,65 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+import { BreadcrumbSkeleton } from "@/components/dashboard/breadcrumb-skeleton";
+import { getTenantId } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { Suspense } from "react";
+
+export const dynamic = "force-dynamic";
+export const runtime = "edge";
+
+type PageProps = {
+  params: { namespaceId: string };
+};
+
+async function AsyncPageBreadcrumb(props: PageProps) {
+  const tenantId = getTenantId();
+
+  const namespace = await db.query.ratelimitNamespaces.findFirst({
+    where: (table, { eq, and, isNull }) =>
+      and(eq(table.id, props.params.namespaceId), isNull(table.deletedAt)),
+    with: {
+      workspace: {
+        columns: {
+          tenantId: true,
+        },
+      },
+    },
+  });
+  if (!namespace || namespace.workspace.tenantId !== tenantId) {
+    return null;
+  }
+
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/ratelimits">Ratelimits</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbLink href={`/ratelimits/${props.params.namespaceId}`}>{namespace.name}</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbPage>Settings</BreadcrumbPage>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+}
+
+export default function PageBreadcrumb(props: PageProps) {
+  return (
+    <Suspense fallback={<BreadcrumbSkeleton levels={3} />}>
+      <AsyncPageBreadcrumb {...props} />
+    </Suspense>
+  );
+}
