3e305bc19e859178bf2b3f412937a87647d85170
chronark <dev@chronark.com>
2024-07-31 20:13:13 +0200

    test(v1_identities_updateIdentity.happy.test.ts): add test case for handling hundreds of keys in ratelimits array

--- apps/api/src/routes/v1_identities_updateIdentity.happy.test.ts
+++ apps/api/src/routes/v1_identities_updateIdentity.happy.test.ts
@@ -132,6 +132,72 @@ test("sets new ratelimits", async (t) => {
   }
 });
 
+
+
+test("works with hundreds of keys", async (t) => {
+  const h = await IntegrationHarness.init(t);
+  const root = await h.createRootKey(["identity.*.update_identity"]);
+
+  const identity = {
+    id: newId("test"),
+    workspaceId: h.resources.userWorkspace.id,
+    externalId: randomUUID(),
+  };
+
+  await h.db.primary.insert(schema.identities).values(identity);
+
+  for (let i = 0; i < 200; i++) {
+    await h.createKey({ identityId: identity.id });
+  }
+
+  await h.db.primary.insert(schema.ratelimits).values({
+    id: newId("test"),
+    identityId: identity.id,
+    name: randomUUID(),
+    limit: 123123,
+    duration: 131251,
+    workspaceId: h.resources.userWorkspace.id,
+  });
+
+  const ratelimits = [
+    {
+      name: randomUUID(),
+      limit: 10,
+      duration: 1000,
+    },
+    {
+      name: randomUUID(),
+      limit: 1000,
+      duration: 11111111,
+    },
+  ];
+
+  const res = await h.post<V1IdentitiesUpdateIdentityRequest, V1IdentitiesUpdateIdentityResponse>({
+    url: "/v1/identities.updateIdentity",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${root.key}`,
+    },
+    body: {
+      identityId: identity.id,
+      ratelimits,
+    },
+  });
+
+  expect(res.status, `expected 200, received: ${JSON.stringify(res, null, 2)}`).toBe(200);
+
+  const found = await h.db.primary.query.ratelimits.findMany({
+    where: (table, { eq }) => eq(table.identityId, identity.id),
+  });
+
+  expect(found.length).toBe(ratelimits.length);
+  for (const rl of ratelimits) {
+    expect(
+      found.some((f) => f.name === rl.name && f.limit === rl.limit && f.duration === rl.duration),
+    );
+  }
+});
+
 test("updates meta", async (t) => {
   const h = await IntegrationHarness.init(t);
   const root = await h.createRootKey(["identity.*.update_identity"]);
