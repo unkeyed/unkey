d8d2c0b68f6372c273ed8e4dce0de9db5b939300
chronark <dev@chronark.com>
2024-07-31 15:45:15 +0200

    feat(server.go): add metrics recording for HTTP requests in New function

--- apps/agent/pkg/api/server.go
+++ apps/agent/pkg/api/server.go
@@ -49,11 +49,22 @@ func New(config Config) *Server {
 
 	s.api.UseMiddleware(func(hCtx huma.Context, next func(huma.Context)) {
 
+		start := time.Now()
+
 		s.logger.Info().Str("method", hCtx.Method()).Str("path", hCtx.URL().Path).Msg("request")
 		ctx, span := tracing.Start(hCtx.Context(), "api.request")
 		defer span.End()
 
 		next(huma.WithContext(hCtx, ctx))
+
+		s.metrics.Record(metrics.HttpRequest{
+			Method:         hCtx.Method(),
+			Path:           hCtx.URL().Path,
+			ServiceLatency: time.Since(start).Milliseconds(),
+			UserAgent:      hCtx.Header("user-agent"),
+			RemoteAddr:     hCtx.RemoteAddr(),
+			SourceIP:       hCtx.Header("Fly-Client-IP"),
+		})
 	})
 
 	return s
