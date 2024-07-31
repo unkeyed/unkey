4ec084bef38816fc7c08cda6d5d5c31dfc0e2667
Guilherme Rodz <gui.rodz.dev@gmail.com>
2024-07-31 04:13:25 -0300

    chore(dashboard/breadcrumb): use react key on fragment of skeleton

--- apps/dashboard/components/dashboard/breadcrumb-skeleton.tsx
+++ apps/dashboard/components/dashboard/breadcrumb-skeleton.tsx
@@ -1,15 +1,16 @@
 import { Breadcrumb, BreadcrumbList, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
 import { Skeleton } from "../ui/skeleton";
+import { Fragment } from "react";
 
 export function BreadcrumbSkeleton(props: { levels: number }) {
   return (
     <Breadcrumb>
       <BreadcrumbList>
         {Array.from({ length: props.levels }).map((_, idx) => (
-          <>
+          <Fragment key={String(idx)}>
             <Skeleton className="w-10 h-4 rounded-md" />
             {idx < props.levels - 1 && <BreadcrumbSeparator />}
-          </>
+          </Fragment>
         ))}
       </BreadcrumbList>
     </Breadcrumb>
