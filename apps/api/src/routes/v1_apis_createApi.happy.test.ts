375396288ee49f51587a21a8d76aca7296f9885a
chronark <dev@chronark.com>
2024-08-05 23:44:26 +0200

    test(v1_apis_createApi.happy.test.ts): remove redundant test case for creating new api without delete protection

--- apps/api/src/routes/v1_apis_createApi.happy.test.ts
+++ apps/api/src/routes/v1_apis_createApi.happy.test.ts
@@ -25,28 +25,5 @@ test("creates new api", async (t) => {
     where: (table, { eq }) => eq(table.id, res.body.apiId),
   });
   expect(found).toBeDefined();
-  expect(found!.deleteProtection).toBe(true);
-});
-
-test("creates new api without delete protection", async (t) => {
-  const h = await IntegrationHarness.init(t);
-  const root = await h.createRootKey(["api.*.create_api"]);
-  const res = await h.post<V1ApisCreateApiRequest, V1ApisCreateApiResponse>({
-    url: "/v1/apis.createApi",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${root.key}`,
-    },
-    body: {
-      name: randomUUID(),
-    },
-  });
-
-  expect(res.status, `expected 200, received: ${JSON.stringify(res, null, 2)}`).toBe(200);
-
-  const found = await h.db.primary.query.apis.findFirst({
-    where: (table, { eq }) => eq(table.id, res.body.apiId),
-  });
-  expect(found).toBeDefined();
   expect(found!.deleteProtection).toBe(false);
 });
