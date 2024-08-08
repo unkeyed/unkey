86a6db970c34e017ec7e93d812f1815f61be0820
chronark <dev@chronark.com>
2024-08-08 12:36:27 +0200

    chore: log path in metrics

--- apps/agent/pkg/prometheus/metrics.go
+++ apps/agent/pkg/prometheus/metrics.go
@@ -12,12 +12,12 @@ var (
 		Name:      "requests_total",
 	}, []string{"method", "path", "status"})
 
-	ServiceLatency = promauto.NewHistogram(prometheus.HistogramOpts{
+	ServiceLatency = promauto.NewHistogramVec(prometheus.HistogramOpts{
 		Namespace: "agent",
 		Subsystem: "http",
 		Name:      "service_latency",
 		Buckets:   []float64{0.001, 0.005, 0.01, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10},
-	})
+	}, []string{"path"})
 	ClusterSize = promauto.NewGauge(prometheus.GaugeOpts{
 		Namespace: "agent",
 		Subsystem: "cluster",
