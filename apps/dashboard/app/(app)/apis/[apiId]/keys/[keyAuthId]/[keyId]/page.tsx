4d2c877f58c17cc980c5210ee2dd1cbc52921993
Sreeharsha Raveendra <dev.sreeharsha@gmail.com>
2024-08-19 16:43:08 +0000

    fix(#1989): Optimize Key Details UI for mobile
    - Refactor Metric component to prevent overflow
    - Implement responsive grid layout in parent component

--- apps/dashboard/app/(app)/apis/[apiId]/keys/[keyAuthId]/[keyId]/page.tsx
+++ apps/dashboard/app/(app)/apis/[apiId]/keys/[keyAuthId]/[keyId]/page.tsx
@@ -160,7 +160,7 @@ export default async function APIKeyDetailPage(props: {
 
       <div className="flex flex-col gap-4 mt-4">
         <Card>
-          <CardContent className="grid grid-cols-6 divide-x">
+          <CardContent className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6 gap-2 sm:divide-x">
             <Metric
               label={key.expires && key.expires.getTime() < Date.now() ? "Expired" : "Expires in"}
               value={key.expires ? ms(key.expires.getTime() - Date.now()) : <Minus />}
@@ -354,8 +354,8 @@ const Metric: React.FC<{
 }> = ({ label, value, tooltip }) => {
   const component = (
     <div className="flex flex-col items-start justify-center px-4 py-2">
-      <p className="text-sm text-content-subtle">{label}</p>
-      <div className="text-2xl font-semibold leading-none tracking-tight">{value}</div>
+      <p className="text-sm text-content-subtle truncate w-full">{label}</p>
+      <div className="text-2xl font-semibold leading-none tracking-tight truncate w-full">{value}</div>
     </div>
   );
 
