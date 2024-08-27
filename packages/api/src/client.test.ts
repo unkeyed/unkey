6e2f4fe8bc82254111f4ce77da2a3f8a75a1db16
Baylee Schmeisser <119542904+mxbaylee@users.noreply.github.com>
2024-08-27 16:36:20 -0700

    fix: 🌐 Enable correct handling of undefined in api package fetch. (#2036)
    * 🎒 Mark rbac as external, allowing tests to pass locally.
    
    * 🧪 Install `vitest` for apis client package.
    
    * 🌐 Enable correct handling of undefined in api package fetch.
    
    * 💱 Add changeset.
    
    * Revert "🎒 Mark rbac as external, allowing tests to pass locally."
    
    This reverts commit a20368b7577ad80e3b2ab8b410083d14052c3e2c.
    
    * 🧹 lintin'.
    
    * 🔑 Update lock file.
    
    * 🧹 lint.
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

---
+++ packages/api/src/client.test.ts
@@ -0,0 +1,14 @@
+import { describe, expect, test } from "vitest";
+import { Unkey } from "./client";
+
+describe("client", () => {
+  test("fetch can encode the params without throwing", async () => {
+    const unkey = new Unkey({ token: "rawr" });
+    expect(() => {
+      unkey.apis.listKeys({
+        apiId: "meow",
+        cursor: undefined,
+      });
+    }).not.toThrow();
+  });
+});
