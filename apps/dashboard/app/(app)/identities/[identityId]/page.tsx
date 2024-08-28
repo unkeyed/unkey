a17034c7c81ec80577d4eadf9bc1630f61b2be26
chronark <dev@chronark.com>
2024-08-28 20:54:24 +0200

    feat: better empty states

--- apps/dashboard/app/(app)/identities/[identityId]/page.tsx
+++ apps/dashboard/app/(app)/identities/[identityId]/page.tsx
@@ -125,8 +125,11 @@ export default async function Page(props: Props) {
               <TableRow>
                 <TableCell className="font-mono">{key.id}</TableCell>
                 <TableCell className="font-mono text-xs">
-                  {key.meta ?
-                    JSON.stringify(JSON.parse(key.meta), null, 2) : <Minus className="text-content-subtle w-4 h-4" />}
+                  {key.meta ? (
+                    JSON.stringify(JSON.parse(key.meta), null, 2)
+                  ) : (
+                    <Minus className="text-content-subtle w-4 h-4" />
+                  )}
                 </TableCell>
                 <LastUsed keyId={key.id} />
               </TableRow>
