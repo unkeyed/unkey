85627bb0babb1ba35780be0f6387e2d2972da98f
chronark <dev@chronark.com>
2024-09-17 17:23:10 +0200

    revert

--- apps/api/src/routes/v1_keys_verifyKey.ratelimit_accuracy.test.ts
+++ apps/api/src/routes/v1_keys_verifyKey.ratelimit_accuracy.test.ts
@@ -94,8 +94,9 @@ for (const { limit, duration, rps, seconds } of testCases) {
 
     const exactLimit = Math.min(results.length, (limit / (duration / 1000)) * seconds);
     const upperLimit = Math.round(exactLimit * 2.5);
-    console.info({ name, passed, exactLimit, upperLimit });
-    t.expect(passed).toBeGreaterThanOrEqual(exactLimit);
+    const lowerLimit = Math.round(exactLimit * 0.75);
+    console.info({ name, passed, exactLimit, upperLimit, lowerLimit });
+    t.expect(passed).toBeGreaterThanOrEqual(lowerLimit);
     t.expect(passed).toBeLessThanOrEqual(upperLimit);
   });
 }
