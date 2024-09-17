6e0114c0eb1da440fa3d86d36018b328c1fef226
chronark <dev@chronark.com>
2024-09-17 17:21:11 +0200

    fix: default ratelimits

--- apps/api/src/routes/v1_keys_verifyKey.ratelimit_accuracy.test.ts
+++ apps/api/src/routes/v1_keys_verifyKey.ratelimit_accuracy.test.ts
@@ -94,9 +94,8 @@ for (const { limit, duration, rps, seconds } of testCases) {
 
     const exactLimit = Math.min(results.length, (limit / (duration / 1000)) * seconds);
     const upperLimit = Math.round(exactLimit * 2.5);
-    const lowerLimit = Math.round(exactLimit * 0.75);
-    console.info({ name, passed, exactLimit, upperLimit, lowerLimit });
-    t.expect(passed).toBeGreaterThanOrEqual(lowerLimit);
+    console.info({ name, passed, exactLimit, upperLimit });
+    t.expect(passed).toBeGreaterThanOrEqual(exactLimit);
     t.expect(passed).toBeLessThanOrEqual(upperLimit);
   });
 }
