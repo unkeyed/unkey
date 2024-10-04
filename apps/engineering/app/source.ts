9813622998a17835c022b4ee4c57be6e8864604e
chronark <dev@chronark.com>
2024-10-04 12:51:42 +0200

    feat: move eng docs to fuma

---
+++ apps/engineering/app/source.ts
@@ -0,0 +1,8 @@
+import { docs, meta } from "@/.source";
+import { loader } from "fumadocs-core/source";
+import { createMDXSource } from "fumadocs-mdx";
+
+export const source = loader({
+  baseUrl: "/docs",
+  source: createMDXSource(docs, meta),
+});
