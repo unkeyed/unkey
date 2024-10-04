31826613cf2966ca6cdd632a4c6e0b874a146896
Akshay <100031493+AkshayBandi027@users.noreply.github.com>
2024-10-04 15:19:06 +0530

    fix: dashboard icon flickering (#2170)
    * fix: dashboard-icons-flickering
    
    * ran format

--- apps/dashboard/app/(app)/authorization/permissions/page.tsx
+++ apps/dashboard/app/(app)/authorization/permissions/page.tsx
@@ -3,11 +3,11 @@ import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { getTenantId } from "@/lib/auth";
 import { asc, db } from "@/lib/db";
+import { permissions } from "@unkey/db/src/schema";
 import { ChevronRight, Scan } from "lucide-react";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import { CreateNewPermission } from "./create-new-permission";
-import { permissions } from "@unkey/db/src/schema";
 
 export const revalidate = 0;
 
@@ -19,7 +19,7 @@ export default async function RolesPage() {
       and(eq(table.tenantId, tenantId), isNull(table.deletedAt)),
     with: {
       permissions: {
-        orderBy:[asc(permissions.name)],
+        orderBy: [asc(permissions.name)],
         with: {
           keys: {
             with: {
