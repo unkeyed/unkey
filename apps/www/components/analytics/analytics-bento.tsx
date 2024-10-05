36a8f72f9eafa6cb48d4fb3a3cfe697deaa04fba
Nazar Poshtarenko <32395926+unrenamed@users.noreply.github.com>
2024-10-05 22:05:19 +0300

    fix(www): add missing langs to analytics bento (#2214)
    * fix(www): add missing langs to analytics bento
    
    Also, add a copy button to the code editor for one-click code copying, removing the need for manual selection.
    
    * refactor(www): extract langs SVG icons into separate file
    
    Also, rename and move <Editor /> components to components/ui folder.
    
    * refactor(www): extract copy code snippet button to separate file
    
    * refactor(www): apply coderabbitai review comments
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- apps/www/components/analytics/analytics-bento.tsx
+++ apps/www/components/analytics/analytics-bento.tsx
@@ -1,19 +1,35 @@
 "use client";
+import type { LangIconProps } from "@/components/svg/lang-icons";
+import { CurlIcon, GoIcon, PythonIcon, RustIcon, TSIcon } from "@/components/svg/lang-icons";
+import { CopyCodeSnippetButton } from "@/components/ui/copy-code-button";
 import { cn } from "@/lib/utils";
 import { motion } from "framer-motion";
 import { Wand2 } from "lucide-react";
-import { Highlight, type PrismTheme } from "prism-react-renderer";
+import type { PrismTheme } from "prism-react-renderer";
 import { useState } from "react";
 import { PrimaryButton } from "../button";
 import { AnalyticsStars } from "../svg/analytics-stars";
 import { WebAppLight } from "../svg/web-app-light";
+import { CodeEditor } from "../ui/code-editor";
 
-export const theme = {
+const theme = {
   plain: {
     color: "#F8F8F2",
     backgroundColor: "#282A36",
   },
   styles: [
+    {
+      types: ["keyword"],
+      style: {
+        color: "#9D72FF",
+      },
+    },
+    {
+      types: ["function"],
+      style: {
+        color: "#FB3186",
+      },
+    },
     {
       types: ["string"],
       style: {
@@ -32,27 +48,14 @@ export const theme = {
         color: "#FB3186",
       },
     },
+    {
+      types: ["comment"],
+      style: {
+        color: "#4D4D4D",
+      },
+    },
   ],
 } satisfies PrismTheme;
-
-const codeBlock = `curl --request GET \\
-    --url https://api.unkey.dev/v1/keys.getKey \\
-    --header 'Authorization: <authorization>'
-    {    
-      "apiId": "api_1234",
-      "createdAt": 123,
-      "deletedAt": 123,
-      "expires": 123,
-      "id": "key_1234",
-      "meta": {
-        "roles": [
-          "admin",
-          "user"
-        ],
-        "stripeCustomerId": "cus_1234"
-      }
-    }
-`;
 
 export function AnalyticsBento() {
   const [showApi, toggleShowApi] = useState(false);
@@ -76,7 +79,109 @@ export function AnalyticsBento() {
   );
 }
 
+type Language = {
+  name: string;
+  Icon: React.FC<LangIconProps>;
+  codeBlock: string;
+};
+
+type LanguageName = "TypeScript" | "Python" | "Rust" | "Go" | "cURL";
+
+const curlCodeBlock = `curl --request GET \\
+    --url https://api.unkey.dev/v1/keys.getKey?keyId=key_123 \\
+    --header 'Authorization: Bearer <UNKEY_ROOT_KEY>'
+`;
+
+const tsCodeBlock = `import { Unkey } from "@unkey/api";
+
+const unkey = new Unkey({ rootKey: "<UNKEY_ROOT_KEY>" });
+
+const { result, error } = await unkey.keys.get({ keyId: "key_123" });
+
+if ( error ) {
+  // handle network error
+}
+
+// handle request
+`;
+
+const pythonCodeBlock = `import asyncio
+import os
+import unkey
+
+async def main() -> None:
+    client = unkey.Client("<UNKEY_ROOT_KEY>")
+    await client.start()
+    
+    result = await client.keys.get_key("key_123")
+  
+    if result.is_ok:
+        data = result.unwrap()
+        print(data.id)
+    else:
+        print(result.unwrap_err())
+    
+    await client.close()
+`;
+
+const goCodeBlock = `package main
+
+import (
+	"context"
+	"log"
+
+	unkeygo "github.com/unkeyed/unkey-go"
+	"github.com/unkeyed/unkey-go/models/operations"
+)
+
+func main() {
+	s := unkeygo.New(
+		unkeygo.WithSecurity("<UNKEY_ROOT_KEY>"),
+	)
+
+	ctx := context.Background()
+	res, err := s.Keys.GetKey(ctx, operations.GetKeyRequest{
+		KeyID: "key_123",
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	if res.Key != nil {
+		// handle response
+	}
+}
+`;
+
+const rustCodeBlock = `use unkey::models::GetKeyRequest;
+use unkey::Client;
+
+async fn get_key() {
+    let c = Client::new("<UNKEY_ROOT_KEY>");
+    let req = GetKeyRequest::new("key_123");
+
+    match c.get_key(req).await {
+        Ok(res) => println!("{res:?}"),
+        Err(err) => eprintln!("{err:?}"),
+    }
+}
+`;
+
+const languagesList = {
+  cURL: { Icon: CurlIcon, name: "cURL", codeBlock: curlCodeBlock, editorLanguage: "tsx" },
+  TypeScript: { Icon: TSIcon, name: "TypeScript", codeBlock: tsCodeBlock, editorLanguage: "tsx" },
+  Python: {
+    Icon: PythonIcon,
+    name: "Python",
+    codeBlock: pythonCodeBlock,
+    editorLanguage: "python",
+  },
+  Go: { Icon: GoIcon, name: "Go", codeBlock: goCodeBlock, editorLanguage: "go" },
+  Rust: { Icon: RustIcon, name: "Rust", codeBlock: rustCodeBlock, editorLanguage: "rust" },
+};
+
 function AnalyticsApiView() {
+  const [language, setLanguage] = useState<LanguageName>("cURL");
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -86,18 +191,59 @@ function AnalyticsApiView() {
       whileInView="visible"
       className="w-full overflow-x-hidden"
     >
-      <div className="w-full analytics-background-gradient bg-black bg-opacity-02 lg:w-[1220px] lg:rounded-3xl  xxl:mr-10 overflow-x-hidden overflow-y-hidden border-white/10 border border-b-0 border-l-0  border-r-0 flex-col md:flex-row relative rounded-tl-3xl h-[600px] xl:h-[576px] flex">
-        <div className="flex flex-col w-[216px] text-white text-sm pt-6 px-4 font-mono md:border-r md:border-white/5">
-          <div className="flex items-center cursor-pointer bg-white/5 py-1 px-2 rounded-lg w-[184px]">
-            <TerminalIcon className="w-6 h-6" />
-            <div className="ml-3">cURL</div>
-          </div>
-        </div>
-        <div className="flex w-full pt-4 pl-8 font-mono text-xs text-white sm:text-sm">
-          <Editor theme={theme} codeBlock={codeBlock} language="tsx" />
+      <div className="w-full analytics-background-gradient bg-black bg-opacity-02 lg:rounded-3xl xxl:mr-10 overflow-x-hidden overflow-y-hidden border-white/10 border border-b-0 border-l-0  border-r-0 flex-col md:flex-row relative rounded-tl-3xl h-[600px] xl:h-[576px] flex">
+        <LanguageSwitcher
+          languages={Object.values(languagesList)}
+          currentLanguage={language}
+          setLanguage={setLanguage}
+        />
+        <div className="flex pt-4 pb-8 pl-8 font-mono text-xs text-white sm:text-sm">
+          <CodeEditor
+            theme={theme}
+            codeBlock={languagesList[language].codeBlock}
+            language={languagesList[language].editorLanguage}
+          />
+          <CopyCodeSnippetButton
+            textToCopy={languagesList[language].codeBlock}
+            className="absolute hidden cursor-pointer top-5 right-5 lg:flex"
+          />
         </div>
       </div>
     </motion.div>
+  );
+}
+
+function LanguageSwitcher({
+  languages,
+  currentLanguage,
+  setLanguage,
+}: {
+  languages: Language[];
+  currentLanguage: LanguageName;
+  setLanguage: React.Dispatch<React.SetStateAction<LanguageName>>;
+}) {
+  return (
+    <div className="flex flex-col w-[216px] text-white text-sm pt-6 px-4 font-mono md:border-r md:border-white/5">
+      <div className="flex items-center space-x-2 sm:flex-col sm:space-x-0 sm:space-y-2">
+        {languages.map(({ Icon, name }) => (
+          <button
+            key={name}
+            type="button"
+            onClick={() => setLanguage(name as LanguageName)}
+            className={cn(
+              "flex items-center cursor-pointer bg-white/5 py-1 px-2 rounded-lg w-[184px]",
+              {
+                "bg-white/10 text-white": currentLanguage === name,
+                "text-white/40": currentLanguage !== name,
+              },
+            )}
+          >
+            <Icon active={currentLanguage === name} />
+            <div className="ml-3">{name}</div>
+          </button>
+        ))}
+      </div>
+    </div>
   );
 }
 
@@ -332,7 +478,7 @@ function AnalyticsWebAppView() {
       whileInView="visible"
       className="w-full overflow-x-hidden"
     >
-      <div className="w-full bg-[#000000] lg:w-[1220px] analytics-background-gradient lg:rounded-2xl relative flex-wrap md:flex-nowrap cursor-default select-none bg-opacity-02 xxl:mr-10 overflow-x-hidden overflow-y-hidden border-white/10 border border-b-0 border-l-0  border-r-0 flex-col md:flex-row relative rounded-tl-3xl h-[600px] xl:h-[576px] flex">
+      <div className="w-full bg-[#000000] lg:w-[1220px] analytics-background-gradient lg:rounded-2xl flex-wrap md:flex-nowrap cursor-default select-none bg-opacity-02 xxl:mr-10 overflow-x-hidden overflow-y-hidden border-white/10 border border-b-0 border-l-0  border-r-0 flex-col md:flex-row relative rounded-tl-3xl h-[600px] xl:h-[576px] flex">
         <WebAppLight className="absolute top-[-100px] left-[40px]" />
         <div className="flex flex-col rounded-2xl w-[216px] h-full text-white/20  text-xs pt-6 px-4 md:border-r md:border-white/5">
           <div className="flex justify-between items-center w-[160px]">
@@ -647,140 +793,6 @@ function AnalyticsWebAppView() {
         </div>
       </div>
     </motion.div>
-  );
-}
-
-export function Editor({
-  codeBlock,
-  language,
-  theme,
-}: { codeBlock: string; language: string; theme?: PrismTheme }) {
-  return (
-    <Highlight theme={theme} code={codeBlock} language={language}>
-      {({ tokens, getLineProps, getTokenProps }) => {
-        const amountLines = tokens.length;
-        const gutterPadLength = Math.max(String(amountLines).length, 2);
-        return (
-          <pre
-            key={codeBlock} // Use codeBlock as a key to trigger animations on change
-            className="leading-6"
-          >
-            {tokens.map((line, i) => {
-              const lineNumber = i + 1;
-              const paddedLineGutter = String(lineNumber).padStart(gutterPadLength, " ");
-              return (
-                // biome-ignore lint/suspicious/noArrayIndexKey: I got nothing better right now
-                <div key={`${line}-${i}`} {...getLineProps({ line })}>
-                  <span className="select-none line-number">{paddedLineGutter}</span>
-                  {line.map((token, key) => (
-                    <span key={`${key}-${token}`} {...getTokenProps({ token })} />
-                  ))}
-                </div>
-              );
-            })}
-          </pre>
-        );
-      }}
-    </Highlight>
-  );
-}
-
-export function TerminalIcon({ className }: { className?: string }) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      className={className}
-    >
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M17.9864 5.36932C18.2076 5.13794 18.5073 5.00771 18.82 5.0072V5C19.1328 5.00051 19.4326 5.13074 19.6538 5.36214C19.875 5.59352 19.9993 5.90721 20 6.23444C20 6.82321 19.606 7.28113 19.0871 7.41197L13.5224 17.3725C13.5623 17.5034 13.5973 17.6342 13.5973 17.7651C13.5968 18.0923 13.4725 18.406 13.2513 18.6374C13.0301 18.8688 12.7303 18.999 12.4175 18.9995C12.1049 18.999 11.8048 18.8688 11.5839 18.6374C11.3627 18.406 11.2382 18.0923 11.2377 17.7651C11.2377 17.1704 11.6478 16.6974 12.183 16.5797L17.7195 6.65246C17.6752 6.52294 17.6402 6.38687 17.6402 6.24164C17.6409 5.9144 17.7652 5.60072 17.9864 5.36932ZM18.3643 6.71855C18.4852 6.84504 18.649 6.91611 18.82 6.91611C18.991 6.91611 19.155 6.84504 19.2758 6.71855C19.3967 6.59207 19.4646 6.42052 19.4646 6.24164C19.4646 6.06275 19.3967 5.89121 19.2758 5.76472C19.155 5.63824 18.991 5.56718 18.82 5.56718C18.649 5.56718 18.4852 5.63824 18.3643 5.76472C18.2435 5.89121 18.1754 6.06275 18.1754 6.24164C18.1754 6.42052 18.2435 6.59207 18.3643 6.71855ZM11.9616 18.242C12.0827 18.3685 12.2465 18.4396 12.4175 18.4396C12.5885 18.4396 12.7525 18.3685 12.8734 18.242C12.9942 18.1155 13.0621 17.9439 13.0621 17.7651C13.0621 17.5862 12.9942 17.4146 12.8734 17.2882C12.7525 17.1617 12.5885 17.0906 12.4175 17.0906C12.2465 17.0906 12.0827 17.1617 11.9616 17.2882C11.8408 17.4146 11.7729 17.5862 11.7729 17.7651C11.7729 17.9439 11.8408 18.1155 11.9616 18.242Z"
-        fill="url(#paint0_linear_840_1992)"
-      />
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M12.8178 6.24152C12.8178 5.56123 13.3435 5.00929 13.9937 5.00708C14.3044 5.00825 14.6024 5.13751 14.8231 5.36688C15.044 5.59687 15.169 5.90888 15.1714 6.23497C15.1714 6.82374 14.7773 7.28166 14.2585 7.4125L8.69378 17.3732C8.73367 17.5039 8.76867 17.6347 8.76867 17.7656C8.7682 18.0929 8.64385 18.4065 8.42268 18.6379C8.20151 18.8693 7.90172 18.9995 7.58887 19C7.27601 18.9995 6.97622 18.8693 6.75505 18.6377C6.53388 18.4061 6.4093 18.0923 6.40906 17.765C6.40906 17.1703 6.81921 16.6973 7.3544 16.5795L12.8972 6.65235C12.8528 6.52282 12.8178 6.38675 12.8178 6.24152ZM13.5418 6.71844C13.6629 6.84493 13.8266 6.91598 13.9976 6.91598C14.1686 6.91598 14.3327 6.84493 14.4535 6.71844C14.5744 6.59195 14.6422 6.4204 14.6422 6.24152C14.6422 6.06264 14.5744 5.89108 14.4535 5.7646C14.3327 5.63811 14.1686 5.56705 13.9976 5.56705C13.8266 5.56705 13.6629 5.63811 13.5418 5.7646C13.4209 5.89108 13.353 6.06264 13.353 6.24152C13.353 6.4204 13.4209 6.59195 13.5418 6.71844ZM7.133 18.2425C7.25408 18.369 7.41786 18.4401 7.58887 18.4401C7.75988 18.4401 7.92389 18.369 8.04474 18.2425C8.16559 18.116 8.23348 17.9446 8.23348 17.7656C8.23348 17.5867 8.16559 17.4151 8.04474 17.2887C7.92389 17.1622 7.75988 17.0912 7.58887 17.0912C7.41786 17.0912 7.25408 17.1622 7.133 17.2887C7.01215 17.4151 6.94426 17.5867 6.94426 17.7656C6.94426 17.9446 7.01215 18.116 7.133 18.2425Z"
-        fill="url(#paint1_linear_840_1992)"
-      />
-      <path
-        d="M13.9937 5.00708C13.9951 5.00708 13.9962 5.00707 13.9976 5.00707H13.9913C13.992 5.00707 13.993 5.00708 13.9937 5.00708Z"
-        fill="url(#paint2_linear_840_1992)"
-      />
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M6.00732 10.5274C5.78615 10.7588 5.48636 10.889 5.1735 10.8895L5.1798 10.883C4.86718 10.8824 4.56715 10.7522 4.34622 10.5208C4.12505 10.2894 4.00047 9.97574 4 9.64851C4 9.0532 4.40384 8.58088 4.93787 8.46312C4.96377 8.45755 4.98896 8.45029 5.01416 8.44305C5.06572 8.42819 5.11704 8.41341 5.17304 8.41341C5.69376 8.41341 6.1221 8.7719 6.27842 9.26253C6.31784 9.39337 6.35354 9.52422 6.35354 9.65506C6.35284 9.98228 6.22849 10.296 6.00732 10.5274ZM5.62937 9.17814C5.50852 9.05165 5.34451 8.98059 5.1735 8.98059C5.00273 8.98059 4.83872 9.05165 4.71787 9.17814C4.59678 9.30461 4.52889 9.47617 4.52889 9.65506C4.52889 9.83392 4.59678 10.0055 4.71787 10.132C4.83872 10.2585 5.00273 10.3295 5.1735 10.3295C5.34451 10.3295 5.50852 10.2585 5.62937 10.132C5.75022 10.0055 5.81811 9.83392 5.81811 9.65506C5.81811 9.47617 5.75022 9.30461 5.62937 9.17814Z"
-        fill="url(#paint3_linear_840_1992)"
-      />
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M6.27842 13.5769C6.31784 13.7077 6.35354 13.8386 6.35354 13.9694L6.3596 13.9628C6.35914 14.2901 6.23479 14.6038 6.01362 14.8352C5.79245 15.0666 5.49266 15.1968 5.1798 15.1973C4.86718 15.1968 4.56715 15.0666 4.34622 14.8352C4.12505 14.6038 4.00047 14.2901 4 13.9628C4 13.3675 4.40384 12.8946 4.93787 12.7768C4.96423 12.7713 4.99013 12.764 5.01579 12.7567C5.06688 12.7422 5.11774 12.7277 5.17304 12.7277C5.69446 12.7277 6.1221 13.0862 6.27842 13.5769ZM4.71787 13.4925C4.59678 13.619 4.52889 13.7905 4.52889 13.9694C4.52889 14.1483 4.59678 14.3198 4.71787 14.4463C4.83872 14.5728 5.00273 14.6439 5.1735 14.6439C5.34451 14.6439 5.50852 14.5728 5.62937 14.4463C5.75022 14.3198 5.81811 14.1483 5.81811 13.9694C5.81811 13.7905 5.75022 13.619 5.62937 13.4925C5.50852 13.366 5.34451 13.2949 5.1735 13.2949C5.00273 13.2949 4.83872 13.366 4.71787 13.4925Z"
-        fill="url(#paint4_linear_840_1992)"
-      />
-      <defs>
-        <linearGradient
-          id="paint0_linear_840_1992"
-          x1="4.15606"
-          y1="3.49029"
-          x2="4.15606"
-          y2="19.8307"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop stopColor="white" stopOpacity="0.4" />
-          <stop offset="1" stopColor="white" />
-        </linearGradient>
-        <linearGradient
-          id="paint1_linear_840_1992"
-          x1="4.15606"
-          y1="3.49029"
-          x2="4.15606"
-          y2="19.8307"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop stopColor="white" stopOpacity="0.4" />
-          <stop offset="1" stopColor="white" />
-        </linearGradient>
-        <linearGradient
-          id="paint2_linear_840_1992"
-          x1="4.15606"
-          y1="3.49029"
-          x2="4.15606"
-          y2="19.8307"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop stopColor="white" stopOpacity="0.4" />
-          <stop offset="1" stopColor="white" />
-        </linearGradient>
-        <linearGradient
-          id="paint3_linear_840_1992"
-          x1="4.15606"
-          y1="3.49029"
-          x2="4.15606"
-          y2="19.8307"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop stopColor="white" stopOpacity="0.4" />
-          <stop offset="1" stopColor="white" />
-        </linearGradient>
-        <linearGradient
-          id="paint4_linear_840_1992"
-          x1="4.15606"
-          y1="3.49029"
-          x2="4.15606"
-          y2="19.8307"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop stopColor="white" stopOpacity="0.4" />
-          <stop offset="1" stopColor="white" />
-        </linearGradient>
-      </defs>
-    </svg>
   );
 }
 
