d55594d0a9769efc344d976ec50d53df1950921a
chronark <dev@chronark.com>
2024-10-10 11:16:35 +0200

    feat: add template

--- apps/www/app/templates/data.ts
+++ apps/www/app/templates/data.ts
@@ -12,6 +12,7 @@ export const frameworks = [
   "Django",
   "Axum",
   "Actix",
+  "Rocket",
 ] as const;
 export type Framework = StrArrayToUnion<typeof frameworks>;
 // id -> label
@@ -51,6 +52,17 @@ export type Template = {
 };
 
 export const templates: Record<string, Template> = {
+  "rust-rocket": {
+    title: "Secure your Rust Rocket API with Unkey",
+    description: "Generative AI REST API built with Rust and Rocket web framework with call quotas",
+    authors: ["unrenamed"],
+    repository: "https://github.com/unrenamed/unkey-rust-rocket",
+    image: "/images/templates/rust-rocket.png",
+    readmeUrl:
+      "https://raw.githubusercontent.com/unrenamed/unkey-rust-rocket/refs/heads/main/README.md",
+    language: "Rust",
+    framework: "Rocket",
+  },
   "rust-actix": {
     title: "Secure your Rust Actix API with Unkey",
     description: "A Rust API service using Unkey for API key validation with the Actix framework.",
