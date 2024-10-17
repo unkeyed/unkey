65c591ebe5b743c7b5f5743261eb20103ec26959
RajuGangitla <gangitlaraju8520@gmail.com>
2024-10-17 18:15:45 +0530

    fix: Enhance API Key Detail Page: Change Permissions Visualization to Tree Format (#2238)
    * fix:changed the permission view
    
    * fixed issue comments
    
    * [autofix.ci] apply automated fixes
    
    * removed font
    
    ---------
    
    Co-authored-by: Andreas Thomas <dev@chronark.com>
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- apps/workflows/app/layout.tsx
+++ apps/workflows/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
