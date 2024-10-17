685b3210189230ab8464677edf3353daf7f2bc1a
chronark <dev@chronark.com>
2024-10-17 11:28:14 +0200

    wip

--- apps/dashboard/pages/api/demo.ts
+++ apps/dashboard/pages/api/demo.ts
@@ -1,10 +1,10 @@
 import { execa } from "execa";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default async function (_req: NextApiRequest, res: NextApiResponse) {
+export default async function(_req: NextApiRequest, res: NextApiResponse) {
   await execa("bun", ["cli.deploy.ts"], {
     cwd: "../gateway",
-    env: { NODE_ENV: "development", FAKE_GIT_BRANCH_NAME: "main" },
+    env: { NODE_ENV: "development", FAKE_GIT_BRANCH_NAME: "deploy" },
   });
 
   res.send("ok");
