12add78dd4bc685a08b58b40921676e966a23932
Andreas Thomas <dev@chronark.com>
2024-10-17 11:41:07 +0200

    Update create-new-override.tsx

--- apps/dashboard/app/(app)/ratelimits/[namespaceId]/overrides/create-new-override.tsx
+++ apps/dashboard/app/(app)/ratelimits/[namespaceId]/overrides/create-new-override.tsx
@@ -29,7 +29,7 @@ import { z } from "zod";
 
 const formSchema = z.object({
   identifier: z.string().min(2).max(250),
-  limit: z.coerce.number().int().min(1).max(1_000),
+  limit: z.coerce.number().int().min(1).max(10_000),
   duration: z.coerce
     .number()
     .int()
