92b935a33036c1501100fd9a9cff1b76d8d482f4
chronark <dev@chronark.com>
2024-11-26 12:55:25 +0100

    refactor: move remaining-refills to cloudflare

--- internal/checkly/src/alert-channels.ts
+++ internal/checkly/src/alert-channels.ts
@@ -1,5 +1,6 @@
-import { WebhookAlertChannel } from "checkly/constructs";
+import { SlackAlertChannel, WebhookAlertChannel } from "checkly/constructs";
 
 // configured in the dashboard
 // https://app.checklyhq.com/alerts/settings/channels/edit/incidentio/218874
 export const incidentIo = WebhookAlertChannel.fromId("218874");
+export const slack = SlackAlertChannel.fromId("240275");
