0afac88ec6616d24c3d55c228be0e2fcad279927
burnedchris <christopher@everfund.com>
2024-11-26 23:52:08 +0000

    fix: :bug: Fixed bug on avatars, where if the image is not square it will look squashed, using object-cover should fix that

--- apps/dashboard/components/ui/avatar.tsx
+++ apps/dashboard/components/ui/avatar.tsx
@@ -23,7 +23,7 @@ const AvatarImage = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <AvatarPrimitive.Image
     ref={ref}
-    className={cn("aspect-square h-full w-full", className)}
+    className={cn("object-cover w-full h-full", className)}
     {...props}
   />
 ));
