09f81639421afd4c880e10cb609213d138c022e5
chronark <dev@chronark.com>
2024-11-28 17:40:16 +0100

    feat: button

--- apps/engineering/app/components/row.tsx
+++ apps/engineering/app/components/row.tsx
@@ -3,3 +3,5 @@ import type { PropsWithChildren } from "react";
 export const Row: React.FC<PropsWithChildren> = (props) => {
   return <div className="flex w-full items-center justify-around gap-8">{props.children}</div>;
 };
+
+Row.displayName = "Row";
