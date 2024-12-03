7793e1f33da0f6ec9eb79543c074a73509402109
chronark <dev@chronark.com>
2024-12-04 00:04:18 +0100

    feat: icons

--- internal/icons/src/icons/chevron-expand-y.tsx
+++ internal/icons/src/icons/chevron-expand-y.tsx
@@ -1,3 +1,15 @@
+/**
+ * Copyright © Nucleo
+ * Version 1.3, January 3, 2024
+ * Nucleo Icons
+ * https://nucleoapp.com/
+ * - Redistribution of icons is prohibited.
+ * - Icons are restricted for use only within the product they are bundled with.
+ *
+ * For more details:
+ * https://nucleoapp.com/license
+ */
+
 import type React from "react";
 
 import type { IconProps } from "../props";
