a37426761fe1b0a20d88cd310455f84ddbe53941
Michael Silva <michael@unkey.dev>
2024-12-04 11:48:09 -0500

    mdx file changes

---
+++ apps/engineering/content/design/components/id.width.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { RenderComponentWithSnippet } from "@/app/components/render";
+import { Id } from "@unkey/ui";
+
+export const WidthExample: React.FC = () => (
+  <RenderComponentWithSnippet>
+    <div className="w-full">
+      <Id value={"api_zTLSnw1YTqUHRwJTgKWrg"} truncate={20} />
+    </div>
+  </RenderComponentWithSnippet>
+);
