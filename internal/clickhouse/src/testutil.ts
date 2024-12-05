4ca5c30bff3424662286f33c0907dabbf3344b7b
Andreas Thomas <dev@chronark.com>
2024-12-05 15:04:40 +0100

    feat: ingesting analytics tags (#2698)
    * wip: analytics tags
    
    * [autofix.ci] apply automated fixes
    
    * feat: add verifications_per_hour_v2
    
    * feat: insert tags
    
    * docs: specify 10 tags per request
    
    * test: run serially cause gh actions suck
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- internal/clickhouse/src/testutil.ts
+++ internal/clickhouse/src/testutil.ts
@@ -30,7 +30,10 @@ export class ClickHouseContainer {
     await this.container.stop();
   }
 
-  static async start(t: TaskContext): Promise<ClickHouseContainer> {
+  static async start(
+    t: TaskContext,
+    opts?: { keepContainer?: boolean },
+  ): Promise<ClickHouseContainer> {
     const network = await new Network().start();
 
     const container = await new GenericContainer("bitnami/clickhouse:latest")
@@ -41,9 +44,12 @@ export class ClickHouseContainer {
       .withNetworkMode(network.getName())
       .withExposedPorts(8123, 9000)
       .start();
-    t.onTestFinished(async () => {
-      await container.stop();
-    });
+    if (!opts?.keepContainer) {
+      t.onTestFinished(async () => {
+        await container.stop();
+      });
+    }
+
     const dsn = `tcp://${ClickHouseContainer.username}:${ClickHouseContainer.password}@${container
       .getName()
       .replace(/^\//, "")}:9000`;
---
+++ internal/clickhouse/src/verification_tags.test.ts
@@ -0,0 +1,132 @@
+import { describe, expect, test } from "vitest";
+import { ClickHouse } from "./index";
+
+import { ClickHouseContainer } from "./testutil";
+
+test(
+  "tags are inserted correctly",
+  {
+    timeout: 300_000,
+  },
+  async (t) => {
+    const container = await ClickHouseContainer.start(t, { keepContainer: true });
+
+    const ch = new ClickHouse({ url: container.url() });
+
+    const tagsCases: Array<Array<string>> = [
+      ["key1:val1"],
+      ["key1:val1", "key2:val2"],
+      Array.from({ length: 100 })
+        .map((_, i) => `tag_${i}`)
+        .sort(),
+    ];
+
+    for (const tags of tagsCases) {
+      const verification = {
+        request_id: "1",
+        time: Date.now(),
+        workspace_id: crypto.randomUUID(),
+        key_space_id: crypto.randomUUID(),
+        key_id: crypto.randomUUID(),
+        outcome: "VALID",
+        region: "test",
+        tags: tags,
+      } as const;
+
+      const { err: insertErr } = await ch.verifications.insert(verification);
+      expect(insertErr).toBeUndefined();
+
+      const latestVerifications = await ch.verifications.logs({
+        workspaceId: verification.workspace_id,
+        keySpaceId: verification.key_space_id,
+        keyId: verification.key_id,
+      });
+
+      expect(latestVerifications.err).toBeUndefined();
+      expect(latestVerifications.val!.length).toBe(1);
+      expect(latestVerifications.val![0].tags).toEqual(verification.tags);
+    }
+  },
+);
+
+describe("materialized views", () => {
+  for (const mv of ["per_hour", "per_day", "per_month"]) {
+    describe(mv, () => {
+      const verificationsWithTags: Array<Array<string>> = [
+        [],
+        ["A"],
+        ["B"],
+        ["C"],
+        ["A", "B"],
+        ["A", "B", "D"],
+        ["B", "A", "C"],
+      ];
+
+      test(
+        `returns ${verificationsWithTags.length} verifications in total`,
+        {
+          timeout: 300_000,
+        },
+        async (t) => {
+          const container = await ClickHouseContainer.start(t);
+
+          const ch = new ClickHouse({ url: container.url() });
+
+          const workspaceId = crypto.randomUUID();
+          const keySpaceId = crypto.randomUUID();
+          const keyId = crypto.randomUUID();
+
+          const { err: insertErr } = await ch.verifications.insert(
+            verificationsWithTags.map((tags, i) => ({
+              request_id: i.toString(),
+              time: Date.now(),
+              workspace_id: workspaceId,
+              key_space_id: keySpaceId,
+              key_id: keyId,
+              outcome: "VALID",
+              region: "test",
+              tags: tags,
+            })),
+          );
+          expect(insertErr).toBeUndefined();
+          const allVerifications = await ch.verifications.logs({
+            workspaceId,
+            keySpaceId,
+            keyId,
+          });
+
+          /**
+           * Assert all of the rows have been written to clickhouse
+           */
+          expect(allVerifications.err).toBeUndefined();
+          expect(allVerifications.val!.length).toBe(verificationsWithTags.length);
+
+          /**
+           * Wait for materialized views to be updated
+           */
+          await new Promise((r) => setTimeout(r, 5000));
+
+          const q = {
+            per_hour: ch.verifications.perHour,
+            per_day: ch.verifications.perDay,
+            per_month: ch.verifications.perMonth,
+          }[mv]!;
+          const mvRes = await q({
+            workspaceId,
+            keySpaceId,
+            keyId,
+            start: Date.now() - 60 * 24 * 60 * 60 * 1000,
+            end: Date.now(),
+          });
+
+          expect(mvRes.err).toBeUndefined();
+
+          const total = mvRes.val!.reduce((sum, v) => {
+            return sum + v.count;
+          }, 0);
+          expect(total).toBe(verificationsWithTags.length);
+        },
+      );
+    });
+  }
+});
