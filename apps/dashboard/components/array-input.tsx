ad525aa347d0cca4853927f7d4b34950e3db5fde
ogzhanolguncu <ogzhan11@gmail.com>
2024-12-06 19:20:55 +0300

    fix: add logic

--- apps/dashboard/components/array-input.tsx
+++ apps/dashboard/components/array-input.tsx
@@ -1,11 +1,8 @@
-"use client";
-
-import { CornerDownLeft, X } from "lucide-react";
-import * as React from "react";
-
 import { Badge } from "@/components/ui/badge";
-import { Command } from "@/components/ui/command";
-import { Command as CommandPrimitive } from "cmdk";
+import { CornerDownLeft, X } from "lucide-react";
+import { useCallback, useState } from "react";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
 
 type Props = {
   title?: string;
@@ -15,85 +12,68 @@ type Props = {
 };
 
 export const ArrayInput: React.FC<Props> = ({ title, placeholder, selected, setSelected }) => {
-  const inputRef = React.useRef<HTMLInputElement>(null);
-  const [inputValue, setInputValue] = React.useState("");
+  console.log(selected);
+  const [items, setItems] = useState<string[]>(selected);
+  const [inputValue, setInputValue] = useState("");
 
-  const handleUnselect = (o: string) => {
-    setSelected(selected.filter((s) => s !== o));
+  const handleUnselect = (item: string) => {
+    const newItems = items.filter((i) => i !== item);
+    setItems(newItems);
+    setSelected(newItems);
   };
 
-  const handleKeyDown = React.useCallback(
-    (e: React.KeyboardEvent<HTMLDivElement>) => {
-      const input = inputRef.current;
-      if (input) {
-        if (e.key === "Delete" || e.key === "Backspace") {
-          if (input.value === "") {
-            setSelected(selected.slice(0, -1));
-          }
-        }
-        if (e.key === "Enter" && input.value !== "") {
-          setSelected(Array.from(new Set([...selected, input.value])));
-          setInputValue("");
-        }
-        // This is not a default behaviour of the <input /> field
-        if (e.key === "Escape") {
-          input.blur();
-        }
+  const handleAdd = () => {
+    if (inputValue.trim()) {
+      const newItems = Array.from(new Set([...items, inputValue.trim()]));
+      setItems(newItems);
+      setSelected(newItems);
+      setInputValue("");
+    }
+  };
+
+  const handleKeyDown = useCallback(
+    (e: React.KeyboardEvent<HTMLInputElement>) => {
+      if (e.key === "Enter") {
+        e.preventDefault();
+        handleAdd();
       }
     },
-    [selected, setSelected],
+    [inputValue, items],
   );
 
   return (
-    <Command onKeyDown={handleKeyDown} className="overflow-visible bg-transparent">
-      <div className="flex items-center h-8 p-1 text-sm border rounded-md group focus-within:border-primary">
-        <div className="flex flex-wrap items-center w-full gap-1 px-2">
-          {title ? <span className="mr-1 text-xs font-medium">{title}:</span> : null}
-          {selected.map((o) => {
-            return (
-              <Badge key={o} variant="secondary">
-                {o}
-                <button
-                  type="button"
-                  className="ml-1 rounded-full outline-none"
-                  onKeyDown={(e) => {
-                    if (e.key === "Enter") {
-                      handleUnselect(o);
-                    }
-                  }}
-                  onMouseDown={(e) => {
-                    e.preventDefault();
-                    e.stopPropagation();
-                  }}
-                  onClick={() => handleUnselect(o)}
-                >
-                  <X className="w-3 h-3 text-content-muted hover:text-content" />
-                </button>
-              </Badge>
-            );
-          })}
-          {/* Avoid having the "Search" Icon */}
-          <CommandPrimitive.Input
-            ref={inputRef}
+    <div className="bg-transparent flex flex-col relative">
+      <div>
+        {title && <span className="text-xs font-medium absolute -top-5">{title}:</span>}
+        <div className="flex gap-1 absolute left-[68px] -top-[26px]">
+          {items?.map((item) => (
+            <Badge key={item} variant="secondary">
+              {item}
+              <button
+                type="button"
+                className="ml-1 rounded-full outline-none"
+                onClick={() => handleUnselect(item)}
+              >
+                <X className="w-3 h-3 text-content-muted hover:text-content" />
+              </button>
+            </Badge>
+          ))}
+        </div>
+      </div>
+      <div className="flex items-center justify-center gap-2">
+        <div className="flex flex-wrap items-center w-full gap-1">
+          <Input
             value={inputValue}
-            onValueChange={setInputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            onKeyDown={handleKeyDown}
             placeholder={placeholder}
             className="flex-1 w-full bg-transparent outline-none placeholder:text-content-subtle"
           />
         </div>
-        <button
-          type="button"
-          className="inline-flex items-center rounded-md border px-2 py-0.5 text-xs border-border bg-secondary text-secondary-foreground hover:bg-secondary/20 font-normal"
-          onClick={() => {
-            if (inputValue !== "") {
-              setSelected(Array.from(new Set([...selected, inputValue])));
-              setInputValue("");
-            }
-          }}
-        >
-          <CornerDownLeft className="w-3 h-4" />
-        </button>
+        <Button size="icon" variant="secondary" onClick={handleAdd}>
+          <CornerDownLeft className="w-4 h-4" />
+        </Button>
       </div>
-    </Command>
+    </div>
   );
 };
