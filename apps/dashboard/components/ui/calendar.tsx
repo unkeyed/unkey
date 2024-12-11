8df7c78424365f1f999cd54323bbebb51798fe5c
ogzhanolguncu <ogzhan11@gmail.com>
2024-12-11 17:33:12 +0300

    chore: run formatter

--- apps/dashboard/components/ui/calendar.tsx
+++ apps/dashboard/components/ui/calendar.tsx
@@ -9,12 +9,7 @@ import { buttonVariants } from "@unkey/ui";
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>;
 
-function Calendar({
-  className,
-  classNames,
-  showOutsideDays = true,
-  ...props
-}: CalendarProps) {
+function Calendar({ className, classNames, showOutsideDays = true, ...props }: CalendarProps) {
   return (
     <DayPicker
       showOutsideDays={showOutsideDays}
@@ -27,19 +22,18 @@ function Calendar({
         nav: "space-x-1 flex items-center",
         nav_button: cn(
           buttonVariants({ variant: "default" }),
-          "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100 flex justify-center items-center mr-2"
+          "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100 flex justify-center items-center mr-2",
         ),
         nav_button_previous: "absolute left-1",
         nav_button_next: "absolute right-1",
         table: "w-full border-collapse space-y-1",
         head_row: "flex",
-        head_cell:
-          "text-muted-foreground rounded-md w-9 font-normal text-[12px]",
+        head_cell: "text-muted-foreground rounded-md w-9 font-normal text-[12px]",
         row: "flex w-full mt-2",
         cell: "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
         day: cn(
           buttonVariants({ variant: "ghost" }),
-          "h-9 w-9 p-0 font-normal aria-selected:opacity-100 text-[13px] flex items-center justify-center"
+          "h-9 w-9 p-0 font-normal aria-selected:opacity-100 text-[13px] flex items-center justify-center",
         ),
         day_range_end: "day-range-end",
         day_selected:
