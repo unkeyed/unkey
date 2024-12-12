d01c96ac1bc3981de06621319eedc56a3a1e2f13
Oğuzhan Olguncu <21091016+ogzhanolguncu@users.noreply.github.com>
2024-12-12 20:37:50 +0300

    feat: add more granular fetch options for timeseries (#2736)
    * feat: add fetch for initial chart data
    
    * [autofix.ci] apply automated fixes
    
    * refactor: fetch charts data from different rpc
    
    * chore: format
    
    * chore: run formatter
    
    * refactor: turn logs table into generic one
    
    * fix: aligment issues of buttons
    
    * [autofix.ci] apply automated fixes
    
    * chore: run formatter
    
    * fix: selected bg style
    
    * chore: coderabbit fix
    
    * refactor: remove redudant memo
    
    * chore: run formatter
    
    * chore: fix imports
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- apps/dashboard/lib/trpc/routers/logs/query-timeseries.ts
+++ apps/dashboard/lib/trpc/routers/logs/query-timeseries.ts
@@ -1,11 +1,12 @@
+import { getTimeseriesGranularity } from "@/app/(app)/logs/utils";
 import { clickhouse } from "@/lib/clickhouse";
 import { db } from "@/lib/db";
 import { rateLimitedProcedure, ratelimit } from "@/lib/trpc/ratelimitProcedure";
 import { TRPCError } from "@trpc/server";
-import { getLogsClickhousePayload } from "@unkey/clickhouse/src/logs";
+import { logsTimeseriesParams } from "@unkey/clickhouse/src/logs";
 
-export const queryLogs = rateLimitedProcedure(ratelimit.update)
-  .input(getLogsClickhousePayload.omit({ workspaceId: true }))
+export const queryTimeseries = rateLimitedProcedure(ratelimit.update)
+  .input(logsTimeseriesParams.omit({ workspaceId: true }))
   .query(async ({ ctx, input }) => {
     const workspace = await db.query.workspaces
       .findFirst({
@@ -15,8 +16,9 @@ export const queryLogs = rateLimitedProcedure(ratelimit.update)
       .catch((_err) => {
         throw new TRPCError({
           code: "INTERNAL_SERVER_ERROR",
+          //TODO: change error message later
           message:
-            "Failed to retrieve workspace logs due to an error. If this issue persists, please contact support@unkey.dev with the time this occurred.",
+            "Failed to retrieve timeseries analytics due to an error. If this issue persists, please contact support@unkey.dev with the time this occurred.",
         });
       });
 
@@ -26,10 +28,19 @@ export const queryLogs = rateLimitedProcedure(ratelimit.update)
         message: "Workspace not found, please contact support using support@unkey.dev.",
       });
     }
-    const result = await clickhouse.api.logs({
+
+    const { startTime, endTime, granularity } = getTimeseriesGranularity(
+      input.startTime,
+      input.endTime,
+    );
+
+    const result = await clickhouse.api.timeseries[granularity]({
       ...input,
+      startTime,
+      endTime,
       workspaceId: workspace.id,
     });
+
     if (result.err) {
       throw new TRPCError({
         code: "INTERNAL_SERVER_ERROR",
