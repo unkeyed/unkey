57c0732c9cb9c138a1a375e29325c2c6870b83f6
Andreas Thomas <dev@chronark.com>
2025-01-02 16:45:06 +0100

    chore: remove a bunch of old stuff an unused deps (#2786)

--- apps/dashboard/app/ratelimit/reset/route.ts
+++
@@ -1,9 +0,0 @@
-import { cookies } from "next/headers";
-export const runtime = "edge";
-
-const UNKEY_RATELIMIT_COOKIE = "UNKEY_RATELIMIT";
-
-export const POST = async (_req: Request): Promise<Response> => {
-  cookies().delete(UNKEY_RATELIMIT_COOKIE);
-  return new Response("ok");
-};
