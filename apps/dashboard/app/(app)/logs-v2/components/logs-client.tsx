efeba35915bec48ee7705e6e5bbac6ed06a47201
ogzhanolguncu <ogzhan11@gmail.com>
2025-01-06 18:50:42 +0300

    refactor: redundant null check

--- apps/dashboard/app/(app)/logs-v2/components/logs-client.tsx
+++ apps/dashboard/app/(app)/logs-v2/components/logs-client.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Log } from "@unkey/clickhouse/src/logs";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { LogsChart } from "./charts";
 import { LogsFilters } from "./filters";
 import { LogDetails } from "./table/log-details";
@@ -11,26 +11,24 @@ export const LogsClient = () => {
   const [selectedLog, setSelectedLog] = useState<Log | null>(null);
   const [tableDistanceToTop, setTableDistanceToTop] = useState(0);
 
-  const handleDistanceToTop = (distanceToTop: number) => {
+  const handleDistanceToTop = useCallback((distanceToTop: number) => {
     setTableDistanceToTop(distanceToTop);
-  };
+  }, []);
 
-  const handleLogSelection = (log: Log | null) => {
+  const handleLogSelection = useCallback((log: Log | null) => {
     setSelectedLog(log);
-  };
+  }, []);
 
   return (
     <>
       <LogsFilters />
       <LogsChart onMount={handleDistanceToTop} />
       <LogsTable onLogSelect={handleLogSelection} selectedLog={selectedLog} />
-      {selectedLog && (
-        <LogDetails
-          log={selectedLog}
-          onClose={() => handleLogSelection(null)}
-          distanceToTop={tableDistanceToTop}
-        />
-      )}
+      <LogDetails
+        log={selectedLog}
+        onClose={() => handleLogSelection(null)}
+        distanceToTop={tableDistanceToTop}
+      />
     </>
   );
 };
