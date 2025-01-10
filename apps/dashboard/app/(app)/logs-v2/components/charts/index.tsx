d049b08331139c8bffa6811be86f86765a16de81
ogzhanolguncu <ogzhan11@gmail.com>
2025-01-10 12:10:24 +0300

    fix: hover issue of chart timestamps

--- apps/dashboard/app/(app)/logs-v2/components/charts/index.tsx
+++ apps/dashboard/app/(app)/logs-v2/components/charts/index.tsx
@@ -80,12 +80,22 @@ export function LogsChart({
       <div className="px-2 text-accent-11 font-mono absolute top-0 text-xxs w-full flex justify-between">
         {calculateTimePoints(timeseries).map((time, i) => (
           // biome-ignore lint/suspicious/noArrayIndexKey: use of index is acceptable here.
-          <div key={i}>{formatTimestampLabel(time)}</div>
+          <div key={i} className="z-10">
+            {formatTimestampLabel(time)}
+          </div>
         ))}
       </div>
-      <ResponsiveContainer width="100%" height={50} className="border-b border-gray-4">
+      <ResponsiveContainer
+        width="100%"
+        height={50}
+        className="border-b border-gray-4"
+      >
         <ChartContainer config={chartConfig}>
-          <BarChart data={timeseries} barGap={2} margin={{ top: 0, right: 0, bottom: 0, left: 0 }}>
+          <BarChart
+            data={timeseries}
+            barGap={2}
+            margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+          >
             <YAxis domain={[0, (dataMax: number) => dataMax * 1.5]} hide />
             <ChartTooltip
               position={{ y: 50 }}
@@ -116,7 +126,9 @@ export function LogsChart({
                               <span className="capitalize text-accent-9 text-xs w-[2ch] inline-block">
                                 All
                               </span>
-                              <span className="capitalize text-accent-12 text-xs">Total</span>
+                              <span className="capitalize text-accent-12 text-xs">
+                                Total
+                              </span>
                             </div>
                             <div className="ml-auto">
                               <span className="font-mono tabular-nums text-accent-12">
@@ -129,7 +141,8 @@ export function LogsChart({
                     }
                     className="rounded-lg shadow-lg border border-gray-4"
                     labelFormatter={(_, tooltipPayload) => {
-                      const originalTimestamp = tooltipPayload[0]?.payload?.originalTimestamp;
+                      const originalTimestamp =
+                        tooltipPayload[0]?.payload?.originalTimestamp;
                       return originalTimestamp ? (
                         <div>
                           <span className="font-mono text-accent-9 text-xs px-4">
@@ -145,7 +158,12 @@ export function LogsChart({
               }}
             />
             {["success", "error", "warning"].map((key) => (
-              <Bar key={key} dataKey={key} stackId="a" fill={`var(--color-${key})`} />
+              <Bar
+                key={key}
+                dataKey={key}
+                stackId="a"
+                fill={`var(--color-${key})`}
+              />
             ))}
           </BarChart>
         </ChartContainer>
