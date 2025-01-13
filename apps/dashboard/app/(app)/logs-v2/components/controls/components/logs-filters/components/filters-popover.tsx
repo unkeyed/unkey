bd6d6cb313d960139b34158d7eb4d53b1e194602
ogzhanolguncu <ogzhan11@gmail.com>
2025-01-13 18:36:07 +0300

    fix: build issue

--- apps/dashboard/app/(app)/logs-v2/components/controls/components/logs-filters/components/filters-popover.tsx
+++ apps/dashboard/app/(app)/logs-v2/components/controls/components/logs-filters/components/filters-popover.tsx
@@ -1,3 +1,4 @@
+import { useFilters } from "@/app/(app)/logs-v2/hooks/use-filters";
 import { useKeyboardShortcut } from "@/app/(app)/logs-v2/hooks/use-keyboard-shortcut";
 import { KeyboardButton } from "@/components/keyboard-button";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
