ec711b94dfd50bbd13e7d091e3eae9c955c3e158
chronark <dev@chronark.com>
2025-01-27 14:08:04 +0100

    fix: race condition with deleting roles

--- apps/dashboard/lib/trpc/routers/rbac/deleteRole.ts
+++ apps/dashboard/lib/trpc/routers/rbac/deleteRole.ts
@@ -11,20 +11,19 @@ export const deleteRole = t.procedure
     }),
   )
   .mutation(async ({ input, ctx }) => {
-    const role = await db.query.roles.findFirst({
-      where: (table, { and, eq }) =>
-        and(eq(table.workspaceId, ctx.workspace.id), eq(table.id, input.roleId)),
-    });
-
-    if (!role) {
-      throw new TRPCError({
-        code: "NOT_FOUND",
-        message:
-          "We are unable to find the correct role. Please try again or contact support@unkey.dev.",
-      });
-    }
-
     await db.transaction(async (tx) => {
+      const role = await tx.query.roles.findFirst({
+        where: (table, { and, eq }) =>
+          and(eq(table.workspaceId, ctx.workspace.id), eq(table.id, input.roleId)),
+      });
+
+      if (!role) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message:
+            "We are unable to find the correct role. Please try again or contact support@unkey.dev.",
+        });
+      }
       await tx
         .delete(schema.roles)
         .where(
