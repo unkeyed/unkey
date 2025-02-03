2f6c83d20836e988cd970078dc40882c85e447d5
Nelson Isioma <nelsonisioma1@gmail.com>
2025-02-03 04:48:05 +0100

    fix: pnpm local command exits if migration fails

--- tools/local/src/db.ts
+++ tools/local/src/db.ts
@@ -23,18 +23,25 @@ export async function prepareDatabase(url?: string): Promise<{
     const cwd = path.join(__dirname, "../../../internal/db");
 
     await new Promise((resolve, reject) => {
+      let stdoutData = "";
       const p = exec("pnpm drizzle-kit push", {
         env: {
           DRIZZLE_DATABASE_URL: url ?? "mysql://unkey:password@localhost:3306/unkey",
           ...process.env,
         },
-
         cwd,
-      }, (error, stdout, stderr) => {
-          console.error(`stdout: ${stdout}`);
       });
+
+      p.stdout?.on("data", (data) => {
+        stdoutData += data;
+      });
+
       p.on("exit", (code) => {
         if (code === 0) {
+          if (stdoutData.includes("Error")) {
+            reject(new Error(`Migration failed with MySQL error:\n${stdoutData}`));
+          }
+
           resolve(code);
         } else {
           reject(code);
