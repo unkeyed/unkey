d3d4e000998ab7a592bbfd03d17895c1309c326b
Oğuzhan Olguncu <21091016+ogzhanolguncu@users.noreply.github.com>
2025-02-05 17:37:29 +0300

    feat: range selection via chart (#2867)
    * feat: add range selection via chart
    
    * fix: only query last 12 hour paths
    
    * fix: naming
    
    * fix: remove duplicate check
    
    * fix: add more prominent color to chart selection
    
    * fix: use more contrasted colors

--- apps/dashboard/lib/trpc/routers/logs/query-distinct-paths.ts
+++ apps/dashboard/lib/trpc/routers/logs/query-distinct-paths.ts
@@ -4,35 +4,45 @@ import { rateLimitedProcedure, ratelimit } from "@/lib/trpc/ratelimitProcedure";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
-export const queryDistinctPaths = rateLimitedProcedure(ratelimit.update).query(async ({ ctx }) => {
-  const workspace = await db.query.workspaces
-    .findFirst({
-      where: (table, { and, eq, isNull }) =>
-        and(eq(table.tenantId, ctx.tenant.id), isNull(table.deletedAt)),
-    })
-    .catch((_err) => {
-      throw new TRPCError({
-        code: "INTERNAL_SERVER_ERROR",
-        message:
-          "Failed to retrieve distinct paths due to an error. If this issue persists, please contact support@unkey.dev with the time this occurred.",
+export const queryDistinctPaths = rateLimitedProcedure(ratelimit.update)
+  .input(z.object({ currentDate: z.number() }))
+  .query(async ({ ctx, input }) => {
+    const workspace = await db.query.workspaces
+      .findFirst({
+        where: (table, { and, eq, isNull }) =>
+          and(eq(table.tenantId, ctx.tenant.id), isNull(table.deletedAt)),
+      })
+      .catch((_err) => {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message:
+            "Failed to retrieve distinct paths due to an error. If this issue persists, please contact support@unkey.dev with the time this occurred.",
+        });
       });
+
+    if (!workspace) {
+      throw new TRPCError({
+        code: "NOT_FOUND",
+        message: "Workspace not found, please contact support using support@unkey.dev.",
+      });
+    }
+
+    const fromDate = input.currentDate - 12 * 60 * 60 * 1000; // 12 hours ago
+    const result = await clickhouse.querier.query({
+      query: `
+         SELECT DISTINCT path 
+         FROM metrics.raw_api_requests_v1 
+         WHERE workspace_id = {workspaceId: String}
+         AND time >= {fromDate: UInt64}`,
+      schema: z.object({ path: z.string() }),
+      params: z.object({
+        workspaceId: z.string(),
+        fromDate: z.number(),
+      }),
+    })({
+      workspaceId: workspace.id,
+      fromDate,
     });
 
-  if (!workspace) {
-    throw new TRPCError({
-      code: "NOT_FOUND",
-      message: "Workspace not found, please contact support using support@unkey.dev.",
-    });
-  }
-
-  const result = await clickhouse.querier.query({
-    query: `
-          SELECT DISTINCT path
-          FROM metrics.raw_api_requests_v1
-          WHERE workspace_id = {workspaceId: String}`,
-    schema: z.object({ path: z.string() }),
-    params: z.object({ workspaceId: z.string() }),
-  })({ workspaceId: workspace.id });
-
-  return result.val?.map((i) => i.path) ?? [];
-});
+    return result.val?.map((i) => i.path) ?? [];
+  });
