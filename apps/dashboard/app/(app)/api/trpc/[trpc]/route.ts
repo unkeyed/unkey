41de98f659f0f33f55087e4d0e7ee57d0acbfe77
James Perkins <jamesperkins@Jamess-MacBook-Air.local>
2025-02-07 19:40:43 -0500

    Modify handler to be Next 14 compat
    Fix deprecated handler

--- apps/dashboard/app/(app)/api/trpc/[trpc]/route.ts
+++ apps/dashboard/app/(app)/api/trpc/[trpc]/route.ts
@@ -2,7 +2,7 @@ import { createContext } from "@/lib/trpc/context";
 import { router } from "@/lib/trpc/routers";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 // export API handler
-export default async function handler(req: Request) {
+export async function handler(req: Request) {
   try {
     return fetchRequestHandler({
       endpoint: "/api/trpc",
@@ -11,7 +11,7 @@ export default async function handler(req: Request) {
       createContext,
     });
   } catch (err) {
-    console.error(err);
+    console.log(err);
   }
 }
 
