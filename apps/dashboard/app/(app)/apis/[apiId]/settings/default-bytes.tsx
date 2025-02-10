3d678ad20fd9289bbfad7c6d5993c6b534279b0f
Harsh Shrikant Bhat <90265455+harshsbhat@users.noreply.github.com>
2025-02-10 15:38:07 +0530

    fix: Minimum byte length should be consistent (#2880)

--- apps/dashboard/app/(app)/apis/[apiId]/settings/default-bytes.tsx
+++ apps/dashboard/app/(app)/apis/[apiId]/settings/default-bytes.tsx
@@ -21,7 +21,7 @@ const formSchema = z.object({
   keyAuthId: z.string(),
   defaultBytes: z
     .number()
-    .min(8, "Byte size needs to be at least 8")
+    .min(16, "Byte size needs to be at least 16")
     .max(255, "Byte size cannot exceed 255")
     .optional(),
 });
