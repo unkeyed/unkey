5c014076d539b91f517bc9a96c75d18328c5e26d
ogzhanolguncu <ogzhan11@gmail.com>
2025-02-11 18:28:01 +0300

    refactor: Add card back

--- apps/dashboard/app/(app)/ratelimits/[namespaceId]/_overview/components/logs-client.tsx
+++ apps/dashboard/app/(app)/ratelimits/[namespaceId]/_overview/components/logs-client.tsx
@@ -3,11 +3,10 @@
 import { RatelimitOverviewLogsTable } from "./table/logs-table";
 
 export const LogsClient = () => {
-  // const [tableDistanceToTop, setTableDistanceToTop] = useState(0);
-  //
-  // const handleDistanceToTop = useCallback((distanceToTop: number) => {
-  //   setTableDistanceToTop(distanceToTop);
-  // }, []);
-
-  return <RatelimitOverviewLogsTable />;
+  return (
+    <>
+      <div>Chart Here</div>
+      <RatelimitOverviewLogsTable />
+    </>
+  );
 };
