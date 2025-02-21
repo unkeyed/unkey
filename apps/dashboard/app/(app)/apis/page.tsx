151ca05b10cb8e38e2607c3487187c1f7ab152cb
Meg Stepp <mcstepp@gmail.com>
2025-02-21 15:23:17 -0500

    remove unpaid check until we implement a better unpaid block

--- apps/dashboard/app/(app)/apis/page.tsx
+++ apps/dashboard/app/(app)/apis/page.tsx
@@ -40,8 +40,6 @@ export default async function ApisOverviewPage(props: Props) {
     })),
   );
 
-  const unpaid = workspace.tenantId.startsWith("org_") && workspace.plan === "free";
-
   return (
     <div>
       <Navbar>
@@ -56,24 +54,7 @@ export default async function ApisOverviewPage(props: Props) {
         </Navbar.Actions>
       </Navbar>
       <PageContent>
-        {unpaid ? (
-          <div className="mt-10 flex min-h-[400px] flex-col items-center  justify-center space-y-6 rounded-lg border border-dashed px-4 md:mt-24">
-            <h3 className="text-xl font-semibold leading-none tracking-tight text-center md:text-2xl">
-              Upgrade your plan
-            </h3>
-            <p className="text-sm text-center text-gray-500 md:text-base">
-              Team workspaces is a paid feature. Please switch to a paid plan to continue using it.
-            </p>
-            <Link
-              href="/settings/billing"
-              className="px-4 py-2 mr-3 text-sm font-medium text-center text-white bg-gray-800 rounded-lg hover:bg-gray-500 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:bg-gray-600 dark:hover:bg-gray-500 dark:focus:ring-gray-800"
-            >
-              Subscribe
-            </Link>
-          </div>
-        ) : (
           <ApiList apis={apis} />
-        )}
       </PageContent>
     </div>
   );
