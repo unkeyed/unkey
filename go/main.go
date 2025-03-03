5f77745d9e7b03ffa3e06e868a5477235224fa67
chronark <dev@chronark.com>
2025-03-03 22:22:40 +0100

    feat: cli flags and docs

--- go/main.go
+++ go/main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 
 	"github.com/unkeyed/unkey/go/cmd/api"
 	"github.com/unkeyed/unkey/go/cmd/healthcheck"
-	"github.com/urfave/cli/v2"
+	"github.com/unkeyed/unkey/go/pkg/version"
+	"github.com/urfave/cli/v3"
 )
 
 func main() {
-	app := &cli.App{
-		Name:  "unkey",
-		Usage: "Run unkey ",
+	app := &cli.Command{
+		Name:    "unkey",
+		Usage:   "Run unkey ",
+		Version: version.Version,
 
 		Commands: []*cli.Command{
 			api.Cmd,
@@ -20,12 +23,10 @@ func main() {
 		},
 	}
 
-	err := app.Run(os.Args)
+	err := app.Run(context.Background(), os.Args)
 	if err != nil {
-
 		fmt.Println()
 		fmt.Println()
-
 		fmt.Println(err.Error())
 		fmt.Println()
 		os.Exit(1)
