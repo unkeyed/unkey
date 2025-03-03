4d02373f55b059fb0ed4e1652950f7bd9d0e1cac
Oğuzhan Olguncu <21091016+ogzhanolguncu@users.noreply.github.com>
2025-03-03 21:27:57 +0300

    fix: format apis overview numbers (#2930)
    * fix: format number issue and make cards bigger
    
    * refactor: apply same grid structure to ratelimits page
    
    * chore: fmt

--- apps/dashboard/app/(app)/ratelimits/_components/ratelimit-client.tsx
+++ apps/dashboard/app/(app)/ratelimits/_components/ratelimit-client.tsx
@@ -37,7 +37,7 @@ export const RatelimitClient = ({
       <RatelimitListControlCloud />
 
       {namespaces.length > 0 ? (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-3 md:gap-5 w-full p-5">
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 md:gap-5 w-full p-5">
           {namespaces.map((namespace) => (
             <NamespaceCard namespace={namespace} key={namespace.id} />
           ))}
