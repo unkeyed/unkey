e7d3102e850dc5a6aa77eae4a5c07aa18fee973b
Meg Stepp <mcstepp@gmail.com>
2025-03-05 16:48:45 -0500

    fix brand new user workspace creation

--- apps/dashboard/app/new/page.tsx
+++ apps/dashboard/app/new/page.tsx
@@ -26,6 +26,21 @@ type Props = {
   };
 };
 
+function getBaseUrl() {
+  if (typeof window !== "undefined") {
+    // browser should use relative path
+    return "";
+  }
+
+  if (process.env.VERCEL_URL) {
+    // reference for vercel.com
+    return `https://${process.env.VERCEL_URL}`;
+  }
+
+  // assume localhost
+  return `http://localhost:${process.env.PORT ?? 3000}`;
+}
+
 export default async function (props: Props) {
   const user = await auth.getCurrentUser();
   // make typescript happy
