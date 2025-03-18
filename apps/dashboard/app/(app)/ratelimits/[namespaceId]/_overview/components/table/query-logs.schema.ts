8f1b40354be8dc9633c36df2875f9ff424145fbe
ogzhanolguncu <ogzhan11@gmail.com>
2025-03-18 15:53:29 +0300

    fix: missing type

--- apps/dashboard/app/(app)/ratelimits/[namespaceId]/_overview/components/table/query-logs.schema.ts
+++ apps/dashboard/app/(app)/ratelimits/[namespaceId]/_overview/components/table/query-logs.schema.ts
@@ -2,6 +2,8 @@ import { z } from "zod";
 import { ratelimitOverviewFilterOperatorEnum } from "../../filters.schema";
 
 export const sortFields = z.enum(["time", "avg_latency", "p99_latency", "blocked", "passed"]);
+export type SortFields = z.infer<typeof sortFields>;
+
 export const ratelimitQueryOverviewLogsPayload = z.object({
   limit: z.number().int(),
   startTime: z.number().int(),
