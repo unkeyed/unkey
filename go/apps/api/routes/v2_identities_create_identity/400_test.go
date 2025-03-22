fa28f97bd27fc0cc1aa20fc467d012678528f243
Flo <53355483+Flo4604@users.noreply.github.com>
2025-03-22 09:40:06 +0100

    make tests pass again

--- go/apps/api/routes/v2_identities_create_identity/400_test.go
+++ go/apps/api/routes/v2_identities_create_identity/400_test.go
@@ -15,102 +15,6 @@ import (
 )
 
 func TestBadRequests(t *testing.T) {
-	metaData := make(map[string]*interface{})
-	entriesNeeded := (handler.MAX_META_LENGTH_MB * 1024 * 1024) / 15
-	for i := 0; i < entriesNeeded+1000; i++ {
-		var data interface{} = fmt.Sprintf("some_%d", i)
-		metaData[fmt.Sprintf("key_%d", i)] = &data
-	}
-
-	rawMeta, _ := json.Marshal(metaData)
-
-	testCases := []struct {
-		name          string
-		req           openapi.V2IdentitiesCreateIdentityRequestBody
-		expectedError openapi.BadRequestError
-	}{
-		{
-			name: "missing external id",
-			req:  openapi.V2IdentitiesCreateIdentityRequestBody{},
-			expectedError: openapi.BadRequestError{
-				Title:     "Bad Request",
-				Detail:    "One or more fields failed validation",
-				Status:    http.StatusBadRequest,
-				Type:      "https://unkey.com/docs/errors/bad_request",
-				Errors:    []openapi.ValidationError{},
-				RequestId: "test",
-				Instance:  nil,
-			},
-		},
-		{
-			name: "empty external id",
-			req: openapi.V2IdentitiesCreateIdentityRequestBody{
-				ExternalId: "",
-			},
-			expectedError: openapi.BadRequestError{
-				Title:     "Bad Request",
-				Detail:    "One or more fields failed validation",
-				Status:    http.StatusBadRequest,
-				Type:      "https://unkey.com/docs/errors/bad_request",
-				Errors:    []openapi.ValidationError{},
-				RequestId: "test",
-				Instance:  nil,
-			},
-		},
-		{
-			name: "too short identifier",
-			req: openapi.V2IdentitiesCreateIdentityRequestBody{
-				ExternalId: "ab",
-			},
-			expectedError: openapi.BadRequestError{
-				Title:     "Bad Request",
-				Detail:    "One or more fields failed validation",
-				Status:    http.StatusBadRequest,
-				Type:      "https://unkey.com/docs/errors/bad_request",
-				Errors:    []openapi.ValidationError{},
-				RequestId: "test",
-				Instance:  nil,
-			},
-		},
-		{
-			name: "too much metadata",
-			req: openapi.V2IdentitiesCreateIdentityRequestBody{
-				ExternalId: "abc",
-				Meta:       &metaData,
-			},
-			expectedError: openapi.BadRequestError{
-				Title:     "Bad Request",
-				Detail:    fmt.Sprintf("Metadata is too large, it must be less than %dMB, got: %.2f", handler.MAX_META_LENGTH_MB, float64(len(rawMeta))/1024/1024),
-				Status:    http.StatusBadRequest,
-				Type:      "https://unkey.com/docs/errors/bad_request",
-				Errors:    []openapi.ValidationError{},
-				RequestId: "test",
-				Instance:  nil,
-			},
-		},
-		{
-			name: "Invalid ratelimit",
-			req: openapi.V2IdentitiesCreateIdentityRequestBody{
-				ExternalId: "abc",
-				Ratelimits: &[]openapi.V2Ratelimit{
-					{
-						Duration: 1,
-						Limit:    1,
-					},
-				},
-			},
-			expectedError: openapi.BadRequestError{
-				Title:     "Bad Request",
-				Detail:    "One or more fields failed validation",
-				Status:    http.StatusBadRequest,
-				Type:      "https://unkey.com/docs/errors/bad_request",
-				Errors:    []openapi.ValidationError{},
-				RequestId: "test",
-				Instance:  nil,
-			},
-		},
-	}
-
 	h := testutil.NewHarness(t)
 
 	rootKey := h.CreateRootKey(h.Resources().UserWorkspace.ID, "identity.*.create_identity")
@@ -124,23 +28,114 @@ func TestBadRequests(t *testing.T) {
 
 	h.Register(route)
 
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			headers := http.Header{
-				"Content-Type":  {"application/json"},
-				"Authorization": {fmt.Sprintf("Bearer %s", rootKey)},
-			}
-
-			res := testutil.CallRoute[handler.Request, openapi.BadRequestError](h, route, headers, tc.req)
-			require.Equal(t, 400, res.Status, "expected 400, received: %v", res.Body)
-			require.NotNil(t, res.Body)
-			require.Equal(t, tc.expectedError.Type, res.Body.Type)
-			require.Equal(t, tc.expectedError.Detail, res.Body.Detail)
-			require.Equal(t, tc.expectedError.Status, res.Body.Status)
-			require.Equal(t, tc.expectedError.Title, res.Body.Title)
-			require.NotEmpty(t, res.Body.RequestId)
-		})
+	headers := http.Header{
+		"Content-Type":  {"application/json"},
+		"Authorization": {fmt.Sprintf("Bearer %s", rootKey)},
 	}
+
+	t.Run("missing external id", func(t *testing.T) {
+		req := openapi.V2IdentitiesCreateIdentityRequestBody{}
+		res := testutil.CallRoute[handler.Request, openapi.BadRequestError](h, route, headers, req)
+		require.Equal(t, 400, res.Status, "expected 400, sent: %+v, received: %s", req, res.RawBody)
+		require.NotNil(t, res.Body)
+
+		require.Equal(t, "https://unkey.com/docs/errors/bad_request", res.Body.Type)
+		require.Equal(t, "POST request body for '/v2/identities.createIdentity' failed to validate schema", res.Body.Detail)
+		require.Equal(t, http.StatusBadRequest, res.Body.Status)
+		require.Equal(t, "Bad Request", res.Body.Title)
+		require.NotEmpty(t, res.Body.RequestId)
+		require.Greater(t, len(res.Body.Errors), 0)
+		require.Nil(t, res.Body.Instance)
+	})
+
+	t.Run("empty external id", func(t *testing.T) {
+		req := openapi.V2IdentitiesCreateIdentityRequestBody{ExternalId: ""}
+		res := testutil.CallRoute[handler.Request, openapi.BadRequestError](h, route, headers, req)
+		require.Equal(t, 400, res.Status, "expected 400, sent: %+v, received: %s", req, res.RawBody)
+		require.NotNil(t, res.Body)
+
+		require.Equal(t, "https://unkey.com/docs/errors/bad_request", res.Body.Type)
+		require.Equal(t, "POST request body for '/v2/identities.createIdentity' failed to validate schema", res.Body.Detail)
+		require.Equal(t, http.StatusBadRequest, res.Body.Status)
+		require.Equal(t, "Bad Request", res.Body.Title)
+		require.NotEmpty(t, res.Body.RequestId)
+		require.Greater(t, len(res.Body.Errors), 0)
+		require.Nil(t, res.Body.Instance)
+	})
+
+	t.Run("external id too short", func(t *testing.T) {
+		req := openapi.V2IdentitiesCreateIdentityRequestBody{ExternalId: "12"}
+		res := testutil.CallRoute[handler.Request, openapi.BadRequestError](h, route, headers, req)
+		require.Equal(t, 400, res.Status, "expected 400, sent: %+v, received: %s", req, res.RawBody)
+		require.NotNil(t, res.Body)
+
+		require.Equal(t, "https://unkey.com/docs/errors/bad_request", res.Body.Type)
+		require.Equal(t, "POST request body for '/v2/identities.createIdentity' failed to validate schema", res.Body.Detail)
+		require.Equal(t, http.StatusBadRequest, res.Body.Status)
+		require.Equal(t, "Bad Request", res.Body.Title)
+		require.NotEmpty(t, res.Body.RequestId)
+		require.Greater(t, len(res.Body.Errors), 0)
+		require.Nil(t, res.Body.Instance)
+	})
+
+	t.Run("metadata exceeds maximum size limit", func(t *testing.T) {
+		metaData := make(map[string]*interface{})
+		entriesNeeded := (handler.MAX_META_LENGTH_MB * 1024 * 1024) / 15
+		for i := 0; i < entriesNeeded+1000; i++ {
+			var data interface{} = fmt.Sprintf("some_%d", i)
+			metaData[fmt.Sprintf("key_%d", i)] = &data
+		}
+
+		rawMeta, _ := json.Marshal(metaData)
+
+		req := openapi.V2IdentitiesCreateIdentityRequestBody{ExternalId: uid.New("test"), Meta: &metaData}
+		res := testutil.CallRoute[handler.Request, openapi.BadRequestError](h, route, headers, req)
+		require.Equal(t, 400, res.Status, "expected 400, sent: %+v, received: %s", req, res.RawBody)
+		require.NotNil(t, res.Body)
+
+		require.Equal(t, "https://unkey.com/docs/errors/bad_request", res.Body.Type)
+		require.Equal(t, fmt.Sprintf("Metadata is too large, it must be less than %dMB, got: %.2f", handler.MAX_META_LENGTH_MB, float64(len(rawMeta))/1024/1024), res.Body.Detail)
+		require.Equal(t, http.StatusBadRequest, res.Body.Status)
+		require.Equal(t, "Bad Request", res.Body.Title)
+		require.NotEmpty(t, res.Body.RequestId)
+		require.Nil(t, res.Body.Instance)
+	})
+
+	t.Run("invalid ratelimit", func(t *testing.T) {
+
+		req := openapi.V2IdentitiesCreateIdentityRequestBody{
+			ExternalId: uid.New("test"),
+			Ratelimits: &[]openapi.V2Ratelimit{
+				{
+					Duration: 1,
+					Limit:    1,
+				},
+			},
+		}
+		res := testutil.CallRoute[handler.Request, openapi.BadRequestError](h, route, headers, req)
+		require.Equal(t, 400, res.Status, "expected 400, sent: %+v, received: %s", req, res.RawBody)
+		require.NotNil(t, res.Body)
+
+		require.Equal(t, "https://unkey.com/docs/errors/bad_request", res.Body.Type)
+		require.Equal(t, "POST request body for '/v2/identities.createIdentity' failed to validate schema", res.Body.Detail)
+		require.Equal(t, http.StatusBadRequest, res.Body.Status)
+		require.Equal(t, "Bad Request", res.Body.Title)
+		require.NotEmpty(t, res.Body.RequestId)
+		require.Greater(t, len(res.Body.Errors), 0)
+		require.Nil(t, res.Body.Instance)
+	})
+
+	t.Run("malformed authorization header", func(t *testing.T) {
+		headers := http.Header{
+			"Content-Type":  {"application/json"},
+			"Authorization": {"malformed_header"},
+		}
+
+		req := handler.Request{ExternalId: uid.New("test")}
+		res := testutil.CallRoute[handler.Request, openapi.BadRequestError](h, route, headers, req)
+		require.Equal(t, http.StatusBadRequest, res.Status)
+		require.NotNil(t, res.Body)
+	})
 
 	t.Run("missing authorization header", func(t *testing.T) {
 		headers := http.Header{
