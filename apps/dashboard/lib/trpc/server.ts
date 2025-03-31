60c0fef815e33ea03a20f1cdb1f861e3d062aa28
James P <james@unkey.dev>
2025-03-31 15:50:40 -0400

    Biome fixes (#3064)
    Set warning on ally stuff that we can revisit

--- apps/dashboard/lib/trpc/server.ts
+++ apps/dashboard/lib/trpc/server.ts
@@ -1,8 +1,8 @@
 import { createTRPCProxyClient, httpLink } from "@trpc/client";
 import superjson from "superjson";
 
-import type { Router } from "./routers";
 import { getBaseUrl } from "../utils";
+import type { Router } from "./routers";
 
 export const trpc = createTRPCProxyClient<Router>({
   transformer: superjson,
