a1bf5ec1c3680679704267febabe1642391e1e57
Meg Stepp <mcstepp@gmail.com>
2025-03-31 13:04:48 -0400

    create a global baseUrl function and update the sso-callback and trpc

--- apps/dashboard/lib/trpc/server.ts
+++ apps/dashboard/lib/trpc/server.ts
@@ -2,21 +2,7 @@ import { createTRPCProxyClient, httpLink } from "@trpc/client";
 import superjson from "superjson";
 
 import type { Router } from "./routers";
-
-function getBaseUrl() {
-  if (typeof window !== "undefined") {
-    // browser should use relative path
-    return "";
-  }
-
-  if (process.env.VERCEL_URL) {
-    // reference for vercel.com
-    return `https://${process.env.VERCEL_URL}`;
-  }
-
-  // assume localhost
-  return `http://localhost:${process.env.PORT ?? 3000}`;
-}
+import { getBaseUrl } from "../utils";
 
 export const trpc = createTRPCProxyClient<Router>({
   transformer: superjson,
