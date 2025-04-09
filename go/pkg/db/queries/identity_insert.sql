e501501efd2ea0682c95d90eefb824267fdd0a38
Flo <53355483+Flo4604@users.noreply.github.com>
2025-04-09 15:41:06 +0200

    feat: api v2 identities.createIdentity (#2955)
    * Add identity creation endpoint
    
    * add bad tests and correct conflict error
    
    * Update identity creation test for duplicate handling
    
    * Remove TooManyRequestsError and clean up tests
    
    * Increase metadata size limit and fix minor issues
    
    * Add audit log service and refactor insertion
    
    * Add Auditlogs to route handler and update tests
    
    * Remove wildcard permission check from RBAC checks
    
    * Add Auditlogs to ratelimit override handlers
    
    * Enhance key verification with workspace status checks
    
    * Add documentation and expand audit log resource types
    
    * Refactor audit log data structure and handling
    
    * Add identity creation test with rate limits
    
    * Improve key verification checks and error handling
    
    * Add type assertion for conflict error in create identity test
    
    * Removes redundant fields from the Session struct
    
    * Add .gitignore and refactor ratelimit schema
    
    * Reduce maximum metadata length for identity creation to 1MB
    
    * Use DifferentWorkspace from test resources
    
    * Remove unused log import and statement
    
    * Improved transaction handling for audit logs
    
    * Remove duplicate for_workspace_id field
    
    * generate openapi and fix nit comment
    
    * Make tests pass again
    
    * Add error handling for JSON unmarshaling in test and fix wording
    
    * make tests pass again
    
    ---------
    
    Co-authored-by: chronark <dev@chronark.com>

---
+++ go/pkg/db/queries/identity_insert.sql
@@ -0,0 +1,16 @@
+-- name: InsertIdentity :exec
+INSERT INTO `identities` (
+    id,
+    external_id,
+    workspace_id,
+    environment,
+    created_at,
+    meta
+) VALUES (
+    sqlc.arg('id'),
+    sqlc.arg('external_id'),
+    sqlc.arg('workspace_id'),
+    sqlc.arg('environment'),
+    sqlc.arg('created_at'),
+    sqlc.arg('meta')
+);
