28b0bf413164c55a7996d8e942fffa78ac1fbaa7
James P <james@unkey.dev>
2025-04-17 07:35:32 -0400

    chore: Removing everything to do with www, play or glossary aka billing (#3122)
    * Removing everything to do with www, play or glossary aka billing
    
    * [autofix.ci] apply automated fixes
    
    * lock
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- apps/www/components/ui/image-glass-filter.tsx
+++
@@ -1,8 +0,0 @@
-export function FrostedGlassFilter({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="relative overflow-hidden">
-      <div className="absolute top-0 right-[-60px] backdrop-blur-xl backdrop-opacity-80 placeholder-parallelogram " />
-      {children}
-    </div>
-  );
-}
