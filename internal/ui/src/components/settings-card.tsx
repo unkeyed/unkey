6284d836eb5d7c84bd571a1b13eb4f0a9168872c
CodeReaper <148160799+MichaelUnkey@users.noreply.github.com>
2025-04-28 09:51:53 -0400

    feat: Move settings card and new inline link (#3198)
    * started
    
    * eng docs for settingcard
    
    * added inline link

--- internal/ui/src/components/settings-card.tsx
+++ internal/ui/src/components/settings-card.tsx
@@ -1,4 +1,6 @@
-import { cn } from "@unkey/ui/src/lib/utils";
+// biome-ignore lint: React in this context is used throughout, so biome will change to types because no APIs are used even though React is needed.
+import * as React from "react";
+import { cn } from "../lib/utils";
 
 type SettingCardProps = {
   title: string | React.ReactNode;
