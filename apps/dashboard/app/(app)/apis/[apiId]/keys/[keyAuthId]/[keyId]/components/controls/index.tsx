83143b27ea55962e487513347e83bee8b212da15
ogzhanolguncu <ogzhan11@gmail.com>
2025-05-09 14:56:34 +0300

    fix: use our new controls component

--- apps/dashboard/app/(app)/apis/[apiId]/keys/[keyAuthId]/[keyId]/components/controls/index.tsx
+++ apps/dashboard/app/(app)/apis/[apiId]/keys/[keyAuthId]/[keyId]/components/controls/index.tsx
@@ -1,3 +1,8 @@
+import {
+  ControlsContainer,
+  ControlsLeft,
+  ControlsRight,
+} from "@/components/logs/controls-container";
 import { LogsDateTime } from "./components/logs-datetime";
 import { LogsFilters } from "./components/logs-filters";
 import { LogsRefresh } from "./components/logs-refresh";
@@ -11,26 +16,16 @@ export function KeysDetailsLogsControls({
   keyspaceId: string;
 }) {
   return (
-    <div className="flex flex-col border-b border-gray-4 w-full">
-      <div className="px-3 py-1 w-full justify-between flex items-center">
-        <div className="flex gap-2 w-full">
-          <div className="flex flex-1 gap-2 items-center">
-            <LogsSearch keyspaceId={keyspaceId} keyId={keyId} />
-          </div>
-          <div className="flex gap-2 md:w-full max-md:justify-end">
-            <div className="flex gap-2 items-center">
-              <LogsFilters />
-            </div>
-            <div className="flex gap-2 items-center">
-              <LogsDateTime />
-            </div>
-          </div>
-        </div>
-
-        <div className="flex gap-2 max-md:hidden">
-          <LogsRefresh />
-        </div>
-      </div>
-    </div>
+    <ControlsContainer>
+      <ControlsLeft>
+        <LogsSearch keyspaceId={keyspaceId} keyId={keyId} />
+        <LogsFilters />
+        <LogsDateTime />
+      </ControlsLeft>
+      <ControlsRight>
+        {/* <LogsLiveSwitch /> */}
+        <LogsRefresh />
+      </ControlsRight>
+    </ControlsContainer>
   );
 }
--- apps/dashboard/app/(app)/apis/[apiId]/keys/[keyAuthId]/_components/components/controls/index.tsx
+++ apps/dashboard/app/(app)/apis/[apiId]/keys/[keyAuthId]/_components/components/controls/index.tsx
@@ -1,19 +1,19 @@
+import { LogsDateTime } from "@/app/(app)/apis/_components/controls/components/logs-datetime";
+import {
+  ControlsContainer,
+  ControlsLeft,
+} from "@/components/logs/controls-container";
 import { LogsFilters } from "./components/logs-filters";
 import { LogsSearch } from "./components/logs-search";
 
 export function KeysListControls({ keyspaceId }: { keyspaceId: string }) {
   return (
-    <div className="flex flex-col border-b border-gray-4 ">
-      <div className="px-3 py-1 w-full justify-between flex items-center">
-        <div className="flex gap-2">
-          <div className="flex gap-2 items-center">
-            <LogsSearch keyspaceId={keyspaceId} />
-          </div>
-          <div className="flex gap-2 items-center">
-            <LogsFilters />
-          </div>
-        </div>
-      </div>
-    </div>
+    <ControlsContainer>
+      <ControlsLeft>
+        <LogsSearch keyspaceId={keyspaceId} />
+        <LogsFilters />
+        <LogsDateTime />
+      </ControlsLeft>
+    </ControlsContainer>
   );
 }
