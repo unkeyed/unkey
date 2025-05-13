f9114f5ee5880bfe034f841184b8a54d29ca8dcf
Michael Silva <michael@unkey.com>
2025-05-13 14:44:14 -0400

    remove comments

--- apps/dashboard/app/(app)/apis/[apiId]/_overview/components/controls/index.tsx
+++ apps/dashboard/app/(app)/apis/[apiId]/_overview/components/controls/index.tsx
@@ -15,26 +15,6 @@ export function KeysOverviewLogsControls({ apiId }: { apiId: string }) {
       <ControlsRight>
         <LogsRefresh />
       </ControlsRight>
-      {/* <div className="flex flex-col border-b border-gray-4 w-full"> */}
-      {/* <div className="px-3 py-1 w-full justify-between flex items-center"> */}
-      {/* <div className="flex gap-2 w-full"> */}
-      {/* <div className="flex flex-1 gap-2 items-center">
-          <LogsSearch apiId={apiId} />
-        </div>
-        <div className="flex gap-2 md:w-full max-md:justify-end">
-          <div className="flex gap-2 items-center">
-            <LogsFilters />
-          </div>
-          <div className="flex gap-2 items-center">
-            <LogsDateTime />
-          </div>
-        </div>
-      {/* </div> */}
-
-      {/*<div className="flex gap-2 max-md:hidden">
-        <LogsRefresh />
-      </div> */}
-      {/* </div> */}
     </ControlsContainer>
   );
 }
