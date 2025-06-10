cc2d04b3415eb45bf151e7f632402383422900fe
ogzhanolguncu <ogzhan11@gmail.com>
2025-06-10 14:13:25 +0300

    feat: add initial structrue

---
+++ apps/dashboard/app/(app)/authorization/permissions/navigation.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { Navbar } from "@/components/navigation/navbar";
+import { ShieldKey } from "@unkey/icons";
+
+export function Navigation() {
+  return (
+    <Navbar className="w-full flex justify-between">
+      <Navbar.Breadcrumbs icon={<ShieldKey />} className="flex-1 w-full">
+        <Navbar.Breadcrumbs.Link href="/authorization/roles">
+          Authorization
+        </Navbar.Breadcrumbs.Link>
+        <Navbar.Breadcrumbs.Link href="/authorization/permissions" active>
+          Permissions
+        </Navbar.Breadcrumbs.Link>
+      </Navbar.Breadcrumbs>
+    </Navbar>
+  );
+}
---
+++ apps/dashboard/app/(app)/authorization/permissions/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import { Navigation } from "./navigation";
+
+export default function RolesPage() {
+  return (
+    <div>
+      <Navigation />
+      {/* <div className="flex flex-col"> */}
+      {/*   <RoleListControls /> */}
+      {/*   <RolesListControlCloud /> */}
+      {/*   <RolesList /> */}
+      {/* </div> */}
+    </div>
+  );
+}
