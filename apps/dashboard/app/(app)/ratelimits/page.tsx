289d91ef18816c04f739efac9787ee69d523d67c
ogzhanolguncu <ogzhan11@gmail.com>
2025-06-17 16:07:41 +0300

    refactor: move page to client using trpc

--- apps/dashboard/app/(app)/ratelimits/page.tsx
+++ apps/dashboard/app/(app)/ratelimits/page.tsx
@@ -1,35 +1,13 @@
-import { getAuth } from "@/lib/auth";
-import { db } from "@/lib/db";
-import { redirect } from "next/navigation";
+"use client";
 
 import { RatelimitClient } from "./_components/ratelimit-client";
 import { Navigation } from "./navigation";
 
-export const dynamic = "force-dynamic";
-
-export default async function RatelimitOverviewPage() {
-  const { orgId } = await getAuth();
-  const workspace = await db.query.workspaces.findFirst({
-    where: (table, { and, eq, isNull }) => and(eq(table.orgId, orgId), isNull(table.deletedAtM)),
-    with: {
-      ratelimitNamespaces: {
-        where: (table, { isNull }) => isNull(table.deletedAtM),
-        columns: {
-          id: true,
-          name: true,
-        },
-      },
-    },
-  });
-
-  if (!workspace) {
-    return redirect("/new");
-  }
-
+export default function RatelimitOverviewPageAlt() {
   return (
     <div>
       <Navigation />
-      <RatelimitClient ratelimitNamespaces={workspace.ratelimitNamespaces} />
+      <RatelimitClient />
     </div>
   );
 }
