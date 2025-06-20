ada90e2e255681f0427ad1c7d9c5733f4100f3ff
Michael Silva <michael@unkey.com>
2025-06-20 07:59:28 -0400

    eng docs change

--- apps/engineering/content/design/components/code.example.tsx
+++ apps/engineering/content/design/components/code.example.tsx
@@ -1,17 +1,40 @@
+"use client";
 import { RenderComponentWithSnippet } from "@/app/components/render";
-import { Code } from "@unkey/ui";
-
+import { Code, CopyButton, VisibleButton } from "@unkey/ui";
+import { useState } from "react";
+const EXAMPLE_SNIPPET = `curl -XPOST 'https://api.unkey.dev/v1/ratelimits.limit' \\
+  -H 'Content-Type: application/json' \\
+  -H 'Authorization: Bearer <UNKEY_ROOT_KEY>' \\
+    -d '{
+      "namespace": "demo_namespace",
+    "identifier": "user_123",
+    "limit": 10,
+    "duration": 10000
+}'`;
 export function CodeExample() {
+  const [showKeyInSnippet, setShowKeyInSnippet] = useState(false);
   return (
     <RenderComponentWithSnippet>
       <div className="flex flex-col gap-4">
         <div>
-          <div className="flex flex-wrap items-center gap-4">
-            <Code>const greeting = "Hello, World!";</Code>
-            <Code>
-              function add(a, b) {"{"} return a + b; {"}"}
+          <div className=" items-center gap-4 w-full p-3">
+            <Code className="relative w-full">
+              {showKeyInSnippet
+                ? EXAMPLE_SNIPPET
+                : EXAMPLE_SNIPPET.replace("<UNKEY_ROOT_KEY>", "********")}
+              <div className="absolute top-0 right-0 p-2 flex items-center gap-2">
+                <VisibleButton
+                  isVisible={showKeyInSnippet}
+                  setIsVisible={(visible) => setShowKeyInSnippet(visible)}
+                  title="Key Snippet"
+                />
+
+                <CopyButton
+                  value={EXAMPLE_SNIPPET}
+                  className="bg-transparent border border-grayA-6 rounded-md hover:bg-grayA-2"
+                />
+              </div>
             </Code>
-            <Code>npm install @unkey/ui</Code>
           </div>
         </div>
       </div>
--- internal/ui/src/components/code.tsx
+++ internal/ui/src/components/code.tsx
@@ -1,5 +1,6 @@
-import * as React from "react";
+"use client";
 import { type VariantProps, cva } from "class-variance-authority";
+import type * as React from "react";
 import { cn } from "../lib/utils";
 
 const codeVariants = cva(
