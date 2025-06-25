67d85cfddba639c08a30a39721027c552cbf977e
ogzhanolguncu <ogzhan11@gmail.com>
2025-06-26 00:11:52 +0300

    fix: typo

--- apps/dashboard/app/(app)/settings/root-keys/components/controls/index.tsx
+++ apps/dashboard/app/(app)/settings/root-keys/components/controls/index.tsx
@@ -1,6 +1,6 @@
 import { ControlsContainer, ControlsLeft } from "@/components/logs/controls-container";
 import { RootKeysFilters } from "./components/root-keys-filters";
-import { RootKeysSearch } from "./components/rootk-keys-search";
+import { RootKeysSearch } from "./components/root-keys-search";
 
 export function RootKeysListControls() {
   return (
