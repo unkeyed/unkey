ee258428f3290c2e92ade70293a89edefe486a0d
MichaelUnkey <michael@unkey.com>
2025-07-02 12:23:35 -0400

    rabbit and missing library

--- internal/ui/src/index.ts
+++ internal/ui/src/index.ts
@@ -23,4 +23,3 @@ export * from "./components/timestamp-info";
 export * from "./components/tooltip";
 export * from "./components/separator";
 export * from "./components/toaster";
-export * from "./components/badge";
