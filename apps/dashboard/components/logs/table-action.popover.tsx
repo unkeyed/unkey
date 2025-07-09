78eb77f6093a4d515d41c6ada4960c211708ca09
ogzhanolguncu <ogzhan11@gmail.com>
2025-07-09 23:02:48 +0300

    fix: button styling

--- apps/dashboard/components/logs/table-action.popover.tsx
+++ apps/dashboard/components/logs/table-action.popover.tsx
@@ -1,5 +1,6 @@
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { Dots } from "@unkey/icons";
+import { Button } from "@unkey/ui";
 import { cn } from "@unkey/ui/src/lib/utils";
 import { type FC, type PropsWithChildren, useEffect, useRef, useState } from "react";
 
@@ -148,14 +149,8 @@ export const TableActionPopover = ({
 
 export const TableActionPopoverDefaultTrigger = () => {
   return (
-    <button
-      type="button"
-      className={cn(
-        "group-data-[state=open]:bg-gray-6 group-hover:bg-gray-6 group size-5 p-0 rounded m-0 items-center flex justify-center",
-        "border border-gray-6 group-hover:border-gray-8 ring-2 ring-transparent focus-visible:ring-gray-7 focus-visible:border-gray-7",
-      )}
-    >
+    <Button variant="outline" className="size-5 [&_svg]:size-3 rounded">
       <Dots className="group-hover:text-gray-12 text-gray-11" size="sm-regular" />
-    </button>
+    </Button>
   );
 };
