2292a5733471336bdfa54eeb58f45915d3241f41
MichaelUnkey <michael@unkey.com>
2025-07-10 15:34:22 -0400

    swap button

--- apps/dashboard/app/(app)/apis/[apiId]/settings/components/copy-api-id.tsx
+++ apps/dashboard/app/(app)/apis/[apiId]/settings/components/copy-api-id.tsx
@@ -1,4 +1,5 @@
-import { CopyButton, SettingCard } from "@unkey/ui";
+import { SettingCard } from "@unkey/ui";
+import { CopyButton } from "@unkey/ui";
 
 export const CopyApiId = ({ apiId }: { apiId: string }) => {
   return (
@@ -11,13 +12,15 @@ export const CopyApiId = ({ apiId }: { apiId: string }) => {
       contentWidth="w-full lg:w-[420px] justify-end items-center"
     >
       {/* TODO: make this a Code component in UI for CopyKeys with optional hidden button like in Code.*/}
-      <div
-        className={
-          "flex flex-row justify-between min-w-[328px] pl-4 pr-2 py-1.5 bg-gray-2 dark:bg-black border rounded-lg border-grayA-5"
-        }
-      >
-        <div className="text-sm text-gray-11">{apiId}</div>
-        <CopyButton value={apiId} variant="ghost" toastMessage={apiId} />
+      <div className="flex flex-row justify-end items-center">
+        <div
+          className={
+            "flex flex-row justify-between min-w-[327px] pl-4 pr-2 py-1.5 bg-gray-2 dark:bg-black border rounded-lg border-grayA-5"
+          }
+        >
+          <div className="text-sm text-gray-11">{apiId}</div>
+          <CopyButton value={apiId} variant="ghost" toastMessage={apiId} />
+        </div>
       </div>
     </SettingCard>
   );
