7d01859b52a073d831d8b8d318aa46cc3229b13a
MichaelUnkey <michael@unkey.com>
2025-07-25 17:04:00 -0400

    re ordered badge list collapse

--- apps/dashboard/app/(app)/settings/root-keys/components/create-root-key/components/permission-badge-list.tsx
+++ apps/dashboard/app/(app)/settings/root-keys/components/create-root-key/components/permission-badge-list.tsx
@@ -15,11 +15,17 @@ type Props = {
 };
 
 type InfoType = { permission: UnkeyPermission, category: string, action: string }[]
+
 const PermissionBadgeList = ({ apiId, name, selectedPermissions, title, expandCount, removePermission }: Props) => {
-   
+
     const workspace = workspacePermissions;
     const allPermissions = apiId === "workspace" ? workspace : apiPermissions(apiId);
 
+    const findPermission = (allPermissions: InfoType, selectedPermissions: string[]) => {
+        return selectedPermissions.map((permission) => {
+            return allPermissions.find((p) => p.permission === permission);
+        }).filter(Boolean);
+    }
     // Flatten allPermissions into an array of {permission, action} objects
     const allPermissionsArray = Object.entries(allPermissions).flatMap(([category, permissions]) =>
         Object.entries(permissions).map(([action, permissionData]) => ({
@@ -28,12 +34,14 @@ const PermissionBadgeList = ({ apiId, name, selectedPermissions, title, expandCo
             action,
         }))
     );
+
     const info = findPermission(allPermissionsArray, selectedPermissions);
+
     if (info.length === 0) return null;
+
     return info.length > expandCount ? (
         <div className="flex flex-col gap-2">
-            <ListTitle title={title} count={info.length} category={name} />
-            <CollapsibleList info={info as InfoType} title={title} expandCount={expandCount} removePermission={removePermission} />
+            <CollapsibleList info={info as InfoType} title={title} expandCount={expandCount} removePermission={removePermission} name={name} />
         </div>
     ) : (
         <div className="flex flex-col gap-2">
@@ -44,18 +52,13 @@ const PermissionBadgeList = ({ apiId, name, selectedPermissions, title, expandCo
 
 };
 
-const findPermission = (allPermissions: InfoType, selectedPermissions: string[]) => {
-    return selectedPermissions.map((permission) => {
-        return allPermissions.find((p) => p.permission === permission);
-    }).filter(Boolean);
-}
 const ListBadges = ({ info, removePermission }: { info: InfoType, removePermission: (permission: string) => void }) => {
     const handleRemovePermission = (e: React.MouseEvent<HTMLButtonElement>, permission: string) => {
         e.stopPropagation();
         removePermission(permission);
     }
     return (
-        <div className="flex flex-wrap gap-2">
+        <div className="flex flex-wrap gap-2 pt-2">
             {info?.map((permission) => {
                 if (!permission) return null;
                 return (
@@ -71,47 +74,43 @@ const ListBadges = ({ info, removePermission }: { info: InfoType, removePermissi
     )
 }
 
-const CollapsibleList = ({ info, title, expandCount, removePermission }: { info: InfoType, title: string, expandCount: number, removePermission: (permission: string) => void }) => {
-    const infoFirst = info.slice(0, expandCount) as InfoType;
-    const infoRest = info.slice(expandCount) as InfoType;
+type CollapsibleListProps = {
+    info: InfoType;
+    title: string;
+    expandCount: number;
+    removePermission: (permission: string) => void;
+    name: string;
+    className?: string;
+} & React.ComponentProps<typeof CollapsibleTrigger>; 
+
+const CollapsibleList = ({
+    info,
+    title,
+    expandCount,
+    removePermission,
+    name,
+    className,
+    ...props
+}: CollapsibleListProps) => {
+
     return (
         <Collapsible>
             <CollapsibleTrigger
-                className={cn(
-                    "flex flex-col gap-3 transition-all [&[data-state=open]>svg]:rotate-180 w-full",
-                )}
+                {...props}
+                className={cn("flex flex-row gap-3 transition-all [&[data-state=open]>svg]:rotate-180 w-full",
+                className)}
             >
-                <div className="flex flex-row items-center w-full">
-                    <ListBadges info={infoFirst} removePermission={removePermission} />
-                    <ChevronDown className="w-4 h-4 transition-transform duration-200 ml-auto text-grayA-8" />
-                </div>
+                <ListTitle title={title} count={info.length} category={name} />
+                <ChevronDown className="w-4 h-4 transition-transform duration-200 ml-auto text-grayA-8" />
             </CollapsibleTrigger>
             <CollapsibleContent>
-                <div className="flex flex-wrap gap-2 pt-2">
-                    <ListBadges info={infoRest} removePermission={removePermission} />
-                </div>
+                <ListBadges info={info} removePermission={removePermission} />
             </CollapsibleContent>
         </Collapsible>
     )
 }
 
-// const ListTitleBadges = ({ info }: { info: InfoType }) => {
-//     return (
-//         <div className="flex flex-wrap gap-2">
-//             {info?.map((permission) => {
-//                 if (!permission) return null;
-//                 return (
-//                     <Badge key={permission.permission}
-//                         variant="primary"
-//                         className="flex items-center h-[22px] p-[6px] px-2 text-xs font-normal text-grayA-11 hover:bg-grayA-2 hover:text-grayA-12 gap-2">
-//                         <span>{permission.action}</span>
-//                         <XMark className="w-4 h-4" />
-//                     </Badge>
-//                 )
-//             })}
-//         </div>
-//     )
-// }
+
 const ListTitle = ({ title, count, category }: { title: string, count: number, category: string }) => {
     return (
         <p className="text-sm w-full text-grayA-10 justify-start text-left">
