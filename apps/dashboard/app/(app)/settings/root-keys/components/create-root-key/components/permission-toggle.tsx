6b3e2a541f216a7cc91b37bb6720163f6f5fe4de
MichaelUnkey <michael@unkey.com>
2025-07-30 11:40:08 -0400

    style change

--- apps/dashboard/app/(app)/settings/root-keys/components/create-root-key/components/permission-toggle.tsx
+++ apps/dashboard/app/(app)/settings/root-keys/components/create-root-key/components/permission-toggle.tsx
@@ -24,7 +24,7 @@ export const PermissionToggle: React.FC<PermissionToggleProps> = ({
   const labelString = typeof label === 'string' ? label : String(label);
 
   return (
-    <div className="flex flex-row items-center justify-evenly gap-4 transition-all pl-3 h-full my-1 ml-2 w-full hover:bg-grayA-3 rounded-lg">
+    <div className="flex flex-row items-center justify-evenly gap-4 transition-all pl-3 h-full mb-1 ml-2 pb-1 w-full hover:bg-grayA-3 rounded-lg">
       <Checkbox
         size="lg"
         checked={checked}
@@ -36,14 +36,14 @@ export const PermissionToggle: React.FC<PermissionToggleProps> = ({
           }
         }}
       />
-      <div className="flex flex-col text-left min-w-48 w-full ml-2">
+      <div className="flex flex-col text-left min-w-48 w-full mr-2 gap-1">
         <div className="inline-flex items-center gap-2">
           <span className="text-sm w-fit">{category}</span>
           <ChevronRight size="sm-regular" className="text-grayA-8" />
           <span className="text-sm w-full">{label}</span>
         </div>
         <InfoTooltip content={description} className="w-full text-left">
-          <p className="text-xs text-gray-10 text-left max-w-[235px] truncate mt-1">
+          <p className="text-xs text-gray-10 text-left max-w-[245px] truncate mr-2">
             {description}
           </p>
         </InfoTooltip>
