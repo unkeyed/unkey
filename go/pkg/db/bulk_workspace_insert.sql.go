043df727b77d8c690d5016cc0a7d9e91d4adbdc2
Meg Stepp <mcstepp@gmail.com>
2025-08-26 13:39:48 -0400

    fmt regen files

--- go/pkg/db/bulk_workspace_insert.sql.go
+++ go/pkg/db/bulk_workspace_insert.sql.go
@@ -36,6 +36,6 @@ func (q *BulkQueries) InsertWorkspaces(ctx context.Context, db DBTX, args []Inse
 	}
 
 	// Execute the bulk insert
-    _, err := db.ExecContext(ctx, bulkQuery, allArgs...)
-    return err
+	_, err := db.ExecContext(ctx, bulkQuery, allArgs...)
+	return err
 }
