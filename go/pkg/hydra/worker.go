4e590db2bcaf5ed38b9f3bbf769d7a77da790b7a
Flo <flo@unkey.com>
2025-08-27 19:51:15 +0200

    fix: hydra rollback err

--- go/pkg/hydra/worker.go
+++ go/pkg/hydra/worker.go
@@ -898,7 +898,7 @@ func (w *worker) acquireWorkflowLease(ctx context.Context, workflowID, workerID 
 		return err
 	}
 	defer func() {
-		if rollbackErr := tx.Rollback(); rollbackErr != nil {
+		if rollbackErr := tx.Rollback(); rollbackErr != nil && rollbackErr != sql.ErrTxDone {
 			w.engine.logger.Error("failed to rollback transaction", "error", rollbackErr)
 		}
 	}()
