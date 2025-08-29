a8676def63d6130492a7c74630fb453f9e6e9014
ogzhanolguncu <ogzhan11@gmail.com>
2025-08-29 17:18:10 +0300

    fix: double fetch

--- apps/dashboard/app/(app)/ratelimits/_components/controls/components/namespace-list-refresh.tsx
+++ apps/dashboard/app/(app)/ratelimits/_components/controls/components/namespace-list-refresh.tsx
@@ -1,17 +1,11 @@
 import { trpc } from "@/lib/trpc/client";
-import { useQueryTime } from "@/providers/query-time-provider";
 import { RefreshButton } from "@unkey/ui";
-import { useRouter } from "next/navigation";
 
 export const NamespaceListRefresh = () => {
-  const { refreshQueryTime } = useQueryTime();
   const { ratelimit } = trpc.useUtils();
-  const { refresh } = useRouter();
 
   const handleRefresh = () => {
-    refreshQueryTime();
     ratelimit.logs.queryRatelimitTimeseries.invalidate();
-    refresh();
   };
 
   return <RefreshButton onRefresh={handleRefresh} isEnabled />;
