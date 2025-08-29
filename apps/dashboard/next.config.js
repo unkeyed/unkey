79701929128702fe5fbce86a129d100628daa52e
James Perkins <james@unkey.dev>
2025-08-29 09:20:07 -0400

    Adds CRUD checks
    Adds CRUD checks to certain tRPC routes to make sure the current user is
    able to do the action.

--- apps/dashboard/next.config.js
+++ apps/dashboard/next.config.js
@@ -13,6 +13,7 @@ const nextConfig = {
   experimental: {
     esmExternals: "loose",
   },
+  poweredByHeader: false,
   webpack: (config) => {
     config.cache = Object.freeze({
       type: "memory",
