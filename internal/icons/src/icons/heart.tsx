299ca54933263e09fce4e12d7fc80317aafafff4
Oğuzhan Olguncu <21091016+ogzhanolguncu@users.noreply.github.com>
2025-09-01 13:13:17 +0300

    feat: project details UI (#3853)
    * refactor: move deployments page
    
    * feat: add actions to nvabar
    
    * feat: add sub tab navigation
    
    * feat: add details sectiopn
    
    * fix: build error
    
    * refactor: sections
    
    * feat: add animated project details
    
    * feat: add active deployments section
    
    * feat: add domains section
    
    * feat: add build logs section
    
    * feat: add log ops
    
    * feat: add env update section
    
    * refactor: use proper layout
    
    * refactor: animations for envs
    
    * refactor: allow details open by default
    
    * fix: details mount
    
    * fix: ui inconsistencies
    
    * feat: get active project details from tRPC
    
    * feat: use tRPC
    
    * feat: add env fetch
    
    * fix: import paths
    
    * fix: import paths
    
    * fix: build issue
    
    * fix: coderabbit issues

--- internal/icons/src/icons/heart.tsx
+++ internal/icons/src/icons/heart.tsx
@@ -12,19 +12,20 @@
 import type React from "react";
 import { type IconProps, sizeMap } from "../props";
 
-export const Bolt: React.FC<IconProps> = ({ size = "xl-thin", filled, ...props }) => {
+export const Heart: React.FC<IconProps> = ({ size = "xl-thin", filled, ...props }) => {
   const { size: pixelSize, strokeWidth } = sizeMap[size];
+
   return (
     <svg
-      {...props}
       height={pixelSize}
       width={pixelSize}
       viewBox="0 0 18 18"
       xmlns="http://www.w3.org/2000/svg"
+      {...props}
     >
-      <g>
+      <g fill="currentColor">
         <path
-          d="m8.597,16.41l5.872-8.265c.118-.166,0-.395-.204-.395h-5.016l.604-5.98c.037-.26-.299-.394-.451-.18L3.531,9.855c-.118.166,0,.395.204.395h5.016l-.604,5.98c-.037.26.299.394.451.18Z"
+          d="M8.529,15.222c.297,.155,.644,.155,.941,0,1.57-.819,6.529-3.787,6.529-8.613,.008-2.12-1.704-3.846-3.826-3.859-1.277,.016-2.464,.66-3.173,1.72-.71-1.06-1.897-1.704-3.173-1.72-2.123,.013-3.834,1.739-3.826,3.859,0,4.826,4.959,7.794,6.529,8.613Z"
           fill="none"
           stroke="currentColor"
           strokeLinecap="round"
