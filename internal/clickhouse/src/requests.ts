20b53da9fcbe65ba614451c92b2d78e1c96c8200
ogzhanolguncu <ogzhan11@gmail.com>
2025-09-12 18:43:20 +0300

    revert: insert

---
+++ internal/clickhouse/src/requests.ts
@@ -0,0 +1,27 @@
+import type { Inserter } from "./client/interface";
+
+import { z } from "zod";
+
+export function insertApiRequest(ch: Inserter) {
+  return ch.insert({
+    table: "metrics.raw_api_requests_v1",
+    schema: z.object({
+      request_id: z.string(),
+      time: z.number().int(),
+      workspace_id: z.string(),
+      host: z.string(),
+      method: z.string(),
+      path: z.string(),
+      request_headers: z.array(z.string()),
+      request_body: z.string(),
+      response_status: z.number().int(),
+      response_headers: z.array(z.string()),
+      response_body: z.string(),
+      error: z.string().optional().default(""),
+      service_latency: z.number().int(),
+      user_agent: z.string(),
+      ip_address: z.string(),
+      region: z.string(),
+    }),
+  });
+}
