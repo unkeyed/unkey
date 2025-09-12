0159a3902ed1302ca4f64e07cf28ec38d608b215
ogzhanolguncu <ogzhan11@gmail.com>
2025-09-12 14:36:17 +0300

    chore: replace rest of the queries

--- internal/clickhouse/src/requests.ts
+++ internal/clickhouse/src/requests.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
 
 export function insertApiRequest(ch: Inserter) {
   return ch.insert({
-    table: "metrics.raw_api_requests_v1",
+    table: "default.api_requests_raw_v2",
     schema: z.object({
       request_id: z.string(),
       time: z.number().int(),
