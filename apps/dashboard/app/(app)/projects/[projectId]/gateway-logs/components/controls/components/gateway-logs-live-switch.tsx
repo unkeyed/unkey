580f9e652cc86453c4a4fd8910f6a26b9dc8bf98
ogzhanolguncu <ogzhan11@gmail.com>
2025-09-18 17:46:54 +0300

    fix: live switch

--- apps/dashboard/app/(app)/projects/[projectId]/gateway-logs/components/controls/components/gateway-logs-live-switch.tsx
+++ apps/dashboard/app/(app)/projects/[projectId]/gateway-logs/components/controls/components/gateway-logs-live-switch.tsx
@@ -1,33 +1,15 @@
-import { HISTORICAL_DATA_WINDOW } from "@/components/logs/constants";
 import { LiveSwitchButton } from "@/components/logs/live-switch-button";
+import { useQueryTime } from "@/providers/query-time-provider";
 import { useGatewayLogsContext } from "../../../context/gateway-logs-provider";
-import { useGatewayLogsFilters } from "../../../hooks/use-gateway-logs-filters";
 
 export const GatewayLogsLiveSwitch = () => {
   const { toggleLive, isLive } = useGatewayLogsContext();
-  const { filters, updateFilters } = useGatewayLogsFilters();
+  const { refreshQueryTime } = useQueryTime();
 
   const handleSwitch = () => {
     toggleLive();
-    // To able to refetch historic data again we have to update the endTime
     if (isLive) {
-      const timestamp = Date.now();
-      const activeFilters = filters.filter((f) => !["endTime", "startTime"].includes(f.field));
-      updateFilters([
-        ...activeFilters,
-        {
-          field: "endTime",
-          value: timestamp,
-          id: crypto.randomUUID(),
-          operator: "is",
-        },
-        {
-          field: "startTime",
-          value: timestamp - HISTORICAL_DATA_WINDOW,
-          id: crypto.randomUUID(),
-          operator: "is",
-        },
-      ]);
+      refreshQueryTime();
     }
   };
   return <LiveSwitchButton onToggle={handleSwitch} isLive={isLive} />;
