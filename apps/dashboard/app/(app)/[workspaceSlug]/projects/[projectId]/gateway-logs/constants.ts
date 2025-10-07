a41faa62237439d6151ad014194441723a6650a8
Oğuzhan Olguncu <21091016+ogzhanolguncu@users.noreply.github.com>
2025-10-07 20:40:31 +0300

    fix: excluded hosts for fireworks (#4084)

--- apps/dashboard/app/(app)/[workspaceSlug]/projects/[projectId]/gateway-logs/constants.ts
+++ apps/dashboard/app/(app)/[workspaceSlug]/projects/[projectId]/gateway-logs/constants.ts
@@ -5,4 +5,4 @@ export const METHODS = ["GET", "POST", "PUT", "DELETE", "PATCH"] as const;
 export const STATUSES = [200, 400, 500] as const;
 
 // If we don't exclude those host names, gateway logs will behave just like regular logs
-export const EXCLUDED_HOSTS = ["api.unkey.com", "api.unkey.dev"];
+export const EXCLUDED_HOSTS = ["api.unkey.com", "api.unkey.dev", "fireworks.unkey.dev"];
