9058cc679dfa57adb3da06e73a3630841d43c7a8
ogzhanolguncu <ogzhan11@gmail.com>
2025-10-14 17:42:17 +0300

    revert: s3 changes

--- go/pkg/vault/storage/s3.go
+++ go/pkg/vault/storage/s3.go
@@ -11,15 +11,15 @@ import (
 	awsConfig "github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	awsS3 "github.com/aws/aws-sdk-go-v2/service/s3"
+
 	"github.com/unkeyed/unkey/go/pkg/fault"
 	"github.com/unkeyed/unkey/go/pkg/otel/logging"
 )
 
 type s3 struct {
-	client        *awsS3.Client
-	presignClient *awsS3.PresignClient
-	config        S3Config
-	logger        logging.Logger
+	client *awsS3.Client
+	config S3Config
+	logger logging.Logger
 }
 
 type S3Config struct {
@@ -32,6 +32,7 @@ type S3Config struct {
 
 func NewS3(config S3Config) (Storage, error) {
 	logger := config.Logger.With("service", "storage")
+
 	logger.Info("using s3 storage")
 
 	// nolint:staticcheck
@@ -55,8 +56,6 @@ func NewS3(config S3Config) (Storage, error) {
 	}
 
 	client := awsS3.NewFromConfig(cfg)
-	presignClient := awsS3.NewPresignClient(client)
-
 	logger.Info("creating bucket if necessary")
 	_, err = client.CreateBucket(context.Background(), &awsS3.CreateBucketInput{
 		Bucket: aws.String(config.S3Bucket),
@@ -66,12 +65,8 @@ func NewS3(config S3Config) (Storage, error) {
 	}
 
 	logger.Info("s3 storage initialized")
-	return &s3{
-		config:        config,
-		client:        client,
-		presignClient: presignClient,
-		logger:        logger,
-	}, nil
+
+	return &s3{config: config, client: client, logger: logger}, nil
 }
 
 func (s *s3) Key(workspaceId string, dekID string) string {
@@ -100,18 +95,17 @@ func (s *s3) GetObject(ctx context.Context, key string) ([]byte, bool, error) {
 		Key:    aws.String(key),
 	})
 	if err != nil {
+
 		if strings.Contains(err.Error(), "StatusCode: 404") {
 			return nil, false, nil
 		}
 		return nil, false, fmt.Errorf("failed to get object: %w", err)
 	}
 	defer o.Body.Close()
-
 	b, err := io.ReadAll(o.Body)
 	if err != nil {
 		return nil, false, fmt.Errorf("failed to read object: %w", err)
 	}
-
 	return b, true, nil
 }
 
@@ -127,11 +121,9 @@ func (s *s3) ListObjectKeys(ctx context.Context, prefix string) ([]string, error
 	if err != nil {
 		return nil, fmt.Errorf("failed to list objects: %w", err)
 	}
-
 	keys := make([]string, len(o.Contents))
 	for i, obj := range o.Contents {
 		keys[i] = *obj.Key
 	}
-
 	return keys, nil
 }
