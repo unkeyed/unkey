fb3f0420e7eaa0bb947478bc1130b770847a5368
Flo <flo@unkey.com>
2025-10-17 15:46:42 +0200

    fix: rename file

--- go/pkg/db/queries/identity_find_by_external_id.sql
+++ go/pkg/db/queries/identity_find_by_external_id.sql
@@ -1,10 +1,3 @@
--- name: FindIdentityByID :one
-SELECT *
-FROM identities
-WHERE workspace_id = sqlc.arg(workspace_id)
-  AND id = sqlc.arg(identity_id)
-  AND deleted = sqlc.arg(deleted);
-
 -- name: FindIdentityByExternalID :one
 SELECT *
 FROM identities
