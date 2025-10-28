c99d21f0f6a9285fa04d57d1effa3ccbeb6549f2
Flo <flo@unkey.com>
2025-10-28 23:40:39 +0100

    fix: undo unnecessary change

--- go/pkg/db/key_find_live_by_id.sql_generated.go
+++ go/pkg/db/key_find_live_by_id.sql_generated.go
@@ -16,7 +16,9 @@ SELECT
     a.id, a.name, a.workspace_id, a.ip_whitelist, a.auth_type, a.key_auth_id, a.created_at_m, a.updated_at_m, a.deleted_at_m, a.delete_protection,
     ka.id, ka.workspace_id, ka.created_at_m, ka.updated_at_m, ka.deleted_at_m, ka.store_encrypted_keys, ka.default_prefix, ka.default_bytes, ka.size_approx, ka.size_last_updated_at,
     ws.id, ws.org_id, ws.name, ws.slug, ws.partition_id, ws.plan, ws.tier, ws.stripe_customer_id, ws.stripe_subscription_id, ws.beta_features, ws.features, ws.subscriptions, ws.enabled, ws.delete_protection, ws.created_at_m, ws.updated_at_m, ws.deleted_at_m,
-
+    i.id as identity_table_id,
+    i.external_id as identity_external_id,
+    i.meta as identity_meta,
     ek.encrypted as encrypted_key,
     ek.encryption_key_id as encryption_key_id,
 
@@ -87,11 +89,6 @@ SELECT
         JSON_ARRAY()
     ) as ratelimits,
 
-    -- Identity
-    i.id as identity_table_id,
-    i.external_id as identity_external_id,
-    i.meta as identity_meta,
-
     -- Credits Key/Identity based
     kc.id as credit_id,
     kc.remaining as credit_remaining,
@@ -147,15 +144,15 @@ type FindLiveKeyByIDRow struct {
 	Api                        Api            `db:"api"`
 	KeyAuth                    KeyAuth        `db:"key_auth"`
 	Workspace                  Workspace      `db:"workspace"`
+	IdentityTableID            sql.NullString `db:"identity_table_id"`
+	IdentityExternalID         sql.NullString `db:"identity_external_id"`
+	IdentityMeta               []byte         `db:"identity_meta"`
 	EncryptedKey               sql.NullString `db:"encrypted_key"`
 	EncryptionKeyID            sql.NullString `db:"encryption_key_id"`
 	Roles                      interface{}    `db:"roles"`
 	Permissions                interface{}    `db:"permissions"`
 	RolePermissions            interface{}    `db:"role_permissions"`
 	Ratelimits                 interface{}    `db:"ratelimits"`
-	IdentityTableID            sql.NullString `db:"identity_table_id"`
-	IdentityExternalID         sql.NullString `db:"identity_external_id"`
-	IdentityMeta               []byte         `db:"identity_meta"`
 	CreditID                   sql.NullString `db:"credit_id"`
 	CreditRemaining            sql.NullInt32  `db:"credit_remaining"`
 	CreditRefillAmount         sql.NullInt32  `db:"credit_refill_amount"`
@@ -175,7 +172,9 @@ type FindLiveKeyByIDRow struct {
 //	    a.id, a.name, a.workspace_id, a.ip_whitelist, a.auth_type, a.key_auth_id, a.created_at_m, a.updated_at_m, a.deleted_at_m, a.delete_protection,
 //	    ka.id, ka.workspace_id, ka.created_at_m, ka.updated_at_m, ka.deleted_at_m, ka.store_encrypted_keys, ka.default_prefix, ka.default_bytes, ka.size_approx, ka.size_last_updated_at,
 //	    ws.id, ws.org_id, ws.name, ws.slug, ws.partition_id, ws.plan, ws.tier, ws.stripe_customer_id, ws.stripe_subscription_id, ws.beta_features, ws.features, ws.subscriptions, ws.enabled, ws.delete_protection, ws.created_at_m, ws.updated_at_m, ws.deleted_at_m,
-//
+//	    i.id as identity_table_id,
+//	    i.external_id as identity_external_id,
+//	    i.meta as identity_meta,
 //	    ek.encrypted as encrypted_key,
 //	    ek.encryption_key_id as encryption_key_id,
 //
@@ -245,11 +244,6 @@ type FindLiveKeyByIDRow struct {
 //	            OR rl.identity_id = i.id),
 //	        JSON_ARRAY()
 //	    ) as ratelimits,
-//
-//	    -- Identity
-//	    i.id as identity_table_id,
-//	    i.external_id as identity_external_id,
-//	    i.meta as identity_meta,
 //
 //	    -- Credits Key/Identity based
 //	    kc.id as credit_id,
@@ -341,15 +335,15 @@ func (q *Queries) FindLiveKeyByID(ctx context.Context, db DBTX, id string) (Find
 		&i.Workspace.CreatedAtM,
 		&i.Workspace.UpdatedAtM,
 		&i.Workspace.DeletedAtM,
+		&i.IdentityTableID,
+		&i.IdentityExternalID,
+		&i.IdentityMeta,
 		&i.EncryptedKey,
 		&i.EncryptionKeyID,
 		&i.Roles,
 		&i.Permissions,
 		&i.RolePermissions,
 		&i.Ratelimits,
-		&i.IdentityTableID,
-		&i.IdentityExternalID,
-		&i.IdentityMeta,
 		&i.CreditID,
 		&i.CreditRemaining,
 		&i.CreditRefillAmount,
