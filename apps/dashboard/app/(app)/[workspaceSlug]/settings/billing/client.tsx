18a7421a42504f9c86a6b73aac32f4a314c74e12
Eeyoritron <michael@unkey.com>
2025-10-28 14:15:22 -0400

    small fix

--- apps/dashboard/app/(app)/[workspaceSlug]/settings/billing/client.tsx
+++ apps/dashboard/app/(app)/[workspaceSlug]/settings/billing/client.tsx
@@ -50,7 +50,8 @@ export const Client: React.FC = () => {
         <Empty>
           <Empty.Title>Failed to load billing information</Empty.Title>
           <Empty.Description>
-            There was an error loading your billing information. Please try again later.
+            There was an error loading your billing information. Please try
+            again later.
           </Empty.Description>
         </Empty>
       </div>
@@ -62,11 +63,15 @@ export const Client: React.FC = () => {
   const subscription = billingInfo?.subscription;
   const currentProductId = billingInfo?.currentProductId;
 
-  const allowUpdate = subscription && ["active", "trialing"].includes(subscription.status);
+  const allowUpdate =
+    subscription && ["active", "trialing"].includes(subscription.status);
 
   const isFreeTier = !subscription || subscription.status !== "active";
-  const allowCancel = subscription && subscription.status === "active" && !subscription.cancelAt;
-  const currentProduct = allowUpdate ? products.find((p) => p.id === currentProductId) : undefined;
+  const allowCancel =
+    subscription && subscription.status === "active" && !subscription.cancelAt;
+  const currentProduct = allowUpdate
+    ? products.find((p) => p.id === currentProductId)
+    : undefined;
 
   return (
     <div>
@@ -95,7 +100,6 @@ export const Client: React.FC = () => {
               isOpen={showPlanModal}
               onOpenChange={setShowPlanModal}
               products={products}
-              workspaceSlug={workspace.slug}
               currentProductId={currentProductId}
               isChangingPlan={true}
             />
@@ -110,7 +114,9 @@ export const Client: React.FC = () => {
           >
             <div className="flex justify-end w-full">
               <Button variant="primary">
-                <Link href={`/${workspace.slug}/settings/billing/stripe/checkout`}>
+                <Link
+                  href={`/${workspace.slug}/settings/billing/stripe/checkout`}
+                >
                   Add payment method
                 </Link>
               </Button>
@@ -127,7 +133,11 @@ export const Client: React.FC = () => {
           >
             <div className="w-full flex h-full items-center justify-end gap-4">
               <Button variant="outline" size="lg">
-                <Link href={`/${workspace.slug}/settings/billing/stripe/portal`}>Open Portal</Link>
+                <Link
+                  href={`/${workspace.slug}/settings/billing/stripe/portal`}
+                >
+                  Open Portal
+                </Link>
               </Button>
             </div>
           </SettingCard>
