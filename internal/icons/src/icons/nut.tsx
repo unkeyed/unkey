44cf4f8239d4312ff6dca0fdea28a5435a04044f
Eeyoritron <michael@unkey.com>
2025-10-28 14:16:45 -0400

    fix nut icon

--- internal/icons/src/icons/nut.tsx
+++ internal/icons/src/icons/nut.tsx
@@ -12,30 +12,38 @@
 import type React from "react";
 import { type IconProps, sizeMap } from "../props";
 
-export const Nut: React.FC<IconProps> = ({ iconSize = "xl-thin", ...props }) => {
+export const Nut: React.FC<IconProps> = ({
+  iconSize = "xl-thin",
+  ...props
+}) => {
   const { iconSize: pixelSize, strokeWidth } = sizeMap[iconSize];
   return (
     <svg
       height={pixelSize}
       width={pixelSize}
+      {...props}
       viewBox="0 0 18 18"
       xmlns="http://www.w3.org/2000/svg"
-      {...props}
     >
-      <g
-        fill="currentColor"
-        strokeLinecap="square"
-        strokeLinejoin="miter"
-        strokeMiterlimit="10"
-        strokeWidth={strokeWidth}
-      >
-        <polygon
+      <g fill="currentColor">
+        <circle
+          cx="9"
+          cy="9"
           fill="none"
-          points="9 29 1 16 9 3 23 3 31 16 23 29 9 29"
+          r="2.25"
           stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           strokeWidth={strokeWidth}
         />
-        <circle cx="16" cy="16" fill="none" r="6" stroke="currentColor" strokeWidth={strokeWidth} />
+        <path
+          d="M11.473,2.75H6.527c-.713,0-1.372,.38-1.73,.997L2.332,7.997c-.36,.621-.36,1.386,0,2.007l2.465,4.25c.358,.617,1.017,.997,1.73,.997h4.946c.713,0,1.372-.38,1.73-.997l2.465-4.25c.36-.621,.36-1.386,0-2.007l-2.465-4.25c-.358-.617-1.017-.997-1.73-.997Z"
+          fill="none"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={strokeWidth}
+        />
       </g>
     </svg>
   );
