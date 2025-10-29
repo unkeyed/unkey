3715c570d40fd543afb2eb666ae1ede482f6637c
Flo <flo@unkey.com>
2025-10-29 10:58:54 +0100

    feat: delete credits when hard deleting identities

--- go/pkg/db/queries/identity_delete_old_with_ratelimits.sql
+++ go/pkg/db/queries/identity_delete_old_with_ratelimits.sql
@@ -1,7 +1,8 @@
 -- name: DeleteOldIdentityWithRatelimits :exec
-DELETE i, rl
+DELETE i, rl, c
 FROM identities i
 LEFT JOIN ratelimits rl ON rl.identity_id = i.id
+LEFT JOIN credits c ON c.identity_id = i.id
 WHERE i.workspace_id = sqlc.arg(workspace_id)
   AND (i.id = sqlc.arg(identity) OR i.external_id = sqlc.arg(identity))
   AND i.deleted = true;
