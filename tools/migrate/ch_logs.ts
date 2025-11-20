52e9c28b75f1898a01cfb0588a818656ad84c445
chronark <dev@chronark.com>
2025-11-20 22:35:13 +0100

    chore: update all table references

--- tools/migrate/ch_logs.ts
+++ tools/migrate/ch_logs.ts
@@ -8,92 +8,108 @@ async function main() {
   });
 
   const conn = await mysql.createConnection(
-    `mysql://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:3306/unkey?ssl={}`,
+    `mysql://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:3306/unkey?ssl={}`
   );
 
   await conn.ping();
   const db = mysqlDrizzle(conn, { schema, mode: "default" });
 
-  const keySpaceCache = new Map<string, string>();
+  const identityCache = new Map<string, string>();
+
+  const aggregatedSchema = z.object({
+    workspace_id: z.string(),
+    key_space_id: z.string(),
+    identity_id: z.string(),
+    external_id: z.string(),
+    key_id: z.string(),
+    outcome: z.string(),
+    tags: z.array(z.string()),
+    count: z.number(),
+    spent_credits: z.number(),
+    latency_avg: z.number(),
+    latency_p75: z.number(),
+    latency_p99: z.number(),
+    time: z.number(),
+  });
+
+  const insert = ch.inserter.insert({
+    table: "default.key_verifications_per_minute_v3",
+    schema: aggregatedSchema,
+  });
 
   const start = 1724930749353;
   const end = 1738212041696;
-  const workspaceId = "ws_wB4SmWrYkhSbWE2rH61S6gMseWw";
 
-  const query = ch.querier.query({
-    query: `
-    SELECT * FROM default.api_requests_raw_v2
-    WHERE workspace_id = '${workspaceId}'
-    AND time > ${start}
-    AND time < ${end}
-    AND path = '/v1/keys/verify'
-    AND response_status = 200
+  const dt = 5 * 60 * 1000;
+
+  for (let t = start; t < end; t += dt) {
+    console.log(
+      `Processing time range: ${new Date(t).toLocaleString()} - ${new Date(
+        t + dt
+      ).toLocaleString()}`
+    );
+    const query = ch.querier.query({
+      query: `
+    SELECT
+      workspace_id,
+      key_space_id,
+      identity_id,
+      external_id,
+      key_id,
+      outcome,
+      tags,
+      count(*) as count,
+      sum(spent_credits) as spent_credits,
+      avgState (latency) as latency_avg,
+      quantilesTDigestState (0.75) (latency) as latency_p75,
+      quantilesTDigestState (0.99) (latency) as latency_p99,
+      toStartOfMinute (fromUnixTimestamp64Milli (time)) AS time
+    FROM
+      key_verifications_raw_v2
+    WHERE time >= fromUnixTimestamp64Milli(${t})
+    AND time < fromUnixTimestamp64Milli(${t + dt})
+    GROUP BY
+      workspace_id,
+      time,
+      key_space_id,
+      identity_id,
+      external_id,
+      key_id,
+      outcome,
+      tags
     `,
-    schema: z
-      .object({
-        request_id: z.string(),
-        time: z.number(),
-        workspace_id: z.string(),
-        response_body: z.string(),
-      })
-      .passthrough(),
-  });
-  const res = await query({});
+      schema: aggregatedSchema,
+    });
+    const res = await query({});
 
-  const logs = res.val!;
+    const rows = res.val!;
 
-  let i = 1;
-  const inserts = [];
-  for (const log of logs) {
-    console.infi(i++, logs.length);
+    let i = 1;
+    const inserts: Array<z.infer<typeof aggregatedSchema>> = [];
+    for (const row of rows) {
+      console.info(i++, rows.length);
 
-    const body = z
-      .object({
-        keyId: z.string().optional(),
-        valid: z.boolean(),
-        ownerId: z.string().optional(),
-        remaining: z.number().optional(),
-        code: z.string().optional(),
-      })
-      .safeParse(JSON.parse(log.response_body));
-    if (!body.success) {
-      console.error(body.error);
-      console.error(log.response_body);
-      continue;
-    }
-
-    if (!body.data.keyId) {
-      continue;
-    }
-
-    let keySpaceId = keySpaceCache.get(body.data.keyId);
-    if (!keySpaceId) {
-      const key = await db.query.keys.findFirst({
-        where: (table, { eq }) => eq(table.id, body.data.keyId),
-      });
-      if (!key) {
-        console.error("Key not found", body.data.keyId);
-        continue;
+      if (row.external_id === "") {
+        const externalId = identityCache.get(row.identity_id);
+        if (!externalId) {
+          const identity = await db.query.identities.findFirst({
+            where: (table, { eq }) => eq(table.id, row.identity_id),
+          });
+          if (!identity) {
+            console.error("identity not found", row.identity_id);
+            continue;
+          }
+          row.external_id = identity.externalId;
+          identityCache.set(identity.id, identity.externalId);
+        }
       }
-      keySpaceId = key.keyAuthId;
-      keySpaceCache.set(body.data.keyId, keySpaceId);
+
+      inserts.push(row);
     }
 
-    const insert = {
-      workspace_id: workspaceId,
-      request_id: log.request_id,
-      time: log.time,
-      key_space_id: keySpaceId,
-      key_id: body.data.keyId,
-      region: "",
-      tags: [],
-      outcome: body.data.code ?? "VALID",
-    };
-
-    console.info(insert);
-    inserts.push(insert);
+    console.log(inserts);
+    //const insertRes = insert(inserts);
+    //console.log(insertRes);
   }
-  await ch.verifications.insert(inserts);
 }
-
 main();
