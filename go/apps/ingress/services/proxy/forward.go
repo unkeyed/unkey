01c518b95515953b3d026e15e31f3d31d70fa6a2
chronark <dev@chronark.com>
2025-11-26 16:05:20 +0100

    fix: http stuff

--- go/apps/ingress/services/proxy/forward.go
+++ go/apps/ingress/services/proxy/forward.go
@@ -62,10 +62,13 @@ func (s *service) forward(sess *zen.Session, cfg forwardConfig) error {
 				}
 
 				urn := codes.Ingress.Proxy.BadGateway.URN()
-				if resp.StatusCode == 503 {
+				switch resp.StatusCode {
+				case http.StatusServiceUnavailable:
 					urn = codes.Ingress.Proxy.ServiceUnavailable.URN()
-				} else if resp.StatusCode == 504 {
+				case http.StatusGatewayTimeout:
 					urn = codes.Ingress.Proxy.GatewayTimeout.URN()
+				case http.StatusBadGateway:
+					urn = codes.Ingress.Proxy.BadGateway.URN()
 				}
 
 				return fault.New(
