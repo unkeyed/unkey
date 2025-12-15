d94b4b593251208103c89580790de627e9af3f57
Ridhim-RR <ridhimraizada.rr@gmail.com>
2025-12-15 22:25:59 +0530

    chore(lint): linting issue resolved

--- apps/dashboard/app/(app)/[workspaceSlug]/settings/billing/client.tsx
+++ apps/dashboard/app/(app)/[workspaceSlug]/settings/billing/client.tsx
@@ -68,7 +68,7 @@ export const Client: React.FC = () => {
   const subscription = billingInfo.subscription;
   const currentProductId = billingInfo.currentProductId;
 
-  const allowUpdate = subscription && ["active", "trialing"].includes(subscription.status);
+  // const allowUpdate = subscription && ["active", "trialing"].includes(subscription.status);
 
   const hasPaidSubscription = Boolean(
     subscription &&
@@ -77,7 +77,9 @@ export const Client: React.FC = () => {
   );
   const isFreeTier = !hasPaidSubscription;
   const allowCancel = subscription && subscription.status === "active" && !subscription.cancelAt;
-  const currentProduct = hasPaidSubscription ? products.find((p) => p.id === currentProductId) : undefined;
+  const currentProduct = hasPaidSubscription
+    ? products.find((p) => p.id === currentProductId)
+    : undefined;
 
   return (
     <div>
