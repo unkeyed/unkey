696554407801ff52a6158085f8ceb843b92b2fbc
James Perkins <james@unkey.com>
2025-12-17 13:47:25 -0500

    remove PII

--- apps/dashboard/sentry.server.config.ts
+++ apps/dashboard/sentry.server.config.ts
@@ -15,5 +15,5 @@ Sentry.init({
 
   // Enable sending user PII (Personally Identifiable Information)
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
-  sendDefaultPii: true,
+  sendDefaultPii: false,
 });
