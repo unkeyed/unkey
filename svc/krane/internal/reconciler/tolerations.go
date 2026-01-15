01902778cf146a61c2000de42ea27d1ccc3aec04
Flo <flo@unkey.com>
2026-01-15 12:41:41 +0100

    singular name in node class

--- svc/krane/internal/reconciler/tolerations.go
+++ svc/krane/internal/reconciler/tolerations.go
@@ -5,13 +5,13 @@ import corev1 "k8s.io/api/core/v1"
 var untrustedToleration = corev1.Toleration{
 	Key:      "node-class",
 	Operator: corev1.TolerationOpEqual,
-	Value:    "untrusted",
+	Value:    NamespaceUntrusted,
 	Effect:   corev1.TaintEffectNoSchedule,
 }
 
 var sentinelToleration = corev1.Toleration{
 	Key:      "node-class",
 	Operator: corev1.TolerationOpEqual,
-	Value:    "sentinels",
+	Value:    NamespaceSentinel,
 	Effect:   corev1.TaintEffectNoSchedule,
 }
