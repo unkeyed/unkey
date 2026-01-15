6e87967702beb7eb6aa5ad3c9edcb6d47a003a53
Flo <flo@unkey.com>
2026-01-15 13:50:20 +0100

    rename constants

--- svc/krane/internal/reconciler/tolerations.go
+++ svc/krane/internal/reconciler/tolerations.go
@@ -5,13 +5,13 @@ import corev1 "k8s.io/api/core/v1"
 var untrustedToleration = corev1.Toleration{
 	Key:      "node-class",
 	Operator: corev1.TolerationOpEqual,
-	Value:    NamespaceUntrusted,
+	Value:    CustomerNodeClass,
 	Effect:   corev1.TaintEffectNoSchedule,
 }
 
 var sentinelToleration = corev1.Toleration{
 	Key:      "node-class",
 	Operator: corev1.TolerationOpEqual,
-	Value:    NamespaceSentinel,
+	Value:    SentinelNodeClass,
 	Effect:   corev1.TaintEffectNoSchedule,
 }
