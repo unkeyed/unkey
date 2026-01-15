d59f120963b228e99c9e3677b9df917c1765d379
Oz <21091016+ogzhanolguncu@users.noreply.github.com>
2026-01-15 14:45:44 +0300

    feat: add getDeployment endpoint (#4604)
    * feat: add generateUploadUrl to api
    
    * chore: update openapi spec
    
    * refactor: remove redundant checks
    
    * refactor: add missing project_id check
    
    * refactor: Remove expiresIn from response
    
    * chore: cleanup
    
    * feat: add getDeployment to api
    
    * chore: add speakeasy grouping
    
    * fix: prefix in openapi spec
    
    * refactor: add regex check to openapi spec
    
    * refactor: move logic to harness
    
    * refactor: simplify deploy test
    
    * fix: lint
    
    ---------
    
    Co-authored-by: Andreas Thomas <dev@chronark.com>

---
+++ svc/api/routes/v2_deploy_get_deployment/401_test.go
@@ -0,0 +1,38 @@
+package handler_test
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/unkeyed/unkey/pkg/testutil"
+	handler "github.com/unkeyed/unkey/svc/api/routes/v2_deploy_get_deployment"
+)
+
+func TestUnauthorizedAccess(t *testing.T) {
+	h := testutil.NewHarness(t)
+
+	route := &handler.Handler{
+		Logger:     h.Logger,
+		DB:         h.DB,
+		Keys:       h.Keys,
+		CtrlClient: h.CtrlDeploymentClient,
+	}
+	h.Register(route)
+
+	t.Run("invalid authorization token", func(t *testing.T) {
+		headers := http.Header{
+			"Content-Type":  {"application/json"},
+			"Authorization": {"Bearer invalid_token_12345"},
+		}
+
+		req := handler.Request{
+			DeploymentId: "d_123abc",
+		}
+
+		res := testutil.CallRoute[handler.Request, handler.Response](h, route, headers, req)
+
+		require.Equal(t, http.StatusUnauthorized, res.Status, "expected 401, received: %#v", res)
+		require.NotNil(t, res.Body)
+	})
+}
