6c1a7e51426cc3015dbbd6c0eeead1bbe607a899
James Perkins <james@unkey.com>
2026-01-28 09:40:03 -0500

    fix email

--- web/internal/resend/emails/welcome_email.tsx
+++ web/internal/resend/emails/welcome_email.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { Button } from "@react-email/button";
 import { Heading } from "@react-email/heading";
 import { Hr } from "@react-email/hr";
@@ -31,7 +30,7 @@ export function WelcomeEmail() {
         <ul className="pb-4 text-sm">
           <li className="pt-4">
             {" "}
-            <Link href="https://go.unkey.com/api-onboard">Quickstart Guides</Link> - Our complete
+            <Link href="https://www.unkey.com/docs/quickstart/onboarding/onboarding-api>Quickstart Guides</Link> - Our complete
             series of guides will help you integrate Unkey step by step.
           </li>
           <li className="pt-4">
