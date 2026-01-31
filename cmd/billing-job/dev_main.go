015030e54473b2809af731541e627181b96fad1b
James Perkins <james@unkey.com>
2026-01-31 15:14:07 -0500

    Fix dev_main.go to pass os.Args as []string

--- cmd/billing-job/dev_main.go
+++ cmd/billing-job/dev_main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	if err := billingjob.Cmd.Run(context.Background(), billingjob.Cmd); err != nil {
+	if err := billingjob.Cmd.Run(context.Background(), os.Args[1:]); err != nil {
 		os.Exit(1)
 	}
 }
