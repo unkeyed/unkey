8a0043301b326ffbdc96567dd88600c1425dbb76
James Perkins <james@unkey.com>
2026-01-31 17:49:03 -0500

    Add debug prints to dev_main.go

--- cmd/billing-job/dev_main.go
+++ cmd/billing-job/dev_main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
 
 	billingjob "github.com/unkeyed/unkey/cmd/billing-job"
 )
 
 func main() {
+	fmt.Println("DEBUG: main() called, args:", os.Args)
+	fmt.Println("DEBUG: billingjob.Cmd is", billingjob.Cmd)
+
 	if err := billingjob.Cmd.Run(context.Background(), os.Args[1:]); err != nil {
+		fmt.Println("DEBUG: Run returned error:", err)
 		os.Exit(1)
 	}
+	fmt.Println("DEBUG: main() completed")
 }
