2fff8f3ecffd2793f6a3c44e2d7e312a089f6f70
Meg Stepp <mcstepp@gmail.com>
2026-02-06 14:03:24 -0500

    update comment

--- pkg/clickhouse/migrations/20260129000000.sql
+++ pkg/clickhouse/migrations/20260129000000.sql
@@ -1,4 +1,4 @@
--- Create "keys_last_used_v2" table with AggregatingMergeTree for pre-aggregated data
+-- Create "keys_last_used_v1" table with AggregatingMergeTree for pre-aggregated data
 CREATE TABLE IF NOT EXISTS `default`.`keys_last_used_v1` (
   `workspace_id` String,
   `key_space_id` String,
@@ -13,7 +13,7 @@ ORDER BY (`workspace_id`, `key_space_id`, `key_id`, `time`)
 TTL toDateTime(time / 1000) + INTERVAL 90 DAY
 SETTINGS index_granularity = 8192;
 
--- Create "keys_last_used_mv_v2" materialized view that pre-aggregates per key
+-- Create "keys_last_used_mv_v1" materialized view that pre-aggregates per key
 CREATE MATERIALIZED VIEW IF NOT EXISTS `default`.`keys_last_used_mv_v1`
 TO `default`.`keys_last_used_v1`
 AS
