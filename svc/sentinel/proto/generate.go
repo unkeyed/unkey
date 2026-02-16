36e38c026ec698c65cada766255a7b0d05ce1a0b
chronark <dev@chronark.com>
2026-02-16 04:47:08 +0100

    feat: middleware rfc

---
+++ svc/sentinel/proto/generate.go
@@ -0,0 +1,4 @@
+package proto
+
+//go:generate go tool buf generate --template ./buf.gen.yaml --path ./middleware
+//go:generate go tool buf generate --template ./buf.gen.ts.yaml --path ./middleware
