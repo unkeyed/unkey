9a40b0e33b86a75274611a8f396479957db02e02
Flo <flo@unkey.com>
2026-02-19 11:13:33 +0100

    move some files

--- svc/frontline/internal/errorpage/errorpage.go
+++ svc/frontline/internal/errorpage/errorpage.go
@@ -4,42 +4,17 @@ import (
 	"bytes"
 	_ "embed"
 	"html/template"
-	"sync"
 )
 
-//go:embed error.html
+//go:embed error.go.tmpl
 var defaultTemplate string
-
-// Data contains all the fields available to the error page template.
-type Data struct {
-	StatusCode int
-	Title      string
-	Message    string
-	ErrorCode  string
-	DocsURL    string
-	RequestID  string
-}
-
-// Renderer renders error pages from Data.
-type Renderer interface {
-	Render(data Data) ([]byte, error)
-}
 
 // defaultRenderer uses the embedded HTML template.
 type defaultRenderer struct {
-	once sync.Once
 	tmpl *template.Template
-	err  error
 }
 
 func (r *defaultRenderer) Render(data Data) ([]byte, error) {
-	r.once.Do(func() {
-		r.tmpl, r.err = template.New("error").Parse(defaultTemplate)
-	})
-	if r.err != nil {
-		return nil, r.err
-	}
-
 	var buf bytes.Buffer
 	if err := r.tmpl.Execute(&buf, data); err != nil {
 		return nil, err
@@ -48,8 +23,10 @@ func (r *defaultRenderer) Render(data Data) ([]byte, error) {
 	return buf.Bytes(), nil
 }
 
-// NewRenderer returns a Renderer that uses the default embedded error page template.
+// NewRenderer returns a [Renderer] that uses the default embedded error page template.
+// Panics if the template fails to parse (should never happen with an embedded template).
 func NewRenderer() Renderer {
-	//nolint:exhaustruct
-	return &defaultRenderer{}
+	return &defaultRenderer{
+		tmpl: template.Must(template.New("error").Parse(defaultTemplate)),
+	}
 }
