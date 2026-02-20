ce2b2114eebdd2cf7734a29f7d2b6f4b969a2eb9
Flo <flo@unkey.com>
2026-02-20 09:15:45 +0100

    fix: add isRolledBack to project insert in create dialog

--- web/apps/dashboard/app/(app)/[workspaceSlug]/projects/_components/dialogs/create-project-dialog.tsx
+++ web/apps/dashboard/app/(app)/[workspaceSlug]/projects/_components/dialogs/create-project-dialog.tsx
@@ -38,6 +38,7 @@ export const CreateProjectDialog = () => {
         slug: values.slug,
         repositoryFullName: null,
         liveDeploymentId: null,
+        isRolledBack: false,
         id: "will-be-replace-by-server",
         latestDeploymentId: null,
         author: "will-be-replace-by-server",
