f163b6ec73b8b550d4f6ba3db4e10e31fcce4f11
James Perkins <james@unkey.com>
2026-02-24 11:17:37 -0500

    Remove this

--- web/apps/engineering/.source/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { GetOutput } from "fumadocs-mdx/config"
-export declare const components: GetOutput<typeof import("../source.config.ts").components>
-export declare const docs: GetOutput<typeof import("../source.config.ts").docs>
-export declare const meta: GetOutput<typeof import("../source.config.ts").meta>
