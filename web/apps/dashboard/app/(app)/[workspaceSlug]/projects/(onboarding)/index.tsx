c1ea45c589d1cf9fae7516d64ef5aa3091aec49d
ogzhanolguncu <ogzhan11@gmail.com>
2026-02-24 16:19:02 +0300

    feat: add first step

--- web/apps/dashboard/app/(app)/[workspaceSlug]/projects/(onboarding)/index.tsx
+++ web/apps/dashboard/app/(app)/[workspaceSlug]/projects/(onboarding)/index.tsx
@@ -1,6 +1,7 @@
 "use client";
-import { CloudUp, Harddrive, HeartPulse, Location2, Nodes2 } from "@unkey/icons";
+import { BookBookmark, CloudUp, Discord, Github, Harddrive, HeartPulse, Layers3, Location2, Nodes2 } from "@unkey/icons";
 import { cn } from "@unkey/ui/src/lib/utils";
+import { Button } from "@unkey/ui";
 
 type IconBoxProps = {
   children?: React.ReactNode;
@@ -30,31 +31,74 @@ const items: { icon: React.ReactNode; large?: boolean; opacity: string }[] = [
   { icon: null, opacity: "opacity-60" },
 ];
 
-export const Onboarding = () => (
-  <div className="flex flex-col items-center justify-center h-screen">
-    <div
-      className="p-2"
-      style={{
-        maskImage: "linear-gradient(to right, transparent, black 15%, black 85%, transparent)",
-        WebkitMaskImage:
-          "linear-gradient(to right, transparent, black 15%, black 85%, transparent)",
-      }}
-    >
-      <div className="flex gap-6 items-center justify-center text-gray-12">
-        {items.map((item, i) => (
-          <IconBox key={i} large={item.large} className={item.opacity}>
-            {item.icon}
-          </IconBox>
-        ))}
+export const Onboarding = () => {
+  // const state = JSON.stringify({ projectId });
+  // const installUrl = `https://github.com/apps/${process.env.NEXT_PUBLIC_GITHUB_APP_NAME}/installations/new?state=${encodeURIComponent(state)}`;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div
+        className="p-2"
+        style={{
+          maskImage: "linear-gradient(to right, transparent, black 15%, black 85%, transparent)",
+          WebkitMaskImage:
+            "linear-gradient(to right, transparent, black 15%, black 85%, transparent)",
+        }}
+      >
+        <div className="flex gap-6 items-center justify-center text-gray-12">
+          {items.map((item, i) => (
+            <IconBox key={i} large={item.large} className={item.opacity}>
+              {item.icon}
+            </IconBox>
+          ))}
+        </div>
+      </div>
+      <div className="mb-5" />
+      {/* Title-desc */}
+      <div className="flex flex-col items-center justify-center gap-2">
+        <div className="font-semibold text-lg text-gray-12">Deploy your first project</div>
+        <div className="text-[13px] text-gray-11 text-center">
+          Connect a GitHub repo and get a live URL in minutes.
+          <br />
+          Unkey handles builds, infra, scaling, and routing.
+        </div>
+      </div>
+      <div className="mb-6" />
+      <div className="border border-grayA-5 rounded-[14px] flex  justify-center items-center gap-4 py-[18px] px-4">
+        <div className="size-8 rounded-[10px] bg-gray-12 grid place-items-center">
+          <Layers3 className="size-[18px] text-gray-1" iconSize="md-medium" />
+        </div>
+        <div className="flex flex-col gap-3">
+          <span className="font-medium text-gray-12 text-[13px] leading-[9px]">Import project</span>
+          <span className="text-gray-10 text-[13px]  leading-[9px]">Add a repo from your GitHub account</span>
+        </div>
+        <Button variant="outline" className="ml-20 rounded-lg border-grayA-4 hover:bg-grayA-2 shadow-sm hover:shadow-md transition-all">
+          <Github className="!size-[18px] text-gray-12 shrink-0" />
+          <a href={""} className="text-sm text-gray-12 font-medium" target="_blank" rel="noopener noreferrer">
+            Import from GitHub
+          </a>
+        </Button>
+      </div>
+      <div className="mb-7" />
+      <div className="flex gap-3 items-center">
+        <Button
+          variant="outline"
+          className="text-gray-12 text-[13px] font-medium border border-grayA-4 gap-2 rounded-full flex items-center px-3 py-1.5 transition-all"
+          onClick={() => window.open("https://www.unkey.com/docs/introduction", "_blank", "noopener,noreferrer")}
+        >
+          <BookBookmark className="text-gray-12 shrink-0 size-[18px]" iconSize="sm-regular" />
+          View documentation
+        </Button>
+        <Button
+          variant="outline"
+          className="text-gray-12 text-[13px] font-medium border border-grayA-4 gap-2 rounded-full flex items-center px-3 py-1.5 transition-all"
+          onClick={() => window.open("https://discord.gg/fDbezjbJbD", "_blank", "noopener,noreferrer")}
+        >
+          <div className="size-[18px] overflow-hidden flex items-center justify-center">
+            <Discord className="text-feature-11 shrink-0" style={{ width: 18, height: 18 }} iconSize="sm-regular" />
+          </div>
+          Join community
+        </Button>
       </div>
     </div>
-
-    <div className="mb-5" />
-    {/* Title-desc */}
-    <div className="flex flex-col items-center justify-center gap-2" />
-    <div className="mb-6" />
-    {/* actions */}
-    <div className="mb-7" />
-    {/* sub-actions */}
-  </div>
-);
+  )
+};
---
+++ web/internal/icons/src/icons/discord.tsx
@@ -0,0 +1,27 @@
+/**
+ * Copyright © Nucleo
+ * Version 1.3, January 3, 2024
+ * Nucleo Icons
+ * https://nucleoapp.com/
+ * - Redistribution of icons is prohibited.
+ * - Icons are restricted for use only within the product they are bundled with.
+ *
+ * For more details:
+ * https://nucleoapp.com/license
+ */
+
+import { type IconProps, sizeMap } from "../props";
+
+export function Discord({ iconSize = "xl-thin", ...props }: IconProps) {
+  const { iconSize: pixelSize } = sizeMap[iconSize];
+
+  return (
+    <svg height={pixelSize} width=
+      {pixelSize} viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <g fill="currentColor">
+        <path d="M26.413,6.536c-1.971-.902-4.052-1.543-6.189-1.904-.292,.523-.557,1.061-.793,1.612-2.277-.343-4.592-.343-6.869,0-.236-.551-.5-1.089-.793-1.612-2.139,.365-4.221,1.006-6.194,1.909C1.658,12.336,.596,17.987,1.127,23.558h0c2.294,1.695,4.861,2.984,7.591,3.811,.615-.827,1.158-1.704,1.626-2.622-.888-.332-1.744-.741-2.56-1.222,.215-.156,.425-.316,.628-.472,4.806,2.26,10.37,2.26,15.177,0,.205,.168,.415,.328,.628,.472-.817,.483-1.676,.892-2.565,1.225,.467,.918,1.011,1.794,1.626,2.619,2.732-.824,5.301-2.112,7.596-3.808h0c.623-6.461-1.064-12.06-4.46-17.025Zm-15.396,13.596c-1.479,0-2.702-1.343-2.702-2.994s1.18-3.006,2.697-3.006,2.73,1.354,2.704,3.006-1.192,2.994-2.699,2.994Zm9.967,0c-1.482,0-2.699-1.343-2.699-2.994s1.18-3.006,2.699-3.006,2.723,1.354,2.697,3.006-1.189,2.994-2.697,2.994Z" />
+      </g>
+    </svg>
+  );
+};
+
