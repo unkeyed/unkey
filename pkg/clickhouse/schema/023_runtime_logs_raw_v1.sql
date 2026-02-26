9dd826a6825e8411495f3899725f14bc7dda6343
Flo <flo@unkey.com>
2026-02-26 11:28:06 +0100

    remove app internal services

--- pkg/clickhouse/schema/023_runtime_logs_raw_v1.sql
+++ pkg/clickhouse/schema/023_runtime_logs_raw_v1.sql
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS default.runtime_logs_raw_v1
     `workspace_id` String CODEC(ZSTD(1)),
     `project_id` String CODEC(ZSTD(1)),
     `environment_id` String CODEC(ZSTD(1)),
-    `app_id` String DEFAULT '' CODEC(ZSTD(1)),
+    `app_id` String CODEC(ZSTD(1)),
     `deployment_id` String CODEC(ZSTD(1)),
 
     -- K8s metadata (pod name for identifying specific replica)
