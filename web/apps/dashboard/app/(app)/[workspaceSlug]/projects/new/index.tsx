e7b0a3603fa8df305c6203aa680c760057a3784e
ogzhanolguncu <ogzhan11@gmail.com>
2026-03-04 00:13:29 +0300

    fix: add installation step abck

--- web/apps/dashboard/app/(app)/[workspaceSlug]/projects/new/index.tsx
+++ web/apps/dashboard/app/(app)/[workspaceSlug]/projects/new/index.tsx
@@ -45,26 +45,24 @@ export const Onboarding = () => {
             <CreateProjectStep onProjectCreated={setProjectId} />
           </div>
         </StepWizard.Step>
-        {!context?.hasGithubInstallation && (
-          <StepWizard.Step id="connect-github" label="Connect GitHub">
-            {projectId ? (
-              <div className="flex flex-col items-center justify-center h-screen">
-                <OnboardingStepHeader
-                  title={isFirstProject ? "Deploy your first project" : "Deploy your project"}
-                  showIconRow
-                  subtitle={
-                    <>
-                      Connect a GitHub repo and get a live URL in minutes.
-                      <br />
-                      Unkey handles builds, infra, scaling, and routing.
-                    </>
-                  }
-                />
-                <ConnectGithubStep projectId={projectId} />
-              </div>
-            ) : null}
-          </StepWizard.Step>
-        )}
+        <StepWizard.Step id="connect-github" label="Connect GitHub">
+          {projectId ? (
+            <div className="flex flex-col items-center justify-center h-screen">
+              <OnboardingStepHeader
+                title={isFirstProject ? "Deploy your first project" : "Deploy your project"}
+                showIconRow
+                subtitle={
+                  <>
+                    Connect a GitHub repo and get a live URL in minutes.
+                    <br />
+                    Unkey handles builds, infra, scaling, and routing.
+                  </>
+                }
+              />
+              <ConnectGithubStep projectId={projectId} />
+            </div>
+          ) : null}
+        </StepWizard.Step>
         <StepWizard.Step id="select-repo" label="Select repository">
           {projectId ? (
             <div className="flex flex-col items-center justify-center mt-14">
