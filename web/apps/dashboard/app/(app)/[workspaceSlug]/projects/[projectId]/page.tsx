22d78170b1d883c66d1c38ee029c9ec3acb64936
ogzhanolguncu <ogzhan11@gmail.com>
2026-03-03 14:29:53 +0300

    fix: nextjs serverside param fetch issue

--- web/apps/dashboard/app/(app)/[workspaceSlug]/projects/[projectId]/page.tsx
+++ web/apps/dashboard/app/(app)/[workspaceSlug]/projects/[projectId]/page.tsx
@@ -1,8 +1,8 @@
 import { redirect } from "next/navigation";
 
 export default async function ProjectDetails(props: {
-  params: { workspaceSlug: string; projectId: string };
+  params: Promise<{ workspaceSlug: string; projectId: string }>;
 }) {
-  const { workspaceSlug, projectId } = props.params;
+  const { workspaceSlug, projectId } = await props.params;
   redirect(`/${workspaceSlug}/projects/${projectId}/deployments`);
 }
