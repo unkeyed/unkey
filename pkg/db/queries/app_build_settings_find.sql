8578a212aa6e15bdbf6be227314305f61e64dc71
Flo <flo@unkey.com>
2026-03-04 11:18:46 +0100

    chore: remove unused FindAppBuildSettingsByAppAndEnv query

--- pkg/db/queries/app_build_settings_find.sql
+++
@@ -1,5 +0,0 @@
--- name: FindAppBuildSettingsByAppAndEnv :one
-SELECT sqlc.embed(app_build_settings)
-FROM app_build_settings
-WHERE app_id = sqlc.arg(app_id)
-  AND environment_id = sqlc.arg(environment_id);
