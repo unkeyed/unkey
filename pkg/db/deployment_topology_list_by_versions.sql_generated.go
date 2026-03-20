374e6020098789a3fa251741027879b64847f9dd
Flo <flo@unkey.com>
2026-03-20 16:40:46 +0100

    chore: insert some interesting env vars

--- pkg/db/deployment_topology_list_by_versions.sql_generated.go
+++ pkg/db/deployment_topology_list_by_versions.sql_generated.go
@@ -14,11 +14,20 @@ const listDeploymentTopologyByRegion = `-- name: ListDeploymentTopologyByRegion 
 SELECT
     dt.pk, dt.workspace_id, dt.deployment_id, dt.region_id, dt.desired_replicas, dt.version, dt.desired_status, dt.created_at, dt.updated_at,
     d.pk, d.id, d.k8s_name, d.workspace_id, d.project_id, d.environment_id, d.app_id, d.image, d.build_id, d.git_commit_sha, d.git_branch, d.git_commit_message, d.git_commit_author_handle, d.git_commit_author_avatar_url, d.git_commit_timestamp, d.sentinel_config, d.cpu_millicores, d.memory_mib, d.desired_state, d.encrypted_environment_variables, d.command, d.port, d.shutdown_signal, d.healthcheck, d.pr_number, d.fork_repository_full_name, d.github_deployment_id, d.status, d.created_at, d.updated_at,
-    w.k8s_namespace
+    w.k8s_namespace,
+    p.slug AS project_slug,
+    a.slug AS app_slug,
+    e.slug AS environment_slug,
+    r.name AS region_name,
+    grc.repository_full_name AS git_repo
 FROM ` + "`" + `deployment_topology` + "`" + ` dt
 INNER JOIN ` + "`" + `deployments` + "`" + ` d ON dt.deployment_id = d.id
 INNER JOIN ` + "`" + `workspaces` + "`" + ` w ON d.workspace_id = w.id
 INNER JOIN ` + "`" + `regions` + "`" + ` r ON dt.region_id = r.id
+INNER JOIN ` + "`" + `projects` + "`" + ` p ON d.project_id = p.id
+INNER JOIN ` + "`" + `apps` + "`" + ` a ON d.app_id = a.id
+INNER JOIN ` + "`" + `environments` + "`" + ` e ON d.environment_id = e.id
+LEFT JOIN ` + "`" + `github_repo_connections` + "`" + ` grc ON d.app_id = grc.app_id
 WHERE r.id = ? AND dt.version > ?
 ORDER BY dt.version ASC
 LIMIT ?
@@ -34,6 +43,11 @@ type ListDeploymentTopologyByRegionRow struct {
 	DeploymentTopology DeploymentTopology `db:"deployment_topology"`
 	Deployment         Deployment         `db:"deployment"`
 	K8sNamespace       sql.NullString     `db:"k8s_namespace"`
+	ProjectSlug        string             `db:"project_slug"`
+	AppSlug            string             `db:"app_slug"`
+	EnvironmentSlug    string             `db:"environment_slug"`
+	RegionName         string             `db:"region_name"`
+	GitRepo            sql.NullString     `db:"git_repo"`
 }
 
 // ListDeploymentTopologyByRegion returns deployment topologies for a region with version > after_version.
@@ -42,11 +56,20 @@ type ListDeploymentTopologyByRegionRow struct {
 //	SELECT
 //	    dt.pk, dt.workspace_id, dt.deployment_id, dt.region_id, dt.desired_replicas, dt.version, dt.desired_status, dt.created_at, dt.updated_at,
 //	    d.pk, d.id, d.k8s_name, d.workspace_id, d.project_id, d.environment_id, d.app_id, d.image, d.build_id, d.git_commit_sha, d.git_branch, d.git_commit_message, d.git_commit_author_handle, d.git_commit_author_avatar_url, d.git_commit_timestamp, d.sentinel_config, d.cpu_millicores, d.memory_mib, d.desired_state, d.encrypted_environment_variables, d.command, d.port, d.shutdown_signal, d.healthcheck, d.pr_number, d.fork_repository_full_name, d.github_deployment_id, d.status, d.created_at, d.updated_at,
-//	    w.k8s_namespace
+//	    w.k8s_namespace,
+//	    p.slug AS project_slug,
+//	    a.slug AS app_slug,
+//	    e.slug AS environment_slug,
+//	    r.name AS region_name,
+//	    grc.repository_full_name AS git_repo
 //	FROM `deployment_topology` dt
 //	INNER JOIN `deployments` d ON dt.deployment_id = d.id
 //	INNER JOIN `workspaces` w ON d.workspace_id = w.id
 //	INNER JOIN `regions` r ON dt.region_id = r.id
+//	INNER JOIN `projects` p ON d.project_id = p.id
+//	INNER JOIN `apps` a ON d.app_id = a.id
+//	INNER JOIN `environments` e ON d.environment_id = e.id
+//	LEFT JOIN `github_repo_connections` grc ON d.app_id = grc.app_id
 //	WHERE r.id = ? AND dt.version > ?
 //	ORDER BY dt.version ASC
 //	LIMIT ?
@@ -100,6 +123,11 @@ func (q *Queries) ListDeploymentTopologyByRegion(ctx context.Context, db DBTX, a
 			&i.Deployment.CreatedAt,
 			&i.Deployment.UpdatedAt,
 			&i.K8sNamespace,
+			&i.ProjectSlug,
+			&i.AppSlug,
+			&i.EnvironmentSlug,
+			&i.RegionName,
+			&i.GitRepo,
 		); err != nil {
 			return nil, err
 		}
