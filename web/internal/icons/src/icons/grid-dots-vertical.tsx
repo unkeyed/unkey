b7d9edbd29b876c1a9936671748a6243b80a3244
ogzhanolguncu <ogzhan11@gmail.com>
2026-04-03 22:01:04 +0300

    chore: add handle icon

--- web/internal/icons/src/icons/grid-dots-vertical.tsx
+++ web/internal/icons/src/icons/grid-dots-vertical.tsx
@@ -16,16 +16,75 @@ export function GripDotsVertical({ iconSize = "xl-thin", ...props }: IconProps) 
   const { iconSize: pixelSize, strokeWidth } = sizeMap[iconSize];
 
   return (
-    <svg height={pixelSize} width={pixelSize} viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <svg
+      height={pixelSize}
+      width={pixelSize}
+      viewBox="0 0 18 18"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
       <g fill="currentColor">
-        <circle cx="6.75" cy="9" fill="currentColor" r=".5" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={strokeWidth} />
-        <circle cx="6.75" cy="3.75" fill="currentColor" r=".5" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={strokeWidth} />
-        <circle cx="6.75" cy="14.25" fill="currentColor" r=".5" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={strokeWidth} />
-        <circle cx="11.25" cy="9" fill="currentColor" r=".5" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={strokeWidth} />
-        <circle cx="11.25" cy="3.75" fill="currentColor" r=".5" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={strokeWidth} />
-        <circle cx="11.25" cy="14.25" fill="currentColor" r=".5" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={strokeWidth} />
+        <circle
+          cx="6.75"
+          cy="9"
+          fill="currentColor"
+          r=".5"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={strokeWidth}
+        />
+        <circle
+          cx="6.75"
+          cy="3.75"
+          fill="currentColor"
+          r=".5"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={strokeWidth}
+        />
+        <circle
+          cx="6.75"
+          cy="14.25"
+          fill="currentColor"
+          r=".5"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={strokeWidth}
+        />
+        <circle
+          cx="11.25"
+          cy="9"
+          fill="currentColor"
+          r=".5"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={strokeWidth}
+        />
+        <circle
+          cx="11.25"
+          cy="3.75"
+          fill="currentColor"
+          r=".5"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={strokeWidth}
+        />
+        <circle
+          cx="11.25"
+          cy="14.25"
+          fill="currentColor"
+          r=".5"
+          stroke="currentColor"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={strokeWidth}
+        />
       </g>
     </svg>
   );
-};
-
+}
