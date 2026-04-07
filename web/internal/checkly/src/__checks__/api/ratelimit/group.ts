11d4b610e16ab372de83da59cba74f5fed56a42b
chronark <dev@chronark.com>
2026-04-07 18:52:27 +0200

    chore: remove checkly
    we don't use it here at all, if anything we need this in the infra repo

--- web/internal/checkly/src/__checks__/api/ratelimit/group.ts
+++
@@ -1,7 +0,0 @@
-import { CheckGroup } from "checkly/constructs";
-import { ALL_LOCATIONS } from "../../../locations";
-
-export const ratelimitsV1 = new CheckGroup("/v1/ratelimits", {
-  name: "ratelimits",
-  locations: ALL_LOCATIONS,
-});
