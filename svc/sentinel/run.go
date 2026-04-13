dcc9ce75c1d7f49848523cd68c7723c356f4ba4b
Flo <53355483+Flo4604@users.noreply.github.com>
2026-04-13 22:10:42 +0200

    fix: otel prom (#5748)

--- svc/sentinel/run.go
+++ svc/sentinel/run.go
@@ -59,11 +59,12 @@ func Run(ctx context.Context, cfg Config) error {
 	var shutdownGrafana func(context.Context) error
 	if cfg.Observability.Tracing != nil {
 		shutdownGrafana, err = otel.InitGrafana(ctx, otel.Config{
-			Application:     "sentinel",
-			Version:         version.Version,
-			InstanceID:      cfg.SentinelID,
-			CloudRegion:     cfg.Region,
-			TraceSampleRate: cfg.Observability.Tracing.SampleRate,
+			Application:        "sentinel",
+			Version:            version.Version,
+			InstanceID:         cfg.SentinelID,
+			CloudRegion:        cfg.Region,
+			TraceSampleRate:    cfg.Observability.Tracing.SampleRate,
+			PrometheusGatherer: nil,
 		})
 		if err != nil {
 			return fmt.Errorf("unable to init grafana: %w", err)
