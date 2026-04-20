ca9131a62792cd08239edf1531c3a4da323cf7c1
ogzhanolguncu <ogzhan11@gmail.com>
2026-04-20 15:10:33 +0300

    refactor: use validated query from middleware

--- web/apps/dashboard/lib/trpc/routers/deploy/environment-settings/sentinel/generate-policies/index.ts
+++ web/apps/dashboard/lib/trpc/routers/deploy/environment-settings/sentinel/generate-policies/index.ts
@@ -6,6 +6,6 @@ import { generatePoliciesFromLLM } from "./utils";
 export const generatePolicies = workspaceProcedure
   .use(withLlmAccess())
   .input(z.object({ query: z.string() }))
-  .mutation(async ({ input }) => {
-    return await generatePoliciesFromLLM(openai, input.query);
+  .mutation(async ({ ctx }) => {
+    return await generatePoliciesFromLLM(openai, ctx.validatedQuery);
   });
