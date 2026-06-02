a42c3a220d3adb14497113b29d60395d295f948d
ogzhanolguncu <ogzhan11@gmail.com>
2026-06-02 18:09:07 +0300

    chore: fmt

--- web/apps/dashboard/app/(app)/[workspaceSlug]/projects/page.tsx
+++ web/apps/dashboard/app/(app)/[workspaceSlug]/projects/page.tsx
@@ -1,15 +1,17 @@
 "use client";
 
-import { ProjectsListControls } from "./_components/controls"
-import { ProjectsList } from "./_components/list"
-import { ProjectsListNavigation } from "./navigation"
 import { NewNavigationBanner } from "@/components/navigation/new-navigation-banner";
+import { ProjectsListControls } from "./_components/controls";
+import { ProjectsList } from "./_components/list";
+import { ProjectsListNavigation } from "./navigation";
 
 export default function ProjectsPage() {
-  return <div>
-    <ProjectsListNavigation />
-    <ProjectsListControls />
-    <ProjectsList />
+  return (
+    <div>
+      <ProjectsListNavigation />
+      <ProjectsListControls />
+      <ProjectsList />
       <NewNavigationBanner />
-  </div>
+    </div>
+  );
 }
