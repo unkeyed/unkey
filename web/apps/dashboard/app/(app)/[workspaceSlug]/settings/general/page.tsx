1281394e8d9e9d8006c12211df2297bae63250ee
Oz <21091016+ogzhanolguncu@users.noreply.github.com>
2026-06-11 18:19:15 +0300

    fix(dashboard): remove double copy ws (#6407)

--- web/apps/dashboard/app/(app)/[workspaceSlug]/settings/general/page.tsx
+++ web/apps/dashboard/app/(app)/[workspaceSlug]/settings/general/page.tsx
@@ -1,21 +1,9 @@
 "use client";
 
-import { CopyableIDButton } from "@/components/navigation/copyable-id-button";
 import { useWorkspaceNavigation } from "@/hooks/use-workspace-navigation";
-import {
-  PageBody,
-  PageContainer,
-  PageHeader,
-  PageHeaderActions,
-  PageHeaderContent,
-  PageHeaderTitle,
-} from "@unkey/ui";
+import { PageBody, PageContainer, PageHeader, PageHeaderContent, PageHeaderTitle } from "@unkey/ui";
 import { CopyWorkspaceId } from "./copy-workspace-id";
 import { UpdateWorkspaceName } from "./update-workspace-name";
-
-/**
- * TODO: WorkOS doesn't have workspace images
- */
 
 export default function SettingsPage() {
   const workspace = useWorkspaceNavigation();
@@ -26,9 +14,6 @@ export default function SettingsPage() {
         <PageHeaderContent>
           <PageHeaderTitle>General</PageHeaderTitle>
         </PageHeaderContent>
-        <PageHeaderActions>
-          <CopyableIDButton value={workspace.id} />
-        </PageHeaderActions>
       </PageHeader>
       <PageBody>
         <div className="w-full flex flex-col pt-4">
