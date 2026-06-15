e788ae6b401ce8ae7be87ddc562d149ee13f8a72
Flo <53355483+Flo4604@users.noreply.github.com>
2026-06-15 12:55:28 +0200

    fix: dont write metrics via oltp unless necessary (#6449)

--- svc/vault/run.go
+++ svc/vault/run.go
@@ -62,6 +62,7 @@ func Run(ctx context.Context, cfg Config) error {
 	reg.MustRegister(collectors.NewGoCollector())
 	//nolint:exhaustruct
 	reg.MustRegister(collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}))
+	reg.MustRegister(prometheus.NewSystemMetricsCollector())
 	lazy.SetRegistry(reg)
 	buildinfo.RegisterBuildInfoMetrics("vault")
 
