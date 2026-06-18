242a94db63f82266b7c5e47b2da9da8e16b5e277
ogzhanolguncu <ogzhan11@gmail.com>
2026-06-18 17:09:01 +0300

    revert: removed query

--- pkg/db/querier_generated.go
+++ pkg/db/querier_generated.go
@@ -1132,6 +1132,20 @@ type Querier interface {
 	//  WHERE workspace_id = ? AND id = ?
 	//  LIMIT 1
 	FindProjectByWorkspaceAndId(ctx context.Context, db DBTX, arg FindProjectByWorkspaceAndIdParams) (FindProjectByWorkspaceAndIdRow, error)
+	//FindProjectByWorkspaceAndSlug
+	//
+	//  SELECT
+	//      id,
+	//      workspace_id,
+	//      name,
+	//      slug,
+	//      delete_protection,
+	//      created_at,
+	//      updated_at
+	//  FROM projects
+	//  WHERE workspace_id = ? AND slug = ?
+	//  LIMIT 1
+	FindProjectByWorkspaceAndSlug(ctx context.Context, db DBTX, arg FindProjectByWorkspaceAndSlugParams) (FindProjectByWorkspaceAndSlugRow, error)
 	//FindQuotaByWorkspaceID
 	//
 	//  SELECT pk, workspace_id, requests_per_month, logs_retention_days, audit_logs_retention_days, team, ratelimit_api_limit, ratelimit_api_duration, allocated_cpu_millicores_total, allocated_memory_mib_total, allocated_storage_mib_total, max_cpu_millicores_per_instance, max_memory_mib_per_instance, max_storage_mib_per_instance, max_concurrent_builds
