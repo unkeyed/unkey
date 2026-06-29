fb1ddfd528c7679afe687902dfe004db0799d6c1
ogzhanolguncu <ogzhan11@gmail.com>
2026-06-29 13:15:07 +0300

    chore: remove unused query

--- pkg/db/queries/app_regional_settings_delete_by_app_env_region.sql
+++
@@ -1,5 +0,0 @@
--- name: DeleteAppRegionalSettingByAppEnvRegion :exec
-DELETE FROM app_regional_settings
-WHERE app_id = sqlc.arg(app_id)
-  AND environment_id = sqlc.arg(environment_id)
-  AND region_id = sqlc.arg(region_id);
