bcca538d64b717b0023ab651f257fc88cb37de01
Dave Hawkins <dave@davehawkins.co>
2026-07-09 09:16:04 +0100

    fix(dashboard): match new keyspace button to project header button style

--- web/apps/dashboard/app/(app)/[workspaceSlug]/apis/_components/create-api-button.tsx
+++ web/apps/dashboard/app/(app)/[workspaceSlug]/apis/_components/create-api-button.tsx
@@ -1,10 +1,6 @@
 "use client";
 
 import { revalidate } from "@/app/actions";
-import {
-  NavbarActionButton,
-  type NavbarActionButtonProps,
-} from "@/components/navigation/action-button";
 import { routes } from "@/lib/navigation/routes";
 import { trpc } from "@/lib/trpc/client";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -12,7 +8,6 @@ import { Plus } from "@unkey/icons";
 import { Button, FormInput, toast } from "@unkey/ui";
 import dynamic from "next/dynamic";
 import { useRouter } from "next/navigation";
-import type React from "react";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -32,14 +27,9 @@ const formSchema = z.object({
 type Props = {
   defaultOpen?: boolean;
   workspaceSlug: string;
-  size?: NavbarActionButtonProps["size"];
 };
 
-export const CreateApiButton = ({
-  defaultOpen,
-  workspaceSlug,
-  ...rest
-}: Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, "size"> & Props) => {
+export const CreateApiButton = ({ defaultOpen, workspaceSlug }: Props) => {
   const [isOpen, setIsOpen] = useState(defaultOpen ?? false);
   const router = useRouter();
   const { api } = trpc.useUtils();
@@ -72,15 +62,10 @@ export const CreateApiButton = ({
 
   return (
     <>
-      <NavbarActionButton
-        title="New keyspace"
-        {...rest}
-        color="default"
-        onClick={() => setIsOpen(true)}
-      >
-        <Plus />
+      <Button size="md" variant="primary" onClick={() => setIsOpen(true)}>
+        <Plus iconSize="sm-regular" />
         New keyspace
-      </NavbarActionButton>
+      </Button>
 
       <DynamicDialogContainer
         isOpen={isOpen}
