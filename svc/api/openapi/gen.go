fd28099bc66e83d4b30205de579bfad3262d0b36
Andreas Thomas <dev@chronark.com>
2026-07-10 07:52:01 +0200

    feat(api): free-form search for permissions list (#6641)
    * feat(api): free-form search for permissions list
    
    * feat(api): free-form search for projects list (#6642)
    
    * feat(api): free-form search for apps list (#6643)
    
    * feat(api): free-form search for projects list
    
    * feat(api): free-form search for apps list

--- svc/api/openapi/gen.go
+++ svc/api/openapi/gen.go
@@ -1044,6 +1044,9 @@ type V2AppsListAppsRequestBody struct {
 	// Project Identifies a resource by either its unique ID or its slug.
 	// Accepts a prefixed ID (such as 'proj_' or 'app_') or a slug.
 	Project ResourceIdentifier `json:"project"`
+
+	// Search Free-form text to filter apps. Returns apps whose ID, name, or slug contains the search string. Matching is case-insensitive.
+	Search *string `json:"search,omitempty"`
 }
 
 // V2AppsListAppsResponseBody defines model for V2AppsListAppsResponseBody.
@@ -2577,6 +2580,9 @@ type V2PermissionsListPermissionsRequestBody struct {
 
 	// Limit Maximum number of permissions to return in a single response.
 	Limit *int `json:"limit,omitempty"`
+
+	// Search Free-form text to filter permissions. Returns permissions whose ID, name, slug, or description contains the search string. Matching is case-insensitive.
+	Search *string `json:"search,omitempty"`
 }
 
 // V2PermissionsListPermissionsResponseBody defines model for V2PermissionsListPermissionsResponseBody.
@@ -2849,6 +2855,9 @@ type V2ProjectsListProjectsRequestBody struct {
 	// Limit Maximum number of projects to return per request.
 	// Balance between response size and number of pagination calls needed.
 	Limit *int `json:"limit,omitempty"`
+
+	// Search Free-form text to filter projects. Returns projects whose ID, name, or slug contains the search string. Matching is case-insensitive.
+	Search *string `json:"search,omitempty"`
 }
 
 // V2ProjectsListProjectsResponseBody defines model for V2ProjectsListProjectsResponseBody.
