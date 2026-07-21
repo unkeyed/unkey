97d3a1d5bc45dd7cde91e9a228adb2258a453880
chronark <dev@chronark.com>
2026-07-21 13:03:00 +0200

    fix(ci): harden database schema gate

--- pkg/mysql/schema/quota.sql
+++ pkg/mysql/schema/quota.sql
@@ -15,7 +15,6 @@ CREATE TABLE `quota` (
 	`max_storage_mib_per_instance` int unsigned NOT NULL DEFAULT 10240,
 	`max_concurrent_builds` int unsigned NOT NULL DEFAULT 1,
 	`max_replicas_per_region` int unsigned NOT NULL DEFAULT 4,
-	`schema_gate_test` int,
 	CONSTRAINT `quota_pk` PRIMARY KEY(`pk`),
 	CONSTRAINT `quota_workspace_id_unique` UNIQUE(`workspace_id`)
 );
