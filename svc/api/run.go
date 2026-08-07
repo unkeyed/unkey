918812fa0285332ca6146a7e75d35b140c4ef8a6
Flo <flo@unkey.com>
2026-08-07 20:36:41 +0000

    perf(test): remove slow integration-test work

--- svc/api/run.go
+++ svc/api/run.go
@@ -171,6 +171,11 @@ func Run(ctx context.Context, cfg Config) error {
 	ratelimits := batch.NewNoop[schema.Ratelimit]()
 
 	if cfg.ClickHouse.URL != "" {
+		flushInterval := 5 * time.Second
+		if cfg.Test.ClickHouseFlushInterval > 0 {
+			flushInterval = cfg.Test.ClickHouseFlushInterval
+		}
+
 		chClient, chErr := clickhouse.New(clickhouse.Config{
 			URL: cfg.ClickHouse.URL,
 		})
@@ -183,7 +188,7 @@ func Run(ctx context.Context, cfg Config) error {
 			Name:          "api_requests",
 			BatchSize:     10_000,
 			BufferSize:    20_000,
-			FlushInterval: 5 * time.Second,
+			FlushInterval: flushInterval,
 			Consumers:     2,
 			Drop:          true,
 			OnFlushError:  nil,
@@ -192,7 +197,7 @@ func Run(ctx context.Context, cfg Config) error {
 			Name:          "key_verifications",
 			BatchSize:     10_000,
 			BufferSize:    20_000,
-			FlushInterval: 5 * time.Second,
+			FlushInterval: flushInterval,
 			Consumers:     2,
 			Drop:          true,
 			OnFlushError:  nil,
@@ -201,7 +206,7 @@ func Run(ctx context.Context, cfg Config) error {
 			Name:          "ratelimits",
 			BatchSize:     10_000,
 			BufferSize:    20_000,
-			FlushInterval: 5 * time.Second,
+			FlushInterval: flushInterval,
 			Consumers:     2,
 			Drop:          true,
 			OnFlushError:  nil,
